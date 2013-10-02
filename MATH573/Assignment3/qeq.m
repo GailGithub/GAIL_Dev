df8a7b6e8f90ab2d3b8d110f461e65baa22cb613
fjhickernell <hickernell@iit.edu>
2013-10-01 22:37:20 -0500

    revised this file

--- MATH573/Assignment3/qeq.m
+++ MATH573/Assignment3/qeq.m
@@ -1,23 +1,19 @@
 function x=qeq(a,b,c)
-
-% x=QUADRATICEQ(a,b,c) finds the roots of the quadratic equation
-% a x^2 + b x + c = 0
+% x=QEQ(a,b,c) finds the roots of the quadratic equation
+%   a x^2 + b x + c = 0
 
 x=NaN(2,1); %initialize roots
-
-%scale the inputs
+%% scale the inputs
 scale=max(abs([a b c]));
-a1=a/scale;
+a1=a/scale; %scale coefficients to avoid overflow or underflow
 b1=b/scale;
 c1=c/scale;
-if scale==0, return, end
-
-%compute the roots
-disc=sqrt(b1^2-4*a1*c1);
-term=(-b1 - sign(b1)*disc);
-if abs(term)>0
-    x(1)=(2*c1)/(-b1 - sign(b1)*disc);
-    x(2)=(-b1 - sign(b1)*disc)/(2*a1);
+if scale==0, return, end %zero polynomial
+%% compute the roots
+term=-b1 - sign(b1)*sqrt(b1^2-4*a1*c1); %no cancellation error here
+if abs(term)>0 % at least one root is nonzero
+    x(1)=(2*c1)/term;
+    x(2)=term/(2*a1);
 else
     x=zeros(2,1);
 end
