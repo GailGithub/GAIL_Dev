8330ada22420c4d9eab484262e8b9dbe2f9e2fd9
fjhickernell <hickernell@iit.edu>
2013-10-02 13:54:47 -0500

    revised .m files for Lect. 3

--- MATH573/Assignment3/RootsQuadraticEquation.m
+++ MATH573/Assignment3/RootsQuadraticEquation.m
@@ -4,92 +4,31 @@ clear all, close all, format long e
 %% Ordinary quadratic formula
 quadeq=@(a,b,c) (-b +[-1 1]'*sqrt(b^2-4*a*c))/(2*a);
 
-%% Roots of x^2 + 3x + 2
-a=1; b=3; c=2;
-xq=quadeq(a,b,c); 
-xr=roots([a b c]);
-xqeq=qeq(a,b,c);
-disp(['The solutions of the quadratic equation ',...
-    num2str(a) ' x^2 + ' num2str(b) ' x + ' num2str(c) ' = 0'])
-fprintf(['   using ''quadeq'' are %16.14e, and \n' ...
-    '                      %16.14e \n'],xq)
-fprintf(['    using ''roots'' are %16.14e, and \n' ...
-    '                      %16.14e \n'],xr)
-fprintf(['      using ''qeq'' are %16.14e, and \n' ...
-    '                      %16.14e \n'],xqeq)
-fprintf('\n')
-
-%% Roots of x^2 + 3e8 x + 2
-a=1; b=3e8; c=2;
-xq=quadeq(a,b,c); 
-xr=roots([a b c]);
-xqeq=qeq(a,b,c);
-disp(['The solutions of the quadratic equation ',...
-    num2str(a) ' x^2 + ' num2str(b) ' x + ' num2str(c) ' = 0'])
-fprintf(['   using ''quadeq'' are %16.14e, and \n' ...
-    '                      %16.14e \n'],xq)
-fprintf(['    using ''roots'' are %16.14e, and \n' ...
-    '                      %16.14e \n'],xr)
-fprintf(['      using ''qeq'' are %16.14e, and \n' ...
-    '                      %16.14e \n'],xqeq)
-fprintf('\n')
-
-%% Roots of 1e200x^2 + 3e200 x + 2e200
-a=1e200; b=3e200; c=2e200;
-xq=quadeq(a,b,c); 
-xr=roots([a b c]);
-xqeq=qeq(a,b,c);
-disp(['The solutions of the quadratic equation ',...
-    num2str(a) ' x^2 + ' num2str(b) ' x + ' num2str(c) ' = 0'])
-fprintf(['   using ''quadeq'' are %16.14e, and \n' ...
-    '                      %16.14e \n'],xq)
-fprintf(['    using ''roots'' are %16.14e, and \n' ...
-    '                      %16.14e \n'],xr)
-fprintf(['      using ''qeq'' are %16.14e, and \n' ...
-    '                      %16.14e \n'],xqeq)
-fprintf('\n')
-
-%% Roots of 1e-200x^2 + 3e-200 x + 2e-200
-a=1e-200; b=3e-200; c=2e-200;
-xq=quadeq(a,b,c); 
-xr=roots([a b c]);
-xqeq=qeq(a,b,c);
-disp(['The solutions of the quadratic equation ',...
-    num2str(a) ' x^2 + ' num2str(b) ' x + ' num2str(c) ' = 0'])
-fprintf(['   using ''quadeq'' are %16.14e, and \n' ...
-    '                      %16.14e \n'],xq)
-fprintf(['    using ''roots'' are %16.14e, and \n' ...
-    '                      %16.14e \n'],xr)
-fprintf(['      using ''qeq'' are %16.14e, and \n' ...
-    '                      %16.14e \n'],xqeq)
-fprintf('\n')
-
-%% Roots of x^2
-a=1; b=0; c=0;
-xq=quadeq(a,b,c); 
-xr=roots([a b c]);
-xqeq=qeq(a,b,c);
-disp(['The solutions of the quadratic equation ',...
-    num2str(a) ' x^2 + ' num2str(b) ' x + ' num2str(c) ' = 0'])
-fprintf(['   using ''quadeq'' are %16.14e, and \n' ...
-    '                      %16.14e \n'],xq)
-fprintf(['    using ''roots'' are %16.14e, and \n' ...
-    '                      %16.14e \n'],xr)
-fprintf(['      using ''qeq'' are %16.14e, and \n' ...
-    '                      %16.14e \n'],xqeq)
-fprintf('\n')
-
-%% Roots of x + 1
-a=0; b=1; c=1;
-xq=quadeq(a,b,c); 
-xr=roots([a b c]);
-xqeq=qeq(a,b,c);
-disp(['The solutions of the quadratic equation ',...
-    num2str(a) ' x^2 + ' num2str(b) ' x + ' num2str(c) ' = 0'])
-fprintf(['   using ''quadeq'' are %16.14e, and \n' ...
-    '                      %16.14e \n'],xq)
-fprintf(['    using ''roots'' are %16.14e, and \n' ...
-    '                      %16.14e \n'],xr)
-fprintf(['      using ''qeq'' are %16.14e, and \n' ...
-    '                      %16.14e \n'],xqeq)
-fprintf('\n')
+%% Trying several cases
+abccases=[1      3      2
+          1      3e8    2
+          1e200  3e200  2e200
+          1e-200 3e-200 2e-200
+          1      0      0
+          0      1      1
+          0      0      1
+          0      0      0];
+ncases=size(abccases,1);
+    
+for i=1:ncases
+    a=abccases(i,1); 
+    b=abccases(i,2); 
+    c=abccases(i,3);
+    xq=quadeq(a,b,c); %one-liner
+    xr=roots([a b c]); %MATLAB's own
+    xqeq=qeq(a,b,c); %an .m file
+    disp('The solutions of the quadratic equation ')
+    disp(['   ' num2str(a) ' x^2 + ' num2str(b) ' x + ' num2str(c) ' = 0'])
+    disp('   using ''quadeq'' are:')
+    fprintf('%16.14e,  %16.14e',xq)
+    fprintf('\n   using ''roots'' are:\n')
+    fprintf('%16.14e,  %16.14e',xr)
+    fprintf('\n   using ''qeq'' are:\n')
+    fprintf('%16.14e,  %16.14e',xqeq)
+    fprintf('\n\n')
+end
--- MATH573/Assignment3/qeq.m
+++ MATH573/Assignment3/qeq.m
@@ -2,7 +2,7 @@ function x=qeq(a,b,c)
 % x=QEQ(a,b,c) finds the roots of the quadratic equation
 %   a x^2 + b x + c = 0
 
-x=NaN(2,1); %initialize roots
+x=[]; %initialize roots
 %% scale the inputs
 scale=max(abs([a b c]));
 a1=a/scale; %scale coefficients to avoid overflow or underflow
@@ -11,10 +11,10 @@ c1=c/scale;
 if scale==0, return, end %zero polynomial
 %% compute the roots
 term=-b1 - sign(b1)*sqrt(b1^2-4*a1*c1); %no cancellation error here
-if abs(term)>0 % at least one root is nonzero
+if term~=0 % at least one root is nonzero
     x(1)=(2*c1)/term;
-    x(2)=term/(2*a1);
-else
+    if a1~=0; x(2)=term/(2*a1); end %second root exists
+elseif a1~=0
     x=zeros(2,1);
 end
 x=sort(x);
