1afcf688e15d8ecc0a5bb8cd2cc22c705f22d00b
Sou-Cheng Terrya Choi <sctchoi@>
2013-11-21 11:14:50 -0600

    Reviewed and refined content in DownloadInstallGail_1_0_0.m

--- DownloadInstallGail_1_0_0.m
+++ DownloadInstallGail_1_0_0.m
@@ -1,30 +1,33 @@
-%This mfile may be used to download and install GAIL into 
-%   the location you choose
+%Download and install GAIL into the location you choose
 %
-%   Step 1.  Place this .m file where you want GAIL to go
+%   Step 1.  Place this M-file where you want GAIL to go
 %
-%   Step 2.  Run this mfile
+%   Step 2.  Run this M-file in MATLAB
+%
+%This file installs GAIL version 1 in the subdirectory
+%"GAIL_1.0.0/GAIL_Matlab".
 
 %% Download the package and change the directory
-disp('The GAIL package is now downloading ...')
+disp('The GAIL package is now begin downloaded...')
 unzip('https://gail.googlecode.com/files/GAIL_1.0.0.zip') %download and unzip
-cd('GAIL_1.0.0') %get to the right subdirectory
+cd('GAIL_1.0.0')  %get to the right subdirectory
 cd('GAIL_Matlab') %get to the right subdirectory
 
-%% Install Gail
-GAIL_Install %this installs GAIL
-fprintf('\n\nNext we will run a quick test. Press return to continue...\n')
+%% Install GAIL
+GAIL_Install      %this installs GAIL
+fprintf('\n\nNext we will run a quick test. Press RETURN to continue...\n')
 pause
 
 %% Run a quick test
 fprintf('\nmuhat=meanMC_g(@(n) rand(n,1))\n')
 muhat=meanMC_g(@(n) rand(n,1)) %run meanMC_g once
 fprintf('\nThis answer should be close to 0.5.\n\n')
-%   Then you should be ready to use GAIL
-disp('Next README.txt will be displayed.  Press return to continue...')
+%Then you should be ready to use GAIL
+disp('Next README.txt will be displayed. Press RETURN to continue to ');
+disp('the next line, or press the spacebar to advance to the next page...');
 pause
 
-%% Printing out README.txt
+%% Print out README.txt
 more on
 type('README.txt')
 more off
