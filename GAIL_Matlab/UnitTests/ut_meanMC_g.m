3c146c650f07f65dd0c14d9d7d5ec0c395edcbab
Lan Jiang <tojianglan@gmail.com>
2013-11-21 16:10:23 -0600

    modify ut meanMC_G

--- GAIL_Matlab/UnitTests/ut_meanMC_g.m
+++ GAIL_Matlab/UnitTests/ut_meanMC_g.m
@@ -1,38 +1,42 @@
 %UT_MEANMC_G unit test for meanMC_g
 classdef ut_meanMC_g < matlab.unittest.TestCase
     
-  methods(Test)
-      
-    function meanMC_gOfexp(testCase)
-      f = @(n) exp(rand(n,1));
-      in_param.abstol = 1e-2; 
-      meanf = meanMC_g(f,in_param);
-      exactf = exp(1)-1;
-      actualerr = abs(meanf-exactf);
-      testCase.verifyLessThanOrEqual(actualerr,in_param.abstol);
+    methods(Test)
+        
+        function meanMC_gOfexp(testCase)
+            Y = @(n) exp(rand(n,1));
+            in_param.abstol = 1e-2;
+            meanY = meanMC_g(Y,in_param);
+            exactY = exp(1)-1;
+            actualerr = abs(meanY-exactY);
+            testCase.verifyLessThanOrEqual(actualerr,in_param.abstol);
+        end
+        
+        function meanMC_gOfxsquare(testCase)
+            Y = @(n) rand(n,1).^2;
+            in_param.abstol = 1e-2;
+            meanY = meanMC_g(Y,in_param);
+            exactY = 1/3;
+            actualerr = abs(meanY-exactY);
+            testCase.verifyLessThanOrEqual(actualerr,in_param.abstol);
+        end
+        
+        function meanMC_gOfsin(testCase)
+            Y = @(n) sin(rand(n,1));
+            in_param.abstol = 1e-2;
+            meanY = meanMC_g(Y,in_param);
+            exactY = 1-cos(1);
+            actualerr = abs(meanY-exactY);
+            testCase.verifyLessThanOrEqual(actualerr,in_param.abstol);
+        end
+        
+        function meanMC_gOfparsing(testCase)
+            in_param.abstol = -1e-2;
+            meanY = testCase.verifyWarning(@()meanMC_g(@(n) rand(n,1).^2,...
+                in_param.abstol),'MATLAB:meanMC_g:abstolneg');
+            exactY = 1/3;
+            actualerr = abs(meanY-exactY);
+            testCase.verifyLessThanOrEqual(actualerr,abs(in_param.abstol));
+        end
     end
-    
-    function meanMC_gOfxsquare(testCase)
-      f = @(n) rand(n,1).^2;
-      in_param.abstol = 1e-2; 
-      meanf = meanMC_g(f,in_param);
-      exactf = 1/3;
-      actualerr = abs(meanf-exactf);
-      testCase.verifyLessThanOrEqual(actualerr,in_param.abstol);
-    end
-    
-    function meanMC_gOfsin(testCase)
-      f = @(n) sin(rand(n,1));
-      in_param.abstol = 1e-2; 
-      meanf = meanMC_g(f,in_param);
-      exactf = 1-cos(1);
-      actualerr = abs(meanf-exactf);
-      testCase.verifyLessThanOrEqual(actualerr,in_param.abstol);
-    end
-    
-    function meanMC_gOfparsing(testCase)
-      testCase.verifyWarning(@()meanMC_g(@(n) rand(n,1).^2,-1e-2),...
-          'MATLAB:meanMC_g:abstolneg')
-    end 
-  end
 end
