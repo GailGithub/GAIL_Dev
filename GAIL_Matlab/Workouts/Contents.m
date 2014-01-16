9d53331ef7625ee2466b35f463dad61e9b291a22
Sou-Cheng Terrya Choi <sctchoi@>
2014-01-16 09:08:00 -0600

    Minor enhancements.

--- GAIL_Matlab/Workouts/Contents.m
+++ GAIL_Matlab/Workouts/Contents.m
@@ -1,8 +1,8 @@
 % WORKOUTS
 %
 % Folders
-% cubMC_workouts                     - workouts of cub MC method 
-% meanMC_workouts                    - workouts of mean MC method 
-% MonteCarloFinance                  - workouts of Monte Carlo in Finance
-% univariate_appx_workouts           - workouts of univariate approximation
-% univariate_integration_workouts    - workouts of univariate integration
+%   cubMC_workouts                  - workouts of cubMC_g method
+%   meanMC_workouts                 - workouts of meanMC_g method
+%   MonteCarloFinance               - workouts of Monte Carlo in Finance
+%   univariate_appx_workouts        - workouts of univariate function approximation
+%   univariate_integration_workouts - workouts of univariate integration
