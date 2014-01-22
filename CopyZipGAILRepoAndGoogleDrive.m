e609b4657b2666c9e202eb4b7d91272cc666b7be
Lan Jiang <tojianglan@gmail.com>
2014-01-21 23:57:16 -0600

    copy and zip files in repo and google drive...not finished.

---
+++ CopyZipGAILRepoAndGoogleDrive.m
@@ -0,0 +1,14 @@
+% This script is to copy and combine files in both repository and Google drive
+% to make a release zip packge.
+clear all; close all; clc;
+newpath = '/Users/BabyAudrey/Documents/Study/Research/GAIL_1_3_0';
+% set up a new path where you want to copy things
+GAILPATH = GAILstart;%Get the path of repository from GAILstart
+GoogleDrivePath = '/Users/BabyAudrey/Google Drive/GAIL_Dev/GAIL_Matlab';
+% get the google drive path.
+copyfile(GAILPATH,newpath)%copy files in repository to new path
+copyfile(GoogleDrivePath,newpath)% copy files in google drive to new path
+zip('GAIL_1_3_0',newpath)% zip it.
+
+% Lan is still working on how to excluded hidden file such as .dsstore and
+% .icon in every single folder. please give her some time.
