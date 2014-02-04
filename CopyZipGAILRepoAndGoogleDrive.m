c7ff9d8e5087942b453074f831d2972c97bce6a9
Lan Jiang <tojianglan@gmail.com>
2014-02-04 10:43:31 -0600

    copy zip

--- CopyZipGAILRepoAndGoogleDrive.m
+++ CopyZipGAILRepoAndGoogleDrive.m
@@ -8,6 +8,8 @@ GoogleDrivePath = '/Users/BabyAudrey/Google Drive/GAIL_Dev/GAIL_Matlab';
 % get the google drive path.
 copyfile(GAILPATH,newpath)%copy files in repository to new path
 copyfile(GoogleDrivePath,newpath)% copy files in google drive to new path
-
-% zip files recursively excluding hidden file such as .ds_store and .icon  
-system(horzcat('zip -r GAIL_1_3_0 ', newpath, ' -x@exclude1_3.lst'));  
+% zip files recursively excluding hidden file such as .ds_store and .icon 
+cd('/Users/BabyAudrey/Documents/Study/Research/')
+system('zip -r GAIL130.zip GAIL_1_3_0 -x@exclude1_3.lst');  %-x@exclude1_3.lst
+%zip -r GAIL130.zip GAIL_1_3_0 -x@exclude1_3.lst
+%system(horzcat('zip -r GAIL_1_3_0 ', newpath, ' -x@exclude1_3.lst'));  
