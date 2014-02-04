f5ed93e20c0c68a8b398edda747be5cd27fdb455
Sou-Cheng Terrya Choi <sctchoi@>
2014-02-04 11:37:11 -0500

    Attempt to eliminate top level directories "Users/BabyAudrey/Documents/Study/Research" in zip.

--- CopyZipGAILRepoAndGoogleDrive.m
+++ CopyZipGAILRepoAndGoogleDrive.m
@@ -1,7 +1,8 @@
 % This script is to copy and combine files in both repository and Google drive
 % to make a release zip package.
 clear all; close all; clc;
-newpath = '/Users/BabyAudrey/Documents/Study/Research/GAIL_1_3_0';
+newfolder = 'GAIL_1_3_0';
+newpath = horzcat('/Users/BabyAudrey/Documents/Study/Research/',newfolder);
 % set up a new path where you want to copy things
 GAILPATH = GAILstart;%Get the path of repository from GAILstart
 GoogleDrivePath = '/Users/BabyAudrey/Google Drive/GAIL_Dev/GAIL_Matlab';
@@ -10,4 +11,4 @@ copyfile(GAILPATH,newpath)%copy files in repository to new path
 copyfile(GoogleDrivePath,newpath)% copy files in google drive to new path
 
 % zip files recursively excluding hidden file such as .ds_store and .icon  
-system(horzcat('zip -r GAIL_1_3_0 ', newpath, ' -x@exclude1_3.lst'));  
+system(horzcat('zip -r GAIL_1_3_0', newfolder, ' -x@exclude1_3.lst'));  
