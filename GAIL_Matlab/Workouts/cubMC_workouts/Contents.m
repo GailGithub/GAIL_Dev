a85d6612f8f807ebd18880be8765c6613d7b6809
Sou-Cheng Terrya Choi <sctchoi@>
2014-02-04 10:10:12 -0500

    Update documentation.

--- GAIL_Matlab/Workouts/cubMC_workouts/Contents.m
+++ GAIL_Matlab/Workouts/cubMC_workouts/Contents.m
@@ -1,7 +1,7 @@
 % CUBMC_WORKOUTS
 %
 % Files
-%   choosetestfun      - Chooses and sets up a test function
-%   genz_test_fun      - A suite of test functions from Allan Genz and Keister
+%   genz_test_fun      - A suite of test functions from Alan Genz and Keister
 %   genz_test_fun_true - Provides true integrals of seven test functions
-%   test_cubMC_g       - This is the driver script to test cubMC_g algorithm
+%   test_cubMC_g       - Driver script to test the cubMC_g algorithm
+%   dt_cubMC_g         - Lengthy doc tests for cubMC_g
