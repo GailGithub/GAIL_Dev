45ad229c6fa3996b369f4f673705085c7e6f727d
Yizhi Zhang <harryzyz@hotmail.com>
2014-02-12 14:14:12 -0600

    documentation

--- GAIL_Matlab/Documentation/help_integral_g.m
+++ GAIL_Matlab/Documentation/help_integral_g.m
@@ -4,9 +4,9 @@
 %% Syntax
 % q = *integral_g*(f)
 %
-% q = *integral_g*(f,abstol,ninit,nmax)
+% q = *integral_g*(f,a,b,abstol,nlo,nhi,nmax)
 %   
-% q = *integral_g*(f,'abstol',abstol,'ninit',ninit,'nmax',nmax)
+% q = *integral_g*(f,'abstol',abstol,'nlo',nlo,'nhi',nhi,'nmax',nmax)
 %
 % q = *integral_g*(f,in_param)
 %
@@ -44,20 +44,45 @@
 %
 % *Input Arguments*
 %   
-% * in_param.abstol --- |absolute error tolerance required by user.|
-% 
-% * in_param.ninit --- |starting number of points specified by user.|
+% * f --- |function handle|
 %
-% * in_param.nmax --- |cost budget required by user.|
+% * in_param.a --- |left end point of interval, default value is 0|
+%
+% * in_param.b --- |right end point of interval, default value is 1|
+%
+% * in_param.abstol --- |guaranteed absolute error tolerance, default value
+%  is 1e-6|
+%
+% * in_param.nlo --- |lower bound of initial number of points we used,
+%  default value is 10|
+%
+% * in_param.nhi --- |upper bound of initial number of points we used,
+%  default value is 1000|
+%
+% * in_param.nmax --- |cost budget, default value is 1e7|
 %
 % *Output Arguments*
 %
 % * out_param.exceedbudget --- |it is true if the algorithm tries to use 
 % more points than cost budget, false otherwise.|
 %
-% * out_param.ninit --- |the minimum requirement of number of points to 
-% start|
+% * out_param.nlo --- lowest initial number of function values
+%
+% * out_param.nhi --- highest initial number of function values
 % 
+% * out_param.ninit --- initial number of points we use, computed by nlo
+%   and nhi
+%
+% * out_param.tau --- final value of the parameter defining the cone of
+%   functions for which this algorithm is guaranteed; tau = (ninit-1)*2
+%   initially and is increased as necessary
+%
+% * out_param.nmax --- cost budget (maximum number of function values)
+% 
+% * out_param.a --- low end of the integral
+%
+% * out_param.b --- high end of the integral
+%
 % * out_param.tauchange --- |it is true if the cone constant has been
 % changed, false otherwise. See [1] for details.  If true, you may wish 
 % to change the input in_param.ninit to a larger number.|
@@ -68,10 +93,29 @@
 % * out_param.errest --- |approximation error defined as the differences
 % between the true value and the approximated value of the integral.|
 % 
+%% Guarantee
+%    
+% |If the function to be integrated,|  $f$  |satisfies the cone condition|
+%
+% $$\|f''\|_1 \le \frac { \tau }{b-a} \left\|f'-\frac{f(b)-f(a)}{b-a}\right\|_1,$$
+% 
+% |then the |  $q$  |output by this algorithm is guaranteed to
+% satisfy|
+%
+% $$\| \int_{a}^{b} f(x) dx - q \|_{1} \le \mathrm{abstol},$$
+%
+% |provided the flag| $\mathrm{exceedbudget} = 0.$
+%
+% |And the upper bound of the cost is|
+%
+% $$\sqrt{ \frac{\tau (b-a)^2 
+% \mathrm{Var}(f')}{4 \times \mathrm{abstol}}} + \tau+4.$$
+%
+%
 %% Examples
 %  Example 1
 
-f = @(x) x; [q, out_param] = integral_g(f)
+f = @(x) x.^2; [q, out_param] = integral_g(f)
 
 % Integrate function x with default input parameter to make the error
 % less than 1e-7.
@@ -79,7 +123,7 @@ f = @(x) x; [q, out_param] = integral_g(f)
 %%
 % Example 2
 
-[q, out_param] = integral_g(@(x) 3*x.^2,'ninit',52,'nmax',1e7,'abstol',1e-8)
+[q, out_param] = integral_g(@(x) exp(-x.^2),'a',1,'b',2,'nlo',100,'nhi',10000,'abstol',1e-5,'nmax',1e7)
 
 % Integrate function x^2 with starting number of points 52, cost budget 10000000
 % and error toerence 1e-8
