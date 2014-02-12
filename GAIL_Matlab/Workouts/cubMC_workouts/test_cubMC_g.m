3bf3e75b8709ccc26e1038a13201aee17b913de6
Lan Jiang <tojianglan@gmail.com>
2014-02-12 00:25:01 -0600

    add true solutions for genz test fun, with the help of Xuan

--- GAIL_Matlab/Workouts/cubMC_workouts/test_cubMC_g.m
+++ GAIL_Matlab/Workouts/cubMC_workouts/test_cubMC_g.m
@@ -1,34 +1,32 @@
-% This is the driver script to test cubMC_g algorithm
+% TEST_CUBMC_G This is the driver script to test cubMC_g algorithm
 %using integrands of dimensions up to 3 
 %clear all;close all;clc;
 in_param.measure  = 'uniform';
 disp(horzcat('Dim ', 'FcnIdx ',   ' Error')); 
 disp(        '---------------------------');
-%f=@(x) exp(-x(1).^2-x(2).^2);% the test function
-%f=@(x) x(:,1)+x(:,2);
-for dim=1:2
+for dim=1:3
   in_param.dim =dim;%the function dimension
   startingpoint = zeros(1,in_param.dim);
   endingpoint = ones(1,in_param.dim);
   hyperbox = [startingpoint;endingpoint];% the integration interval
-  in_param.abstol = 1e-3;% the absolute tolerance
+  in_param.abstol = 1e-2;% the absolute tolerance
   in_param.alpha = 0.01;% the uncertainty
   in_param.n_sigma = 1e4;% the sample size to estimate sigma
   in_param.fudge =1.1;% standard deviation inflation factor
   in_param.timebudget = 100;% time budget
   in_param.nbudget = 1e9;% sample budget
-  alpha = ones(in_param.dim,1); % one coefficient
-  beta = [1/3  1/4 2];
-  %beta = ones(1:in_param.dim);% the other coefficent
+  alpha = ones(1,in_param.dim); 
+  beta = 1./ (1:in_param.dim); 
   r=2;
-  for index=[1]
+  % three coefficients in genz_test_fun and genz_test_fun_true
+  for index=[1:7]
     test_function = @(x)genz_test_fun(x,index,in_param.dim,alpha,beta,r);
     f_true = genz_test_fun_true (hyperbox,index,in_param.dim,alpha,beta,r);
     % true solution
     [Q,out_param]=cubMC_g(test_function,hyperbox,in_param);% the results using cubMC_g
     error = abs(Q-f_true);
     numstr=horzcat(num2str(dim), '   ', num2str(index), '       ', num2str(error));
-    if error > in_param.abstol,
+    if error > in_param.abstol,% if error does not meet tolerance, mark it
       disp([numstr,'   ****']);
     else
       disp(numstr);
@@ -36,23 +34,29 @@ for dim=1:2
   end
 end
 
-%% The following output was obtained on 2014-January-01 by
+%% The following output was obtained on 2014-02-11  by Lan Jiang
 %  
-% >> test_cubMC_g
+% Test_cubMC_g
 % Dim FcnIdx  Error
 % ---------------------------
-% 1   2       0.00033852
-% 1   4       0.00031599
-% 1   5       6.1558e-05
-% 1   6       0.0001926
-% 1   7       0.00020457
-% 2   2       0.00021932
-% 2   4       0.00018337
-% 2   5       4.8054e-05
-% 2   6       0.00045727
-% 2   7       0.0004521
-% 3   2       0.00036581
-% 3   4       0.00023658
-% 3   5       0.00022292
-% 3   6       0.00024788
-% 3   7       0.00067544
+% 1   1       0.00066017
+% 1   2       0.0029365
+% 1   3       0.0013206
+% 1   4       0.00026806
+% 1   5       0.0027263
+% 1   6       0.00039949
+% 1   7       0.00056086
+% 2   1       0.00093275
+% 2   2       0.00036589
+% 2   3       0.00072042
+% 2   4       0.0013418
+% 2   5       0.00067463
+% 2   6       0.00062401
+% 2   7       0.001117
+% 3   1       0.0019781
+% 3   2       0.00050505
+% 3   3       0.00037568
+% 3   4       0.0012184
+% 3   5       4.7978e-05
+% 3   6       0.00010979
+% 3   7       0.0016297
