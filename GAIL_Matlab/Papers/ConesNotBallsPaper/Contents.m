28368d21f413b6c6d8e62ca6e47ce6b9514d1879
YuhanDing <jinliang98@hotmail.com>
2014-02-12 21:05:05 -0600

    update Contents.m file

--- GAIL_Matlab/Papers/ConesNotBallsPaper/Contents.m
+++ GAIL_Matlab/Papers/ConesNotBallsPaper/Contents.m
@@ -1,7 +1,8 @@
 % CONESNOTBALLSPAPER
 %
 % Files
-%   ConesPaperFoolFunctions   - Generate Figure 1. Construct functions that fool QUAD or INTEGRAL like Lyness says
-%   conepaper_test_integral_g - Generate Table 2. Run automatic guaranteed algorithm for function integration
-%   conepaper_test_funappx_g  - Generate Table 3. Run automatic guaranteed algorithm for function approximation
-
+%   ConesPaperFoolFunctions   - Generate Figure 1. in Cones not ball paper Construct functions that fool QUAD or INTEGRAL like Lyness says
+%   conepaper_test_integral_g - Generate Table 2. in Cones not ball paper Run automatic guaranteed algorithm for function integration
+%   conepaper_test_funappx_g  - Generate Table 3. in Cones not ball paper Run automatic guaranteed algorithm for function approximation
+%   foolfunmaker              - Generata fooling function in Cones not ball paper
+%   Cones not ball.pdf        - Paper 
--- GAIL_Matlab/UnitTests/Contents.m
+++ GAIL_Matlab/UnitTests/Contents.m
@@ -2,10 +2,12 @@
 %
 % Files
 %
-%   dt_funappx_g      - fast doc tests for funappx_h
+%   dt_funappx_g      - fast doc tests for funappx_g
 %   dt_integral_g     - fast doc tests for integral_g
 %   ut_funappx_g      - unit test for funappx_g
 %   runtests          - Drives all doctests and unit tests
 %   ut_cubMC_g        - coming soon
 %   ut_integral_g     - unit test for integral_g
 %   ut_meanMC_g       - unit test for meanMC_g
+%   ut_funappx01_g    - unit test for funappx01_g
+%   ut_integral01_g   - unit test for integral_g
