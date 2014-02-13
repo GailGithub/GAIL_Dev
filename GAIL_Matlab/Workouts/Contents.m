69971d8c2fa88b1f25b8370460d7fda5ec500488
YuhanDing <jinliang98@hotmail.com>
2014-02-12 22:58:08 -0600

    modification of contents.m

--- GAIL_Matlab/Workouts/Contents.m
+++ GAIL_Matlab/Workouts/Contents.m
@@ -3,7 +3,6 @@
 % Folders
 %   cubMC_workouts                  - workouts of cubMC_g method
 %   meanMC_workouts                 - workouts of meanMC_g method
-%   MonteCarloFinance               - workouts of Monte Carlo in Finance
 %   univariate_integration_workouts - workouts of univariate integration
 %
 % Files
