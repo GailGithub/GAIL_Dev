88409f7a444f99ffc8d4135ba9635b9fe0833c3d
Xuan Zhou <xzhou23@hawk.iit.edu>
2014-02-20 19:16:16 -0600

    delete files

--- MATH573/Projects/Xin Tong/funmin_g/Contents.m
+++
@@ -1,7 +0,0 @@
-% funmin_g
-%
-%Files:
-
-%   funmin_g     - Guaranteed minimum value of one-dimensional function
-%                  on interval [0,1] and the subset containing point(s) where the minimum occurs 
-%   ut_funmin_g  - Unit test for funmin_g
