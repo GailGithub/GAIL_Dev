682133ff6aae056b694f3cc87fd10d5f9ebdb257
Xuan Zhou <xzhou23@hawk.iit.edu>
2014-02-20 19:09:08 -0600

    small script change

--- PurgeFile.py
+++ PurgeFile.py
@@ -1,6 +1,6 @@
-# This script can be used to remove files from the commit history.
+# This script can be used to remove files from the commit history. Note that the files should already be removed from the current repository by using the "git rm" command.
 # To run this script in a Linux system, navigate to the folder that contains this file and run 'python PurgeFile.py'.
-# Manually force updating the remote repository by running "git push --force origin master". 
+# Manually force updating the remote repository by running 'git push --force origin master'. 
 
 import os
 
@@ -12,7 +12,7 @@ try:
 except AssertionError, args:
     print '%s: %s' % (args.__class__.__name__, args)
     exit()
-os.system('git filter-branch --index-filter \'git rm --cached --ignore-unmatch ' + FileName +'\'')
+os.system('git filter-branch --index-filter \'git rm --cached --ignore-unmatch ' + FileName + '\'')
 os.system('rm -Rf .git/refs/original')
 os.system('rm -Rf .git/logs/')
 os.system('git reflog expire --expire=now --all')
