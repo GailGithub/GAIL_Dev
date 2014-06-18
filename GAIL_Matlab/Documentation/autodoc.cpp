0539784863c159e06f1353b1c9d289f1845b49cf
Xuan Zhou <xuanjzhou@gmail.com>
2014-06-17 22:09:54 -0500

    autodoc individual fcn

--- GAIL_Matlab/Documentation/autodoc.cpp
+++ GAIL_Matlab/Documentation/autodoc.cpp
@@ -9,29 +9,29 @@ using std::ofstream;
 //using std::fstream;
 using std::cout;
 using std::endl;
+using std::flush;
 
 int main()
 {
   const string dataFolder("doc_data");
-  ifstream ifs(dataFolder+"/DocList.txt");
-  //intro(), website(");
-  vector<string> fcnList, introList, websiteList;
+  ifstream ifs(dataFolder + "/DocList.txt");
+  vector<string> fcnList, introList, websiteList, fcnDoc;
   string line;
   while (ifs >> line) {
     fcnList.push_back(line);
   }
   ifs.close();
-  ifs.open(dataFolder+"/intro.m");
+  ifs.open(dataFolder + "/intro.m");
   while (getline(ifs, line)) {
     introList.push_back(line);
   }
   ifs.close();
-  ifs.open(dataFolder+"/website.m");
+  ifs.open(dataFolder + "/website.m");
   while (getline(ifs, line)) {
     websiteList.push_back(line);
   }
   ifs.close();
-  ofstream gail("GAIL_t.m");
+  ofstream gail("GAIL_t.m"), ofs;
   for (const auto &s : introList) {
     gail << s << "\n";
   }
@@ -40,6 +40,17 @@ int main()
   gail << "% <html>" << endl;
   for (const auto &s : fcnList) {
     gail << "% <a href=\"help_" << s << ".html\">" << s << "</a>\n";
+    ifs.open("../Algorithms/" + s + ".m");
+    while (getline(ifs, line) && line != "") {
+      fcnDoc.push_back(line);
+    }
+    cout << fcnDoc.size() << endl;
+    fcnDoc.clear();
+    ifs.close();
+    ofs.open("help_" + s + ".m");
+    ofs << "%% " << s << "\n";
+    ofs << flush;
+    ofs.close();
   }
   gail << "% </html>\n";
   gail << "%\n";
@@ -47,6 +58,6 @@ int main()
   for (const auto &s : websiteList) {
     gail << s << "\n";
   }
-  gail << std::flush;
+  gail << flush;
   gail.close();
 }
