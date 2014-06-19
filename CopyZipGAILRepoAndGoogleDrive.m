b365840ff471ee7ca9dfe2bbb37de283e6fb9f74
Yuhan Ding <jinliang98@hotmail.com>
2014-06-19 12:18:05 -0500

    Modify CopyZipGAILRepoandGoogleDrive.m to be a function with parameters

--- CopyZipGAILRepoAndGoogleDrive.m
+++ CopyZipGAILRepoAndGoogleDrive.m
@@ -1,17 +1,25 @@
 % This script is to copy and combine files in both repository and Google drive
 % to make a release zip package.
-clear all; close all; clc;
-newfolder = 'GAIL_1_3';
-newpath = horzcat('E:\',newfolder);
-% set up a new path where you want to copy things
-GAILPATH = GAILstart;%Get the path of repository from GAILstart
-GoogleDrivePath1 = 'E:\GoogleDrive\GAIL_Dev\GAIL_Matlab';
-GoogleDrivePath = 'E:\GoogleDrive\GAIL_Dev';
-% get the google drive path.
-copyfile(GAILPATH,newpath)%copy files in repository to new path
-copyfile(GoogleDrivePath1,newpath)% copy files in google drive to new path
+function CopyZipGAILRepoAndGoogleDrive(newpath,GoogleDrivePath,zipfilename,excludefilelist)
+% newpath is where you want to copy things including folder name
+% eg. 'E:\GAIL_Version#'
+% GoogleDrivePath is where your google drive path
+% eg. 'E:\GoogleDrive\GAIL_Dev'
+% Get the path of repository from GAILstart
+% zipfilename is the name of zip file
+% eg. 'GAIL_Version#'
+% excludefilelist is the list of file we want to exclude
+% eg. 'excludeVersion#.lst'
+[GAILPATH,~,PATHNAMESEPARATOR] = GAILstart;
+% Get the path where GAIL_Matlab locate
+GoogleDrivePath1 = horzcat(GoogleDrivePath,PATHNAMESEPARATOR,'GAIL_Matlab');
+%copy files in repository to new path
+copyfile(GAILPATH,newpath)
+% copy files in google drive to new path
+copyfile(GoogleDrivePath1,newpath)
 % zip files recursively excluding hidden file such as .ds_store and .icon
 % in current folder
-system(horzcat('zip -r GAIL_1_3.zip ', newpath, ' -x@exclude1_3.lst'));  
+system(horzcat('zip -r ',zipfilename,' ',newpath, ' -x@',excludefilelist));  
 % move zip to google drive path
-movefile('GAIL_1_3.zip', GoogleDrivePath,'f')
+movefile(zipfilename, GoogleDrivePath,'f')
+display(horzcat(zipfilename,' is already put in ', GoogleDrivePath))
