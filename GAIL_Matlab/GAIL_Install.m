c9b2405909455857dc6d0e424b521612d54170a1
Xuan Zhou <xuanjzhou@gmail.com>
2014-06-30 15:46:26 -0500

    modify installation files to build searchable html

--- GAIL_Matlab/GAIL_Install.m
+++ GAIL_Matlab/GAIL_Install.m
@@ -4,9 +4,12 @@ clear all; close all; clc;
 fprintf('\nWelcome to GAIL version %g.\n', GAILVERSION);
 if MATLABVERSION < 7,
   error('This version is only supported on Matlab 7.x and above.');
-else
-  gailp=genpath(GAILPATH);% Generate strings of paths to GAIL subdirectories 
 end
+gailp=genpath(GAILPATH);% Generate strings of paths to GAIL subdirectories 
+docPath = strcat(GAILPATH,'Documentation');
+cd(docPath);
+helpPath = strcat(docPath,PATHNAMESEPARATOR,'html');
+builddocsearchdb(helpPath);
 addpath(gailp);           % Add GAIL directories and subdirectories
 savepath;                 % Save the changes
 fprintf('\nGAIL version %g has been installed successfully.\n\n', GAILVERSION);
