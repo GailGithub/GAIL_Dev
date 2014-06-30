ee9775cdb1ae450c2236f63777a7113c5e4e25bc
Xuan Zhou <xuanjzhou@gmail.com>
2014-06-30 17:16:21 -0500

    autodoc funclist.m

--- GAIL_Matlab/Documentation/autodoc.cpp
+++ GAIL_Matlab/Documentation/autodoc.cpp
@@ -23,7 +23,7 @@ int main()
   ifstream ifs(dataFolder + "/DocList.txt");
   vector<string> fcnList, introList, websiteList, fcnDoc;
   string line;
-  while (ifs >> line) {
+  while (getline(ifs,line)) {
     fcnList.push_back(line);
   }
   ifs.close();
@@ -37,7 +37,7 @@ int main()
     websiteList.push_back(line);
   }
   ifs.close();
-  ofstream gail("GAIL_t.m"), helptoc("html/helptoc_t.xml"), ofs;
+  ofstream gail("GAIL_t.m"), helptoc("html/helptoc_t.xml"), funclist("funclist_t.m"), ofs;
   helptoc << "<?xml version='1.0' encoding='ISO-8859-1' ?>\n\n<toc version=\"1.0\">\n\n"
           << "<tocitem target=\"GAIL.html\">GAIL Toolbox\n"
           << "    <tocitem target=\"funclist.html\" image=\"HelpIcon.FUNCTION\">Functions" << endl;
@@ -45,35 +45,43 @@ int main()
     gail << s << "\n";
   }
   gail << "%% Functions\n" << "%\n" << "% <html>" << endl;
+  funclist << "%% Functions\n%" << endl;
   for (const auto &s : fcnList) {
-    helptoc << "            <tocitem target=\"help_" << s << ".html\">" << s << "</tocitem>\n";
-    gail << "% <a href=\"help_" << s << ".html\">" << s << "</a>\n";
-    ifs.open("../Algorithms/" + s + ".m");
-    while (getline(ifs, line) && line != "") {
-      // string::size_type pos = 0;
-      // auto uStr = upperString(s);
-      // while ((pos = line.find(uStr, pos)) != string::npos) {
-      // 	line.replace(pos, s.size(), uStr);
-      // 	pos += uStr.length();
-      // }
-      fcnDoc.push_back(line);
-    }
-    ifs.close();
-    ofs.open("help_" + s + ".m");
-    ofs << "%% " << s << "\n% |";
-    auto space1 = find(fcnDoc[1].cbegin(), fcnDoc[1].cend(), ' ');
-    fcnDoc[1] = fcnDoc[1].substr(space1 - fcnDoc[1].cbegin() + 1, fcnDoc[1].size());
-    auto emptyLine1 = find(fcnDoc.cbegin(), fcnDoc.cend(), "%");
-    for (auto iter = ++fcnDoc.begin(); iter != emptyLine1; ++iter) {
-      if (iter != emptyLine1 -1) { 
-	ofs << *iter << "\n";
-      } else {
-	ofs << *iter;
+    if (s.empty()) {
+      funclist << "%\n";
+    } else if (*(s.cend()-1) == ':') {
+      funclist << "%% " << s.substr(0,s.size() - 1) << "\n%\n";
+    } else {
+      helptoc << "            <tocitem target=\"help_" << s << ".html\">" << s << "</tocitem>\n";
+      gail << "% <a href=\"help_" << s << ".html\">" << s << "</a>\n";
+      funclist << "% <html>\n% <a href=\"help_" << s << ".html\">meanMC_g</a>\n% </html>\n%\n";
+      ifs.open("../Algorithms/" + s + ".m");
+      while (getline(ifs, line) && line != "") {
+	// string::size_type pos = 0;
+	// auto uStr = upperString(s);
+	// while ((pos = line.find(uStr, pos)) != string::npos) {
+	// 	line.replace(pos, s.size(), uStr);
+	// 	pos += uStr.length();
+	// }
+	fcnDoc.push_back(line);
       }
+      ifs.close();
+      ofs.open("help_" + s + ".m");
+      ofs << "%% " << s << "\n% |";
+      auto space1 = find(fcnDoc[1].cbegin(), fcnDoc[1].cend(), ' ');
+      fcnDoc[1] = fcnDoc[1].substr(space1 - fcnDoc[1].cbegin() + 1, fcnDoc[1].size());
+      auto emptyLine1 = find(fcnDoc.cbegin(), fcnDoc.cend(), "%");
+      for (auto iter = ++fcnDoc.begin(); iter != emptyLine1; ++iter) {
+	if (iter != emptyLine1 -1) { 
+	  ofs << *iter << "\n";
+	} else {
+	  ofs << *iter;
+	}
+      }
+      ofs << ".|" << endl;
+      ofs.close();
+      fcnDoc.clear();
     }
-    ofs << ".|" << endl;
-    ofs.close();
-    fcnDoc.clear();
   }
   helptoc << "        </tocitem>\n    </tocitem>\n</toc>" << endl;
   gail << "% </html>\n" << "%\n" << "%" << endl;
@@ -81,7 +89,10 @@ int main()
     gail << s << "\n";
   }
   gail << flush;
+  funclist << flush;
+  helptoc.close();
   gail.close();
+  funclist.close();
 }
 
 string upperString(const string &s) noexcept
