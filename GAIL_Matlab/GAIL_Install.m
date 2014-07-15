cf9099f7db3fa636ff15733bb155976712bf961c
Xuan Zhou <xzhou23@hawk.iit.edu>
2014-07-14 22:01:40 -0500

    Revert "little modification of GAIL_Install.m"
    This reverts commit 5f7c0189f281d11d467ce98eaf1864b82f6eea20.

--- GAIL_Matlab/GAIL_Install.m
+++ GAIL_Matlab/GAIL_Install.m
@@ -5,8 +5,8 @@ fprintf('\nWelcome to GAIL version %g.\n', GAILVERSION);
 if MATLABVERSION < 7,
   error('This version is only supported on Matlab 7.x and above.');
 else
-  %gailp=genpath(GAILPATH);% Generate strings of paths to GAIL subdirectories 
-  addpath(GAILPATH);           % Add GAIL directories and subdirectories
-  savepath;                 % Save the changes
-  fprintf('\nGAIL version %g has been installed successfully.\n\n', GAILVERSION);
+  gailp=genpath(GAILPATH);% Generate strings of paths to GAIL subdirectories 
 end
+addpath(gailp);           % Add GAIL directories and subdirectories
+savepath;                 % Save the changes
+fprintf('\nGAIL version %g has been installed successfully.\n\n', GAILVERSION);
