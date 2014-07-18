14c2c6889179f495048bc5056dd39b2fe6f47296
Xuan Zhou <xzhou23@hawk.iit.edu>
2014-07-18 16:09:06 -0500

    add paths before publishing html when installing

--- GAIL_Matlab/GAIL_Install.m
+++ GAIL_Matlab/GAIL_Install.m
@@ -1,17 +1,19 @@
 % GAIL_INSTALL   Install GAIL. Add GAIL paths to MATLAB search path.
 clear all; close all; clc;
 [GAILPATH,GAILVERSION,PATHNAMESEPARATOR,MATLABVERSION] = GAILstart;
-fprintf('\nWelcome to GAIL version %g.\n', GAILVERSION);
+fprintf('\nWelcome to GAIL version %g.\n\n', GAILVERSION);
 if MATLABVERSION < 7,
   error('This version is only supported on Matlab 7.x and above.');
 end
 gailp=genpath(GAILPATH);% Generate strings of paths to GAIL subdirectories
+addpath(gailp);           % Add GAIL directories and subdirectories
+savepath;                 % Save the changes
 cd(strcat(GAILPATH,'Utilities'));
 GAILpublish; % Publish searchable html documentation
 docPath = strcat(GAILPATH,'Documentation');
 cd(docPath);
 helpPath = strcat(docPath,PATHNAMESEPARATOR,'html');
 builddocsearchdb(helpPath);
-addpath(gailp);           % Add GAIL directories and subdirectories
-savepath;                 % Save the changes
+cd('..');
 fprintf('\nGAIL version %g has been installed successfully.\n\n', GAILVERSION);
+
--- GAIL_Matlab/GAIL_Reinstall.m
+++ GAIL_Matlab/GAIL_Reinstall.m
@@ -2,19 +2,18 @@
 clear all; close all; clc;
 [GAILPATH,GAILVERSION,PATHNAMESEPARATOR,MATLABVERSION] = GAILstart;
 fprintf('\nYou are reinstalling GAIL version %g.\n\n',GAILVERSION);
-
 if MATLABVERSION < 7,
   error('This version is only supported on Matlab 7.x and above.');
 end
 gailp=genpath(GAILPATH); % Generate strings of paths to GAIL subdirectories 
+rmpath(gailp);  % Remove GAIL paths from MATLAB search path.
+addpath(gailp); % Add GAIL paths to MATLAB search path.
+savepath;       % Save the changes.
 cd(strcat(GAILPATH,'Utilities'));
 GAILpublish; % Publish searchable html documentation
 docPath = strcat(GAILPATH,'Documentation');
 cd(docPath);
 helpPath = strcat(docPath,PATHNAMESEPARATOR,'html');
 builddocsearchdb(helpPath);
-rmpath(gailp);  % Remove GAIL paths from MATLAB search path.
-addpath(gailp); % Add GAIL paths to MATLAB search path.
-savepath;       % Save the changes.
-
+cd('..');
 fprintf('\nGAIL version %g has been reinstalled successfully.\n\n',GAILVERSION);
