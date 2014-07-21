c91abaa71ef959a1c45bc59ea9fffaa419746c6f
Sou-Cheng Terrya Choi <sou.cheng.terrya.choi@gmail.com>
2014-07-21 07:21:04 -0500

    Remove duplicated calls of doctests.

--- GAIL_Matlab/UnitTests/runtests.m
+++ GAIL_Matlab/UnitTests/runtests.m
@@ -7,30 +7,8 @@ diary(filename)
 
 tic; 
 % Call doctest 
-format short
-
-doctest funappx_g
-doctest funappxlocal_g
-doctest funappxtau_g
-doctest funappx01_g
-clear in_param; doctest dt_funappx_g
-
-doctest funmin_g
-
-doctest integral_g
-doctest integralsim_g
-doctest integral01_g
-doctest integraltau_g
-doctest dt_integral_g 
-
-doctest meanMC_g
-doctest meanMCRel_g
-doctest meanMCBernoulli_g
  
-doctest cubMC_g
-
-doctest cubLattice_g
-%doctest cubSobol_g
+rundoctests
 
 % Call unit tests
 [~,~,~,MATLABVERSION]=GAILstart(0);
