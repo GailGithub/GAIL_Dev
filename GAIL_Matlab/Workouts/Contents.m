d52568db0fcc70354ae72d5640eb4fca02f80169
Sou-Cheng Terrya Choi <sou.cheng.terrya.choi@gmail.com>
2014-08-10 12:51:05 -0500

    Update Contents.m of Workouts.

--- GAIL_Matlab/Workouts/Contents.m
+++ GAIL_Matlab/Workouts/Contents.m
@@ -1,9 +1,11 @@
 % WORKOUTS
 %
 % Folders
-%   cubMC_workouts                  - workouts of cubMC_g method
-%   meanMC_workouts                 - workouts of meanMC_g method
-%   univariate_integration_workouts - workouts of univariate integration
+%   cubMC_workouts                   - workouts of cubMC_g method
+%   meanMC_workouts                  - workouts of meanMC_g method
+%   meanMCBernoulli_g_workouts       - workouts of meanMCBernoulli_g method
+%   univariate_integration_workouts  - workouts of integral_g method
+%   univariate_minimization_workouts - workouts of funmin_g method
 %
 % Files
-%   longtests                       - lengthy tests of GAIL algorithms
+%  --
