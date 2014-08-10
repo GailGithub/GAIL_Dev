dc4cb47ad7ccfd55049ab1a7e7d26338891a151d
Sou-Cheng Terrya Choi <sou.cheng.terrya.choi@gmail.com>
2014-08-10 14:57:42 -0500

    Change 'epsilon' to 'abstol' and 'delta' to 'TolX'.

--- GAIL_Matlab/Workouts/univariate_minimization_workouts/Contents.m
+++ GAIL_Matlab/Workouts/univariate_minimization_workouts/Contents.m
@@ -1,8 +1,8 @@
 % UNIVARIATE_MINIMIZATION_WORKOUTS
 %
 % Files
-%   workout_ErrorToleranceTest  - Experiment 1: Bump test functions with epsilon=10^(-8) & delta=0
-%   workout_XToleranceTest      - Experiment 2: Bump test functions with epsilon=0 & delta=10^(-6)
-%   workout_ErrorXToleranceTest - Experiment 3: Bump test functions with epsilon=10^(-8) & delta=10^(-6)
+%   workout_ErrorToleranceTest  - Experiment 1: Bump test functions with abstol=10^(-8) & TolX=0
+%   workout_XToleranceTest      - Experiment 2: Bump test functions with abstol=0 & TolX=10^(-6)
+%   workout_ErrorXToleranceTest - Experiment 3: Bump test functions with abstol=10^(-8) & TolX=10^(-6)
 %   workout_TwoExtremeTest      - Experiment 4: Functions with two local minimum points 
 
