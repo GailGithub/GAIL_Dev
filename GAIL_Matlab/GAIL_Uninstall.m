8dc77be1afc2a8a5285596bea234e37b8869b72e
Sou-Cheng Terrya Choi <sou.cheng.terrya.choi@gmail.com>
2014-08-10 11:54:02 -0500

    Spell out 'v' as 'version'.

--- GAIL_Matlab/GAIL_Uninstall.m
+++ GAIL_Matlab/GAIL_Uninstall.m
@@ -7,18 +7,18 @@ if MATLABVERSION < 7,
 else
   gailp=genpath(GAILPATH);
 end
-fprintf('\nYou are going to remove GAIL v %g path.\n', GAILVERSION);
+fprintf('\nYou are going to remove GAIL version %g path.\n', GAILVERSION);
 reply = input('\nDo you want to remove GAIL path from MATLAB search path?\n(Your answer is not case sensitive.) Y/N [N]:','s');
 if any(strcmpi(reply,{'yes','y'}));
     rmpath(gailp);% Remove path from MATLAB search path
     savepath; % Save the changes
     fprintf('\nGAIL path has been removed from MATLAB search path successfully.\n')
-    fprintf('\nYou are going to delete GAIL v %g files.\n ', GAILVERSION);
+    fprintf('\nYou are going to delete GAIL version %g files.\n ', GAILVERSION);
     reply = input('\nDo you want to delete ALL GAIL files?\n (Your answer is not case sensitive.) Y/N [N]: ', 's');
     if any(strcmpi(reply,{'yes','y'}));
         delete('GAILPATH/*.*') % delete all GAIL files
         rmdir(GAILPATH,'s')
-        fprintf('\nAll GAIL v %g files have been deleted successfully.\n', GAILVERSION);
+        fprintf('\nAll GAIL version %g files have been deleted successfully.\n', GAILVERSION);
     else
         fprintf('\nGAIL files have not been deleted.\n');
     end
