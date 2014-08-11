2cf4afcb158f0b0e322a2e0c433aaa0488ae894a
Xuan Zhou <xzhou23@hawk.iit.edu>
2014-08-11 15:43:52 -0500

    update GAIL intro

--- GAIL_Matlab/Documentation/doc_data/intro.m
+++ GAIL_Matlab/Documentation/doc_data/intro.m
@@ -1,12 +1,11 @@
 %% GAIL
 %
-% GAIL is created, developed, and maintained by Fred Hickernell (Illinois
-% Institute of Technology), Sou-Cheng Choi (University of Chicago and 
-% Argonne National Laboratory), and their collaborators including Yuhan 
-% Ding (IIT), Lan Jiang (IIT), and Yizhi Zhang (IIT). 
+% GAIL (Guaranteed Automatic Integration Library) is created, developed, 
+% and maintained by Fred Hickernell (Illinois Institute of Technology), 
+% Sou-Cheng Choi (University of Chicago and Argonne National Laboratory), 
+% Yuhan Ding (IIT), Lan Jiang (IIT), Lluis Antoni Jimenez Rugama (IIT), Xin
+% Tong (IIT), Yizhi Zhang (IIT), and Xuan Zhou (IIT). 
 %
-% GAIL (Guaranteed Automatic Integration Library) is a suite of 
-% algorithms for integration problems in one, many, and infinite 
-% dimensions, and whose answers are guaranteed to be correct.
-%
+% GAIL is a suite of algorithms for integration problems in one, many, and 
+% infinite dimensions, and whose answers are guaranteed to be correct.
 %
