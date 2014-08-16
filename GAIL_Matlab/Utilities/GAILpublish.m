fe2f300e7370934b7c295061598a9dd8d45e69b1
Sou-Cheng Terrya Choi <sou.cheng.terrya.choi@gmail.com>
2014-08-16 14:23:17 -0500

    Make figure windows invisible.

--- GAIL_Matlab/Utilities/GAILpublish.m
+++ GAIL_Matlab/Utilities/GAILpublish.m
@@ -1,7 +1,11 @@
 %GAILPUBLISH  To generate html files in the GAIL subdirectory Documentation
+set(0, 'DefaultFigureVisible', 'off')
 publish('GAIL');
 publish('funclist');
 publish('help_funappx_g');
+publish('help_funappxlocal_g');
 publish('help_integral_g');
-publish('help_meanMC_g');
-publish('help_cubMC_g');
+publish('help_meanMCabs_g');
+%publish('help_meanMCBernoulli_g');
+publish('help_cubMCabs_g');
+set(0, 'DefaultFigureVisible', 'on')
