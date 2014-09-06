d51957f0673a4ef83970533c002c6258ccdb7fdf
Lan Jiang <ljiang14@hawk.iit.edu>
2014-09-06 00:15:46 -0500

    modify copy zip script

--- CopyZipGAILRepoAndGoogleDrive.m
+++ CopyZipGAILRepoAndGoogleDrive.m
@@ -1,13 +1,13 @@
 % This script is to copy and combine files in both repository and Google drive
 % to make a release zip package.
 clear all; close all; clc;
-newfolder = 'GAIL_1_3_0';
-newpath = horzcat('/Users/BabyAudrey/Documents/Study/Research/GAIL_Dev_vol2/',newfolder);
+newfolder = 'GAIL_1_3_1';
+newpath = horzcat('/Users/lanjiang/Documents/Study/Research/GAIL_Dev_Aug8th/',newfolder);
 % set up a new path where you want to copy things
 GAILPATH = GAILstart;%Get the path of repository from GAILstart
-GoogleDrivePath = '/Users/BabyAudrey/Google Drive/GAIL_Dev/GAIL_Matlab';
+GoogleDrivePath = '/Users/lanjiang/Google Drive/GAIL_Dev/GAIL_Matlab';
 % get the google drive path.
 copyfile(GAILPATH,newpath)%copy files in repository to new path
 copyfile(GoogleDrivePath,newpath)% copy files in google drive to new path
 % zip files recursively excluding hidden file such as .ds_store and .icon  
-system(horzcat('zip -r GAIL_1_3_0.zip ', newfolder, ' -x@exclude1_3.lst'));  
+system(horzcat('zip -r GAIL_1_3_1.zip ', newfolder, ' -x@exclude1_3.lst'));  
