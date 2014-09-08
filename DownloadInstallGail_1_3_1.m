144f06bc048e418ec35deb9eae1a48ab94df8c1c
Lan Jiang <ljiang14@hawk.iit.edu>
2014-09-08 15:51:05 -0500

    modify downloadinstall scripts

--- DownloadInstallGail_1_3_1.m
+++ DownloadInstallGail_1_3_1.m
@@ -1,16 +1,16 @@
-% This scripts is to download and install GAIL version 1.3.0 into the
+% This scripts is to download and install GAIL version 1.3.1 into the
 % location you choose
 %
 %   Step 1.  Place this M-file where you want GAIL to go
 %
 %   Step 2.  Run this M-file in MATLAB
 %
-%This file installs GAIL version 1.3.0 in the subdirectory
+%This file installs GAIL version 1.3.1 in the subdirectory
 %"GAIL_1.3.0/GAIL_Matlab".
 %% Download the package and change the directory
 disp('The GAIL package is now being downloaded...')
-unzip('http://math.iit.edu/~openscholar/sites/default/files/meshfree/files/gail_1_3_0.zip') %download and unzip
-cd('GAIL_1_3_0')  %get to the right subdirectory
+unzip('http://math.iit.edu/~openscholar/sites/default/files/meshfree/files/gail_1_3_1.zip') %download and unzip
+cd('GAIL_1_3_1')  %get to the right subdirectory
 
 %% Install GAIL
 GAIL_Install      %this installs GAIL
