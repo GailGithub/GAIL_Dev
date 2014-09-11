c71e127054d3058bd2fe691a0494d8bb7bb0d03a
Fred J. Hickernell <hickernell@iit.edu>
2014-09-11 17:45:51 -0500

    Delete Monte Carlo mfiles

--- GAIL_Matlab/Workouts/MonteCarloFinance/Contents.m
+++
@@ -1,8 +0,0 @@
-% MONTECARLOFINANCE
-%
-% Files
-%   AsianOptionExample - Example of Asian Arithmetic Mean Option
-%   brownmotion        - generates discrete Brownian motion paths
-%   payoff             - generates payoffs for certain options
-%   payoff_XS          - generates payoffs for certain options
-%   stockpath          - generates stock price paths for certain models
