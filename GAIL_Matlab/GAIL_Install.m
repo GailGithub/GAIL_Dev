480f18352fa70d2f11eb7845295b41f2b04a9aeb
Sou-Cheng Terrya Choi <sou.cheng.terrya.choi@gmail.com>
2014-09-27 10:45:38 -0500

    Fix GAIL_Uninstall error.

--- GAIL_Matlab/GAIL_Install.m
+++ GAIL_Matlab/GAIL_Install.m
@@ -5,16 +5,17 @@ fprintf('\nWelcome to GAIL version %g.\n\n', GAILVERSION);
 if MATLABVERSION < 7,
   error('This version is only supported on Matlab 7.x and above.');
 end
+
+reply = input('\nDo you want to install html documentation files?\n (Your answer is not case sensitive.) Y/N [N]: ', 's');
+if any(strcmpi(reply,{'yes','y'}));
+    GAILpublish;        
+    builddocsearchdb(strcat(GAILPATH,'Documentation',PATHNAMESEPARATOR,'html'));
+    fprintf('\nYou can go to help documentation ---> Supplemental Software to learn how to use GAIL.\n');
+else
+    fprintf('\nYou can use test documentation to learn how to use GAIL.\n');
+end
 gailp=genpath(GAILPATH);% Generate strings of paths to GAIL subdirectories
 addpath(gailp);           % Add GAIL directories and subdirectories
 savepath;                 % Save the changes
-reply = input('\nDo you want to install html documentation files?\n (Your answer is not case sensitive.) Y/N [N]: ', 's');
-    if any(strcmpi(reply,{'yes','y'}));
-        GAILpublish;        
-        builddocsearchdb(strcat(GAILPATH,'Documentation',PATHNAMESEPARATOR,'html'));
-        fprintf('\nYou can go to help documentation ---> Supplemental Software to learn how to use GAIL.\n');
-    else
-        fprintf('\nYou can use test documentation to learn how to use GAIL.\n');
-    end
 fprintf('\nGAIL version %g has been installed successfully.\n\n', GAILVERSION);
 
