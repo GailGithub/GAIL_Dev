82209f0b52e67b3bfe6f23bd51c6841d7f0cf640
lluisantoni <lluisantoni@gmail.com>
2014-10-10 11:19:44 -0500

    Sovling conflicts with automaticworkouts.m

--- GAIL_Matlab/Tests/automatic_workouts/automaticworkouts.m
+++ GAIL_Matlab/Tests/automatic_workouts/automaticworkouts.m
@@ -5,17 +5,9 @@ GAIL_Install
 % Go to the tests direcoty (% is comment in matlab environment)
 cd /home/lantoni/GAIL_tests/repo/gail-development/GAIL_Matlab/Tests/
 % Running workouts of univariate_minimazation
-<<<<<<< HEAD
 % workout_ErrorToleranceTest
 % workout_ErrorXToleranceTest
 % workout_TwoExtremeTest
 % workout_XToleranceTest
 
 longtests
-=======
-workout_ErrorToleranceTest
-workout_ErrorXToleranceTest
-workout_TwoExtremeTest
-workout_XToleranceTest
-longtests
->>>>>>> 5aec6dccbeedf4a01fa82da49e97a91903b4128d
