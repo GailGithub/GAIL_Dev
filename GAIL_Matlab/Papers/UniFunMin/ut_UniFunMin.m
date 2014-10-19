68f95f62e30dd83465eb9fc925d8d7dfa8327bf8
Sou-Cheng Terrya Choi <sou.cheng.terrya.choi@gmail.com>
2014-10-19 12:47:32 -0500

    Add semicolons to suppess outputs.

--- GAIL_Matlab/Papers/UniFunMin/ut_UniFunMin.m
+++ GAIL_Matlab/Papers/UniFunMin/ut_UniFunMin.m
@@ -5,25 +5,25 @@ classdef ut_UniFunMin < matlab.unittest.TestCase
       
     function testUniFunMin_test_ErrorTolerance(testCase)
       UniFunMin_test_ErrorTolerance
-      succrates = succnowarn + succwarn   
+      succrates = succnowarn + succwarn;   
       testCase.verifyGreaterThanOrEqual(succrates,[0.2,0.5,0.8]);
     end
     
     function testUniFunMin_test_ErrorXTolerance(testCase)
       UniFunMin_test_ErrorXTolerance
-      succrates = succnowarn + succwarn   
+      succrates = succnowarn + succwarn;   
       testCase.verifyGreaterThanOrEqual(succrates,[0.2,0.5,0.8]);
     end
     
     function testUniFunMin_test_TwoExtreme(testCase)
       UniFunMin_test_TwoExtreme
-      succrates = probfunmin   
+      succrates = probfunmin;   
       testCase.verifyGreaterThanOrEqual(succrates,[0.2,0.5,0.8]);
     end
     
     function testUniFunMin_test_XTolerance(testCase)
       UniFunMin_test_XTolerance
-      succrates = succnowarn + succwarn   
+      succrates = succnowarn + succwarn;   
       testCase.verifyGreaterThanOrEqual(succrates,[0.2,0.5,0.8]);
     end
     
--- GAIL_Matlab/Tests/ut_ConesPaper.m
+++ GAIL_Matlab/Tests/ut_ConesPaper.m
@@ -5,13 +5,13 @@ classdef ut_ConesPaper < matlab.unittest.TestCase
              
     function testConepaper_test_integral_g(testCase)
       conepaper_test_integral_g
-      succrates = succnowarn + succwarn   
+      succrates = succnowarn + succwarn;   
       testCase.verifyGreaterThanOrEqual(succrates,[0.1,0.5,0.8]);
     end
     
     function testConepaper_test_funappx_g(testCase)
       conepaper_test_funappx_g
-      succrates = succnowarn + succwarn   
+      succrates = succnowarn + succwarn;   
       testCase.verifyGreaterThanOrEqual(succrates,[0.1,0.5,0.8]);
     end
   end
