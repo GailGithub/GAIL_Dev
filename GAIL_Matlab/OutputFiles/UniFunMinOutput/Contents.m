d7c7e9b613657d74a187fe1eb997e72954201e2c
Xin Tong <xtong5@hawk.iit.edu>
2014-10-22 20:25:38 -0500

    add contents

--- GAIL_Matlab/OutputFiles/UniFunMinOutput/Contents.m
+++ GAIL_Matlab/OutputFiles/UniFunMinOutput/Contents.m
@@ -3,9 +3,17 @@
 %
 % Files
 %
-% Mat file
+% ErrorToleranceTest-20141009.mat --- dataset of univariate function
+% minimization for Bump test functions with abstol=10^(-8) & TolX=0
 %
-%    
+% XToleranceTest-20141009.mat --- dataset of univariate function
+% minimization for Bump test functions with abstol=0 & TolX=10^(-6)
+%
+% ErrorXToleranceTest-20141009.mat --- dataset of univariate function
+% minimization for Bump test functions with abstol=10^(-8) & TolX=10^(-6)
+% 
+% TwoExtremeTest-20141009.mat --- dataset of univariate function
+% minimization for Functions with two local minimum points 
 
 
 
