fcd6b38e5cefab94f8ef4620a9c0d624473f91b0
YuhanDing <jinliang98@hotmail.com>
2014-10-25 01:02:23 -0500

    Combine files from Google drive

--- GAIL_Matlab/Workouts/univariate_integration_workouts/Contents.m
+++
@@ -1,7 +0,0 @@
-% UNIVARIATE_INTEGRATION_WORKOUTS
-%
-% Files
-%
-%   workout_integral_g    - Calls automatic guaranteed algorithm for univariate integration
-%   ConesPaperIntegrationTest16-Aug-2013-12-24-10.mat - Result from
-%   workout_integral_g
