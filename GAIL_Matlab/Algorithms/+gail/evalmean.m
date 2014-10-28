c93670afe53588faa15599295367f18c0d3f3551
Lan Jiang <ljiang14@hawk.iit.edu>
2014-10-28 18:24:38 -0500

    typos

--- GAIL_Matlab/Algorithms/+gail/evalmean.m
+++ GAIL_Matlab/Algorithms/+gail/evalmean.m
@@ -1,6 +1,6 @@
 function p =evalmean(RV,n,npcmax)
-%%  Split n samples into columns and evalute the mean recursively
-% RV --- the nx1 samples whose mean we want to estimate
+%%  Split n samples into columns and evaluate the mean recursively
+% RV --- the function to generate the random variables
 % n --- the number of samples
 % npcmax --- the maximum samples per loop
 nopt=min(npcmax,n);
@@ -12,7 +12,7 @@ nloop=repmat(nopt,1,nn);
 %vector of numbers of samples per loop step
 if nremain>0; nloop=[nloop nremain]; nn=nn+1; end
 sumY=0;
-for iloop=1:nn %loops to save memory
+parfor iloop=1:nn %loops to save memory
     sumY=sumY+sum(RV(nloop(iloop)));
 end
 %%  Estimate p
