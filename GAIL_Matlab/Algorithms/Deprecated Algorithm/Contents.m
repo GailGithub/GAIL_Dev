536f3ab38094fb087cf7852739d2913e219025c6
Lan Jiang <ljiang14@hawk.iit.edu>
2014-10-31 21:38:38 -0500

    edit some unclear description

--- GAIL_Matlab/Algorithms/Deprecated Algorithm/Contents.m
+++ GAIL_Matlab/Algorithms/Deprecated Algorithm/Contents.m
@@ -2,12 +2,12 @@
 %
 % Files
 %   funappxtau_g  - One dimensional guaranteed function recovery on interval [0,1] with cone condition tau
-%   integraltau_g - 1-D guaranteed function integration using trapezoidal rule
-%   funappx01_g   - One-dimensional guaranteed function recovery on interval [0,1]
-%   integral01_g  - 1-D guaranteed function integration using trapezoidal rule
-%   cubMCabs_g      - Monte Carlo method to evaluate a multidimensional integral to
-%   cubSobol_old_g  - CUBSOBOL_G is a Quasi-Monte Carlo method using Sobol' cubature over the
+%   integraltau_g - 1-D guaranteed function integration using trapezoidal rule on interval [0,1] with cone condition tau
+%   funappx01_g  - One-dimensional guaranteed function recovery on interval [0,1]
+%   integral01_g  - 1-D guaranteed function integration using trapezoidal rule on interval [0,1]
+%   cubMCabs_g      - Monte Carlo method to evaluate a multidimensional integral with absolute error criterion
+%   cubSobol_old_g  - Quasi-Monte Carlo method using Sobol' cubature 
 %   funappxglobal_g - 1-D guaranteed function recovery on a closed interval [a,b]
-%   funmin01_g      - Guaranteed global minimum value of univariate function
-%   meanMCabs_g     - Monte Carlo method to estimate the mean of a random variable to
+%   funmin01_g      - Guaranteed global minimum value of univariate function on interval [0,1]
+%   meanMCabs_g     - Monte Carlo method to estimate the mean of a random variable with absolute error criterion
 %   integralab_g    - 1-D guaranteed function integration using trapezoidal rule
