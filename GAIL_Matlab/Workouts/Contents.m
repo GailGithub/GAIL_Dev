41cc4ceb09f519fc8a65b24c16c8324c12235d40
Lan Jiang <ljiang14@hawk.iit.edu>
2014-10-31 21:43:18 -0500

    still contents

--- GAIL_Matlab/Workouts/Contents.m
+++ GAIL_Matlab/Workouts/Contents.m
@@ -3,6 +3,6 @@
 % Folders
 %   cubMC_workouts                   - workouts of cubMC_g method
 %   meanMC_workouts                  - workouts of meanMC_g method
-%   meanMCBernoulli_g_workouts       - workouts of meanMCBernoulli_g method
+%   meanMCBer_g_workouts             - workouts of meanMCBer_g method
 %   univariate_integration_workouts  - workouts of integral_g method
 %   univariate_minimization_workouts - workouts of funmin_g method
