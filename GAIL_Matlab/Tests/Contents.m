fb6467159832237506496725c36708e15c93b187
Lan Jiang <ljiang14@hawk.iit.edu>
2014-10-31 21:43:18 -0500

    contents

--- GAIL_Matlab/Tests/Contents.m
+++ GAIL_Matlab/Tests/Contents.m
@@ -11,14 +11,13 @@
 %   ut_cubSobol_g        - fast unit tests for cubSobol_g
 %   ut_cubLattice_g      - fast unit tests for cubLattice_g
 %   ut_meanMC_g          - fast unit tests for meanMC_g
-%   ut_meanMCBernoulli_g - fast unit tests for meanMCBernoulli_g
 %   ut_funappx01_g       - fast unit tests for funappx01_g
 %   ut_integral01_g      - fast unit tests for integral01_g
 %   ut_ConesPaper        - long unit tests for conepaper_test_integral_g
 %   dt_funappxglobal_g   - fast doctest for funappxglobal_g
 %   ut_cubMCabs_g        - fast unit test for cubMCabs_g
 %   ut_meanMCabs_g       - fast unit test for meanMCabs_g
-%   ut_meanMCBer_g       - fast unit test for meanMCBernoulli_g
+%   ut_meanMCBer_g       - fast unit test for meanMCBer_g
 %   ut_UniFunMin         - fast unit test for Xin Tong's thesis
 %   ut_funappxglobal_g   - fast unit test for funappxglobal_g
 %   ut_funmin01_g        - fast unit test for funmin01_g
--- GAIL_Matlab/Workouts/Contents.m
+++ GAIL_Matlab/Workouts/Contents.m
@@ -4,4 +4,5 @@
 %   cubMC_workouts                   - workouts of cubMC_g method
 %   meanMC_workouts                  - workouts of meanMC_g method
 %   meanMCBer_g_workouts             - workouts of meanMCBer_g method
+%   univariate_integration_workouts  - workouts of integral_g method
 %   univariate_minimization_workouts - workouts of funmin_g method
