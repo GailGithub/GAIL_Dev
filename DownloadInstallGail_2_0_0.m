9ebad06b1c3a18a8396107509a4c734583a1a6a0
Sou-Cheng Terrya Choi <sou.cheng.terrya.choi@gmail.com>
2014-11-01 22:06:52 -0500

    Debugged Download script.

--- DownloadInstallGail_2_0_0.m
+++ DownloadInstallGail_2_0_0.m
@@ -9,8 +9,8 @@
 %"GAIL_2.0.0/GAIL_Matlab".
 %% Download the package and change the directory
 disp('The GAIL package is now being downloaded...')
-unzip('http://math.iit.edu/~openscholar/sites/default/files/meshfree/files/gail_1_3_0.zip') %download and unzip
-cd('GAIL_2_0_0')  %get to the right subdirectory
+unzip('http://math.iit.edu/~openscholar/sites/default/files/meshfree/files/gail_2_0.zip') %download and unzip
+cd('GAIL_2_0')  %get to the right subdirectory
 
 %% Install GAIL
 GAIL_Install      %this installs GAIL
