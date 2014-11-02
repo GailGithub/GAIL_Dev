3bc2a4db4d08753b5739fe5d8df6d07e9045c833
Xuan Zhou <xzhou23@hawk.iit.edu>
2014-11-01 21:31:24 -0500

    delete private files

--- GAIL_Matlab/Tests/Private_not_to_publish/automatic_tests/automatictests.m
+++
@@ -1,8 +0,0 @@
-%% Set the directory for running our matlab test
-cd /home/lantoni/GAIL_tests/repo/gail-development/GAIL_Matlab/
-% Install the latest
-GAIL_Install
-% Go to the tests direcoty (% is comment in matlab environment)
-cd /home/lantoni/GAIL_tests/repo/gail-development/GAIL_Matlab/Tests/Private_not_to_publish/
-%runtests %> /home/lantoni/GAIL_tests/test_results.txt former tests
-runtests
--- GAIL_Matlab/Tests/Private_not_to_publish/automatic_tests/mail_c.sh
+++
@@ -1,4 +0,0 @@
-echo "Daily test results were OK" | mail -s "GAIL test results" gail_dev@googlegroups.com -- -r "ljimene1@hawk.iit.edu"
-# (echo "Daily test results were OK";uuencode Test_Results.txt Test_Results.txt) | mail -s "GAIL test results" gail_dev@googlegroups.com -- -r "ljimene1@hawk.iit.edu"
-# And if you want mail to read the content from a file:
-# mail -s "GAIL daily test results" gail_dev@googlegroups.com </home/lantoni/GAIL_tests/mail_c.txt
