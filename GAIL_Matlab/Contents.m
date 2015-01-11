f1770238659901131b6a01af33986cf9d4053d65
Sou-Cheng Terrya Choi <sou.cheng.terrya.choi@gmail.com>
2015-01-11 07:41:56 -0600

    Remove Reinstall.

--- GAIL_Matlab/Contents.m
+++ GAIL_Matlab/Contents.m
@@ -1,10 +1,9 @@
 % Guaranteed Automatic Integration Library (GAIL)
-% Version 2.0 01-Nov-2014
+% Version 2.1 14-Feb-2015
 %
 % Files
 %   GAILstart      - Initialize all the GAIL paths and system parameters.
 %   GAIL_Install   - Install GAIL. Add GAIL paths to MATLAB search path.
-%   GAIL_Reinstall - Reinstall GAIL. Remove existing GAIL paths and add new ones.
 %   GAIL_Uninstall - Uninstall GAIL. Remove GAIL paths from MATLAB search path
 %   LICENSE        - License of GAIL
 %   README         - Installation of and introduction to GAIL.
