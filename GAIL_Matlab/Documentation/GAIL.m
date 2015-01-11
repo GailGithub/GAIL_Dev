6294203f46f83e4f432231ff8540d13c6ba2710b
Sou-Cheng Terrya Choi <sou.cheng.terrya.choi@gmail.com>
2015-01-11 08:28:56 -0600

    Added Installation deatils.

--- GAIL_Matlab/Documentation/GAIL.m
+++ GAIL_Matlab/Documentation/GAIL.m
@@ -9,27 +9,31 @@
 % GAIL is a suite of algorithms for integration problems in one, many, and 
 % infinite dimensions, and whose answers are guaranteed to be correct.
 %
+%% Installation
+%
+% <html>
+% <a href="help_install.html">Instructions</a>
+% </html>
+%
 %% Functions
 %
 % <html>
-% <a href="help_funappx_g.html">funappx_g</a>
-% <a href="help_funmin_g.html">funmin_g</a>
-% <a href="help_integral_g.html">integral_g</a>
-% <a href="help_meanMC_g.html">meanMC_g</a>
-% <a href="help_meanMCBer_g.html">meanMCBer_g</a>
-% <a href="help_cubMC_g.html">cubMC_g</a>
-% <a href="help_cubSobol_g.html">cubSobol_g</a>
-% <a href="help_cubLattice_g.html">cubLattice_g</a>
+% <a href="help_funappx_g.html">funappx_g</a>,
+% <a href="help_funmin_g.html">funmin_g</a>,
+% <a href="help_integral_g.html">integral_g</a>,
+% <a href="help_meanMC_g.html">meanMC_g</a>,
+% <a href="help_meanMCBer_g.html">meanMCBer_g</a>,
+% <a href="help_cubMC_g.html">cubMC_g</a>,
+% <a href="help_cubSobol_g.html">cubSobol_g</a>,
+% <a href="help_cubLattice_g.html">cubLattice_g</a>.
 % </html>
 %
-%
+
 %% Tests
 % We provide quick doctests for each of the functions above. To run
-% doctests in *funappx_g*, for example, issue the following command:
-% 
-    doctest funappx_g
-
-%%
+% doctests in *funappx_g*, for example, issue the command *doctest
+% funappx_g*.
+%
 % We also provide unit tests for MATLAB version 8 or later. To run unit
 % tests for *funmin_g*, for instance, execute *run(ut_funmin_g);*
 %
