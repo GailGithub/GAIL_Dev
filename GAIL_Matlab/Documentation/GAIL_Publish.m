820cfd1dd39f8bc0466b8b257b4ef3deed68614a
Sou-Cheng Terrya Choi <sou.cheng.terrya.choi@gmail.com>
2015-01-17 13:01:54 -0600

    MATLAB publish needs java.

--- GAIL_Matlab/Documentation/GAIL_Publish.m
+++ GAIL_Matlab/Documentation/GAIL_Publish.m
@@ -1,19 +1,21 @@
 function GAIL_Publish()
-% GAIL_PUBLISH  To generate html files in the GAIL subdirectory Documentation
-oldStatus = get(0,'DefaultFigureVisible');
-set(0, 'DefaultFigureVisible', 'off')
-[GAILPATH,GAILVERSION,PATHNAMESEPARATOR,MATLABVERSION] = GAILstart(0);
-delete(strcat(GAILPATH,'Documentation',PATHNAMESEPARATOR,'html',PATHNAMESEPARATOR,'*.png'))
-publish('GAIL');
-publish('funclist');
-publish('help_funappx_g');
-publish('help_integral_g');
-publish('help_meanMC_g');
-publish('help_meanMCBer_g');
-publish('help_cubMC_g');
-publish('help_funmin_g');
-publish('help_cubLattice_g');
-publish('help_cubSobol_g');
-publish('help_install');
-set(0, 'DefaultFigureVisible', oldStatus)
+if usejava('jvm')
+  % GAIL_PUBLISH  To generate html files in the GAIL subdirectory Documentation
+  oldStatus = get(0,'DefaultFigureVisible');
+  set(0, 'DefaultFigureVisible', 'off')
+  [GAILPATH,GAILVERSION,PATHNAMESEPARATOR,MATLABVERSION] = GAILstart(0);
+  delete(strcat(GAILPATH,'Documentation',PATHNAMESEPARATOR,'html',PATHNAMESEPARATOR,'*.png'))
+  publish('GAIL');
+  publish('funclist');
+  publish('help_funappx_g');
+  publish('help_integral_g');
+  publish('help_meanMC_g');
+  publish('help_meanMCBer_g');
+  publish('help_cubMC_g');
+  publish('help_funmin_g');
+  publish('help_cubLattice_g');
+  publish('help_cubSobol_g');
+  publish('help_install');
+  set(0, 'DefaultFigureVisible', oldStatus)
+end
 end
