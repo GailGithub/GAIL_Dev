fb78497275654b942995856757b1a7153f90232b
Sou-Cheng Terrya Choi <sou.cheng.terrya.choi@gmail.com>
2015-01-21 09:11:58 -0600

    Move save_mat and save_eps to +gail.

---
+++ GAIL_Matlab/Algorithms/+gail/save_eps.m
@@ -0,0 +1,16 @@
+function path = save_eps(subdir, filename)
+% SAVE_MAT: Save figure to an eps file in a subdirectory in 'OutputFiles'
+% 
+% Inputs:
+%   subdir          string, name of subdirectory 
+%   filename        string, filename of eps file
+%   
+% Example:
+%   save_eps('ConesPaperOutput','ConesPaperFunAppxTest', ...
+%   'tauvec','pini','pfin','succnowarn', 'succwarn','failnowarn','failwarn');
+
+[GAILPATH,~,PATHNAMESEPARATOR] = GAILstart(0);
+path = strcat(GAILPATH,'OutputFiles',PATHNAMESEPARATOR,...
+    subdir, PATHNAMESEPARATOR, filename, '-',...
+    datestr(now,'yyyy-mmm-dd-HH-MM-SS'),'.eps');
+print('-depsc', path)
