36ae2de9b369668a3d47b2a33376e841a5320dd7
Xin Tong <xtong5@hawk.iit.edu>
2015-01-22 17:16:51 -0600

    Error to Err

--- GAIL_Matlab/Workouts/univariate_minimization_workouts/Contents.m
+++ GAIL_Matlab/Workouts/univariate_minimization_workouts/Contents.m
@@ -3,13 +3,13 @@
 %
 % Files
 %
-% workout_ErrorToleranceTest.m --- test for Bump test functions with 
+% workout_ErrToleranceTest.m --- test for Bump test functions with 
 % abstol=10^(-8) & TolX=0
 %
 % workout_XToleranceTest.m --- test for Bump test functions with abstol=0 
 % & TolX=10^(-6)
 %
-% workout_ErrorXToleranceTest.m --- test for Bump test functions with 
+% workout_ErrXToleranceTest.m --- test for Bump test functions with 
 % abstol=10^(-8) & TolX=10^(-6)
 % 
 % workout_TwoExtremeTest.m --- test for Functions with two local minimum 
