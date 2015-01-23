d97d360cb753072ffd8b366f5aea3ad93bff31ca
Lan Jiang <ljiang14@hawk.iit.edu>
2015-01-23 22:33:13 +0800

    lan's attempt to fix the bug of save_mat

--- GAIL_Matlab/Algorithms/+gail/save_mat.m
+++ GAIL_Matlab/Algorithms/+gail/save_mat.m
@@ -1,4 +1,4 @@
-function fullfilename = save_mat(subdir, filename, varargin)
+function fullfilename = save_mat(subdir, filename,varargin)
 % SAVE_MAT: Save data to a MAT file in a subdirectory in 'OutputFiles'
 % 
 % Inputs:
@@ -11,7 +11,12 @@ function fullfilename = save_mat(subdir, filename, varargin)
 %   'tauvec','pini','pfin','succnowarn', 'succwarn','failnowarn','failwarn');
 
 [GAILPATH,~,PATHNAMESEPARATOR] = GAILstart(0);
-fullfilename = strcat(GAILPATH,'OutputFiles',PATHNAMESEPARATOR,...
-    subdir, PATHNAMESEPARATOR', filename,'-',...
-    datestr(now,'yyyy-mmm-dd-HH-MM-SS'),'.mat');
-save(fullfilename, varargin)
+path = strcat(GAILPATH,'OutputFiles',PATHNAMESEPARATOR,...
+    subdir, PATHNAMESEPARATOR');
+fn = strcat(filename,'-',datestr(now,'yyyy-mmm-dd-HH-MM-SS'),'.mat');
+% fullfilename = strcat(GAILPATH,'OutputFiles',PATHNAMESEPARATOR,...
+%     subdir, PATHNAMESEPARATOR', filename,'-',...
+%     datestr(now,'yyyy-mmm-dd-HH-MM-SS'),'.mat');
+fullfilename = [path,fn];
+save([path,fn],varargin)
+%, varargin)
