12fcd66e0d20833429e8abddc1176d85c54587ca
Xin Tong <xtong5@hawk.iit.edu>
2015-01-22 21:50:11 -0600

    Update affiliation

--- GAIL_Matlab/Documentation/doc_data/intro.m
+++ GAIL_Matlab/Documentation/doc_data/intro.m
@@ -4,7 +4,7 @@
 % and maintained by Fred Hickernell (Illinois Institute of Technology), 
 % Sou-Cheng Choi (NORC at the University of Chicago and IIT), 
 % Yuhan Ding (IIT), Lan Jiang (IIT), Lluis Antoni Jimenez Rugama (IIT), Xin
-% Tong (IIT), Yizhi Zhang (IIT), and Xuan Zhou (IIT). 
+% Tong (UIC), Yizhi Zhang (IIT), and Xuan Zhou (IIT). 
 %
 % GAIL is a suite of algorithms for integration problems in one, many, and 
 % infinite dimensions, and whose answers are guaranteed to be correct.
