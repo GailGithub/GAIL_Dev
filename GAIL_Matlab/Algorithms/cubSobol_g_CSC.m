4be82d81bf079edd85cb839fe47475968fda86a0
Sou-Cheng Terrya Choi <sou.cheng.terrya.choi@gmail.com>
2015-01-23 08:39:00 -0600

    Change exitflag to logical.

--- GAIL_Matlab/Algorithms/cubSobol_g_CSC.m
+++ GAIL_Matlab/Algorithms/cubSobol_g_CSC.m
@@ -1,9 +1,9 @@
-function [q,out_param] = cubSobol_g(varargin)
-%CUBSOBOL_G is a Quasi-Monte Carlo method using Sobol' cubature over the
+function [q,out_param] = cubSobol_g_CSC(varargin)
+%cubSobol_g_CSC is a Quasi-Monte Carlo method using Sobol' cubature over the
 %d-dimensional region to integrate within a specified generalized error
 %tolerance with guarantees under Walsh-Fourier coefficients cone decay assumptions.
 %
-%   [q,out_param] = CUBSOBOL_G(f,d) estimates the integral of f over the
+%   [q,out_param] = cubSobol_g_CSC(f,d) estimates the integral of f over the
 %   d-dimensional region with an error guaranteed not to be greater than 
 %   a specific generalized error tolerance, 
 %   tolfun:=max(abstol,reltol*|integral(f)|). The generalized tolerance function can
@@ -14,20 +14,20 @@ function [q,out_param] = cubSobol_g(varargin)
 %   is the dimension in which the function f is defined. Given the
 %   construction of Sobol', d must be a positive integer with 1<=d<=1111.
 %
-%   q = CUBSOBOL_G(f,d,abstol,reltol,density,mmin,mmax,fudge,errtype,theta)
+%   q = cubSobol_g_CSC(f,d,abstol,reltol,density,mmin,mmax,fudge,errtype,theta)
 %   estimates the integral of f over a d-dimensional region. The answer
 %   is given within the generalized error tolerance tolfun. All parameters
 %   should be input in the order specified above. If an input is not specified,
 %   the default value is used. Note that if an input is not specified,
 %   the remaining tail cannot be specified either.
 %
-%   q = CUBSOBOL_G(f,d,'abstol',abstol,'reltol',reltol,'density',density,'mmin',mmin,'mmax',mmax,'fudge',fudge,'errtype',errtype,'theta',theta)
+%   q = cubSobol_g_CSC(f,d,'abstol',abstol,'reltol',reltol,'density',density,'mmin',mmin,'mmax',mmax,'fudge',fudge,'errtype',errtype,'theta',theta)
 %   estimates the integral of f over a d-dimensional region. The answer
 %   is given within the generalized error tolerance tolfun. All the field-value
 %   pairs are optional and can be supplied with any order. If an input is not
 %   specified, the default value is used.
 %
-%   q = CUBSOBOL_G(f,d,in_param) estimates the integral of f over the
+%   q = cubSobol_g_CSC(f,d,in_param) estimates the integral of f over the
 %   d-dimensional region. The answer is given within the generalized error 
 %   tolerance tolfun.
 % 
@@ -83,13 +83,13 @@ function [q,out_param] = cubSobol_g(varargin)
 %
 %     q --- the estimated value of the integral.
 %
-%     out_param.n --- number of points used when calling cubSobol_g for f.
+%     out_param.n --- number of points used when calling cubSobol_g_CSC for f.
 %
 %     out_param.pred_err --- predicted bound on the error based on the cone
 %     condition. If the function lies in the cone, the real error should be
 %     smaller than this predicted error.
 %
-%     out_param.time --- time elapsed in seconds when calling cubSobol_g for f.
+%     out_param.time --- time elapsed in seconds when calling cubSobol_g_CSC for f.
 %
 %     out_param.exit --- this is a number defining the conditions of
 %     success or failure satisfied when finishing the algorithm. The 
@@ -124,7 +124,7 @@ function [q,out_param] = cubSobol_g(varargin)
 % Example 1:
 % Estimate the integral with integrand f(x) = x1.*x2 in the interval [0,1)^2:
 %
-% >> f = @(x) x(:,1).*x(:,2); d = 2; q = cubSobol_g(f,d,1e-5,1e-1,'uniform')
+% >> f = @(x) x(:,1).*x(:,2); d = 2; q = cubSobol_g_CSC(f,d,1e-5,1e-1,'uniform')
 % q = 0.25***
 % 
 % 
@@ -132,7 +132,7 @@ function [q,out_param] = cubSobol_g(varargin)
 % Estimate the integral with integrand f(x) = x1.^2.*x2.^2.*x3.^2+0.11
 % in the interval R^3 where x1, x2 and x3 are normally distributed:
 %
-% >> f = @(x) x(:,1).^2.*x(:,2).^2.*x(:,3).^2+0.11; d = 3; q = cubSobol_g(f,d,1e-3,1e-3,'normal')
+% >> f = @(x) x(:,1).^2.*x(:,2).^2.*x(:,3).^2+0.11; d = 3; q = cubSobol_g_CSC(f,d,1e-3,1e-3,'normal')
 % q = 1.1***
 % 
 %
@@ -140,7 +140,7 @@ function [q,out_param] = cubSobol_g(varargin)
 % Estimate the integral with integrand f(x) = exp(-x1^2-x2^2) in the
 % interval [0,1)^2:
 % 
-% >> f = @(x) exp(-x(:,1).^2-x(:,2).^2); d = 2; q = cubSobol_g(f,d,1e-3,1e-1,'uniform')
+% >> f = @(x) exp(-x(:,1).^2-x(:,2).^2); d = 2; q = cubSobol_g_CSC(f,d,1e-3,1e-1,'uniform')
 % q = 0.55***
 %
 %
@@ -148,7 +148,7 @@ function [q,out_param] = cubSobol_g(varargin)
 % Estimate the price of an European call with S0=100, K=100, r=sigma^2/2,
 % sigma=0.05 and T=1.
 % 
-% >> f = @(x) exp(-0.05^2/2)*max(100*exp(0.05*x)-100,0); d = 1; q = cubSobol_g(f,d,1e-4,1e-1,'normal','fudge',@(m) 2.^-(2*m))
+% >> f = @(x) exp(-0.05^2/2)*max(100*exp(0.05*x)-100,0); d = 1; q = cubSobol_g_CSC(f,d,1e-4,1e-1,'normal','fudge',@(m) 2.^-(2*m))
 % q = 2.05***
 %
 %
@@ -156,7 +156,7 @@ function [q,out_param] = cubSobol_g(varargin)
 % Estimate the integral with integrand f(x) = 8*x1.*x2.*x3.*x4.*x5 in the interval
 % [0,1)^5 with pure absolute error 1e-5.
 % 
-% >> f = @(x) 8*prod(x,2); d = 5; q = cubSobol_g(f,d,1e-5,'errtype','comb','theta',1)
+% >> f = @(x) 8*prod(x,2); d = 5; q = cubSobol_g_CSC(f,d,1e-5,'errtype','comb','theta',1)
 % q = 0.25***
 %
 %
@@ -178,7 +178,7 @@ function [q,out_param] = cubSobol_g(varargin)
 
 tic
 %% Check and initialize parameters
-[f,out_param] = cubSobol_g_param(varargin{:});
+[f,out_param] = cubSobol_g_CSC_param(varargin{:});
 
 if strcmp(out_param.density,'normal')
    f=@(x) f(gail.stdnorminv(x));
@@ -195,7 +195,7 @@ cond2=(1+out_param.fudge(mlag-(1:mlag)))*(1+2*out_param.fudge(mlag))/(1+out_para
 errest=zeros(out_param.mmax-out_param.mmin+1,1); %initialize error estimates
 appxinteg=zeros(out_param.mmax-out_param.mmin+1,1); %initialize approximations to integral
 exit_len = 2;
-out_param.exit=zeros(exit_len,1); %we start the algorithm with all warning flags down
+out_param.exit=logical(zeros(exit_len,1)); %we start the algorithm with all warning flags down
 outside_cone=false; %internal flag that becomes true if the function lies outside the cone
 
 
@@ -253,17 +253,21 @@ deltaminus = 0.5*(gail.tolfun(out_param.abstol,...
     out_param.errtype)-gail.tolfun(out_param.abstol,out_param.reltol,...
     out_param.theta,abs(q+errest(1)),out_param.errtype));
 
+is_done = false;
 if out_param.pred_err <= deltaplus
    q=q+deltaminus;
    appxinteg(1)=q;
    out_param.time=toc;
-   return
+   is_done = true;
 elseif out_param.mmin == out_param.mmax % We are on our max budget and did not meet the error condition => overbudget
-       out_param.exit(1)=1;
+   out_param.exit(1)=1;
 end
 
 %% Loop over m
 for m=out_param.mmin+1:out_param.mmax 
+   if is_done,
+       break;
+   end    
    out_param.n=2^m;
    mnext=m-1;
    nnext=2^mnext;
@@ -314,11 +318,9 @@ for m=out_param.mmin+1:out_param.mmax
    end
    % disp((Stilde(meff)*cond1(1:min(meff-1,mlag)))>=(StildeNC(meff-1,1:min(meff-1,mlag)))) % Displaying necessary condition 1 results (1 if satisfied)
    % disp((StildeNC(meff-1,1:min(meff-1,mlag)).*cond2(1:min(meff-1,mlag)))>=(Stilde(meff)*ones(1,min(meff-1,mlag)))) % Displaying necessary condition 2 results (1 if satisfied)
-   if ~(prod(1 * ( (Stilde(meff)*cond1(1:min(meff-1,mlag))) >= ...
-                 (StildeNC(meff-1,1:min(meff-1,mlag))))) * ...
-        prod(1 * ( (StildeNC(meff-1,1:min(meff-1,mlag)).*cond2(1:min(meff-1,mlag))) ...
-          >= (Stilde(meff)*ones(1,min(meff-1,mlag)))))...
-       ) && outside_cone == false
+   if ~(all((Stilde(meff)*cond1(1:min(meff-1,mlag)))>=(StildeNC(meff-1,1:min(meff-1,mlag))))*   ...
+    all((StildeNC(meff-1,1:min(meff-1,mlag)).*cond2(1:min(meff-1,mlag)))>=(Stilde(meff)*ones(1,min(meff-1,mlag)))))  ...
+    && outside_cone == false,
         outside_cone = true; % We are outside the cone
         out_param.exit(2) = 1;
    end
@@ -342,16 +344,17 @@ for m=out_param.mmin+1:out_param.mmax
       q=q+deltaminus;
       appxinteg(meff)=q;
       out_param.time=toc;
-      return
+      is_done = false;
    elseif m == out_param.mmax % We are on our max budget and did not meet the error condition => overbudget
       out_param.exit(1) = 1;
    end
 
+   is_done = any(out_param.exit);
 end
 
 exit_str='';
 if sum(out_param.exit) == 0
-  out_param.exitflag = '0';
+  exit_str = '0';
 else
   for i=1:exit_len
     if out_param.exit(i)==1,
@@ -369,8 +372,8 @@ out_param.time=toc;
 end
 
 
-%% Parsing for the input of cubSobol_g
-function [f, out_param] = cubSobol_g_param(varargin)
+%% Parsing for the input of cubSobol_g_CSC
+function [f, out_param] = cubSobol_g_CSC_param(varargin)
 
 % Default parameter values
 default.abstol  = 1e-4;
@@ -383,8 +386,8 @@ default.errtype  = 'max';
 default.theta  = 1;
 
 if numel(varargin)<2
-    help cubSobol_g
-    warning('MATLAB:cubSobol_g:fdnotgiven',...
+    help cubSobol_g_CSC
+    warning('MATLAB:cubSobol_g_CSC:fdnotgiven',...
         'At least, function f and dimension d need to be specified. Example for f(x)=x^2:')
     f = @(x) x.^2;
     out_param.f=f;
@@ -392,7 +395,7 @@ if numel(varargin)<2
 else
     f = varargin{1};
     if ~gail.isfcn(f)
-        warning('MATLAB:cubSobol_g:fnotfcn',...
+        warning('MATLAB:cubSobol_g_CSC:fnotfcn',...
             'The given input f was not a function. Example for f(x)=x^2:')
         f = @(x) x.^2;
         out_param.f=f;
@@ -401,7 +404,7 @@ else
         out_param.f=f;
         d = varargin{2};
         if ~isnumeric(d) || ~gail.isposint(d) || ~(d<101)
-            warning('MATLAB:cubSobol_g:dnotposint',...
+            warning('MATLAB:cubSobol_g_CSC:dnotposint',...
                 'The dimension d must be a positive integer less than 101. Example for f(x)=x^2:')
             f = @(x) x.^2;
             out_param.f=f;
@@ -420,7 +423,7 @@ if validvarargin
         || ischar(in3{j}) || isstruct(in3{j}) || gail.isfcn(in3{j}));
     end
     if ~validvarargin
-        warning('MATLAB:cubSobol_g:validvarargin','Optional parameters must be numeric or strings. We will use the default optional parameters.')
+        warning('MATLAB:cubSobol_g_CSC:validvarargin','Optional parameters must be numeric or strings. We will use the default optional parameters.')
     end
     in3=varargin{3};
 end
@@ -478,28 +481,28 @@ end;
 
 % Force absolute tolerance greater than 0
 if (out_param.abstol <= 0 )
-    warning('MATLAB:cubSobol_g:abstolnonpos',['Error tolerance should be greater than 0.' ...
+    warning('MATLAB:cubSobol_g_CSC:abstolnonpos',['Error tolerance should be greater than 0.' ...
             ' Using default error tolerance ' num2str(default.abstol)])
     out_param.abstol = default.abstol;
 end
 
 % Force relative tolerance greater than 0 and smaller than 1
 if (out_param.reltol <= 0) || (out_param.reltol > 1)
-    warning('MATLAB:cubSobol_g:reltolnonunit',['Relative tolerance should be chosen in (0,1].' ...
+    warning('MATLAB:cubSobol_g_CSC:reltolnonunit',['Relative tolerance should be chosen in (0,1].' ...
             ' Using default relative tolerance ' num2str(default.reltol)])
     out_param.reltol = default.reltol;
 end
 
 % Force density to be uniform or normal only
 if ~(strcmp(out_param.density,'uniform') || strcmp(out_param.density,'normal') )
-    warning('MATLAB:cubSobol_g:notdensity',['The density can only be uniform or normal.' ...
+    warning('MATLAB:cubSobol_g_CSC:notdensity',['The density can only be uniform or normal.' ...
             ' Using default density ' num2str(default.density)])
     out_param.density = default.density;
 end
 
 % Force mmin to be integer greater than 0
 if (~gail.isposint(out_param.mmin) || ~(out_param.mmin < out_param.mmax+1))
-    warning('MATLAB:cubSobol_g:lowmmin',['The minimum starting exponent ' ...
+    warning('MATLAB:cubSobol_g_CSC:lowmmin',['The minimum starting exponent ' ...
             'should be an integer greater than 0 and smaller or equal than the maxium.' ...
             ' Using default mmin ' num2str(default.mmin)])
     out_param.mmin = default.mmin;
@@ -508,28 +511,28 @@ end
 % Force exponent budget number of points be a positive integer greater than
 % or equal to mmin an smaller than 54
 if ~(gail.isposint(out_param.mmax) && out_param.mmax>=out_param.mmin && out_param.mmax<=53)
-    warning('MATLAB:cubSobol_g:wrongmmax',['The maximum exponent for the budget should be an integer smaller or equal to 53.' ...
+    warning('MATLAB:cubSobol_g_CSC:wrongmmax',['The maximum exponent for the budget should be an integer smaller or equal to 53.' ...
             ' Using default mmax ' num2str(default.mmax)])
     out_param.mmax = default.mmax;
 end
 
 % Force fudge factor to be greater than 0
 if ~((gail.isfcn(out_param.fudge) && (out_param.fudge(1)>0)))
-    warning('MATLAB:cubSobol_g:fudgenofcn',['The fudge factor should be a positve function.' ...
+    warning('MATLAB:cubSobol_g_CSC:fudgenofcn',['The fudge factor should be a positve function.' ...
             ' Using default fudge factor ' func2str(default.fudge)])
     out_param.fudge = default.fudge;
 end
 
 % Force errtype to be max or comb
 if ~(strcmp(out_param.errtype,'max') || strcmp(out_param.errtype,'comb') )
-    warning('MATLAB:cubSobol_g:noterrtype',['The error type can only be max or comb.' ...
+    warning('MATLAB:cubSobol_g_CSC:noterrtype',['The error type can only be max or comb.' ...
             ' Using default errtype ' num2str(default.errtype)])
     out_param.errtype = default.errtype;
 end
 
 % Force theta to be in [0,1]
 if (out_param.theta < 0) || (out_param.theta > 1)
-    warning('MATLAB:cubSobol_g:thetanonunit',['Theta should be chosen in [0,1].' ...
+    warning('MATLAB:cubSobol_g_CSC:thetanonunit',['Theta should be chosen in [0,1].' ...
             ' Using default theta ' num2str(default.theta)])
     out_param.theta = default.theta;
 end
