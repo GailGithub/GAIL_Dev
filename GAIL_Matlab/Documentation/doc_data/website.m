ab60496c0180e121a33d57784836372a8792eb64
Sou-Cheng Terrya Choi <sou.cheng.terrya.choi@gmail.com>
2015-01-25 09:48:28 -0600

    Hacking more GAIL info into website.m to be propagated into GAIL.m by autodoc and GAIL_publish.

--- GAIL_Matlab/Documentation/doc_data/website.m
+++ GAIL_Matlab/Documentation/doc_data/website.m
@@ -1,3 +1,22 @@
+%% Installation
+%
+% <html>
+% <a href="help_install.html">Instructions</a>
+% </html>
+%
+%% Tests
+% We provide quick doctests for each of the functions above. To run
+% doctests in *funappx_g*, for example, issue the command *doctest
+% funappx_g*.
+%
+% We also provide unit tests for MATLAB version 8 or later. To run unit
+% tests for *funmin_g*, for instance, execute *run(ut_funmin_g);*
+%
+% To run all the fast doctests and unit tests in the suite, execute the 
+% script *runtests.m*. 
+%
+% A collection of long tests are contained in *longtests.m*. 
+%
 %% Website
 % For more information about GAIL, visit
 % <http://code.google.com/p/gail/ Gailteam>
