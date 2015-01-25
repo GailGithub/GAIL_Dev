05cd6d3a993962741a9699bd8ac01f7473fd1908
Sou-Cheng Terrya Choi <sou.cheng.terrya.choi@gmail.com>
2015-01-25 10:34:44 -0600

    Collect all filenames in a cell array.

--- GAIL_Matlab/Documentation/GAIL_Publish.m
+++ GAIL_Matlab/Documentation/GAIL_Publish.m
@@ -5,23 +5,22 @@ if usejava('jvm')
     set(0, 'DefaultFigureVisible', 'off')
     [GAILPATH,GAILVERSION,PATHNAMESEPARATOR,MATLABVERSION] = GAILstart(0);
     delete(strcat(GAILPATH,'Documentation',PATHNAMESEPARATOR,'html',PATHNAMESEPARATOR,'*.png'))
-    publish('GAIL');
-    publish('funclist');
-    publish('help_funappx_g');
-    publish('help_integral_g');
-    publish('help_meanMC_g');
-    publish('help_meanMCBer_g');
-    publish('help_cubMC_g');
-    publish('help_funmin_g');
-    %publish('help_cubLattice_g');
-    %publish('help_cubSobol_g');
-    publish('help_install');
+    mfile_list = {'GAIL','funclist','help_funappx_g','help_funmin_g',...
+        'help_integral_g', 'help_meanMC_g','help_meanMCBer_g', ...
+        'help_cubMC_g','help_cubLattice_g','help_cubSobol_g','help_install'};
+    for i=1:length(mfile_list),
+        publish('help_install');
+    end
     set(0, 'DefaultFigureVisible', oldStatus)
 end
 
 delete(strcat(GAILPATH,'Documentation',PATHNAMESEPARATOR,'gail_ug.m'))
 delete(strcat(GAILPATH,'Documentation',PATHNAMESEPARATOR,'html',PATHNAMESEPARATOR,'gail_ug.*'))
-system('cat GAIL.m funclist.m help_meanMC_g.m help_funappx_g.m help_integral_g.m GAIL_Publish.m help_cubMC_g.m help_funmin_g.m help_meanMCBer_g.m >> gail_ug.m');
-
+cat_cmd = 'cat ';
+for i=1:length(mfile_list),
+    cat_cmd = strcat([cat_cmd, ' ', GAILPATH,'Documentation',PATHNAMESEPARATOR,mfile_list{i},'.m', ' ']);
+end
+cat_cmd = strcat(cat_cmd, '>> gail_ug.m');
+system(cat_cmd);
 publish('gail_ug','pdf');
 end
