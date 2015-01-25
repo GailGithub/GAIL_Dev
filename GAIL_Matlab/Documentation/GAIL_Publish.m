f0b4db0655b0ffe57b2367fe8f8acecf6b00f161
Sou-Cheng Terrya Choi <sou.cheng.terrya.choi@gmail.com>
2015-01-25 11:05:36 -0600

    Fix bugs. Add documentation.

--- GAIL_Matlab/Documentation/GAIL_Publish.m
+++ GAIL_Matlab/Documentation/GAIL_Publish.m
@@ -4,23 +4,28 @@ if usejava('jvm')
     oldStatus = get(0,'DefaultFigureVisible');
     set(0, 'DefaultFigureVisible', 'off')
     [GAILPATH,GAILVERSION,PATHNAMESEPARATOR,MATLABVERSION] = GAILstart(0);
+    
+    %% generate GAIL User Guide in HTML format
     delete(strcat(GAILPATH,'Documentation',PATHNAMESEPARATOR,'html',PATHNAMESEPARATOR,'*.png'))
     mfile_list = {'GAIL','funclist','help_funappx_g','help_funmin_g',...
         'help_integral_g', 'help_meanMC_g','help_meanMCBer_g', ...
-        'help_cubMC_g','help_cubLattice_g','help_cubSobol_g','help_install'};
+        'help_cubMC_g','help_install'};
     for i=1:length(mfile_list),
-        publish('help_install');
+        publish(mfile_list{i});
     end
-    set(0, 'DefaultFigureVisible', oldStatus)
+    
+    %% generate GAIL User Guide in PDF format
+    delete(strcat(GAILPATH,'Documentation',PATHNAMESEPARATOR,'html',PATHNAMESEPARATOR,'gail_ug.*'))
+    cat_cmd = 'cat ';
+    for i=1:length(mfile_list),
+        cat_cmd = strcat([cat_cmd, ' ', GAILPATH,'Documentation',PATHNAMESEPARATOR,mfile_list{i},'.m', ' ']);
+    end
+    gailug_filename = strcat([GAILPATH,'Documentation',PATHNAMESEPARATOR,...
+        'gail_ug',strrep(GAILVERSION, '.', '_'),'.m']);
+    delete(gailug_filename)
+    cat_cmd = strcat([cat_cmd, '>> ', gailug_filename]);
+    system(cat_cmd);
+    publish(gailug_filename,'pdf');
 end
-
-delete(strcat(GAILPATH,'Documentation',PATHNAMESEPARATOR,'gail_ug.m'))
-delete(strcat(GAILPATH,'Documentation',PATHNAMESEPARATOR,'html',PATHNAMESEPARATOR,'gail_ug.*'))
-cat_cmd = 'cat ';
-for i=1:length(mfile_list),
-    cat_cmd = strcat([cat_cmd, ' ', GAILPATH,'Documentation',PATHNAMESEPARATOR,mfile_list{i},'.m', ' ']);
-end
-cat_cmd = strcat(cat_cmd, '>> gail_ug.m');
-system(cat_cmd);
-publish('gail_ug','pdf');
+set(0, 'DefaultFigureVisible', oldStatus)
 end
