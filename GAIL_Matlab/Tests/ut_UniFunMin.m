fc9f16fef49e84df15f3673622671ea4731a885e
Sou-Cheng Terrya Choi <sou.cheng.terrya.choi@gmail.com>
2015-02-03 06:26:34 -0600

    Update funmin_g paper unit tests.

--- GAIL_Matlab/Tests/ut_UniFunMin.m
+++ GAIL_Matlab/Tests/ut_UniFunMin.m
@@ -4,27 +4,35 @@ classdef ut_UniFunMin < matlab.unittest.TestCase
   methods(Test)
       
     function testUniFunMin_test_ErrTolerance(testCase)
-      UniFunMin_test_ErrTolerance
-      succrates = succnowarn + succwarn;   
+      nrep=10000; abstol=10^(-8); TolX=0; nmax=10^7;
+      [tauvec,prob] = UniFunMin_test_ErrTolerance(nrep,abstol,TolX,nmax);
+      succrates = prob.succnowarn + prob.succwarn;   
       testCase.verifyGreaterThanOrEqual(succrates,[0.2,0.5,0.8]);
+      testCase.verifyLessThanOrEqual(succrates,[1,1,1]);
     end
     
     function testUniFunMin_test_ErrXTolerance(testCase)
-      UniFunMin_test_ErrXTolerance
-      succrates = succnowarn + succwarn;   
+      nrep=10000; abstol=10^(-8); TolX=10^(-6); nmax=10^7;
+      [tauvec,prob] = UniFunMin_test_ErrXTolerance(nrep,abstol,TolX,nmax);
+      succrates = prob.succnowarn + prob.succwarn;   
       testCase.verifyGreaterThanOrEqual(succrates,[0.2,0.5,0.8]);
+      testCase.verifyLessThanOrEqual(succrates,[1,1,1]);
     end
     
     function testUniFunMin_test_TwoExtreme(testCase)
-      UniFunMin_test_TwoExtreme
-      succrates = probfunmin;   
+      nrep=10000; TolX=[10^(-2) 10^(-4) 10^(-7)]; nmax=10^7; 
+      [TolXvec,prob] = UniFunMin_test_TwoExtreme(nrep,TolX,nmax);
+      succrates = prob.probfunmin;   
       testCase.verifyGreaterThanOrEqual(succrates,[0.2,0.5,0.8]);
+      testCase.verifyLessThanOrEqual(succrates,[1,1,1]);
     end
     
     function testUniFunMin_test_XTolerance(testCase)
-      UniFunMin_test_XTolerance
-      succrates = succnowarn + succwarn;   
+      nrep=10000; abstol=0; TolX=10^(-6);  nmax=10^7;
+      [tauvec,prob] = UniFunMin_test_XTolerance(nrep,abstol,TolX,nmax);
+      succrates = prob.succnowarn + prob.succwarn;   
       testCase.verifyGreaterThanOrEqual(succrates,[0.2,0.5,0.8]);
+      testCase.verifyLessThanOrEqual(succrates,[1,1,1]);
     end
     
   end
