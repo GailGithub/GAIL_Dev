e7a15cd3ea3cf4554510c31c50a6d1ec6e46bfcc
Fred J. Hickernell <hickernell@iit.edu>
2015-02-23 09:06:17 -0600

    Revised integralNoPenalty_g so it passes the doctests and unit tests provided by Sou-Cheng.  Was not handling a=b case correctly.  Now it should be doing so.

--- GAIL_Matlab/Algorithms/integralNoPenalty_g.m
+++ GAIL_Matlab/Algorithms/integralNoPenalty_g.m
@@ -156,20 +156,19 @@ out_param.exceedbudget=false;   % if the number of points used in the calculatio
 out_param.conechange=false;  % if the cone constant has been changed
 ntrap=out_param.ninit-1; % number of trapezoids
 intervallen=out_param.b-out_param.a; % length of integration interval (>=0)
-steplen=intervallen/ntrap; % length of subinterval
-steplenvec=zeros(10,1); %vector to record subinterval lengths
-hcut=2*intervallen/(ntrap-1); %minimum interval size
-inflatelim=1.5;
-xpts=(out_param.a:steplen:out_param.b)'; % generate ninit uniformly spaced points in [a,b]
-fpts=f(xpts);   % get function values at xpts
-sumf=(fpts(1)+fpts(out_param.ninit))/2+sum(fpts(2:ntrap)); % computes the sum of trapezoidal rule
+Varfp=zeros(10,1); %initialize vector of approximations to Var(f')
+Varfpup=[inf; zeros(10,1)]; %initialize vector of upper bounds to Var(f')
+ii=1; %index
 
-if steplen>0
-    Varfp=zeros(10,1); %initialize vector of approximations to Var(f')
-    Varfpup=[inf; zeros(10,1)]; %initialize vector of upper bounds to Var(f')
-    ii=0; %index
+if intervallen>0   
+    steplen=intervallen/ntrap; % length of subinterval
+    steplenvec=zeros(10,1); %vector to record subinterval lengths
+    hcut=2*intervallen/(ntrap-1); %minimum interval size
+    inflatelim=1.5;
+    xpts=(out_param.a:steplen:out_param.b)'; % generate ninit uniformly spaced points in [a,b]
+    fpts=f(xpts);   % get function values at xpts
+    sumf=(fpts(1)+fpts(out_param.ninit))/2+sum(fpts(2:ntrap)); % computes the sum of trapezoidal rule
     while true
-        ii=ii+1; %increment the counter
         steplenvec(ii)=steplen;
         
         %Compute approximation to Var(f')
@@ -223,7 +222,8 @@ if steplen>0
         xpts = [reshape([xpts(1:ntrap)'; xnew],ntrapnew,1); xpts(ntrap+1)];
         fpts = [reshape([fpts(1:ntrap)'; ynew],ntrapnew,1); fpts(ntrap+1)];
         ntrap=ntrapnew; %new number of trapezoids
-        
+        ii=ii+1; %increment the counter
+
     end
 else
     q = 0;
