854d91c8044bb687e1f6abecab80cdf7ff3fa212
Sou-Cheng Terrya Choi <sou.cheng.terrya.choi@gmail.com>
2015-02-25 06:19:04 -0600

    Restore wordings from attorney.

--- GAIL_Matlab/LICENSE.m
+++ GAIL_Matlab/LICENSE.m
@@ -1,4 +1,4 @@
-%LICENSE License of GAIL
+% Copyright � 2014, Illinois Institute of Technology. All rights reserved.
 %  
 % Redistribution and use in source and binary forms, with or without 
 % modification, are permitted provided that the following conditions are 
