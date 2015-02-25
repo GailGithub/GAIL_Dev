d24b4867a682cf7b4c2a65e37e03b3cd778c4838
Sou-Cheng Terrya Choi <sou.cheng.terrya.choi@gmail.com>
2015-02-25 14:08:01 -0600

    Placeholder for Lan to fill in.

---
+++ GAIL_Matlab/Papers/meanMCBerPaper/Contents.m
@@ -0,0 +1,6 @@
+% MEANMCBERPAPER
+%
+% Files
+%   PlotmeanMCBernoulli_gResults - 
+%   PlotRatioHoeffCLT            - 
+%   Test_meanMCBer_g             - This script is to test meanMCBer_g algorithm
