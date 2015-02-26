687b52777590ad2a887c958f433c945944f43069
Sou-Cheng Terrya Choi <sou.cheng.terrya.choi@gmail.com>
2015-02-26 12:46:59 -0600

    Potential fix for failed "rm -f autodoc"

--- GAIL_Matlab/Documentation/Developers_only/autodoc.sh
+++ GAIL_Matlab/Documentation/Developers_only/autodoc.sh
@@ -12,10 +12,14 @@ cd /home/gail/GAIL_tests/repo/gail-development/
 cd GAIL_Matlab/Documentation/
 g++ -std=c++11 autodoc.cpp -o autodoc
 ./autodoc
+sleep 5
+
+rm -f autodoc
+
+cd /home/gail/GAIL_tests/repo/gail-development/
 git add /home/gail/GAIL_tests/repo/gail-development/GAIL_Matlab/Documentation
 git add /home/gail/GAIL_tests/repo/gail-development/GAIL_Matlab/Documentation/html
 git commit -m 'Update automatically generated Matlab files for HTML documentation'
-rm -f autodoc
 git push origin develop
 
 # matlab < ./Developers_only/GAIL_Publish.m 
