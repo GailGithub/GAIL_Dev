067ee2715e4463da857e237914be79058db6fc94
Yuhan Ding <jinliang98@hotmail.com>
2015-02-27 11:51:37 -0600

    Update funappx_g_data

--- GAIL_Matlab/Documentation/doc_data/funappx_g_data.m
+++ GAIL_Matlab/Documentation/doc_data/funappx_g_data.m
@@ -11,20 +11,20 @@
 % for each sub interval $[t_{l-1},t_l]$, where $1 \le l \le L$,
 % then the $pp$ |output by this algorithm is guaranteed to satisfy
 %
-% $$\| f-ppval(pp,)\|_{\infty} \le \mathrm{abstol}.$$
+% $$\| f-fappx\|_{\infty} \le \mathrm{abstol}.$$
 %
 %
 %% Examples
 % *Example 1*
 
-f = @(x) x.^2; [pp, out_param] = funappx_g(f)
+f = @(x) x.^2; [fappx, out_param] = funappx_g(f)
 
 % Approximate function x^2 with default input parameter to make the error
 % less than 1e-6.
 %%
 % *Example 2*
 
-[pp, out_param] = funappx_g(@(x) x.^2,0,100,1e-7,10,1000,1e8)
+[fappx, out_param] = funappx_g(@(x) x.^2,0,100,1e-7,10,1000,1e8)
 
 % Approximate function x^2 on [0,100] with error tolerence 1e-7, cost
 % budget 10000000, lower bound of initial number of points 10 and upper
@@ -35,7 +35,7 @@ f = @(x) x.^2; [pp, out_param] = funappx_g(f)
 
 clear in_param; in_param.a = -20; in_param.b = 20; in_param.nlo = 10;
 in_param.nhi = 100; in_param.nmax = 1e8; in_param.abstol = 1e-7; 
-[pp, out_param] = funappx_g(@(x) x.^2, in_param)
+[fappx, out_param] = funappx_g(@(x) x.^2, in_param)
 
 % Approximate function x^2 on [-20,20] with error tolerence 1e-7, cost
 % budget 1000000, lower bound of initial number of points 10 and upper
@@ -44,7 +44,7 @@ in_param.nhi = 100; in_param.nmax = 1e8; in_param.abstol = 1e-7;
 % *Example 4*
 
 clear in_param; f = @(x) x.^2;
-[pp, out_param] = funappx_g(f,'a',-10,'b',50,'nmax',1e6,'abstol',1e-8)
+[fappx, out_param] = funappx_g(f,'a',-10,'b',50,'nmax',1e6,'abstol',1e-8)
 
 % Approximate function x^2 with error tolerence 1e-8, cost budget 1000000,
 % lower bound of initial number of points 10 and upper
