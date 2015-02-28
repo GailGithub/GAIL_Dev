4ba0d9f4e6322cc5f04b3fa0f82b95342120c4b0
Sou-Cheng Terrya Choi <sou.cheng.terrya.choi@gmail.com>
2015-02-28 14:57:22 -0600

    mv help_<algo>_raw.m files to help_<algo>.m

--- GAIL_Matlab/Documentation/Developers_only/autodoc.sh
+++ GAIL_Matlab/Documentation/Developers_only/autodoc.sh
@@ -15,12 +15,24 @@ g++ -std=c++11 autodoc.cpp -o autodoc
 sleep 5
 
 rm -f autodoc
+mv -f help_cubLattice_g_raw.m help_cubLattice_g.m
+mv -f help_cubMC_g_raw.m help_cubMC_g.m
+mv -f help_cubSobol_g_raw.m help_cubSobol_g.m
+mv -f help_funappx_g_raw.m help_funappx_g.m
+mv -f help_funmin_g_raw.m help_funmin_g.m
+mv -f help_integral_g_raw.m help_integral_g.m
+mv -f help_meanMCBer_g_raw.m help_meanMCBer_g.m
+mv -f help_meanMC_g_raw.m help_meanMC_g.
 
-cd /home/gail/GAIL_tests/repo/gail-development/
-git add /home/gail/GAIL_tests/repo/gail-development/GAIL_Matlab/Documentation
-git add /home/gail/GAIL_tests/repo/gail-development/GAIL_Matlab/Documentation/html
-git commit -m 'Update automatically generated Matlab files for HTML documentation'
-git push origin develop
+
+scp gail@karlin.math.iit.edu:/home/gail/GAIL_tests/repo/gail-development/GAIL_Matlab/Documentation .
+scp -r schoi32@anubis.e1.iit.edu:~/Documentation .
+
+#cd /home/gail/GAIL_tests/repo/gail-development/
+#git add /home/gail/GAIL_tests/repo/gail-development/GAIL_Matlab/Documentation
+#git add /home/gail/GAIL_tests/repo/gail-development/GAIL_Matlab/Documentation/html
+#git commit -m 'Update automatically generated Matlab files for HTML documentation'
+#git push origin develop
 
 # matlab < ./Developers_only/GAIL_Publish.m 
 
