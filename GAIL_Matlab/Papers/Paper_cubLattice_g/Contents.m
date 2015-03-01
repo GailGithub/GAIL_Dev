e6d2f6053f3b9d16f7566c2f426626594aaed529
Sou-Cheng Terrya Choi <sou.cheng.terrya.choi@gmail.com>
2015-03-01 09:38:53 -0600

    Fill in more details about eps filenames.

--- GAIL_Matlab/Papers/Paper_cubLattice_g/Contents.m
+++ GAIL_Matlab/Papers/Paper_cubLattice_g/Contents.m
@@ -6,14 +6,18 @@
 %
 %	RunTestCubatureonGeoAsianCallLattice - Runs the test for the geometric 
 %   Brownian motion Asian option call, stores the results in a .mat file
-%   and generates the plot in .eps format (article Figure 3).
+%   and generates the plot in .eps format (article Figure 3). The name of 
+%   the eps file is Paper_cubLattice_g_TestGeoAsianCall-*.eps.
 %
 %	RunTestCubatureonKeisterLattice - Runs the test for the geometric 
 %   Brownian motion Asian option call, stores the results in a .mat file
-%   and generates the plot in .eps format.
+%   and generates the plot in .eps format. 
 %
 %	lattice_example - produces the plot of the lattice node set and dual
-%	set example in .eps format (article Figure 1).
+%	set example in .eps format (article Figure 1). The names of the eps 
+%	files are Paper_cubLattice_g_NodeSetExample-*.eps and 
+%   Paper_cubLattice_g_DualSetExample*.eps
 %	
 %   FourierCoeffDecayPict - produces the plot of the Fourier coefficients
-%	decay in .eps format (article Figure 2).
+%	decay in .eps format (article Figure 2). The name of the eps file is
+%   Paper_cubLattice_g_FourierCoeffDecay-*.eps.
