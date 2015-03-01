2592ed855aebd4663f2bcb5ae6ee28bfd7dd325a
Sou-Cheng Terrya Choi <sou.cheng.terrya.choi@gmail.com>
2015-03-01 09:02:06 -0600

    Line breaks. Remove unnecessary "-" and ".m".

--- GAIL_Matlab/Papers/Paper_cubLattice_g/Contents.m
+++ GAIL_Matlab/Papers/Paper_cubLattice_g/Contents.m
@@ -4,13 +4,16 @@
 %
 % Files
 %
-%	- RunTestCubatureonGeoAsianCallLattice.m - Runs the test for the geometric 
+%	RunTestCubatureonGeoAsianCallLattice - Runs the test for the geometric 
 %   Brownian motion Asian option call, stores the results in a .mat file
 %   and generates the plot in .eps format (article Figure 3).
-%	- RunTestCubatureonKeisterLattice.m - Runs the test for the geometric 
+%
+%	RunTestCubatureonKeisterLattice - Runs the test for the geometric 
 %   Brownian motion Asian option call, stores the results in a .mat file
 %   and generates the plot in .eps format.
-%	- lattice_example.m - produces the plot of the lattice node set and dual
+%
+%	lattice_example - produces the plot of the lattice node set and dual
 %	set example in .eps format (article Figure 1).
-%	- FourierCoeffDecayPict.m - produces the plot of the Fourier coefficients
+%	
+%   FourierCoeffDecayPict - produces the plot of the Fourier coefficients
 %	decay in .eps format (article Figure 2).
--- GAIL_Matlab/Papers/Paper_cubSobol_g/Contents.m
+++ GAIL_Matlab/Papers/Paper_cubSobol_g/Contents.m
@@ -4,13 +4,16 @@
 %
 % Files
 %
-%	- RunTestCubatureonGeoAsianCallSobol.m - Runs the test for the geometric 
+%	RunTestCubatureonGeoAsianCallSobol - Runs the test for the geometric 
 %   Brownian motion Asian option call, stores the results in a .mat file
 %   and generates the plot in .eps format.
-%	- RunTestCubatureonKeisterSobol.m - Runs the test for the geometric 
+%	
+%   RunTestCubatureonKeisterSobol - Runs the test for the geometric 
 %   Brownian motion Asian option call, stores the results in a .mat file
 %   and generates the plot in .eps format (article Figure 3).
-%	- SobolWalshPict.m - produces the plot of the Sobol points and the
+%	
+%   SobolWalshPict - produces the plot of the Sobol points and the
 %   scrambled and shifted Sobol points in .eps format (article Figure 1).
-%	- WalshFourierCoeffDecay.m - produces the plot of the Walsh-Fourier
+%	
+%   WalshFourierCoeffDecay - produces the plot of the Walsh-Fourier
 %   coefficients decay in .eps format (article Figure 2).
