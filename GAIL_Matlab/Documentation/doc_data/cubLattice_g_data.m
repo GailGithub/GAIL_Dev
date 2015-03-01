78e16dc4303a8b31ee45313de177d6b903a3df18
Sou-Cheng Terrya Choi <sou.cheng.terrya.choi@gmail.com>
2015-03-01 12:54:45 -0600

    Wrap comments.

--- GAIL_Matlab/Documentation/doc_data/cubLattice_g_data.m
+++ GAIL_Matlab/Documentation/doc_data/cubLattice_g_data.m
@@ -1,20 +1,21 @@
 %%  Guarantee
 %
-% This algorithm computes the integral of real valued functions in dimension d 
-% with a prescribed generalized error tolerance. The Fourier coefficients of 
-% the integrand are assumed to be absolutely convergent.
-% If the algorithm terminates without warning messages, the output is 
-% given with guarantees under the assumption that the integrand lies inside
-% a cone of functions. The guarantee is based on the decay rate of the 
-% Fourier coefficients. For more details on how the cone is defined, 
-% please refer to the references below.
+% This algorithm computes the integral of real valued functions in
+% dimension d with a prescribed generalized error tolerance. The Fourier
+% coefficients of the integrand are assumed to be absolutely convergent. If
+% the algorithm terminates without warning messages, the output is given
+% with guarantees under the assumption that the integrand lies inside a
+% cone of functions. The guarantee is based on the decay rate of the
+% Fourier coefficients. For more details on how the cone is defined, please
+% refer to the references below.
 %
 %% Examples
 %
 %%
 % *Example 1*
 
-% Estimate the integral with integrand f(x) = x1.*x2 in the interval [0,1)^2:
+% Estimate the integral with integrand f(x) = x1.*x2 in the interval
+% [0,1)^2:
 
   f = @(x) prod(x,2); d = 2;
   q = cubLattice_g(f,d,1e-5,1e-1,'uniform','transform','C1sin')
@@ -44,13 +45,14 @@
 % sigma=0.05 and T=1.
 
   f = @(x) exp(-0.05^2/2)*max(100*exp(0.05*x)-100,0); d = 1;
-  q = cubLattice_g(f,d,1e-4,1e-1,'normal','fudge',@(m) 2.^-(2*m),'transform','C1sin')
+  q = cubLattice_g(f,d,1e-4,1e-1,'normal','fudge',@(m) 2.^-(2*m),...
+      'transform','C1sin')
 
 %%
 % *Example 5*
 
-% Estimate the integral with integrand f(x) = 8*x1.*x2.*x3.*x4.*x5 in the interval
-% [0,1)^5 with pure absolute error 1e-5.
+% Estimate the integral with integrand f(x) = 8*x1.*x2.*x3.*x4.*x5 in the
+% interval [0,1)^5 with pure absolute error 1e-5.
 
   f = @(x) 8*prod(x,2); d = 5;
   q = cubLattice_g(f,d,1e-5,0)
