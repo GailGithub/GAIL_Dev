5bedc2c55bce0aa813f5b7dfbfae4a40b0507d58
Sou-Cheng Terrya Choi <sou.cheng.terrya.choi@gmail.com>
2015-03-02 07:42:16 -0600

    Align -

--- GAIL_Matlab/Workouts/Contents.m
+++ GAIL_Matlab/Workouts/Contents.m
@@ -2,10 +2,10 @@
 %
 % Folders
 %
-%   cubMC_workouts                   - workouts of cubMC_g method
-%   meanMC_workouts                  - workouts of meanMC_g method
-%   meanMCBer_g_workouts             - workouts of meanMCBer_g method
-%   univariate_approximation_workouts- workouts of funappx_g method
-%   univariate_integration_workouts  - workouts of integral_g method
-%   univariate_minimization_workouts - workouts of funmin_g method
-%   Workout_cubSobol_g               - workouts of cubSobol_g
+%   cubMC_workouts                    - workouts of cubMC_g method
+%   meanMC_workouts                   - workouts of meanMC_g method
+%   meanMCBer_g_workouts              - workouts of meanMCBer_g method
+%   univariate_approximation_workouts - workouts of funappx_g method
+%   univariate_integration_workouts   - workouts of integral_g method
+%   univariate_minimization_workouts  - workouts of funmin_g method
+%   Workout_cubSobol_g                - workouts of cubSobol_g
