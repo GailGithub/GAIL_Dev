f4977f46ee03b484d052d676f713248c010ef872
Sou-Cheng Terrya Choi <sou.cheng.terrya.choi@gmail.com>
2015-03-02 08:37:22 -0600

    Add a few more steps to use autodoc.

--- GAIL_Matlab/Documentation/Developers_only/autodoc.sh
+++ GAIL_Matlab/Documentation/Developers_only/autodoc.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
+# ssh to anubis and ssh to karlin
 cd /home/gail/GAIL_tests/repo/gail-development/
+
+rm -f GAIL_Matlab/Documentation/help_*_g.m  
+
 # GIT
 # Pulling the latest repository before testing
 # /usr/local/bin/git --git-dir /home/gail/GAIL_tests/repo/gail-development/.git checkout .
@@ -13,8 +17,8 @@ cd GAIL_Matlab/Documentation/
 g++ -std=c++11 autodoc.cpp -o autodoc
 ./autodoc
 sleep 5
-
 rm -f autodoc
+
 mv -f help_cubLattice_g_raw.m help_cubLattice_g.m
 mv -f help_cubMC_g_raw.m help_cubMC_g.m
 mv -f help_cubSobol_g_raw.m help_cubSobol_g.m
@@ -24,8 +28,14 @@ mv -f help_integral_g_raw.m help_integral_g.m
 mv -f help_meanMCBer_g_raw.m help_meanMCBer_g.m
 mv -f help_meanMC_g_raw.m help_meanMC_g.
 
-
+# exit from karlin
+exit
+rm -r Documentation
 scp gail@karlin.math.iit.edu:/home/gail/GAIL_tests/repo/gail-development/GAIL_Matlab/Documentation .
+
+# exit from anubis
+exit
+# cd in local GAIL_Matlab directory
 scp -r schoi32@anubis.e1.iit.edu:~/Documentation .
 
 #cd /home/gail/GAIL_tests/repo/gail-development/
