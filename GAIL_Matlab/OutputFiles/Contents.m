97c0c60359223e9064f5c3b262877b11474b13e4
Sou-Cheng Terrya Choi <sou.cheng.terrya.choi@gmail.com>
2015-03-02 07:36:53 -0600

    Add three more folders.

--- GAIL_Matlab/OutputFiles/Contents.m
+++ GAIL_Matlab/OutputFiles/Contents.m
@@ -1,11 +1,13 @@
 % OUTPUTFILES
 %
 % Folders
-%   ConesPaperOutput    - output files of cones paper
-%   MCQMCPaperOutput    - output files of MCQMC paper
-%   meanMCBerPaperOutput- output files of meanMCBer paper
-%   UniFunMinOutput     - output files of Xin Tong's thesis
-%   Workout_cubSobol_g  - output files of cubSobol_g
-%   WorkoutFunappxOutput- output files of funappx_g
-%   WorkoutFunminOutput - output files of funmin_g
-%   WorkoutIntegralOutput- output files of integral_g
+%   ConesPaperOutput      - output files of cones paper
+%   WorkoutFunappxOutput  - output files of funappx_g workouts
+%   WorkoutIntegralOutput - output files of integral_g workouts
+%   MCQMCPaperOutput      - output files of MCQMC paper
+%   UniFunMinOutput       - output files of Xin Tong's thesis
+%   WorkoutFunminOutput   - output files of funmin_g workouts
+%   meanMCBerPaperOutput  - output files of meanMCBer paper
+%   Paper_cubSobol_g      - output files of cubLattice_g paper
+%   Workout_cubSobol_g    - output files of cubSobol_g workouts
+%   Paper_cubLattice_g    - output files of cubLattice_g paper
