0bfd791f60ed127adbc9d845c14dffb38355abab
Sou-Cheng Terrya Choi <sou.cheng.terrya.choi@gmail.com>
2015-03-02 08:13:55 -0600

    Wrap comments.

--- GAIL_Matlab/Algorithms/cubSobol_g.m
+++ GAIL_Matlab/Algorithms/cubSobol_g.m
@@ -1,34 +1,37 @@
 function [q,out_param] = cubSobol_g(varargin)
 %CUBSOBOL_G Quasi-Monte Carlo method using Sobol' cubature over the
 %d-dimensional region to integrate within a specified generalized error
-%tolerance with guarantees under Walsh-Fourier coefficients cone decay assumptions
+%tolerance with guarantees under Walsh-Fourier coefficients cone decay
+%assumptions
 %
 %   [q,out_param] = CUBSOBOL_G(f,d) estimates the integral of f over the
-%   d-dimensional region with an error guaranteed not to be greater than 
-%   a specific generalized error tolerance, 
-%   tolfun:=max(abstol,reltol*|integral(f)|). The generalized tolerance function can
-%   aslo be cosen as tolfun:=theta*abstol+(1-theta)*reltol*|integral(f)| 
-%   where theta is another input parameter. Input f is a function handle. f should
-%   accept an n x d matrix input, where d is the dimension of the hypercube,
-%   and n is the number of points being evaluated simultaneously. The input d
-%   is the dimension in which the function f is defined. Given the
+%   d-dimensional region with an error guaranteed not to be greater than a
+%   specific generalized error tolerance,
+%   tolfun:=max(abstol,reltol*|integral(f)|). The generalized tolerance
+%   function can aslo be cosen as
+%   tolfun:=theta*abstol+(1-theta)*reltol*|integral(f)| where theta is
+%   another input parameter. Input f is a function handle. f should accept
+%   an n x d matrix input, where d is the dimension of the hypercube, and n
+%   is the number of points being evaluated simultaneously. The input d is
+%   the dimension in which the function f is defined. Given the
 %   construction of Sobol', d must be a positive integer with 1<=d<=1111.
 %
 %   q = CUBSOBOL_G(f,d,abstol,reltol,measure,mmin,mmax,fudge,toltype,theta)
-%   estimates the integral of f over a d-dimensional region. The answer
-%   is given within the generalized error tolerance tolfun. All parameters
-%   should be input in the order specified above. If an input is not specified,
-%   the default value is used. Note that if an input is not specified,
-%   the remaining tail cannot be specified either.
+%   estimates the integral of f over a d-dimensional region. The answer is
+%   given within the generalized error tolerance tolfun. All parameters
+%   should be input in the order specified above. If an input is not
+%   specified, the default value is used. Note that if an input is not
+%   specified, the remaining tail cannot be specified either.
 %
-%   q = CUBSOBOL_G(f,d,'abstol',abstol,'reltol',reltol,'measure',measure,'mmin',mmin,'mmax',mmax,'fudge',fudge,'toltype',toltype,'theta',theta)
-%   estimates the integral of f over a d-dimensional region. The answer
-%   is given within the generalized error tolerance tolfun. All the field-value
-%   pairs are optional and can be supplied with any order. If an input is not
-%   specified, the default value is used.
+%   q = CUBSOBOL_G(f,d,'abstol',abstol,'reltol',reltol,'measure',measure,
+%   'mmin',mmin,'mmax',mmax,'fudge',fudge,'toltype',toltype,'theta',theta)
+%   estimates the integral of f over a d-dimensional region. The answer is
+%   given within the generalized error tolerance tolfun. All the
+%   field-value pairs are optional and can be supplied with any order. If
+%   an input is not specified, the default value is used.
 %
 %   q = CUBSOBOL_G(f,d,in_param) estimates the integral of f over the
-%   d-dimensional region. The answer is given within the generalized error 
+%   d-dimensional region. The answer is given within the generalized error
 %   tolerance tolfun.
 % 
 %   Input Arguments
@@ -53,35 +56,35 @@ function [q,out_param] = cubSobol_g(varargin)
 % 
 %   Optional Input Arguments
 % 
-%     in_param.mmin --- the minimum number of points to start is 2^mmin. The
-%     cone condition on the Fourier coefficients decay requires a minimum
-%     number of points to start. The advice is to consider at least mmin=10.
-%     mmin needs to be a positive integer with mmin<=mmax. By default it is 10.
+%     in_param.mmin --- the minimum number of points to start is 2^mmin.
+%     The cone condition on the Fourier coefficients decay requires a
+%     minimum number of points to start. The advice is to consider at least
+%     mmin=10. mmin needs to be a positive integer with mmin<=mmax. By
+%     default it is 10.
 %
-%     in_param.mmax --- the maximum budget is 2^mmax. By construction of the
-%     Sobol' generator, mmax is a positive integer such that mmin<=mmax<=53.
-%     The default value is 24.
+%     in_param.mmax --- the maximum budget is 2^mmax. By construction of
+%     the Sobol' generator, mmax is a positive integer such that
+%     mmin<=mmax<=53. The default value is 24.
 %
-%     in_param.fudge --- the positive function multiplying the finite 
-%     sum of Fast Walsh coefficients specified in the cone of functions.
-%     For more information about this parameter, refer to the references.
-%     By default it is @(x) 5*2.^-x.
+%     in_param.fudge --- the positive function multiplying the finite sum
+%     of Fast Walsh coefficients specified in the cone of functions. For
+%     more information about this parameter, refer to the references. By
+%     default it is @(x) 5*2.^-x.
 %
 %     in_param.toltype --- this is the tolerance function. There are two
 %     choices, 'max' (chosen by default) which takes
-%     max(abstol,reltol*|integral(f)|) and 'comb' which is a linear combination
-%     theta*abstol+(1-theta)*reltol*|integral(f)|. Theta is another 
-%     parameter that can be specified (see below). For pure absolute error,
-%     either choose 'max' and set reltol=0 or choose 'comb' and set
-%     theta=1.
+%     max(abstol,reltol*|integral(f)|) and 'comb' which is a linear
+%     combination theta*abstol+(1-theta)*reltol*|integral(f)|. Theta is
+%     another parameter that can be specified (see below). For pure
+%     absolute error, either choose 'max' and set reltol=0 or choose 'comb'
+%     and set theta=1.
 % 
-%     in_param.theta --- this input is parametrizing the toltype 
-%     'comb'. Thus, it is only afecting when the toltype
-%     chosen is 'comb'. It stablishes the linear combination weight
-%     between the absolute and relative tolerances
-%     theta*abstol+(1-theta)*reltol*|integral(f)|. Note that for theta=1, 
-%     we have pure absolute tolerance while for theta=0, we have pure 
-%     relative tolerance. By default, theta=1.
+%     in_param.theta --- this input is parametrizing the toltype 'comb'.
+%     Thus, it is only afecting when the toltype chosen is 'comb'. It
+%     stablishes the linear combination weight between the absolute and
+%     relative tolerances theta*abstol+(1-theta)*reltol*|integral(f)|. Note
+%     that for theta=1, we have pure absolute tolerance while for theta=0,
+%     we have pure relative tolerance. By default, theta=1.
 %
 %   Output Arguments
 %
@@ -93,9 +96,10 @@ function [q,out_param] = cubSobol_g(varargin)
 %     condition. If the function lies in the cone, the real error should be
 %     smaller than this predicted error.
 %
-%     out_param.time --- time elapsed in seconds when calling cubSobol_g for f.
+%     out_param.time --- time elapsed in seconds when calling cubSobol_g
+%     for f.
 %
-%     out_param.exitflag --- this is a binary vector stating whether 
+%     out_param.exitflag --- this is a binary vector stating whether
 %     warning flags arise. These flags tell about which conditions make the
 %     final result certainly not guaranteed. One flag is considered arisen
 %     when its value is 1. The following list explains the flags in the
@@ -115,13 +119,13 @@ function [q,out_param] = cubSobol_g(varargin)
 %                       below.
 % 
 %  Guarantee
-% This algorithm computes the integral of real valued functions in [0,1)^d 
-% with a prescribed generalized error tolerance. The Walsh-Fourier 
-% coefficients of the integrand are assumed to be absolutely convergent.
-% If the algorithm terminates without warning messages, the output is 
-% given with guarantees under the assumption that the integrand lies inside
-% a cone of functions. The guarantee is based on the decay rate of the 
-% Walsh-Fourier coefficients. For more details on how the cone is defined, 
+% This algorithm computes the integral of real valued functions in [0,1)^d
+% with a prescribed generalized error tolerance. The Walsh-Fourier
+% coefficients of the integrand are assumed to be absolutely convergent. If
+% the algorithm terminates without warning messages, the output is given
+% with guarantees under the assumption that the integrand lies inside a
+% cone of functions. The guarantee is based on the decay rate of the
+% Walsh-Fourier coefficients. For more details on how the cone is defined,
 % please refer to the references below.
 % 
 %  Examples
