97cc108c77fda6e9cf6b38fc9eae6b2a5aa404a8
Sou-Cheng Terrya Choi <sou.cheng.terrya.choi@gmail.com>
2015-03-02 07:41:48 -0600

    Reduce --- to -

--- GAIL_Matlab/OutputFiles/ConesPaperOutput/Contents.m
+++ GAIL_Matlab/OutputFiles/ConesPaperOutput/Contents.m
@@ -1,11 +1,11 @@
 % CONESPAPEROUTPUT
 %
 % Files
-%   ConesPaperFunAppxTest-03-Sep-2013-18-37-05 --- dataset of function
+%   ConesPaperFunAppxTest-03-Sep-2013-18-37-05 - dataset of function
 %   approximation in Cones paper
-%   ConesPaperFunAppxTest-2015-Jan-24-13-34-24 --- reproduced dataset of
+%   ConesPaperFunAppxTest-2015-Jan-24-13-34-24 - reproduced dataset of
 %   function approximation in Cones paper
-%   ConesPaperIntegralTest-03-Sep-2013-22-46-01 --- dataset of integral in
+%   ConesPaperIntegralTest-03-Sep-2013-22-46-01 - dataset of integral in
 %   Cones paper
-%   ConesPaperFlukyquad --- figure 1b) in Cones paper
-%   ConesPaperSpikyquad --- figure 1a) in Cones Paper
+%   ConesPaperFlukyquad - figure 1b) in Cones paper
+%   ConesPaperSpikyquad - figure 1a) in Cones Paper
