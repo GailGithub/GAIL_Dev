a58488212c14cfb50b9ffa70788e544e4431a8da
Sou-Cheng Terrya Choi <sou.cheng.terrya.choi@gmail.com>
2015-03-03 08:18:32 -0600

    Minor changes to fix typo and order.

--- GAIL_Matlab/Documentation/GAIL.m
+++ GAIL_Matlab/Documentation/GAIL.m
@@ -9,18 +9,19 @@
 %
 % GAIL is a suite of algorithms for integration problems in one, many, and 
 % infinite dimensions, and whose answers are guaranteed to be correct.
-%help GAU
+%
 %% Functions
 %
 % <html>
 % <a href="help_funappx_g.html">funappx_g</a>
+% <a href="help_funmin_g.html">funmin_g</a>
 % <a href="help_integral_g.html">integral_g</a>
 % <a href="help_meanMC_g.html">meanMC_g</a>
-% <a href="help_cubMC_g.html">cubMC_g</a>
 % <a href="help_meanMCBer_g.html">meanMCBer_g</a>
-% <a href="help_cubSobol_g.html">cubSobol_g</a>
+% <a href="help_cubMC_g.html">cubMC_g</a>
 % <a href="help_cubLattice_g.html">cubLattice_g</a>
-% <a href="help_funmin_g.html">funmin_g</a>
+% <a href="help_cubSobol_g.html">cubSobol_g</a>
+
 % </html>
 %
 %
