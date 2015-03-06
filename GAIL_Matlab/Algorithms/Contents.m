11017018f9b05cfea5e2ca93bc7f134037d4823d
Xin Tong <xtong5@hawk.iit.edu>
2015-03-05 20:28:32 -0600

    contents and workout

--- GAIL_Matlab/Algorithms/Contents.m
+++ GAIL_Matlab/Algorithms/Contents.m
@@ -13,4 +13,3 @@
 %   integral_g        - 1-D guaranteed function integration using trapezoidal rule
 %   meanMC_g          - Monte Carlo method to estimate the mean of a random variable
 %   meanMCBer_g       - Monte Carlo method to estimate the mean of a Bernoulli random variable
-%   
--- GAIL_Matlab/Tests/ut_thesis_funmin01_g.m
+++ GAIL_Matlab/Tests/ut_thesis_funmin01_g.m
@@ -3,35 +3,35 @@ classdef ut_thesis_funmin01_g < matlab.unittest.TestCase
 
   methods(Test)
       
-%     function test_UniFunMin_ErrTolerance(testCase)
-%       nrep=10000; abstol=10^(-8);nmax=10^7;
-%       [tauvec,prob] = UniFunMin_test_ErrTolerance(nrep,abstol,nmax);
-%       succrates = prob.succnowarn + prob.succwarn;   
-%       testCase.verifyLessThanOrEqual(succrates,[1,1,1]);
-%     end
-%     
-%     function test_UniFunMin_XTolerance(testCase)
-%       nrep=10000; TolX=10^(-6); nmax=10^7;
-%       [tauvec,prob] = UniFunMin_test_XTolerance(nrep,TolX,nmax);
-%       succrates = prob.succnowarn + prob.succwarn;   
-%       testCase.verifyLessThanOrEqual(succrates,[1,1,1]);
-%     end
-%     
-%     function test_UniFunMin_ErrXTolerance(testCase)
-%       nrep=10000; abstol=10^(-8); TolX=10^(-6); nmax=10^7;
-%       [tauvec,prob] = UniFunMin_test_ErrXTolerance(nrep,abstol,TolX,nmax);
-%       succrates = prob.succnowarn + prob.succwarn;   
-%       testCase.verifyLessThanOrEqual(succrates,[1,1,1]);
-%     end
-%     
-%     function test_UniFunMin_TwoExtreme(testCase)
-%       nrep=10000; TolX=[10^(-2) 10^(-4) 10^(-7)]; nmax=10^7; 
-%       [TolXvec,prob] = UniFunMin_test_TwoExtreme(nrep,TolX,nmax);
-%       succrates1 = prob.probfunmin;   
-%       succrates2 = prob.probfminbnd;
-%       testCase.verifyLessThanOrEqual(succrates1,[1,1,1]);
-%       testCase.verifyLessThanOrEqual(succrates2,[1,1,1]);
-%     end
+    function test_UniFunMin_ErrTolerance(testCase)
+      nrep=10000; abstol=10^(-8);nmax=10^7;
+      [tauvec,prob] = UniFunMin_test_ErrTolerance(nrep,abstol,nmax);
+      succrates = prob.succnowarn + prob.succwarn;   
+      testCase.verifyLessThanOrEqual(succrates,[1,1,1]);
+    end
+    
+    function test_UniFunMin_XTolerance(testCase)
+      nrep=10000; TolX=10^(-6); nmax=10^7;
+      [tauvec,prob] = UniFunMin_test_XTolerance(nrep,TolX,nmax);
+      succrates = prob.succnowarn + prob.succwarn;   
+      testCase.verifyLessThanOrEqual(succrates,[1,1,1]);
+    end
+    
+    function test_UniFunMin_ErrXTolerance(testCase)
+      nrep=10000; abstol=10^(-8); TolX=10^(-6); nmax=10^7;
+      [tauvec,prob] = UniFunMin_test_ErrXTolerance(nrep,abstol,TolX,nmax);
+      succrates = prob.succnowarn + prob.succwarn;   
+      testCase.verifyLessThanOrEqual(succrates,[1,1,1]);
+    end
+    
+    function test_UniFunMin_TwoExtreme(testCase)
+      nrep=10000; TolX=[10^(-2) 10^(-4) 10^(-7)]; nmax=10^7; 
+      [TolXvec,prob] = UniFunMin_test_TwoExtreme(nrep,TolX,nmax);
+      succrates1 = prob.probfunmin;   
+      succrates2 = prob.probfminbnd;
+      testCase.verifyLessThanOrEqual(succrates1,[1,1,1]);
+      testCase.verifyLessThanOrEqual(succrates2,[1,1,1]);
+    end
     
     function test_UniFunMin_Plot_Bump(testCase)
         a=0.03; z=0.4;
@@ -47,8 +47,6 @@ classdef ut_thesis_funmin01_g < matlab.unittest.TestCase
         a=0.5; b=1;
         y=UniFunMin_Plot_Flat(a,b);
     end
-    
-    
-       
+          
   end
 end
