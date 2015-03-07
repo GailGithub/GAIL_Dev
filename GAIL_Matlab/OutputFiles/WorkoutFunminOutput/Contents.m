34a8d8e9dd6be5af325124595d1913efd7a29a89
Xin Tong <xtong5@hawk.iit.edu>
2015-03-07 15:51:06 -0600

    check in the output files for workout

--- GAIL_Matlab/OutputFiles/WorkoutFunminOutput/Contents.m
+++ GAIL_Matlab/OutputFiles/WorkoutFunminOutput/Contents.m
@@ -3,18 +3,19 @@
 %
 % Files
 %
-% workout_ErrToleranceTest.mat --- dataset of test for Bump test functions
-% with specific number of iteration, absolut error tolerance and cost 
-% budget nmax
+% workout_ErrToleranceTest-2015-03-06-01-59-47.mat --- dataset of test for 
+% Bump test functions with specific number of iteration, absolut error 
+% tolerance and cost budget nmax
 %
-% workout_XToleranceTest.mat --- dataset of test for Bump test functions 
-% with specific number of iteration, X tolerance and cost budget nmax
-%
-% workout_ErrXToleranceTest.mat --- dataset of test for Bump test functions 
-% with a specific number of iteration, absolut error tolerance, X tolerance
-% and cost budget nmax
-%  
-% workout_TwoExtremeTest.mat --- dataset of test for Functions with two 
-% local minimum points with specific number of iteration, X tolerance, and 
+% workout_XToleranceTest-2015-03-06-04-21-37.mat --- dataset of test for
+% Bump test functions with specific number of iteration, X tolerance and 
 % cost budget nmax
+%
+% workout_ErrXToleranceTest-2015-03-06-05-05-09.mat --- dataset of test for
+% Bump test functions with a specific number of iteration, absolut error 
+% tolerance, X tolerance and cost budget nmax
+%  
+% workout_TwoExtremeTest-2015-03-06-08-55-15.mat --- dataset of test for 
+% Functions with two local minimum points with specific number of 
+% iteration, X tolerance, and cost budget nmax
 
