f9252ac23bcb3639aa5a0d0da35091de3619eca3
Sou-Cheng Terrya Choi <sou.cheng.terrya.choi@gmail.com>
2015-03-07 15:02:19 -0600

    Restoring 100 to 10000 runs for ut_workout_integral_g.

--- GAIL_Matlab/Tests/ut_workout_integral_g.m
+++ GAIL_Matlab/Tests/ut_workout_integral_g.m
@@ -4,7 +4,7 @@ classdef ut_workout_integral_g < matlab.unittest.TestCase
   methods(Test)
              
     function testWorkout_integral_g(testCase)
-      nrep=100; nmax=1e7; abstol=1e-8;
+      nrep=10000; nmax=1e7; abstol=1e-8;
       [succnowarn, succwarn, pfin] = workout_integral_g(nrep,nmax,abstol);
       succrates = succnowarn + succwarn;   
       testCase.verifyGreaterThanOrEqual(succrates,pfin);
