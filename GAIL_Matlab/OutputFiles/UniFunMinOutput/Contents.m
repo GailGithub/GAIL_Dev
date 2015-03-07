5c616b42fb735975b043a0b676fec6ef2fa32893
Xin Tong <xtong5@hawk.iit.edu>
2015-03-07 15:41:31 -0600

    check in the eps files

--- GAIL_Matlab/OutputFiles/UniFunMinOutput/Contents.m
+++ GAIL_Matlab/OutputFiles/UniFunMinOutput/Contents.m
@@ -15,14 +15,14 @@
 % TwoExtremeTest-2015-02-21-13-51-03.mat --- dataset of Table 3.4 in the thesis for Functions 
 % with two local minimum points 
 % 
-% UniFunMinPlotBump.eps --- dataset of Figure 3.1 in the thesis using
+% UniFunMinPlotBump-2015-03-07-15-36-30.mat --- dataset of Figure 3.1 in the thesis using
 % a=0.03 and z=0.4 to construct an example of the bump test functions 
 % 
-% UniFunMinPlotFlat.eps --- dataset of Figure 3.2 in the thesis using
+% UniFunMinPlotFlat-2015-03-07-15-36-32.eps --- dataset of Figure 3.2 in the thesis using
 % a1=0.3 and a2=0.75 to construct a example function with two local minimum
 % points 
 % 
-% UniFunMinTwoExtreme.eps --- dataset of Figure 4.1 in the thesis to 
+% UniFunMinTwoExtreme-2015-03-07-15-36-31.eps --- dataset of Figure 4.1 in the thesis to 
 % construct an example of a flat function
 
 
