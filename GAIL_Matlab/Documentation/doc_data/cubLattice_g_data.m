c5cc4898dfd420524eacfb8535885d2368b193a9
lluisantoni <lluisantoni@gmail.com>
2015-03-07 15:07:47 -0600

    Changing doc_data files for cubatures

--- GAIL_Matlab/Documentation/doc_data/cubLattice_g_data.m
+++ GAIL_Matlab/Documentation/doc_data/cubLattice_g_data.m
@@ -17,8 +17,8 @@
 % Estimate the integral with integrand f(x) = x1.*x2 in the interval
 % [0,1)^2:
 
-  f = @(x) prod(x,2); d = 2;
-  q = cubLattice_g(f,d,1e-5,1e-1,'uniform','transform','C1sin')
+  f = @(x) prod(x,2); hyperbox = [zeros(1,2);ones(1,2)];
+  q = cubLattice_g(f,hyperbox,'uniform',1e-5,1e-1,'transform','C1sin')
 
 %%
 % *Example 2*
@@ -26,17 +26,17 @@
 % Estimate the integral with integrand f(x) = x1.^2.*x2.^2.*x3.^2+0.11
 % in the interval R^3 where x1, x2 and x3 are normally distributed:
 
-  f = @(x) x(:,1).^2.*x(:,2).^2.*x(:,3).^2+0.11; d = 3;
-  q = cubLattice_g(f,d,1e-3,1e-3,'normal','transform','C1sin')
+  f = @(x) x(:,1).^2.*x(:,2).^2.*x(:,3).^2+0.11; hyperbox = [zeros(1,3);ones(1,3)];
+  q = cubLattice_g(f,hyperbox,'normal',1e-3,1e-3,'transform','C1sin')
 
 %%
 % *Example 3*
 
 % Estimate the integral with integrand f(x) = exp(-x1^2-x2^2) in the
-% interval [0,1)^2:
+% interval [-1,2)^2:
 
-  f = @(x) exp(-x(:,1).^2-x(:,2).^2); d = 2;
-  q = cubLattice_g(f,d,1e-3,1e-1,'uniform','transform','C1')
+  f = @(x) exp(-x(:,1).^2-x(:,2).^2); hyperbox = [-ones(1,2);2*ones(1,2)];
+  q = cubLattice_g(f,hyperbox,'uniform',1e-3,1e-1,'transform','C1')
 
 %%
 % *Example 4*
@@ -44,9 +44,8 @@
 % Estimate the price of an European call with S0=100, K=100, r=sigma^2/2,
 % sigma=0.05 and T=1.
 
-  f = @(x) exp(-0.05^2/2)*max(100*exp(0.05*x)-100,0); d = 1;
-  q = cubLattice_g(f,d,1e-4,1e-1,'normal','fudge',@(m) 2.^-(2*m),...
-      'transform','C1sin')
+  f = @(x) exp(-0.05^2/2)*max(100*exp(0.05*x)-100,0); hyperbox = [zeros(1,1);ones(1,1)];
+  q = cubLattice_g(f,hyperbox,'normal',1e-4,1e-1,'fudge',@(m) 2.^-(2*m),'transform','C1sin')
 
 %%
 % *Example 5*
@@ -54,5 +53,5 @@
 % Estimate the integral with integrand f(x) = 8*x1.*x2.*x3.*x4.*x5 in the
 % interval [0,1)^5 with pure absolute error 1e-5.
 
-  f = @(x) 8*prod(x,2); d = 5;
-  q = cubLattice_g(f,d,1e-5,0)
+  f = @(x) 8*prod(x,2); hyperbox = [zeros(1,5);ones(1,5)];
+  q = cubLattice_g(f,hyperbox,'uniform',1e-5,0)
--- GAIL_Matlab/Documentation/doc_data/cubSobol_g_data.m
+++ GAIL_Matlab/Documentation/doc_data/cubSobol_g_data.m
@@ -17,8 +17,8 @@
 % Estimate the integral with integrand f(x) = x1.*x2 in the interval
 % [0,1)^2:
 
-  f = @(x) x(:,1).*x(:,2); d = 2;
-  q = cubSobol_g(f,d,1e-5,1e-1,'uniform')
+  f = @(x) prod(x,2); hyperbox = [zeros(1,2);ones(1,2)];
+  q = cubSobol_g(f,hyperbox,'uniform',1e-5,1e-1)
 
 %%
 % *Example 2*
@@ -26,17 +26,17 @@
 % Estimate the integral with integrand f(x) = x1.^2.*x2.^2.*x3.^2+0.11
 % in the interval R^3 where x1, x2 and x3 are normally distributed:
 
-  f = @(x) x(:,1).^2.*x(:,2).^2.*x(:,3).^2+0.11; d = 3;
-  q = cubSobol_g(f,d,1e-3,1e-3,'normal')
+  f = @(x) x(:,1).^2.*x(:,2).^2.*x(:,3).^2+0.11; hyperbox = [zeros(1,3);ones(1,3)];
+  q = cubSobol_g(f,hyperbox,'normal',1e-3,1e-3)
 
 %%
 % *Example 3*
 
 % Estimate the integral with integrand f(x) = exp(-x1^2-x2^2) in the
-% interval [0,1)^2:
+% interval [-1,2)^2:
 
-  f = @(x) exp(-x(:,1).^2-x(:,2).^2); d = 2;
-  q = cubSobol_g(f,d,1e-3,1e-1,'uniform')
+  f = @(x) exp(-x(:,1).^2-x(:,2).^2); hyperbox = [-ones(1,2);2*ones(1,2)];
+  q = cubSobol_g(f,hyperbox,'uniform',1e-3,1e-1)
 
 %%
 % *Example 4*
@@ -44,8 +44,8 @@
 % Estimate the price of an European call with S0=100, K=100, r=sigma^2/2,
 % sigma=0.05 and T=1.
 
-  f = @(x) exp(-0.05^2/2)*max(100*exp(0.05*x)-100,0); d = 1;
-  q = cubSobol_g(f,d,1e-4,1e-1,'normal','fudge',@(m) 2.^-(2*m))
+  f = @(x) exp(-0.05^2/2)*max(100*exp(0.05*x)-100,0); hyperbox = [zeros(1,1);ones(1,1)];
+  q = cubSobol_g(f,hyperbox,'normal',1e-4,1e-1,'fudge',@(m) 2.^-(2*m))
 
 %%
 % *Example 5*
@@ -53,5 +53,5 @@
 % Estimate the integral with integrand f(x) = 8*x1.*x2.*x3.*x4.*x5 in the
 % interval [0,1)^5 with pure absolute error 1e-5.
 
-  f = @(x) 8*prod(x,2); d = 5;
-  q = cubSobol_g(f,d,1e-5,0)
+  f = @(x) 8*prod(x,2); hyperbox = [zeros(1,5);ones(1,5)];
+  q = cubSobol_g(f,hyperbox,'uniform',1e-5,0)
