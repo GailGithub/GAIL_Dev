1cbb55995e8994cb1ff3acd23ec9c98526e41a54
Sou-Cheng Terrya Choi <sou.cheng.terrya.choi@gmail.com>
2015-03-07 15:05:31 -0600

    Add check to unit tests.

--- GAIL_Matlab/Tests/ut_workout_integral_g.m
+++ GAIL_Matlab/Tests/ut_workout_integral_g.m
@@ -4,10 +4,13 @@ classdef ut_workout_integral_g < matlab.unittest.TestCase
   methods(Test)
              
     function testWorkout_integral_g(testCase)
-      nrep=10000; nmax=1e7; abstol=1e-8;
+      nrep=100; nmax=1e7; abstol=1e-8;
       [succnowarn, succwarn, pfin] = workout_integral_g(nrep,nmax,abstol);
       succrates = succnowarn + succwarn;   
       testCase.verifyGreaterThanOrEqual(succrates,pfin);
+      for i=2:len(succrates)-1
+        testCase.verifyGreaterThan(succrates(i), succrates(i-1));
+      end
     end
     
   end
