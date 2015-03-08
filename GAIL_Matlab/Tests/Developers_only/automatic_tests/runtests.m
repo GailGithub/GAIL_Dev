eff0bef5c06e17ba32c233a567a16614b71c946f
Sou-Cheng Terrya Choi <sou.cheng.terrya.choi@gmail.com>
2015-03-08 08:15:36 -0500

    Version 8.1 already has unit tests.

--- GAIL_Matlab/Tests/Developers_only/automatic_tests/runtests.m
+++ GAIL_Matlab/Tests/Developers_only/automatic_tests/runtests.m
@@ -27,8 +27,8 @@ doctest cubSobol_g; time=toc
 
 %% CALL UNIT TESTS
 [~,~,~,MATLABVERSION]=GAILstart(0);
-if MATLABVERSION < 8.3
-    warning('Cannot run unit tests in MATLAB version before 8.3');
+if MATLABVERSION < 8.1
+    warning('Cannot run unit tests in MATLAB version before 8.1');
 else
     warning('off','MATLAB:integral_g:peaky')
     warning('off','MATLAB:integral_g:exceedbudget')
