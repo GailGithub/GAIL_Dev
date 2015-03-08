39e474ea6172b1085706639a783509c3da805b02
lluisantoni <lluisantoni@gmail.com>
2015-03-07 23:16:01 -0600

    Contents for Papers output files

---
+++ GAIL_Matlab/OutputFiles/Paper_cubLattice_g/Contents.m
@@ -0,0 +1,22 @@
+% PAPER_CUBLATTICE_G
+%
+% Articles:
+%   - Adaptive Multidimensional Integration Based on Rank-1 Lattices
+%
+% Files
+%
+%	RunTestCubatureonGeoAsianCallLattice - Results from running this file
+%	under the form Paper_cubLattice_g_TestGeoAsianCall-*.eps and 
+%   Paper_cubLattice_g_TestGeoAsianCall-*.mat.
+%
+%	RunTestCubatureonKeisterLattice - Results from running this file under
+%   the form Paper_cubLattice_g_TestKeister-*.eps and
+%   Paper_cubLattice_g_TestKeister-*.mat.
+%
+%   lattice_example - Results from running this file under the form
+%   Paper_cubLattice_g_NodeSetExample-*.eps and 
+%   Paper_cubLattice_g_DualSetExample*.eps.
+%	
+%   FourierCoeffDecayPict - Results from running this file under the form
+%   Paper_cubLattice_g_FourierCoeffDecay-*.eps and
+%   Paper_cubLattice_g_FunctionFourierCoeffDecay-*.eps.
---
+++ GAIL_Matlab/OutputFiles/Paper_cubSobol_g/Contents.m
@@ -0,0 +1,22 @@
+% PAPER_CUBSOBOL_G
+% 
+% Articles:
+%   - Reliable Adaptive Cubature Using Digital Sequences
+%
+% Files
+%
+%   RunTestCubatureonGeoAsianCallSobol - Results from running this file
+%   under the form Paper_cubSobol_g_TestGeoAsianCall-*.eps and 
+%   Paper_cubSobol_g_TestGeoAsianCall-*.mat.
+%	
+%   RunTestCubatureonKeisterSobol - Results from running this file under
+%   the form Paper_cubSobol_g_TestKeister-*.eps and
+%   Paper_cubSobol_g_TestKeister-*.mat.
+%	
+%   SobolWalshPict - Results from running this file under the form
+%   Paper_cubSobol_g_256ScrambledShiftedSobolPoints-*.eps and
+%   Paper_cubSobol_g_256SobolPoints-*.eps.
+%	
+%   WalshFourierCoeffDecay - Results from running this file under the form
+%   Paper_cubSobol_g_WalshFourierCoeffDecay-*.eps and
+%   Paper_cubSobol_g_WalshFourierCoeffDecay-*.eps.
