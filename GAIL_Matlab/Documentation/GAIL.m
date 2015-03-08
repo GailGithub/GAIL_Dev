247d954c9d4ea0a3b1dedb71c583249e438b9579
Sou-Cheng Terrya Choi <sou.cheng.terrya.choi@gmail.com>
2015-03-08 10:55:53 -0500

    Update GAIL.m structure.

--- GAIL_Matlab/Documentation/GAIL.m
+++ GAIL_Matlab/Documentation/GAIL.m
@@ -9,39 +9,28 @@
 %
 % GAIL is a suite of algorithms for integration problems in one, many, and 
 % infinite dimensions, and whose answers are guaranteed to be correct.
-%help GAU
+%
+%% Introduction
+%
+% <html>
+% <a href="help_license.html">GAIL License</a>
+% <a href="help_readme.html">README</a>
+% <a href="help_tests.html">Tests</a>
+% </html>
+%
 %% Functions
 %
 % <html>
 % <a href="help_funappx_g.html">funappx_g</a>
 % <a href="help_integral_g.html">integral_g</a>
+% <a href="help_funmin_g.html">funmin_g</a>
 % <a href="help_meanMC_g.html">meanMC_g</a>
 % <a href="help_cubMC_g.html">cubMC_g</a>
 % <a href="help_meanMCBer_g.html">meanMCBer_g</a>
 % <a href="help_cubSobol_g.html">cubSobol_g</a>
 % <a href="help_cubLattice_g.html">cubLattice_g</a>
-% <a href="help_funmin_g.html">funmin_g</a>
 % </html>
 %
-%
-%% Installation
-%
-% <html>
-% <a href="help_install.html">Instructions</a>
-% </html>
-%
-%% Tests
-% We provide quick doctests for each of the functions above. To run
-% doctests in *funappx_g*, for example, issue the command *doctest
-% funappx_g*.
-%
-% We also provide unit tests for MATLAB version 8 or later. To run unit
-% tests for *funmin_g*, for instance, execute *run(ut_funmin_g)*.
-%
-% To run all the fast doctests and unit tests in the suite, execute the 
-% script *runtests.m*. 
-%
-% A collection of long tests are contained in *longtests.m*. 
 %
 %% Website
 % For more information about GAIL, visit
