09030e1c81f64c4b156e756334d7ecf14a51ccfb
Sou-Cheng Terrya Choi <sou.cheng.terrya.choi@gmail.com>
2015-03-08 10:17:20 -0500

    Adding documentation from README.

--- GAIL_Matlab/Documentation/doc_data/intro.m
+++ GAIL_Matlab/Documentation/doc_data/intro.m
@@ -9,3 +9,12 @@
 %
 % GAIL is a suite of algorithms for integration problems in one, many, and 
 % infinite dimensions, and whose answers are guaranteed to be correct.
+%
+%% Introduction
+%
+% <html>
+% <a href="help_license.html">GAIL License</a>
+% <a href="help_install.html">Instructions</a>
+% <a href="help_tests.html">Tests</a>
+% </html>
+%
