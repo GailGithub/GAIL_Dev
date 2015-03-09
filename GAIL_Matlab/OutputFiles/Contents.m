ae050716478f1a22af16bca57fde00e41cedbe98
Xin Tong <xtong5@hawk.iit.edu>
2015-03-09 17:50:53 -0500

    update contents

--- GAIL_Matlab/OutputFiles/Contents.m
+++ GAIL_Matlab/OutputFiles/Contents.m
@@ -4,6 +4,8 @@
 %   ConesPaperOutput      - output files of cones paper
 %   MCQMC2012PaperOutput  - output files of MCQMC2012 paper
 %   meanMCBerPaperOutput  - output files of meanMCBer paper
+%   Paper_cubLattice_g    - output files of cubLattice paper
+%   Paper_cubSobol_g      - output files of cubSobol paper
 %   UniFunMinOutput       - output files of Xin Tong's thesis
 %   WorkoutFunappxOutput  - output files of funappx_g workouts
 %   WorkoutFunminOutput   - output files of funmin_g workouts
