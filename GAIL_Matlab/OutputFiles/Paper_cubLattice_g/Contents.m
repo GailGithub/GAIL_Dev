e7436dd8e7feff71ddb97fd742fa91297fe05544
lluisantoni <lluisantoni@gmail.com>
2015-03-09 19:04:43 -0500

    Modifying contents

--- GAIL_Matlab/OutputFiles/Paper_cubLattice_g/Contents.m
+++ GAIL_Matlab/OutputFiles/Paper_cubLattice_g/Contents.m
@@ -1,22 +1,16 @@
 % PAPER_CUBLATTICE_G
 %
-% Articles:
-%   - Adaptive Multidimensional Integration Based on Rank-1 Lattices
 %
 % Files
 %
-%	RunTestCubatureonGeoAsianCallLattice - Results from running this file
-%	under the form Paper_cubLattice_g_TestGeoAsianCall-*.eps and 
-%   Paper_cubLattice_g_TestGeoAsianCall-*.mat.
-%
-%	RunTestCubatureonKeisterLattice - Results from running this file under
-%   the form Paper_cubLattice_g_TestKeister-*.eps and
-%   Paper_cubLattice_g_TestKeister-*.mat.
-%
-%   lattice_example - Results from running this file under the form
-%   Paper_cubLattice_g_NodeSetExample-*.eps and 
-%   Paper_cubLattice_g_DualSetExample*.eps.
-%	
-%   FourierCoeffDecayPict - Results from running this file under the form
-%   Paper_cubLattice_g_FourierCoeffDecay-*.eps and
-%   Paper_cubLattice_g_FunctionFourierCoeffDecay-*.eps.
+%   Paper_cubLattice_g_TestKeister-2015-03-07-17-17-37.mat - 
+% 
+%   Paper_cubLattice_g_TestKeister-2015-03-07-17-17-36.eps - 
+% 
+%   Paper_cubLattice_g_TestGeoAsianCall-2015-03-07-17-16-16.mat - 
+%  
+%   Paper_cubLattice_g_TestGeoAsianCall-2015-03-07-17-16-15.eps
+% 
+%   Paper_cubLattice_g_NodeSetExample-2015-03-07-17-05-57.eps
+% 
+%   Paper_cubLattice_g_DualSetExample-2015-03-07-17-05-58.eps
--- GAIL_Matlab/OutputFiles/Paper_cubSobol_g/Contents.m
+++ GAIL_Matlab/OutputFiles/Paper_cubSobol_g/Contents.m
@@ -1,22 +1,19 @@
 % PAPER_CUBSOBOL_G
-% 
-% Articles:
-%   - Reliable Adaptive Cubature Using Digital Sequences
 %
 % Files
 %
-%   RunTestCubatureonGeoAsianCallSobol - Results from running this file
-%   under the form Paper_cubSobol_g_TestGeoAsianCall-*.eps and 
-%   Paper_cubSobol_g_TestGeoAsianCall-*.mat.
-%	
-%   RunTestCubatureonKeisterSobol - Results from running this file under
-%   the form Paper_cubSobol_g_TestKeister-*.eps and
-%   Paper_cubSobol_g_TestKeister-*.mat.
-%	
-%   SobolWalshPict - Results from running this file under the form
-%   Paper_cubSobol_g_256ScrambledShiftedSobolPoints-*.eps and
-%   Paper_cubSobol_g_256SobolPoints-*.eps.
-%	
-%   WalshFourierCoeffDecay - Results from running this file under the form
-%   Paper_cubSobol_g_WalshFourierCoeffDecay-*.eps and
-%   Paper_cubSobol_g_WalshFourierCoeffDecay-*.eps.
+%  Paper_cubSobol_g_256ScrambledShiftedSobolPoints-2015-03-07-17-21-15.eps -
+% 
+%  Paper_cubSobol_g_256SobolPoints-2015-03-07-17-21-14.eps - 
+% 
+%  Paper_cubSobol_g_FunctionWalshFourierCoeffDecay-2015-03-07-17-21-20.eps -
+% 
+%  Paper_cubSobol_g_TestGeoAsianCall-2015-03-07-17-21-04.eps - 
+% 
+%  Paper_cubSobol_g_TestGeoAsianCall-2015-03-07-17-21-06.mat - 
+% 
+%  Paper_cubSobol_g_TestKeister-2015-03-07-17-20-12.eps - 
+% 
+%  Paper_cubSobol_g_TestKeister-2015-03-07-17-20-13.mat - 
+% 
+%  Paper_cubSobol_g_WalshFourierCoeffDecay-2015-03-07-17-21-36.eps - 
