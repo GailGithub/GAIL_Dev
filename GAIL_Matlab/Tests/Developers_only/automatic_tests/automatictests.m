701f94abd606ba5092e26857819c099f27eb1e6a
lluisantoni <lluisantoni@gmail.com>
2015-03-11 15:19:12 -0500

    Changing the name shorttests to fasttests

--- GAIL_Matlab/Tests/Developers_only/automatic_tests/automatictests.m
+++ GAIL_Matlab/Tests/Developers_only/automatic_tests/automatictests.m
@@ -5,5 +5,5 @@ GAIL_Install
 % Go to the tests direcoty (% is comment in matlab environment)
 cd /home/gail/GAIL_tests/repo/gail-development/GAIL_Matlab/Tests/Developers_only/
 %runtests %> /home/gail/GAIL_tests/test_results.txt former tests
-shorttests
+fasttests
 exit
