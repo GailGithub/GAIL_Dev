67772b9c15cd6f60baee45e5e5162a85005cc3c8
Fred J. Hickernell <hickernell@iit.edu>
2015-03-11 18:23:19 -0500

    reduce time of tests

--- GAIL_Matlab/Tests/ut_ConesPaper.m
+++ GAIL_Matlab/Tests/ut_ConesPaper.m
@@ -4,14 +4,16 @@ classdef ut_ConesPaper < matlab.unittest.TestCase
   methods(Test)
              
     function testConepaper_test_integral_g(testCase)
-      nrep=10000; nmax=1e7; abstol=1e-8;
+%      nrep=10000; nmax=1e7; abstol=1e-8; %for a good table
+      nrep=1000; nmax=1e7; abstol=1e-8; %for faster testing
       [succnowarn, succwarn] = conepaper_test_integral_g(nrep,nmax,abstol);
       succrates = succnowarn + succwarn;   
       testCase.verifyGreaterThanOrEqual(succrates,[0.1,0.5,0.8]);
     end
     
     function testConepaper_test_funappx_g(testCase)
-      nrep=10000; nmax=1e7; abstol=1e-8;
+%      nrep=10000; nmax=1e7; abstol=1e-8; %for a good table
+      nrep=1000; nmax=1e7; abstol=1e-8; %for faster testing
       [succnowarn,succwarn] = conepaper_test_funappx_g(nrep,nmax,abstol);
       succrates = succnowarn + succwarn;   
       testCase.verifyGreaterThanOrEqual(succrates,[0.1,0.5,0.8]);
