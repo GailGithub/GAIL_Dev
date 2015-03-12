51af87bd0f71b60f21c2ea12981c8f0f09a64ed6
Lan Jiang <ljiang14@hawk.iit.edu>
2015-03-11 21:54:55 -0500

    fix Fred's scripts save to the right dir

--- GAIL_Matlab/Papers/MCQMC2012Paper/snooper.m
+++ GAIL_Matlab/Papers/MCQMC2012Paper/snooper.m
@@ -1,10 +1,12 @@
 function y=snooper(x,info)
 %This records all the values of x it is asked for in a file named what you
 %like (.txt, .mat, or other suffix must be specially typed)
-
-load(info.filename,'xsample')
+[GAILPATH,~,PATHNAMESEPARATOR] = GAILstart(0);
+outputfolder =  [GAILPATH,'OutputFiles',PATHNAMESEPARATOR,'MCQMC2012PaperOutput'];
+fullfilename = strcat(outputfolder, PATHNAMESEPARATOR', info.filename);
+load(fullfilename,'xsample')
 xsample=[xsample; x(:)];
-save(info.filename,'xsample')
+save(fullfilename,'xsample')
 if(info.RegFunc(x)==0)
     y=zeros(size(x));
 else
