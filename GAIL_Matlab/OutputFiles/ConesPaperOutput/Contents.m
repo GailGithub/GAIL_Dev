1e5744e4f267ad1c8835ad13fee243e0d7e12e3a
YuhanDing <jinliang98@hotmail.com>
2015-03-11 22:42:23 -0500

    fix contents in ConesPaperOutPut and update helptoc.xml

--- GAIL_Matlab/OutputFiles/ConesPaperOutput/Contents.m
+++ GAIL_Matlab/OutputFiles/ConesPaperOutput/Contents.m
@@ -1,5 +1,12 @@
 % CONESPAPEROUTPUT
 %
 % Files
-%   ConePaperIntegralTest-2015-02-15-16-57-04 - dataset of integral in
+%   ConesPaperFunAppxTest-03-Sep-2013-18-37-05 - dataset of function
+%   approximation in Cones paper
+%   ConesPaperFunAppxTest-2015-Jan-24-13-34-24 - reproduced dataset of
+%   function approximation in Cones paper
+%   ConePaperIntegralTest-2015-02-05-16-57-04 - dataset of integral in
 %   Cones paper
+%   ConesPaperFlukyquad - figure 1b) in Cones paper
+%   ConesPaperSpikyquad - figure 1a) in Cones Paper
+
