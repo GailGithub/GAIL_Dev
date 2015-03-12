568451d2f7317c021c6b4d69f71b74f802d5ab9b
Lan Jiang <ljiang14@hawk.iit.edu>
2015-03-12 14:31:23 -0500

    test new meanMC_g

--- GAIL_Matlab/Workouts/meanMC_g_workouts/Test_meanMCnew_g.m
+++ GAIL_Matlab/Workouts/meanMC_g_workouts/Test_meanMCnew_g.m
@@ -1,13 +1,9 @@
 % This is the driver script to test the meanMC_g algorithm
-%clear all; close all; clc;
+clear all; close all; clc;
 %y = @(n) rand(n,1).^2;% the test function
 %function [mumeanMC,mumeanMCabs,mumeanMCnew] = Test_meanMCnew_g
-%y = @Ytrafficmodel; % this is the traffic model
-clear all
-clc
-close all
-
-y = @(n) rand(n,1).^2;% the test function
+y = @Ytrafficmodel; % this is the traffic model
+%y = @(n) rand(n,1).^2/10;% the test function
 in_param.abstol = 1e-2;% the absolute error tolerance
 in_param.reltol =0;%the relative error tolerance
 in_param.tbudget = 50;%the time budget
@@ -17,14 +13,17 @@ in_param.alpha = 0.01;% uncertainty
 in_param.nbudget = 1e10;
 in_param.fudge = 1.2;%standard deviation inflation factor
 tic
-[mumeanMC, out_param_meanMC] = meanMC_g(y,in_param) % the results
-t_meanMC_g = toc
+[mumeanMC, out_param_meanMC] = meanMC_g(y,in_param); % the results
+t_meanMC_g = toc;
 tic
-[mumeanMCabs, out_param_meanMCabs] = meanMCabs_g(y,in_param) % the results
-t_meanMCabs_g = toc
+[mumeanMCabs, out_param_meanMCabs] = meanMCabs_g(y,in_param); % the results
+t_meanMCabs_g = toc;
 tic
-[mumeanMCnew, out_param_meanMCnew] = meanMCnew_g(y,in_param) % the results
-t_meanMCnew_g = toc
-
+[mumeanMCnew, out_param_meanMCnew] = meanMCnew_g(y,in_param); % the results
+t_meanMCnew_g = toc;
+tic
+[mumeanMCnew2, out_param_meanMCnew2] = meanMCnew2_g(y,in_param); % the results
+t_meanMCnew2_g = toc;
+timevec = [t_meanMC_g t_meanMCabs_g t_meanMCnew_g t_meanMCnew2_g]
 
 %end
