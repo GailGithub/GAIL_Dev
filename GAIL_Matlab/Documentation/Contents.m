c008adbfcd24864afa4c4429bb85b53710db0bcb
Xin Tong <xtong5@hawk.iit.edu>
2015-03-11 22:06:32 -0500

    update contents in documentation folder

--- GAIL_Matlab/Documentation/Contents.m
+++ GAIL_Matlab/Documentation/Contents.m
@@ -12,7 +12,10 @@
 %   help_meanMC_g     - meanMC_g
 %   help_meanMCBer_g  - meanMCBer_g
 %   help_install      - Installation Instructions
+%   help_readme       - Readme
+%   help_license      - License
+%   GAIL_Publish      - To generate html files in the GAIL subdirectory Documentation
+%   gail_doc_2_1.pdf  - Documentation for Version 2.1
 %
 % Folders
 %   html              - folder of all html files
-
