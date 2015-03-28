fd43655706d352bbb7237ebb3783749e272d5eda
Sou-Cheng Terrya Choi <sou.cheng.terrya.choi@gmail.com>
2015-03-28 15:44:11 +0800

    ut_save_eps: unit test for gail.save_eps.m

---
+++ GAIL_Matlab/OutputFiles/ut_save_eps.m
@@ -0,0 +1,19 @@
+% ut_save_eps unit tests for gail.save_eps
+classdef ut_save_eps < matlab.unittest.TestCase
+    
+    methods(Test)
+        
+        function test_save_eps_1(testCase)
+            [GAILPATH,~,PATHNAMESEPARATOR,~] = GAILstart(false);
+            dir_name = strcat(GAILPATH,'OutputFiles',PATHNAMESEPARATOR,'temp',PATHNAMESEPARATOR);
+            x = 0:0.1:1; y = sin(x);
+            figure(1);
+            plot(x,y)
+            eps_file_name = gail.save_eps('temp', 'sin');
+            testCase.verifyEqual(exist(eps_file_name, 'file'), 2);
+            testCase.verifyGreaterThan(length(eps_file_name), length(strcat(dir_name,'sin.eps')));
+            rmdir(dir_name,'s');
+        end
+        
+    end
+end
