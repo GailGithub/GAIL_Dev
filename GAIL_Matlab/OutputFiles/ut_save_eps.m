606575624cb3b23234acb07523a50473eb39df52
Sou-Cheng Terrya Choi <sou.cheng.terrya.choi@gmail.com>
2015-03-28 15:57:57 +0800

    gail.save_eps:  Added an option to exclude timestamp from filename.

--- GAIL_Matlab/OutputFiles/ut_save_eps.m
+++ GAIL_Matlab/OutputFiles/ut_save_eps.m
@@ -11,9 +11,32 @@ classdef ut_save_eps < matlab.unittest.TestCase
             plot(x,y)
             eps_file_name = gail.save_eps('temp', 'sin');
             testCase.verifyEqual(exist(eps_file_name, 'file'), 2);
-            testCase.verifyGreaterThan(length(eps_file_name), length(strcat(dir_name,'sin.eps')));
+            testCase.verifyEqual(length(eps_file_name), 20 + length(strcat(dir_name,'sin.eps')));
             rmdir(dir_name,'s');
         end
         
+        function test_save_eps_2a(testCase)
+            [GAILPATH,~,PATHNAMESEPARATOR,~] = GAILstart(false);
+            dir_name = strcat(GAILPATH,'OutputFiles',PATHNAMESEPARATOR,'temp',PATHNAMESEPARATOR);
+            x = 0:0.1:1; y = sin(x);
+            figure(1);
+            plot(x,y)
+            eps_file_name = gail.save_eps('temp', 'sin', true);
+            testCase.verifyEqual(exist(eps_file_name, 'file'), 2);
+            testCase.verifyEqual(length(eps_file_name), 20 + length(strcat(dir_name,'sin.eps')));
+            rmdir(dir_name,'s');
+        end
+        
+        function test_save_eps_2b(testCase)
+            [GAILPATH,~,PATHNAMESEPARATOR,~] = GAILstart(false);
+            dir_name = strcat(GAILPATH,'OutputFiles',PATHNAMESEPARATOR,'temp',PATHNAMESEPARATOR);
+            x = 0:0.1:1; y = sin(x);
+            figure(1);
+            plot(x,y)
+            eps_file_name = gail.save_eps('temp', 'sin', false);
+            testCase.verifyEqual(exist(eps_file_name, 'file'), 2);
+            testCase.verifyEqual(length(eps_file_name), length(strcat(dir_name,'sin.eps')));
+            rmdir(dir_name,'s');
+        end
     end
 end
