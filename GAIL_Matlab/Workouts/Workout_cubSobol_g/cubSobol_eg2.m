f83343941c1ab977c8a8eaa3719aef9bfd8be6c4
Sou-Cheng Terrya Choi <sou.cheng.terrya.choi@gmail.com>
2015-04-01 16:53:05 -0500

    cubSobol_eg2: add function definition.

--- GAIL_Matlab/Workouts/Workout_cubSobol_g/cubSobol_eg2.m
+++ GAIL_Matlab/Workouts/Workout_cubSobol_g/cubSobol_eg2.m
@@ -1,9 +1,10 @@
 format compact
+f = @(x) x(:,1).^2.*x(:,2).^2.*x(:,3).^2; hyperbox = [-inf(1,3);inf(1,3)];
 for i=1:100
-    q = cubSobol_g(f,hyperbox,'normal',1e-3,1e-3);
+    [q,out_param] = cubSobol_g(f,hyperbox,'normal',1e-3,1e-3);
     exactsol = 1; check = abs(exactsol-q) < gail.tolfun(1e-3,1e-3,1,exactsol,'max');
     if check==0, 
-        i, exactsol, q, abs(exactsol-q), gail.tolfun(1e-3,1e-3,1,exactsol,'max')
-        keyboard
+        i, exactsol, q, out_param.exitflag, abs(exactsol-q), gail.tolfun(1e-3,1e-3,1,exactsol,'max')
+        %keyboard
     end;
 end;
