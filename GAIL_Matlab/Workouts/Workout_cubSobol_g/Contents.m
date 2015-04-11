ffdf66bb0af5fe8211ba6b86bd2351593f591e2d
Sou-Cheng Terrya Choi <sou.cheng.terrya.choi@gmail.com>
2015-04-11 16:28:03 -0500

    Update Contents.m

--- GAIL_Matlab/Workouts/Workout_cubSobol_g/Contents.m
+++ GAIL_Matlab/Workouts/Workout_cubSobol_g/Contents.m
@@ -1,5 +1,6 @@
 % WORKOUTS_CUBSOBOL_G
 % 
 % Files 
+%   dt_cubSobol_g       - Lengthy doc tests for cubSobol_g
 %   test_cubSobol       - driver script to test cubSobol_g algorithm using integrands of dimensions up to 3 
 %   test_cubSobol_ver_b - driver script to test cubSobol_g algorithm using integrands of various dimensions
