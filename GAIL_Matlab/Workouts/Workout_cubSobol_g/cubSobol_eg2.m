89a07f7b61cfc79226fcde1c2e180da63647445c
Sou-Cheng Terrya Choi <sou.cheng.terrya.choi@gmail.com>
2015-04-12 14:23:44 -0500

    A more stable example for cubQMC.

--- GAIL_Matlab/Workouts/Workout_cubSobol_g/cubSobol_eg2.m
+++ GAIL_Matlab/Workouts/Workout_cubSobol_g/cubSobol_eg2.m
@@ -1,10 +1,11 @@
 % cubSobol_eg2.m
 format compact
-f = @(x) x(:,1).^2.*x(:,2).^2.*x(:,3).^2; hyperbox = [-inf(1,3);inf(1,3)];
+mu = 0.001;
+f = @(x) mu * x(:,1).^2.*x(:,2).^2.*x(:,3).^2; hyperbox = [-inf(1,3);inf(1,3)];
 count = 0;
 for i=1:100
     [q,out_param] = cubSobol_g(f,hyperbox,'normal',1e-3,1e-3);
-    exactsol = 1; check = abs(exactsol-q) < gail.tolfun(1e-3,1e-3,1,exactsol,'max');
+    exactsol = mu; check = abs(exactsol-q) < gail.tolfun(1e-3,1e-3,1,exactsol,'max');
     if check==0 || isfinite(q) ==0 %|| out_param.exitflag > 0, 
         i, exactsol, q, exitflag = out_param.exitflag, 
         abserr = abs(exactsol-q), tol = gail.tolfun(1e-3,1e-3,1,exactsol,'max')
