65c649d504d1fc4f0de72a954353a21813030608
Sou-Cheng Terrya Choi <sou.cheng.terrya.choi@gmail.com>
2015-04-14 14:42:08 -0500

    It seems n=2^21 is sufficient to contain the function in a cone to guarantee accuracy of 1e-3.

--- GAIL_Matlab/Workouts/Workout_cubSobol_g/cubSobol_eg2.m
+++ GAIL_Matlab/Workouts/Workout_cubSobol_g/cubSobol_eg2.m
@@ -2,8 +2,8 @@
 format compact
 f = @(x) x(:,1).^2.*x(:,2).^2.*x(:,3).^2; hyperbox = [-inf(1,3);inf(1,3)];
 count = 0;
-for i=1:100
-    [q,out_param] = cubSobol_g(f,hyperbox,'normal',1e-3,1e-3,'mmin',20,'mmax',30);
+for i=1:500
+    [q,out_param] = cubSobol_g(f,hyperbox,'normal',1e-3,1e-3,'mmin',21,'mmax',21);
     exactsol = 1; check = abs(exactsol-q) < gail.tolfun(1e-3,1e-3,1,exactsol,'max');
     if check==0 || isfinite(q) ==0 %|| out_param.exitflag > 0, 
         i, exactsol, q, exitflag = out_param.exitflag, 
