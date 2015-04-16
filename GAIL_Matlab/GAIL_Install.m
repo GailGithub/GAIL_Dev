9500552b394a80f88c95a833c8f2bfa476b7f406
Xuan Zhou <xuanjzhou@gmail.com>
2015-04-16 10:52:12 -0500

    change GAIL_Install

--- GAIL_Matlab/GAIL_Install.m
+++ GAIL_Matlab/GAIL_Install.m
@@ -1,9 +1,12 @@
-function GAIL_Install()
+function GAIL_Install(isBuild)
 %GAIL_INSTALL Install or reinstall GAIL. Add GAIL paths to MATLAB search path.
+if nargin < 1
+    isBuild =  false;
+end
 [GAILPATH,GAILVERSION,~,MATLABVERSION] = GAILstart;
 fprintf('\nWelcome to Guaranteed Automatic Integration Library (GAIL).\nYou are installing GAIL v%s.\n\n', GAILVERSION);
 if MATLABVERSION < 7,
-  error('This version is only supported on Matlab 7.x and above.');
+    error('This version is only supported on Matlab 7.x and above.');
 end
 gailp=genpath(GAILPATH);% Generate strings of paths to GAIL subdirectories
 warninfo = warning('query','MATLAB:rmpath:DirNotFound');
@@ -12,8 +15,6 @@ rmpath(gailp);% Remove path from MATLAB search path
 warning(warninfo.state, warninfo.identifier);
 addpath(gailp);           % Add GAIL directories and subdirectories
 savepath;                 % Save the changes
-gail.GAIL_Publish(true,true,true); 
+gail.GAIL_Publish(isBuild,isBuild,true); 
 fprintf('\nGAIL version %s has been installed successfully.\n\n', GAILVERSION);
 end
-
- 
