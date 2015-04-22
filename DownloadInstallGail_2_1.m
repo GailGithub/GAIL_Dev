1247747330513b45610899b7dd968c397f2a897a
Xuan Zhou <xzhou23@hawk.iit.edu>
2015-04-22 12:10:08 -0500

    change DownloadInstallGail to a function

--- DownloadInstallGail_2_1.m
+++ DownloadInstallGail_2_1.m
@@ -1,4 +1,5 @@
-% This scripts is to download and install GAIL version 2.1 into the
+function DownloadInstallGail_2_1()
+% This function downloads and installs GAIL version 2.1 into the
 % location you choose
 %
 %   Step 1.  Place this M-file where you want GAIL to go
@@ -18,9 +19,10 @@ fprintf('\n\nNext we will run a quick test. Press RETURN to continue...\n')
 pause
 
 %% Run a quick test
-fprintf('\nmuhat=meanMC_g(@(n) rand(n,1))\n')
-muhat=meanMC_g(@(n) rand(n,1)) %run meanMC_g once
-fprintf('\nThis answer should be close to 0.5.\n\n')
+fprintf('\nmuhat=meanMC_g(@(n) rand(n,1))\n');
+muhat=meanMC_g(@(n) rand(n,1)); %run meanMC_g once
+fprintf('muhat = %.4f\n',muhat);
+fprintf('\nThis answer should be close to 0.5.\n\n');
 %Then you should be ready to use GAIL
 disp('Next README.txt will be displayed. Press RETURN to continue to ');
 disp('the next line, or press the spacebar to advance to the next page...');
@@ -31,3 +33,4 @@ moreStatus = get(0,'More');
 more on
 type('README.txt')
 more(moreStatus);
+end
