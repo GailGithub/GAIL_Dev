5939e092cfa690c1218f45ea835f32908f2f6695
Xuan Zhou <xzhou23@hawk.iit.edu>
2015-04-22 14:26:43 -0500

    zip files in google drive

--- DevelopOnly/CopyZipGAILGoogleDrive.m
+++ DevelopOnly/CopyZipGAILGoogleDrive.m
@@ -1,30 +1,16 @@
-function CopyZipGAILGoogleDrive(newpath,GoogleDrivePath,zipfilename,excludefilelist)
-% This script copies and combines files in both repository and GAIL's
-% Google drive to make a release zip package.
-%
-% newpath --- path to where the zip is created, e.g., 'E:\GAIL_2_0\' for
-% GAIL version 2.0
+function CopyZipGAILGoogleDrive(GoogleDrivePath,zipFile)
+% This script copies files in GAIL's Google drive to make a release zip package.
 %
 % GoogleDrivePath --- where your Google drive path, e.g.,
 % 'E:\GoogleDrive\GAIL_Dev\'
 %
-% zipfilename --- the name of zip file, e.g., 'GAIL_2_0.zip' for GAIL 2.0
+% zipFile --- the name of zip file, e.g., 'GAIL_2_0.zip' for GAIL 2.0
 %
-% excludefilelist --- name of an external file that contains the list of
-% files we want to exclude in the zip, e.g., 'exclude2_0.lst' for GAIL 2.0
-%
-% CopyZipGAILRepoAndGoogleDrive('E:\GAIL_2_0','E:\GoogleDrive\GAIL_Dev','GAIL_2_0.zip','exclude2_0.lst')
+% CopyZipGAILGoogleDrive('E:\GoogleDrive\GAIL_Dev','GAIL_2_0.zip')
 
-[GAILPATH,~,PATHNAMESEPARATOR] = GAILstart; % Get the path of GAIL_Matlab
-GoogleDrivePath1 = horzcat(GoogleDrivePath,PATHNAMESEPARATOR,'GAIL_Matlab');
-
-%copy files in repository to new path
-copyfile(GAILPATH,newpath) % copy files in Google drive to new path
-copyfile(GoogleDrivePath1,newpath)
-
-% zip files recursively excluding hidden file such as .ds_store and .icon
-% in current folder
-system(horzcat('zip -r ',zipfilename,' ',newpath,' -x@',excludefilelist));  
-
-movefile(zipfilename, GoogleDrivePath,'f') % move zip to Google drive path
-display(horzcat(zipfilename,' is already put in ',GoogleDrivePath))
+currentPath = pwd;
+cd(GoogleDrivePath);
+system(horzcat('zip -r ',zipFile,' GAIL_Matlab -x@excludeGoogle.m'));
+cd(currentPath);
+display(horzcat(zipFile,' is already put in ',GoogleDrivePath));
+end
