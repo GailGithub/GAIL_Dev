8274f78bcb6817c13777fbfd5b934522c7629d11
Lan Jiang <ljiang14@hawk.iit.edu>
2015-04-28 14:03:18 -0500

    modify meanMCnew

--- GAIL_Matlab/Algorithms/meanMCnew_g.m
+++ GAIL_Matlab/Algorithms/meanMCnew_g.m
@@ -2,7 +2,7 @@ function [tmu,out_param]=meanMCnew_g(varargin)
 % meanMCnew_G Monte Carlo method to estimate the mean of a random variable.
 %
 %   tmu = meanMCnew_G(Yrand) estimates the mean, mu, of a random variable Y to
-%   within a specified generalized error tolerance, 
+%   within a specified generalized error tolerance,
 %   tolfun:=max(abstol,reltol*|mu|), i.e., |mu - tmu| <= tolfun with
 %   probability at least 1-alpha, where abstol is the absolute error
 %   tolerance, and reltol is the relative error tolerance. Usually the
@@ -87,9 +87,9 @@ function [tmu,out_param]=meanMCnew_g(varargin)
 %     out_param.var --- the sample variance.
 %
 %     out_param.exit --- the state of program when exiting.
-%       
+%
 %                      0   Success
-%      
+%
 %                      1   Not enough samples to estimate the mean
 %
 %     out_param.kurtmax --- the upper bound on modified kurtosis.
@@ -97,7 +97,7 @@ function [tmu,out_param]=meanMCnew_g(varargin)
 %     out_param.time --- the time elapsed in seconds.
 %
 %     out_param.flag --- parameter checking status
-%      
+%
 %                           1  checked by meanMCnew_g
 %
 %  Guarantee
@@ -106,13 +106,13 @@ function [tmu,out_param]=meanMCnew_g(varargin)
 % guaranteed confidence level 1-alpha. If the algorithm terminated without
 % showing any warning messages and provide an answer tmu, then the follow
 % inequality would be satisfied:
-% 
+%
 % Pr(|mu-tmu| <= tolfun) >= 1-alpha
 %
 % The cost of the algorithm, N_tot, is also bounded above by N_up, which is
 % defined in terms of abstol, reltol, nSig, n1, fudge, kurtmax, beta. And
 % the following inequality holds:
-% 
+%
 % Pr (N_tot <= N_up) >= 1-beta
 %
 % Please refer to our paper for detailed arguments and proofs.
@@ -192,7 +192,7 @@ elseif tpern>=1e-7 && tpern<1e-5 %each sample use very little time
     booster = 6;
     tic;Yrand(ntry*booster);ttry2 = toc;
     ntry = ntry*[1 booster];
-    ttry = [ttry ttry2];% take six times more samples to try    
+    ttry = [ttry ttry2];% take six times more samples to try
 elseif tpern>=1e-5 && tpern<1e-3 %each sample use little time
     booster = 4;
     tic;Yrand(ntry*booster);ttry2 = toc;
@@ -205,13 +205,13 @@ elseif  tpern>=1e-3 && tpern<1e-1 %each sample use moderate time
     ttry = [ttry ttry2];% take two times more samples to try
 else %each sample use lots of time, stop try
 end
-    [tmu,out_param] =  meanmctolfun(Yrand,out_param,ntry,ttry,nsofar,tstart);
+[tmu,out_param] =  meanmctolfun(Yrand,out_param,ntry,ttry,nsofar,tstart);
 
 end
 
 function [tmu,out_param] =  meanmctolfun(Yrand,out_param,ntry,ttry,nsofar,tstart)
 tic;
-Yval = Yrand(out_param.nSig);% get samples to estimate variance 
+Yval = Yrand(out_param.nSig);% get samples to estimate variance
 t_sig = toc;%get the time for calculating nSig function values.
 nsofar = nsofar+out_param.nSig;% update the samples that have been used
 out_param.nremain = gail.estsamplebudget(out_param.tbudget,...
@@ -221,62 +221,81 @@ out_param.var = var(Yval);% calculate the sample variance--stage 1
 sig0 = sqrt(out_param.var);% standard deviation
 sig0up = out_param.fudge.*sig0;% upper bound on the standard deviation
 alpha_sig = out_param.alpha/2;% the uncertainty for variance estimation
-alphai = (out_param.alpha-alpha_sig)/(2*(1-alpha_sig));
-%uncertainty to do iteration
 out_param.kurtmax = (out_param.nSig-3)/(out_param.nSig-1) ...
     + ((alpha_sig*out_param.nSig)/(1-alpha_sig))...
     *(1-1/out_param.fudge^2)^2;
 %the upper bound on the modified kurtosis
-eps1 = ncbinv(out_param.n1,alphai,out_param.kurtmax);
-%tolerance for initial estimation
-out_param.tol(1) = sig0up*eps1;
-%the width of initial confidence interval for the mean
-i=1;
 npcmax = 1e6;%constant to do iteration and mean calculation
-out_param.n(i) = out_param.n1;% initial sample size to do iteration
-while true
-    out_param.tau = i;%step of the iteration
-    if out_param.n(i) > out_param.nremain;
-        % if the sample size used for initial estimation is
-        % larger than nremain, print warning message and use nremain
-        out_param.exit=1; %pass a flag
-        meanMC_g_err(out_param); % print warning message
-        out_param.n(i) = out_param.nremain;% update n
-        tmu = gail.evalmean(Yrand,out_param.n(i),npcmax);%evaluate the mean
-        nsofar = nsofar+out_param.n(i);%total sample used
-        break;
-    end
-    out_param.hmu(i) = gail.evalmean(Yrand,out_param.n(i),npcmax);%evaluate mean
-    nsofar = nsofar+out_param.n(i);
-    out_param.nremain = out_param.nremain-out_param.n(i);%update n so far and nremain
-    errtype = 'max';
-    % error type, see the function 'tolfun' at Algoithms/+gail/ directory
-    % for more info
-    theta  = 0;% relative error case
-    deltaplus = (gail.tolfun(out_param.abstol,out_param.reltol,...
-        theta,out_param.hmu(i) - out_param.tol(i),errtype)...
-        +gail.tolfun(out_param.abstol,out_param.reltol,...
-        theta,out_param.hmu(i) + out_param.tol(i),errtype))/2;
-    % a combination of tolfun, which used to decide stopping time
-    if deltaplus >= out_param.tol(i) % stopping criterion
-        deltaminus= (gail.tolfun(out_param.abstol,out_param.reltol,...
-            theta,out_param.hmu(i) - out_param.tol(i),errtype)...
-            -gail.tolfun(out_param.abstol,out_param.reltol,...
-            theta,out_param.hmu(i) + out_param.tol(i),errtype))/2;
-        % the other combination of tolfun, which adjust the hmu a bit
-        tmu = out_param.hmu(i)+deltaminus;
-        break;
+
+if out_param.reltol ==0
+    alphai = 1-(1-out_param.alpha)/(1-alpha_sig);
+    if sig0up == 0; % if the variance is zero, just take n_sigma samples
+        out_param.n = out_param.nSig;
     else
-        out_param.tol(i+1) = min(out_param.tol(i)/2,max(out_param.abstol,...
-            0.95*out_param.reltol*abs(out_param.hmu(i))));
-        i=i+1;
+        toloversig = out_param.abstol/sig0up;
+        % absolute error tolerance over sigma
+        out_param.n = nchebe(toloversig,alphai,out_param.kurtmax);
+        if out_param.n > out_param.nremain;
+            out_param.exit=1; %pass a flag
+            meanMC_g_err(out_param); % print warning message
+            out_param.n = out_param.nremain;% update n
+        end
     end
-    toloversig = out_param.tol(i)/sig0up;%next tolerance over sigma
-    alphai = (out_param.alpha-alpha_sig)/(1-alpha_sig)*2.^(-i);
-    %update the next uncertainty
-    out_param.n(i) = nchebe(toloversig,alphai,out_param.kurtmax);
+    tmu = gail.evalmean(Yrand,out_param.n,npcmax);%evaluate the mean
+    nsofar = nsofar+out_param.n;%total sample used
+else
+    alphai = (out_param.alpha-alpha_sig)/(2*(1-alpha_sig));
+    %uncertainty to do iteration
+    eps1 = ncbinv(out_param.n1,alphai,out_param.kurtmax);
+    %tolerance for initial estimation
+    out_param.tol(1) = sig0up*eps1;
+    %the width of initial confidence interval for the mean
+    i=1;
+    out_param.n(i) = out_param.n1;% initial sample size to do iteration
+    while true
+        out_param.tau = i;%step of the iteration
+        if out_param.n(i) > out_param.nremain;
+            % if the sample size used for initial estimation is
+            % larger than nremain, print warning message and use nremain
+            out_param.exit=1; %pass a flag
+            meanMC_g_err(out_param); % print warning message
+            out_param.n(i) = out_param.nremain;% update n
+            tmu = gail.evalmean(Yrand,out_param.n(i),npcmax);%evaluate the mean
+            nsofar = nsofar+out_param.n(i);%total sample used
+            break;
+        end
+        out_param.hmu(i) = gail.evalmean(Yrand,out_param.n(i),npcmax);%evaluate mean
+        nsofar = nsofar+out_param.n(i);
+        out_param.nremain = out_param.nremain-out_param.n(i);%update n so far and nremain
+        errtype = 'max';
+        % error type, see the function 'tolfun' at Algoithms/+gail/ directory
+        % for more info
+        theta  = 0;% relative error case
+        deltaplus = (gail.tolfun(out_param.abstol,out_param.reltol,...
+            theta,out_param.hmu(i) - out_param.tol(i),errtype)...
+            +gail.tolfun(out_param.abstol,out_param.reltol,...
+            theta,out_param.hmu(i) + out_param.tol(i),errtype))/2;
+        % a combination of tolfun, which used to decide stopping time
+        if deltaplus >= out_param.tol(i) % stopping criterion
+            deltaminus= (gail.tolfun(out_param.abstol,out_param.reltol,...
+                theta,out_param.hmu(i) - out_param.tol(i),errtype)...
+                -gail.tolfun(out_param.abstol,out_param.reltol,...
+                theta,out_param.hmu(i) + out_param.tol(i),errtype))/2;
+            % the other combination of tolfun, which adjust the hmu a bit
+            tmu = out_param.hmu(i)+deltaminus;
+            break;
+        else
+            out_param.tol(i+1) = min(out_param.tol(i)/2,max(out_param.abstol,...
+                0.95*out_param.reltol*abs(out_param.hmu(i))));
+            i=i+1;
+        end
+        toloversig = out_param.tol(i)/sig0up;%next tolerance over sigma
+        alphai = (out_param.alpha-alpha_sig)/(1-alpha_sig)*2.^(-i);
+        %update the next uncertainty
+        out_param.n(i) = nchebe(toloversig,alphai,out_param.kurtmax);
+    end
 end
-    %get the next sample size needed
+%get the next sample size needed
 out_param.ntot = nsofar;%total sample size used
 out_param.time=toc(tstart); %elapsed time
 end
@@ -292,7 +311,7 @@ M3upper = kurtmax^(3/4);
 %the upper bound on the third moment by Jensen's inequality
 BEfun2=@(logsqrtn)gail.stdnormcdf(-exp(logsqrtn).*toloversig)...
     +exp(-logsqrtn).*min(A1*(M3upper+A2), ...
-    A*M3upper./(1+(exp(logsqrtn).*toloversig).^3))-alpha/2; 
+    A*M3upper./(1+(exp(logsqrtn).*toloversig).^3))-alpha/2;
 % Berry-Esseen function, whose solution is the sample size needed
 logsqrtnCLT=log(gail.stdnorminv(1-alpha/2)/toloversig);%sample size by CLT
 nbe=ceil(exp(2*fzero(BEfun2,logsqrtnCLT)));
@@ -339,7 +358,7 @@ if isempty(varargin)
         'Yrand must be specified. Now GAIL is using Yrand =@(n) rand(n,1).^2.')
     Yrand = @(n) rand(n,1).^2;
     %if no values are parsed, print warning message and use the default
-    %random variable    
+    %random variable
 else
     Yrand = varargin{1};
 end
@@ -352,7 +371,7 @@ if validvarargin
 end
 
 if ~validvarargin
-    %if there is only input which is Yrand, use all the default parameters  
+    %if there is only input which is Yrand, use all the default parameters
     out_param.abstol = default.abstol;% default absolute error tolerance
     out_param.reltol = default.reltol; % default relative error tolerance
     out_param.alpha = default.alpha;% default uncertainty
@@ -365,7 +384,7 @@ else
     p = inputParser;
     addRequired(p,'Yrand',@gail.isfcn);
     if isnumeric(in2)
-    %if there are multiple inputs with only numeric, they should be put in order.        
+        %if there are multiple inputs with only numeric, they should be put in order.
         addOptional(p,'abstol',default.abstol,@isnumeric);
         addOptional(p,'reltol',default.reltol,@isnumeric);
         addOptional(p,'alpha',default.alpha,@isnumeric);
@@ -421,35 +440,35 @@ if (out_param.reltol < 0 || out_param.reltol > 1)
         'We will use the default value of the relative error tolerance 1e-1.'])
     out_param.reltol = default.reltol;
 end
-if (out_param.alpha <= 0 ||out_param.alpha >= 1) 
+if (out_param.alpha <= 0 ||out_param.alpha >= 1)
     %uncertainty should be in (0,1)
     warning('MATLAB:meanMCnew_g:alphanotin01',...
         ['The uncertainty should be between 0 and 1; '...
         'We will use the default value 0.01.'])
     out_param.alpha = default.alpha;
 end
-if (out_param.fudge <= 1) 
+if (out_param.fudge <= 1)
     %standard deviation inflation factor should be bigger than 1
     warning('MATLAB:meanMCnew_g:fudgelessthan1',...
         ['The fudge factor should be larger than 1; '...
         'We will use the default value 1.2.'])
     out_param.fudge = default.fudge;
 end
-if (~gail.isposge30(out_param.nSig)) 
+if (~gail.isposge30(out_param.nSig))
     %initial sample size should be a positive integer at least 30
     warning('MATLAB:meanMCnew_g:nsignotposint',...
         ['The number nSig should a positive integer at least 30; '...
         'We will use the default value 1e4.'])
     out_param.nSig = default.nSig;
 end
-if (~gail.isposge30(out_param.n1)) 
+if (~gail.isposge30(out_param.n1))
     %initial sample size should be a posotive integer at least 30
     warning('MATLAB:meanMCnew_g:n1notposint',...
         ['The number n1 should a positive integer at least 30; '...
         'We will use the default value 1e4.'])
     out_param.n1 = default.n1;
 end
-if (out_param.tbudget < 0) 
+if (out_param.tbudget < 0)
     %time budget in seconds should be positive
     warning('MATLAB:meanMCnew_g:timebudgetneg',...
         ['Time budget in seconds should be positive; '...
@@ -457,21 +476,21 @@ if (out_param.tbudget < 0)
     out_param.tbudget = abs(out_param.tbudget);
 end
 
-if (out_param.tbudget == 0) 
+if (out_param.tbudget == 0)
     %time budget in seconds should be positive
     warning('MATLAB:meanMCnew_g:timebudget0',...
         ['Time budget in seconds should be positive rather than zero; '...
         'We will take the default value of time budget 100 seconds'])
     out_param.tbudget = abs(out_param.tbudget);
 end
-if (~gail.isposge30(out_param.nbudget)) 
+if (~gail.isposge30(out_param.nbudget))
     %sample budget should be a large positive integer
     warning('MATLAB:meanMCnew_g:nbudgetnotposint',...
         ['The number of sample budget should be a large positive integer; '...
         'We will use the default value 1e9.'])
     out_param.nbudget =default.nbudget;
 end
-out_param.flag = 1; 
+out_param.flag = 1;
 %pass the signal indicating the parameters have been checked
 end
 
