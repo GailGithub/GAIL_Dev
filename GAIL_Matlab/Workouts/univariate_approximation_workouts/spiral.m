ead3cd357e58c9b3bd6115cb137c49d8f53c4f81
Sou-Cheng Terrya Choi <sou.cheng.terrya.choi@gmail.com>
2015-04-29 10:12:26 -0500

    Fun examples for Yuhan.

---
+++ GAIL_Matlab/Workouts/univariate_approximation_workouts/spiral.m
@@ -0,0 +1,18 @@
+
+a=0;
+b=10*pi;
+f1=@(x) sin(x);
+f2=@(x) cos(x);
+
+[q1,out1]=funappx_g(f1, a, b);
+[q2,out2]=funappx_g(f2, a, b);
+subplot(1,2,1);
+x=a:0.001:b;
+plot3(q1(x),q2(x),x);
+axis square
+
+
+subplot(1,2,2);
+plot3(q1(x),q2(x),abs(q1(x)-f1(x))+abs(q2(x)-f2(x)),'r')
+axis square
+axis tight
