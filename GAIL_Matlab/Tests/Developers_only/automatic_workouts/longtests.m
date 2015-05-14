0ca29633b3cc59c261d04ee7ca5bbbc59f90eed7
Sou-Cheng Terrya Choi <sctchoi@uchicago.edu>
2015-05-14 09:57:43 -0500

    Move ut_cubMC_g from fasttests to longtests.m. It's been taking more than 1000 seconds on nightly tests.

--- GAIL_Matlab/Tests/Developers_only/automatic_workouts/longtests.m
+++ GAIL_Matlab/Tests/Developers_only/automatic_workouts/longtests.m
@@ -300,6 +300,20 @@ catch
 end
 
 try
+    Tests = matlab.unittest.TestSuite.fromClass(?ut_cubMC_g);
+    results=run(ut_cubMC_g)
+    if sum([results.Failed])>0
+        failed=find([results.Failed]>0);
+        for i=1:size(failed,2)
+            fprintf(fid,'%s\n',Tests(failed(i)).Name);
+        end
+    end
+catch
+    display('Error: Test ut_cubMC_g is wrongly coded. We skip it.')
+    fprintf(fid,'Error: Test ut_cubMC_g is wrongly coded. We skip it.\n');
+end
+
+try
     Tests = matlab.unittest.TestSuite.fromClass(?ut_cubMCabs_g);
     results=run(ut_cubMCabs_g)
     if sum([results.Failed])>0
