a396d2c72e095accb1601e34199a976e461c2c2d
Sou-Cheng Terrya Choi <sctchoi@uchicago.edu>
2015-05-20 08:07:20 -0500

    Minor changes.

--- GAIL_Matlab/Workouts/univariate_approximation_workouts/floret.m
+++ GAIL_Matlab/Workouts/univariate_approximation_workouts/floret.m
@@ -1,21 +1,20 @@
-f=@(t) sin(2 * t) .* cos(2 * t);
-t=0:0.0001:2*pi;
-subplot(1,2,1)
-polar(t,f(t),'b')
+f = @(t) 2 * sin(2 * t) .* cos(2 * t);
+h = 0.0001;
+t = 0:h:2*pi;
+subplot(1,3,1)
+polar(t,f(t),'r')
 hold on;
-
-pause(2)
-
-a=0;
-b=2 * pi;
-[q,out]=funappx_g(@(t) sin(2 * t) .* cos(2 * t), a, b);
-polar(t,q(t), 'r')
-norm(q(t)-f(t))
+ 
+subplot(1,3,2)
+a = 0;
+b = 2 * pi;
+[q,out] = funappx_g(f, a, b);
+polar(t,q(t), 'b')
 hold off
 
-subplot(1,2,2)
+subplot(1,3,3)
 err = @(x) abs(f(x) - q(x));
-x=0:0.0001:2*pi;
+x = 0:h:2*pi;
 plot(x,err(x))
 axis square
 axis tight
