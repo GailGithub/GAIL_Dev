f9e142d73439b69aad2625466f12227d524212d5
Yuhan Ding <jinliang98@hotmail.com>
2015-05-22 16:40:18 -0500

    make floret.m as a function

--- GAIL_Matlab/Workouts/univariate_approximation_workouts/floret.m
+++ GAIL_Matlab/Workouts/univariate_approximation_workouts/floret.m
@@ -1,8 +1,10 @@
+function [q,out]=floret
 f = @(t) 2 * sin(2 * t) .* cos(2 * t);
 h = 0.0001;
 t = 0:h:2*pi;
 subplot(1,3,1)
 polar(t,f(t),'r')
+title('Original Polar Plot')
 hold on;
  
 subplot(1,3,2)
@@ -10,6 +12,7 @@ a = 0;
 b = 2 * pi;
 [q,out] = funappx_g(f, a, b);
 polar(t,q(t), 'b')
+title('Approximated Polar Plot')
 hold off
 
 subplot(1,3,3)
@@ -18,4 +21,5 @@ x = 0:h:2*pi;
 plot(x,err(x))
 axis square
 axis tight
+title('Error')
 gail.save_eps('WorkoutFunappxOutput', 'WorkoutFunAppxFloret');
