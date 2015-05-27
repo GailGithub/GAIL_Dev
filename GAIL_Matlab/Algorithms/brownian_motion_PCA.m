c41671cd8b97c008caf09118c415aeea0f736033
Maurício Darós Andrade <maudaros@hotmail.com>
2015-05-27 17:17:22 -0400

    brownian motion PCA

--- GAIL_Matlab/Algorithms/brownian_motion_PCA.m
+++ GAIL_Matlab/Algorithms/brownian_motion_PCA.m
@@ -1,21 +1,17 @@
+function Z = brownian_motion_PCA(N,T)
 %% Brownian Motion by PCA
-
-%%
-% The function brownian_motion_PCA returns a $k$-steps sample of a brownian motion given the following inputs:
+% The function brownian_motion_PCA returns $N$ $k$-steps sample of a brownian motion given the following inputs:
 %
 % * A vector $T=[t_1,t_2,\dots,t_k]$, with $0<t_1<t_2<\dots<t_k$ representing the points in time.
-% * An integer $dim$, which is the number of dimensions.
-function brownian = brownian_motion_PCA(T,dim)
-    Sigma = zeros(length(T)); %Alocating space for the matrix Sigma, which is the matrix of covariances.
-    for i=1:length(T) %Sigma(i,j) = min(T(i),T(j)) = T(min(i,j)) (The last equality is true because T(1)<T(2)<...<T(k).
-        Sigma(i,i:length(T)) = T(i); 
-        Sigma(i:length(T),i) = T(i);
-    end
-    
-    [Eigenvectors,Eigenvalues]=eig(Sigma); %Retrieving the eigenvectors and eigenvalues of the matrix Sigma
-    A = Eigenvectors*Eigenvalues.^(1/2);  %Defining A, which is the matrix responsible for the trasformation of the random variables into the brownian sample
+% * An integer $N$, which is the number of samples.     
 
-    Z=A*randn(length(T),dim);  %Creating dim independent brownian motions, which each one will be a component of the brownian motion path.
-    Z %Finally returns the sample
+Sigma=bsxfun(@min,T',T);  %Sigma(i,j) = min(T(i),T(j))
+
+[Eigenvectors,Eigenvalues]=eig(Sigma); %Retrieving the eigenvectors and eigenvalues of the matrix Sigma
+
+A = Eigenvectors*Eigenvalues.^(1/2);  %Defining A, which is the matrix responsible for the trasformation of the random variables into the brownian sample
+
+Z=randn(N,length(T))*A';  %Creating N independent brownian motions
+
 end
 
