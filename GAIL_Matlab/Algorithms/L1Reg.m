be5c042016ee2af0a7d360ee83255f7042c28d61
Da Li <dli37@hawk.iit.edu>
2015-05-29 17:50:20 -0500

    [combine 4 versions into 1]

--- GAIL_Matlab/Algorithms/L1Reg.m
+++ GAIL_Matlab/Algorithms/L1Reg.m
@@ -9,8 +9,9 @@
 %
 %
 % Last modified: Mar-27-2009
+% modified by TatsuLee : Mar-27-2009 
 
-function x = L1LinearRegression(A,b)
+function x = L1Reg(A,b,abstol)
 
 % Determine size of predictor data
 [n m] = size(A);
@@ -18,15 +19,15 @@ function x = L1LinearRegression(A,b)
 % Initialize with least-squares fit
 x       = A\ b;  % Least squares regression
 xOld    = x;
-xOld(1) = xOld(1) + 1e-4;  % Force divergence
+xOld(1) = xOld(1) + 1e-3;  % Force divergence
 
 % Repeat until convergence
-while (max(abs(x - xOld)) > 1e-8)
+while (max(abs(x - xOld)) > 1e-4)
     % Move old coefficients
     xOld = x;
     
     % Calculate new observation weights (based on residuals from old coefficients)
-    W = sqrt(1 ./ max(abs(( A * xOld) - b),1e-8));  % Floor to avoid division by zero
+    W = sqrt(1 ./ max(abs(( A * xOld) - b), 1e-4));  % Floor to avoid division by zero
     
     % Calculate new coefficients
     x = (repmat(W,[1 m]) .* A) \ (W .* b);
