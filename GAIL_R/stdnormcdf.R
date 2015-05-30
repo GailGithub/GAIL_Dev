f0fbda4bf0d6e2e61cbad5bff6e74af6b91e4401
Luana Amancio Terra <lamancio@hawk.iit.edu>
2015-05-29 20:40:49 -0500

    update stdnormcdf

--- GAIL_R/stdnormcdf.R
+++ GAIL_R/stdnormcdf.R
@@ -1,7 +1,7 @@
 stdnormcdf = function (z) {
   # this function is to define cumulative distribution function (CDF) of the
   # standard normal distribution.
-  p = 0.5*(2*pnorm(-z/sqrt(2)*sqrt(2),lower=FALSE))
+  p = 0.5*(2*pnorm(-z,lower=FALSE))
   # Use the complementary error function, rather than .5*(1+erf(z/sqrt(2))),
   # to produce accurate near-zero results for large negative x.
   return(p)
