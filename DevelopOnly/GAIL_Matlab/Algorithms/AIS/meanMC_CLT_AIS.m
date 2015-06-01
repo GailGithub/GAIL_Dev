c3088e661e91abee37d1971229fa9663f1d63b3a
Mariane de Carvalho Sabarense <msabaren@hawk.iit.edu>
2015-06-01 17:24:08 -0400

    Comment and algorithm update

--- DevelopOnly/GAIL_Matlab/Algorithms/AIS/meanMC_CLT_AIS.m
+++ DevelopOnly/GAIL_Matlab/Algorithms/AIS/meanMC_CLT_AIS.m
@@ -1,24 +1,22 @@
 function [tmu,out_param]=meanMC_CLT_AIS(fx,abstol,alpha,nSig,fudge)
 %MEANMC_CLT Monte Carlo method to estimate the mean of a random variable
 %
-%   tmu = MEANMC_CLT(Yrand,abstol,alpha,nSig,fudge) estimates the mean, mu, of a random variable Y to
+%   tmu = meanMC_CLT(fx,abstol,alpha,nSig,fudge) estimates the mean, of a random fuction to
 %   within a specified error tolerance, i.e., | mu - tmu | <= abstol with
 %   probability at least 1-alpha, where abstol is the absolute error
-%   tolerance.  The default values are abstol=1e-2 and alpha=1%. Input
-%   Yrand is a function handle that accepts a positive integer input n and
-%   returns an n x 1 vector of IID instances of the random variable Y.
+%   tolerance using the adaptive importance sampling.  
+%   The default values are abstol=1e-2 and alpha=1%. Input 'fx' 
+%   is a function handle that accepts a positive integer input n and
+%   returns an n x 1 vector of pseudorandom values drawn
+%   from the standard normal distribution of the random function.
 %
-%   Input Arguments
+%                           Input Arguments
 %
-%     Yrand --- the function for generating n IID instances of a random
-%     variable Y whose mean we want to estimate. Y is often defined as a
+%     fx --- the function for generating n pseudorandom values of a random
+%     variable x whose mean we want to estimate. fx is often defined as a
 %     function of some random variable X with a simple distribution. The
-%     input of Yrand should be the number of random variables n, the output
-%     of Yrand should be n function values. For example, if Y = X.^2 where X
-%     is a standard uniform random variable, then one may define Yrand =
-%     @(n) rand(n,1).^2.
-%     
-%     fx --- defined function
+%     input of fx should be a function handle, the output of fx should be
+%     n function values.      
 %
 %     abstol --- the absolute error tolerance, which should be
 %     positive, default value is 1e-2.
@@ -28,13 +26,13 @@ function [tmu,out_param]=meanMC_CLT_AIS(fx,abstol,alpha,nSig,fudge)
 %
 %     nSig --- the number of samples used to compute the sample variance
 %
-%     fudge --- the standard deviation invlation factor
+%     fudge --- the standard deviation inflation factor
 %
-%   Output Arguments
+%                           Output Arguments
 %
-%     tmu --- the estimated mean of Y.
+%     tmu --- the estimated value of the integral.
 %
-%     out_param.ntot --- total sample used.
+%     out_param.ntot --- total samples used.
 %
 %     out_param.var --- the sample variance.
 %
@@ -42,7 +40,8 @@ function [tmu,out_param]=meanMC_CLT_AIS(fx,abstol,alpha,nSig,fudge)
 %
 
 %This is a heuristic algorithm based on a Central Limit Theorem
-%approximation
+%approximation combined with adaptive importance sampling.
+
 if nargin < 5
    fudge = 1.2; %variance inflation factor
    if nargin < 4;
@@ -52,7 +51,7 @@ if nargin < 5
          if nargin < 2
             abstol = 0.01; %absolute error tolerance
             if nargin < 1
-                fx=@(x)max(x,0);
+                fx=@(x)max(x,0);%example function
             end
          end
       end
@@ -63,26 +62,20 @@ out_param.alpha = alpha; %save the input parameters to a structure
 out_param.fudge = fudge;
 out_param.nSig = nSig;
 
-b=[-1,0,1];
-var_b=b;
-%dens_func = @(b)(1/(sqrt(2.*pi)).*exp(-(b.^2)/2));
-%normal distribution density function
-tstart=tic; %start the clock
+b=[-1,0,1];%estimated values for standard deviation.
+tstart=tic; %starts the clock.
 fx_b=@(t,b_value)fx(t+b_value).*exp(-t.*b_value-b_value.^2/2);
+t_value=(randn(nSig,1));%generate the pseudorandom values drawn
+% from the standard normal distribution
+
 for i=1:numel(b)
     b_value=b(i);
-    var_b(i)=var(fx_b(randn(nSig,1),b_value));
+    var_b(i)=var(fx_b(t_value,b_value));
 end
 
-%d_value=dens_func(b);
-    %Yval = x_values(nSig);% get samples to estimate variance 
-    %fx=max(Yval,0);
-    %integrand=fx*d_value;
-    %u=mean(integrand);
-    %variance=var(integrand);
 [S_var,S_pos]=min(var_b);
 out_param.var = S_var; %calculate the sample variance--stage 1
-out_param.b_value=b(S_pos);
+out_param.b_value=b(S_pos);%best variance
 sig0 = sqrt(out_param.var); %standard deviation
 sig0up = out_param.fudge.*sig0; %upper bound on the standard deviation
 nmu = max(1,ceil((-norminv(alpha)*sig0up/abstol).^2)); 
@@ -94,25 +87,6 @@ out_param.ntot=nSig+nmu; %total samples required
 out_param.time=toc(tstart); %elapsed time    
 
 
-%The real value can be calculated through the integral of the following
-%expression between the interval [-3 3]:
 
-%f(x)*p(x)=x.*(1./(2.*pi)).*exp(-(x.^2)/2)
-
-% integral = (-exp(-(x^2)/2))/(2*pi)
-
-%The result for the interval specified is equal to zero.
-
-
-%out_param.var = var(integrand); %calculate the sample variance--stage 1
-%sig0 = sqrt(out_param.var); %standard deviation
-%sig0up = out_param.fudge.*sig0; %upper bound on the standard deviation
-%nmu = max(1,ceil((-norminv(alpha)*sig0up/abstol).^2)); 
-   %number of samples needed for mean
-%assert(nmu<nMax,['nmu = ' int2str(nmu) ', which is too big']) 
-   %don't exceed sample budget
-%tmu=mean(Yrand(nmu)); %estimated mean
-%out_param.ntot=nSig+nmu; %total samples required
-toc(tstart); %elapsed time
 end
 
