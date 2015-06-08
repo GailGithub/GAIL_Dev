d1725ffd8b8c25671cba49507537768de87f6be6
Fred J. Hickernell <hickernell@iit.edu>
2015-06-08 14:43:40 -0500

    modified publishMathJax to restore existing format loose/compact

--- GAIL_Matlab/Documentation/publishMathJax.m
+++ GAIL_Matlab/Documentation/publishMathJax.m
@@ -1,16 +1,20 @@
 function publishMathJax(filename,varargin)
 % publish MATLAB scripts using the XSL file supporting MathJax
 if nargin >= 3
-   formatcompact = varargin{2};
+   formatCompact = varargin{2};
 else
-   formatcompact = true;
+   formatCompact = true;
    if nargin >= 2
       opts=varargin{1};
    end
 end   
-if formatcompact
+formatNow = get(0,'FormatSpacing'); %save existing format
+if formatCompact
    format compact
+else
+   format loose
 end
 opts.stylesheet = 'mxdom2mathjaxbigfont.xsl';
 publish(filename,opts);
+set(0,'FormatSpacing',formatNow); %restore existing format
 
