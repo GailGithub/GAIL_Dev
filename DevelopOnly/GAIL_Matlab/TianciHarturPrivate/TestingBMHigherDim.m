cd80c1a5239f59dc5ce38953e40a11cf9ea6879b
Hartur Santi <hsanti@hawk.iit.edu>
2015-07-02 11:32:26 -0500

    Deleting unused files.

--- DevelopOnly/GAIL_Matlab/TianciHarturPrivate/TestingBMHigherDim.m
+++
@@ -1,7 +0,0 @@
-%Test 
-
-inp.timeDim.dim = 2;
-
-tianci = brownianMotion(inp)
-
-genPaths(tianci,4)
--- DevelopOnly/GAIL_Matlab/TianciHarturPrivate/Tests/vanillaPrice.m
+++
@@ -1,8 +0,0 @@
-function [ vanillaCall, vanillaPut ] = vanillaPrice( S0, K, r, sigma, T, weight )
-% vanillaPrice is a function that calculates the price of vanilla option
-d1 = (log(S0/K)+T.*(r+(sigma.^2)/2))./(sigma.*sqrt(T));
-d2 = (log(S0/K)+T.*(r+(sigma.^2)/2))./(sigma.*sqrt(T))-(sigma.*sqrt(T));
-vanillaCall = weight*(S0.*normcdf(d1) - K.*exp(-r.*T).*normcdf(d2));
-vanillaPut = weight*(K*exp(-r.*T)-S0+(S0.*normcdf(d1) - K*exp(-r.*T).*normcdf(d2)));
-end
-
