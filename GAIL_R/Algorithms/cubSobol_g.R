f1498e241c0a6a38fe654aa259e87f3321d14cba
Anthony Karahalios <akaraha1@jhu.edu>
2015-07-29 14:55:21 -0500

    Include parameter function for cubSobol_g.R but still without input parameter checks

--- GAIL_R/Algorithms/cubSobol_g.R
+++ GAIL_R/Algorithms/cubSobol_g.R
@@ -233,21 +233,21 @@ time1 = proc.time()
 ##Initial important cone factors and Check-initialize parameters
   
 r_lag = 4; #distance between coefficients summed and those computed
-hyperbox = matrix(c(0,1))
-measure <- "uniform"
-abstol = 1e-4
-reltol = 1e-2
-mmin = 10
-mmax = 24
-fudge = function(m) {5*2^(-m)}
-toltype = 'max'
-theta = 1
+#hyperbox = matrix(c(0,1))
+#measure <- "uniform"
+#abstol = 1e-4
+#reltol = 1e-2
+#mmin = 10
+#mmax = 24
+#fudge = function(m) {5*2^(-m)}
+#toltype = 'max'
+#theta = 1
+#out_param=c("measure"=measure,"abstol"=abstol,"reltol"=reltol,"mmin"=mmin,"mmax"=mmax,
+ #             "fudge"=fudge,"toltype"=toltype,"theta"=theta, "d"=out_param.d);
+#check <- list("out_param"=out_param, "hyperbox"=hyperbox)
+source("cubSobol_g_param.R")
+check = cubSobol_g_param(r_lag,hyperbox,measure,abstol,reltol,mmin,mmax,fudge,toltype,theta) #Get the parameters
 out_param.d = size(hyperbox,2);
-out_param=c("measure"=measure,"abstol"=abstol,"reltol"=reltol,"mmin"=mmin,"mmax"=mmax,
-              "fudge"=fudge,"toltype"=toltype,"theta"=theta, "d"=out_param.d);
-check <- list("out_param"=out_param, "hyperbox"=hyperbox)
-#check = cubSobol_g_param(r_lag,hyperbox,measure,abstol,reltol,mmin,mmax,fudge,toltype,theta) #Get the parameters
-
 
 l_star = check$out_param$mmin - r_lag; # Minimum gathering of points for the sums of DFWT
   
