239f7f2b3b3f6b430bf52aa8e523e70c92415529
YuhanDing <jinliang98@hotmail.com>
2015-10-10 13:39:14 -0500

    merge *CSC files to original files

--- GAIL_Matlab/Workouts/univariate_approximation_workouts/funappx_g_gui.m
+++ GAIL_Matlab/Workouts/univariate_approximation_workouts/funappx_g_gui.m
@@ -1,13 +1,13 @@
 function [fappx,npoints,errest] = funappx_g_gui(f,a,b,tol,nlo,nhi)
-%funappx_g_gui  Demonstrate numerical approximation of an univaraite function.
-%   fappx = FUNAPPX_G_GUI(f,a,b) shows the steps in approximating the function
+%funappx_g_gui Demonstrate numerical approximation of an univaraite function.
+%   fappx = funappx_g_gui(f,a,b) shows the steps in approximating the function
 %   f(x) from a to b by locally adaptive guaranteed method.
 %   The color switches to green when the desired accuracy is obtained.
 %
-%   fappx = FUNAPPX_G_GUI(f,a,b,tol) uses the given tolerance instead of 1.e-3
+%   fappx = funappx_g_gui(f,a,b,tol) uses the given tolerance instead of 1.e-3
 %   and returns an approximated function fappx%
 %
-%   [fappx, npoints] = FUNAPPX_G_GUI(f,...) also gives the number points
+%   [fappx, npoints] = funappx_g_gui(f,...) also gives the number points
 %   needed of approximation.
 %
 %   Examples:
@@ -23,6 +23,8 @@ function [fappx,npoints,errest] = funappx_g_gui(f,a,b,tol,nlo,nhi)
 shg
 clf reset
 MATLABVERSION= gail.matlab_version;
+
+warning('off', 'GAIL:funappx_g:exceediter');
 
 % Default tolerance
 if nargin < 5
@@ -83,144 +85,25 @@ q(2) = uicontrol('string','auto', ...
      'units','normal','pos',[.85 .02 .08 .04], ...
     'callback','set(gcf,''userdata'',2)');
 
-index = [1 ninit];
+%index = [1 ninit];
 % initialize nstar
 %nstar = ninit - 2;
 %nstar = floor(ninit/2);
 % initialize error
 err = tol + 1;
+
+
+ 
 while(max(err) > tol)
-    % length of each subinterval
-    len = x(index(2:end))-x(index(1:end-1));
-    reshapey = reshape(y(1:end-1),ninit - 1, (index(end) - 1)/(ninit -1));
-    diffy = diff([reshapey;y(index(2:end))]);
-    %approximate the weaker norm of input function at different subinterval
-    %gn = (ninit-1)./len.*max(abs(diffy-repmat((y(index(2:end))-y(index(1:end-1)))/(ninit-1),ninit-1,1)),[],1);
-    gn = (ninit-1)./len.*max(abs(bsxfun(@minus,diffy,(y(index(2:end))-y(index(1:end-1)))/(ninit-1))),[],1);
-    %approximate the stronger norm of input function at different subinterval
-    fn = (ninit-1)^2./(len.^2).*max(abs(diff(diffy)),[],1);
-    %update cone condition every iteration
-    nstar=ceil(nhi*(nlo/nhi).^(1./(1+len)));
-    
-    %find nstar not large enough then double it
-    smallconeind = find(nstar.*(2*gn+fn.*len/(ninit-1)) <(fn.*len));
-    nstar(smallconeind) = 2*nstar(smallconeind);
-    
-    err = nstar.*len.*gn./(4*(ninit-1).*(ninit-1-nstar));
-    %check if error satisfy the error tolerance
-    %     counterr = sum(err > abstol);
-    %     if(length(x) + counterr *(ninit -1) > out_param.nmax)
-    %         out_param.exit(1) = 1;
-    %         warning('GAIL:funappx_g:exceedbudget',' funappx_g attempted to exceed the cost budget. The answer may be unreliable.')
-    %         break;
-    %     end;
     if max(err) > tol;
-        %flag sub interval error not satisfy error tolerance 1 in whbad
-        whbad = err > tol;
-        %add index for bad sub interval
-        badind = find(whbad == 1);
-        %flag sub interval error satisfy error tolerance 1 in whgood
-        whgood = (whbad ==0);
-        %add index for good sub interval
-        goodind = find(whgood == 1);
-        %find # of new sub intervals need to be added at each sub
-        %interval
-        badcumsum = cumsum(whbad);
-        %pickup # of new sub intervals at bad intervals
-        cumbad = badcumsum(badind);
-        %generate new index of sub intervals splitted from bad intervals
-        newindex = [badind + [0 cumbad(1:end-1)]; badind + cumbad];
-        newindex = newindex(:)';
-        %find the length of each sub interval
-        h = len/2/(ninit-1);
-        %reshape x without end point to a matrix of ninit-1 by # of intervals
-        reshapex =  reshape(x(1:end-1),ninit -1,(index(end) - 1)/(ninit -1));
-        %generate new points newx need to be added
-        %newx = reshapex(:,badind) + repmat(h(badind),ninit-1,1);
-        newx = bsxfun(@plus,reshapex(:,badind),h(badind));
-        %compute value newy of newx
-        newy = f(newx);
-        %initialize a zero matrix of 2*(ninit-1) by # of bad sub intervals
-        %to store all the points after splitting bad sub intervals
-        badmatrix = zeros(2*(ninit-1),sum(whbad));
-        %insert x at bad sub intervals in badmatrix as the row 1,
-        %3,..., end-1
-        badmatrix(1:2:end-1,:) = reshapex(:,badind);
-        %insert newx at bad sub intervals in badmatrix as the row 2,
-        %4,..., end
-        badmatrix(2:2:end,:) = newx;
-        %reshape badmatrix to the size of ninit -1 by 2*# of bad sub
-        %intervals
-        badmatreshape = reshape(badmatrix, ninit - 1, 2*sum(whbad));
-        %initialize a matrix of ninit - 1 by # of sub intervals after
-        %splitting bad sub intervals for x
-        newreshapex = zeros(ninit - 1, 2*sum(whbad)+sum(whgood));
-        %insert all the points after splitting bad sub intervals to correct
-        %column
-        newreshapex(:,newindex) = badmatreshape;
-        %insert all the points on good sub intervals to correct column
-        newreshapex(:,goodind + badcumsum(goodind)) = reshapex(:,goodind);
-        %obtain all the points in vector x
-        x = [newreshapex(:)' x(end)];
-        %insert y at bad sub intervals in badmatrix as the row 1,
-        %3,..., end-1
-        badmatrix(1:2:end-1,:) = reshapey(:,badind);
-        %insert newy at bad sub intervals in badmatrix as the row 2,
-        %4,..., end
-        badmatrix(2:2:end,:) = newy;
-        %reshape badmatrix to the size of ninit -1 by 2*# of bad sub
-        %intervals
-        badmatreshape = reshape(badmatrix, ninit - 1, 2*sum(whbad));
-        %initialize a matrix of ninit - 1 by # of sub intervals after
-        %splitting bad sub intervals for y
-        newreshapey = zeros(ninit - 1, 2*sum(whbad)+sum(whgood));
-        %insert all the values after splitting bad sub intervals to correct
-        %column
-        newreshapey(:,newindex) = badmatreshape;
-        %insert all the original y on good sub intervals to correct column
-        newreshapey(:,goodind + badcumsum(goodind)) = reshapey(:,goodind);
-        %obtain all the values in vector y
-        y = [newreshapey(:)' y(end)];
-        
-        %generate error for new sub intervals
-        %initialize a vertor of # of sub intervals after splitting
-        newerr = zeros(1,2*sum(whbad)+sum(whgood));
-        %use the same error for splitted bad interval
-        baderr = [err(badind); err(badind)];
-        %insert error after splitting bad sub intervals to correct
-        %position
-        newerr(newindex)=baderr(:)';
-        newerr(goodind + badcumsum(goodind)) = err(goodind);
-        %obtain error for all sub intervals
-        err = newerr;
-        %upadte index w.p.t x after splitting
-        %update index of the original endpoints
-        index(2:end) = index(2:end) + badcumsum*(ninit-1);
-        %obtain the index of new endpoins after splitting
-        %if one interval not splitted, will get the same index as in
-        %previous line
-        indexbeg = index(1:end-1) + whbad*(ninit-1);
-        %combine two index together and emlinate duplicate indices
-        indexnew = [index(1:end-1); indexbeg];
-        indexnew = indexnew(:)';
-        index = unique([indexnew index(end)]);
-        %        tt = length(index)-1;
+        [~,out_param] =funappx_g(f,a,b,tol,nlo,nhi,'maxiter',k+1);
+        err = out_param.errest;
+        npoints = out_param.npoints;
+        x = out_param.x;
+        y = f(x);
         k = k + 1;
-          p = flipud(get(gca,'children'));
-        %         p = flipud(allchild(gca));
+        p = flipud(get(gca,'children'));
         set(p(1),'xdata',x,'ydata',y)
-        %         for i =1:tt;
-        %             if err(i) < tol;
-        %                 color = [0 .5 0];
-        %             else
-        %                 color = [.6 .6 .6];
-        %             end
-        %             left = index(i);
-        %             right = index(i+1);
-        %             u = [x(left) x(left:right) x(right)];
-        %             v = [0 y(left:right) 0];
-        %             set(p(i+1),'xdata',u,'ydata',v,'facecolor',color)
-        %         end;
         set(gca,'xtick',x,'xticklabel',[]);
         hTitle=title(['error estimation is ' num2str(max(err)) ' in iteration ' num2str(k)]);
         set(hTitle,'FontSize',25)
@@ -235,7 +118,8 @@ while(max(err) > tol)
         k = k + 1;
         break;
     end;
-end;
+end
+
 p = flipud(get(gca,'child'));
 set(p(1),'xdata',x,'ydata',y)
 set(gca,'xtick',x,'xticklabel',[]);
@@ -248,7 +132,7 @@ end
 if get(gcf,'userdata') == 1
     set(gcf,'userdata',0)
 end
-npoints = index(end);
+%npoints = index(end);
 if MATLABVERSION >= 8.3
     fappx = griddedInterpolant(x,y,'linear');
 else
@@ -258,12 +142,13 @@ end;
 errest = max(err);
 %delete(p)
 delete(q);
+warning('on', 'GAIL:funappx_g:exceediter');
 
 hold on;
 delta = 0.00001;
 x=a:delta:b; 
 plot(x,f(x));
 hold off;
-gail.save_eps('WorkoutFunappxOutput', 'funappx_g_gui');
+gail.save_eps('WorkoutFunappxOutput', 'funappx_g_gui_CSC');
 
 % ---------------------------------------------------------
