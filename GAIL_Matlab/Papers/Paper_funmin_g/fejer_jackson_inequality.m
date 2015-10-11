7252542799ae1fbb2989b277ee75b1131b9c17b8
Sou-Cheng Terrya Choi <sctchoi@uchicago.edu>
2015-10-11 10:28:48 -0500

    Refined two demos for funmin_g

--- GAIL_Matlab/Papers/Paper_funmin_g/fejer_jackson_inequality.m
+++ GAIL_Matlab/Papers/Paper_funmin_g/fejer_jackson_inequality.m
@@ -1,5 +1,8 @@
 function [fmin, fmax, out_min, out_max] = fejer_jackson_inequality(m)
-%  
+% The Fejer-Jackson inequality says that the following partial sum is 
+% positive for a given positive integer n and for x in (0, pi): 
+%     fnx = @(n,x)((1./(1:n)) * sin((1:n)' * x));
+%
 % Example 1:
 % >> [fmin, fmax] =  fejer_jackson_inequality(32);
 % >> abs(fmin) < 1e-6, abs(fmax-1.804097) < 1e-6
@@ -11,7 +14,7 @@ function [fmin, fmax, out_min, out_max] = fejer_jackson_inequality(m)
 %         http://www.chebfun.org/examples/fourier/FejerJackson.html
 
 
-fnx = @(n,x)(sin(x'*(1:n))*(1./(1:n))')';
+fnx = @(n,x)((1./(1:n)) * sin((1:n)' * x));
 f = @(x) fnx(m,x);
 [fmin,out_min]=funmin_g(f,0,pi);
 
--- GAIL_Matlab/Papers/Paper_funmin_g/funmin_g_demo.m
+++ GAIL_Matlab/Papers/Paper_funmin_g/funmin_g_demo.m
@@ -1,27 +1,36 @@
-function [fmin, out] = funmin_g_demo(f,a,b)
+function funmin_g_demo(fmin, out_param)
 % 
 % Example 1: f is a flat bottom function. Cf. Wolfram Alpha's result at 
 % tinyurl.com/ox63f2c
 %
-% >> f = @(x) 1./sqrt(1-x.^4);  a = -0.5; b = 0.5; 
-% >> [fmin, out] = funmin_g_demo(f,a,b);
-%    ***
-% >> abs(fmin-1) < 1e-6  
+% >> f = @(x) 1./sqrt(1-x.^4);  a = -0.2; b = 0.2; tol = 1e-15;
+% >> [fmin, out] = funmin_g(f,a,b,tol,tol);
+% >> abs(fmin-1) < 1e-6
 %    ans = 1 
+% >> funmin_g_demo(fmin, out)
+%    intlen = ***e-04 fval1 = ***e-16 fval2 = ***e-16
 %
-%
-% 
-  
-[fmin, out] = funmin_g(f,a,b);
 
+  
+a = out_param.a;
+b = out_param.b;
+f = out_param.f;
+h = 1.0/out_param.nmax;
 
 h = 0.00001;
 x = a:h:b;
 plot(x,f(x));
 
-if ~isempty(out.intervals)
+if ~isempty(out_param.intervals)
     hold on;
-    plot(mean(out.intervals), fmin, 'bo')
+    if out_param.volumeX > out_param.TolX,
+        intlen = out_param.intervals(2) - out_param.intervals(1)
+        fval1 = f(out_param.intervals(2)) - fmin
+        fval2 = f(out_param.intervals(1)) - fmin
+        plot(out_param.intervals(1), fmin, 'r<', 'MarkerSize', 12,'LineWidth',2)
+        plot(out_param.intervals(2), fmin, 'r>', 'MarkerSize', 12,'LineWidth',2)
+    end
+    plot(mean(out_param.intervals),  fmin, 'ro', 'MarkerSize', 12,'LineWidth',2)
     hold off;
 end
 
