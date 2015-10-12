bcd819c91046fc1b990e9c516bcdbdab0034e46d
Sou-Cheng Terrya Choi <sctchoi@uchicago.edu>
2015-10-12 07:53:33 -0500

    Minor changes.

--- GAIL_Matlab/Papers/Paper_funmin_g/fejer_jackson_inequality.m
+++ GAIL_Matlab/Papers/Paper_funmin_g/fejer_jackson_inequality.m
@@ -4,10 +4,11 @@ function [fmin, fmax, out_min, out_max] = fejer_jackson_inequality(m)
 %     fnx = @(n,x)((1./(1:n)) * sin((1:n)' * x));
 %
 % Example 1:
-% >> [fmin, fmax] =  fejer_jackson_inequality(32);
+% >> [fmin, fmax, out_min, out_max] =  fejer_jackson_inequality(32);
 % >> abs(fmin) < 1e-6, abs(fmax-1.804097) < 1e-6
 %  ans = 1  ans = 1
-%
+% >>  funmin_g_demo(fmin, out_min); hold on; funmin_g_demo(fmax, out_max); hold off;
+%    ***intlen = 0.00***
 %
 %
 % Source: Fejer-Jackson inequality, Nick Trefethen
@@ -16,23 +17,12 @@ function [fmin, fmax, out_min, out_max] = fejer_jackson_inequality(m)
 
 fnx = @(n,x)((1./(1:n)) * sin((1:n)' * x));
 f = @(x) fnx(m,x);
-[fmin,out_min]=funmin_g(f,0,pi);
+[fmin,out_min]=funmin_g(f,0,pi); % Why is 0 not in out_min.intervals?
 
 g = @(x) fnx(m,x)*-1;
 [fmax,out_max]=funmin_g(g,0,pi);
 fmax=-1*fmax;
+out_max.f = f;
 
-h = 0.000001;
-x = 0:h:pi;
-plot(x,f(x))
 
-if ~isempty(out_min.intervals)
-    hold on;
-    plot(mean(out_min.intervals), fmin, 'bo')
-    hold off;
-end
-if ~isempty(out_max.intervals)
-    hold on;
-    plot(mean(out_max.intervals), fmax, 'bo')
-    hold off;
-end
+
