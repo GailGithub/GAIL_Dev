0c908469353eff46c4dd2274895b2771e0000ef0
Sou-Cheng Terrya Choi <sctchoi@uchicago.edu>
2015-10-13 10:46:18 -0500

    Simplify code using Xin's technique.

--- GAIL_Matlab/Papers/Paper_funmin_g/flat_bottom.m
+++ GAIL_Matlab/Papers/Paper_funmin_g/flat_bottom.m
@@ -8,19 +8,10 @@ funmin_g_demo(fmin,out_min)
 intervals = out_min.intervals % gives [ 0.234036344744525 0.765963655255475 ]
 x = out_min.x;
 
-%k = 9
-idx1 = find(x<=out_min.intervals(1));
-k = length(find(abs(f(x(idx1)) - fmin) < out_min.abstol)) - 1;
-xx1 = x(idx1(end-k:end)); % These points xx are not in out_min.intervals
-yy1 = f(xx1);     
-errors1 = prod(abs(yy1 - fmin) < out_min.abstol) % but abs(f(xx) - fmin) < abstol
-
-%k = 10
-idx2 = find(x>=out_min.intervals(2));
-k = length(find(abs(f(x(idx2)) - fmin) < out_min.abstol));
-xx2 = x(idx2(1:k)); % These points xx are not in out_min.intervals
-yy2 = f(xx2);          
-errors2 = prod(abs(yy2 - fmin) < out_min.abstol) % but abs(f(xx) - fmin) < abstol
-
+y = f(x);
+[~, index] = find(abs(y - fmin) < out_min.abstol);
+leftint = find([1 diff(index)~=1]);
+rightint = find([diff(index)~=1 1]);
+[x(index(leftint)), x(index(rightint))] 
 % Shouldn't out_min.intervals be [0.231060606060606   0.768939393939394]
-[xx1(1), xx2(end)] 
+ 
