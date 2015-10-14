5782831003752428d264f152f634037252565a55
Sou-Cheng Terrya Choi <sctchoi@uchicago.edu>
2015-10-14 11:36:24 -0500

    Test for funmin_g_CSC

--- GAIL_Matlab/Papers/Paper_funmin_g/fasttests_funmin_g_CSC.m
+++ GAIL_Matlab/Papers/Paper_funmin_g/fasttests_funmin_g_CSC.m
@@ -10,3 +10,24 @@ doctest fejer_jackon_inequality
 results=run(ut_funmin_g_CSC)
 
 results=run(ut_workout_funmin_g_CSC)    % time-consuming
+
+run(ut_workout_funmin_g_CSC)   
+% Running ut_workout_funmin_g_CSC
+%  
+%         Probability    Success   Success   Failure  Failure
+%  tau      In Cone    No Warning  Warning No Warning Warning
+%    11  1.20%->55.30%   46.30%      3.70%   44.60%    5.40% 
+%   101 32.30%->73.60%   65.90%      3.20%   26.40%    4.50% 
+%  1001 66.50%->92.70%   81.90%      4.80%    7.30%    6.00% 
+% . 
+%         Probability    Success   Success   Failure  Failure
+%  tau      In Cone    No Warning  Warning No Warning Warning
+%    11  1.20%->21.20%   21.20%      0.00%   78.80%    0.00% 
+%   101 34.20%->52.40%   52.40%      0.00%   47.60%    0.00% 
+%  1001 66.00%->85.30%   80.90%      4.40%   14.70%    0.00% 
+% . 
+%         Probability    Success   Success   Failure  Failure
+%  tau      In Cone    No Warning  Warning No Warning Warning
+%    11  2.20%->19.40%   93.60%      0.00%    6.40%    0.00% 
+%   101 34.30%->53.60%   95.70%      0.00%    4.30%    0.00% 
+%  1001 67.80%->85.30%   96.90%      0.00%    3.10%    0.00% 
