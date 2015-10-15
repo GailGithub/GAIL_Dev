9caba0248b8656324f67966918d14d94f8715064
Sou-Cheng Terrya Choi <sctchoi@uchicago.edu>
2015-10-15 08:03:41 -0500

    Revert back ut_funmin_g. Accidental changes.

--- GAIL_Matlab/Tests/ut_funmin_g.m
+++ GAIL_Matlab/Tests/ut_funmin_g.m
@@ -1,9 +1,9 @@
-%UT_FUNMIN_G_CSC unit test for funmin_g_CSC_CSC
-classdef ut_funmin_g_CSC < matlab.unittest.TestCase
+%UT_FUNMIN_G unit test for funmin_g
+classdef ut_funmin_g < matlab.unittest.TestCase
     
     methods(Test)
            
-        function funmin_g_CSCEXM1(testCase)
+        function funmin_gEXM1(testCase)
             in_param.abstol = 10^(-8); 
             in_param.TolX = 0;
             in_param.nmax = 10^7; 
@@ -11,14 +11,14 @@ classdef ut_funmin_g_CSC < matlab.unittest.TestCase
             z = 0.86;
             f = @(x) 0.5/a^2.*(-4*a.^2-(x-z).^2-(x-z-a).*abs(x-z-a)...
                 +(x-z+a).*abs(x-z+a)).*(x>=z-2*a).*(x<=z+2*a);
-            [fmin, result] = funmin_g_CSC(f,in_param); 
+            [fmin, result] = funmin_g(f,in_param); 
             actualmin = -1;
             actualerr = abs(actualmin-fmin);
             testCase.verifyLessThanOrEqual(actualerr,in_param.abstol);
             testCase.verifyLessThanOrEqual(result.npoints,in_param.nmax);
         end
         
-        function funmin_g_CSCEXM2(testCase)
+        function funmin_gEXM2(testCase)
             in_param.abstol = 0; 
             in_param.TolX = 10^(-5);
             in_param.nmax = 10^6; 
@@ -26,7 +26,7 @@ classdef ut_funmin_g_CSC < matlab.unittest.TestCase
             z = 0.33;
             f = @(x) 0.5/a^2.*(-4*a.^2-(x-z).^2-(x-z-a).*abs(x-z-a)...
                 +(x-z+a).*abs(x-z+a)).*(x>=z-2*a).*(x<=z+2*a);
-            [fmin, result] = funmin_g_CSC(f,in_param); 
+            [fmin, result] = funmin_g(f,in_param); 
             intnum = size(result.intervals,2);
             exactsolu = z;
             testflag = 1;
@@ -41,7 +41,7 @@ classdef ut_funmin_g_CSC < matlab.unittest.TestCase
             testCase.verifyLessThanOrEqual(result.npoints,in_param.nmax);
         end
         
-        function funmin_g_CSCEXM3(testCase)
+        function funmin_gEXM3(testCase)
             in_param.abstol = 10^(-8); 
             in_param.TolX = 10^(-6);
             in_param.nmax = 10^7; 
@@ -49,7 +49,7 @@ classdef ut_funmin_g_CSC < matlab.unittest.TestCase
             z = 0.77;
             f = @(x) 0.5/a^2.*(-4*a.^2-(x-z).^2-(x-z-a).*abs(x-z-a)...
                 +(x-z+a).*abs(x-z+a)).*(x>=z-2*a).*(x<=z+2*a);
-            [fmin, result] = funmin_g_CSC(f,in_param); 
+            [fmin, result] = funmin_g(f,in_param); 
             actualmin = -1;
             actualerr = abs(actualmin-fmin);
             intnum = size(result.intervals,2);
@@ -68,7 +68,7 @@ classdef ut_funmin_g_CSC < matlab.unittest.TestCase
             testCase.verifyLessThanOrEqual(result.npoints,in_param.nmax);
         end
         
-        function funmin_g_CSCEXM4(testCase)
+        function funmin_gEXM4(testCase)
             in_param.abstol = 0; 
             in_param.TolX = 10^(-5);
             in_param.nmax = 10^6; 
@@ -76,7 +76,7 @@ classdef ut_funmin_g_CSC < matlab.unittest.TestCase
             a2=1; b2=10; c2=0.5+0.5*rand(1,1);
             f=@(x) -a1*exp(-(b1*(x-c1)).^2)-a2*exp(-(b2*(x-c2)).^2);
             exactsolu = fminbnd(f,0,(c1+c2)/2,optimset('TolX',in_param.TolX*10^(-2)));
-            [fmin, result] = funmin_g_CSC(f,in_param); 
+            [fmin, result] = funmin_g(f,in_param); 
             intnum = size(result.intervals,2);
             testflag = 1;
             for k=1:intnum
@@ -90,14 +90,14 @@ classdef ut_funmin_g_CSC < matlab.unittest.TestCase
             testCase.verifyLessThanOrEqual(result.npoints,in_param.nmax);
         end
         
-        function funmin_g_CSCEXM5(testCase)
+        function funmin_gEXM5(testCase)
             in_param.abstol = 0; 
             in_param.TolX = 10^(-5);
             in_param.nmax = 10^6; 
             f=@(x) cos(4*pi*(x-0.2));
             exactsolu1 = 0.45;
             exactsolu2 = 0.95;
-            [fmin, result] = funmin_g_CSC(f,in_param); 
+            [fmin, result] = funmin_g(f,in_param); 
             intnum = size(result.intervals,2);
             testflag1 = 1;
             testflag2 = 1;
@@ -117,7 +117,7 @@ classdef ut_funmin_g_CSC < matlab.unittest.TestCase
             testCase.verifyLessThanOrEqual(result.npoints,in_param.nmax);
         end
         
-        function funmin_g_CSCEXM6(testCase)
+        function funmin_gEXM6(testCase)
             in_param.abstol = 0; 
             in_param.TolX = 10^(-3);
             in_param.nmax = 10^6; 
@@ -129,7 +129,7 @@ classdef ut_funmin_g_CSC < matlab.unittest.TestCase
             exactsolu3 = -0.5;
             exactsolu4 = 1.5;
             exactsolu5 = 3.5;
-            [fmin, result] = funmin_g_CSC(f,in_param); 
+            [fmin, result] = funmin_g(f,in_param); 
             intnum = size(result.intervals,2);
             testflag = ones(5,1);
             for k=1:intnum
@@ -159,13 +159,13 @@ classdef ut_funmin_g_CSC < matlab.unittest.TestCase
             testCase.verifyLessThanOrEqual(result.npoints,in_param.nmax);
         end  
         
-        function funmin_g_CSCEXM7(testCase)
+        function funmin_gEXM7(testCase)
             in_param.abstol = 10^(-3); 
             in_param.TolX = 0;
             in_param.nmax = 10^7;  
             z = 0.5;
             f = @(x) exp(-1./((x-z).^2)); % flat bottom
-            [fmin,result] = funmin_g_CSC(f,in_param);
+            [fmin,result] = funmin_g(f,in_param);
             testCase.verifyLessThanOrEqual(result.errest,in_param.abstol);
             testCase.verifyLessThanOrEqual(result.npoints,in_param.nmax);
             v=sqrt(-1/(log(in_param.abstol)));
