2974957d40d067340f47706a7ede66f0cbddcbbd
Sou-Cheng Terrya Choi <sctchoi@uchicago.edu>
2016-01-17 14:41:34 -0600

    Add one more unit test for funmin_g

--- GAIL_Matlab/Tests/ut_funmin_g_end.m
+++ GAIL_Matlab/Tests/ut_funmin_g_end.m
@@ -38,6 +38,21 @@ classdef ut_funmin_g_end < matlab.unittest.TestCase
             testCase.verifyEqual(actualminima1,out.intervals(1,1));
             testCase.verifyEqual(actualminima2,out.intervals(2,end));
         end
+        
+        function funmin_gEXM5(testCase)
+            % minimum appears at  left end
+            f=@(x) -(x-1).^2+1;
+            tol = 1e-7;
+            Xtol = 1e-4;
+            [fmin,out] = funmin_g(f,0,1,tol,Xtol,10,10,1000000);
+            fmin_true=0;
+            xmin_true=1;
+            ferror = abs(fmin - fmin_true);
+            xerror = abs(mean(out.intervals) - xmin_true);
+            check = ferror < tol || xerror < Xtol;
+            testCase.verifyEqual(check, true);
+        end
+        
      
     end
 end
