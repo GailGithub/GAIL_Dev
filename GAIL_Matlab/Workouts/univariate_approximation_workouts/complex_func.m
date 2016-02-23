34cb5061ef8532ef5fa38c7c0072a1291ad0515e
Sou-Cheng Terrya Choi <sctchoi@uchicago.edu>
2016-02-23 09:19:05 -0600

    funappxNoPenalty can approximate a complex-valued function on [a,b]

--- GAIL_Matlab/Workouts/univariate_approximation_workouts/complex_func.m
+++ GAIL_Matlab/Workouts/univariate_approximation_workouts/complex_func.m
@@ -1,4 +1,4 @@
-% funappxNoPenalty can approximate a complex-valued function defined on
+% complex_func: funappxNoPenalty can approximate a complex-valued function defined on
 % finite real valued interval [a,b]. 
 %
 % The example is taken from MATLAB documentation "doc INTERP1"
@@ -7,6 +7,13 @@ a = 1;
 b = 10;
 x = a:b;
 f = @(x) (5*x)+(x.^2*1i);
-xq = a:0.001:b;
+xq = a:0.01:b;
 [fappx,out] = funappxNoPenalty_g(f,a,b) 
-errest =  max(abs(f(xq)-fappx(xq)))
+vq = fappx(xq);
+v  = f(xq);
+errest =  max(abs(v-vq))
+
+figure
+plot(xq,real(v),'*r',xq,real(vq),'-r');
+hold on
+plot(xq,imag(v),'*b',xq,imag(vq),'-b');
