24dbc195558ab422eb24291111e43c2291e9739d
Sou-Cheng Terrya Choi <sctchoi@uchicago.edu>
2016-04-02 14:18:42 -0500

    Turning Workout scripts into GAIL demos in HTML format

--- GAIL_Matlab/Algorithms/+gail/GAIL_Publish.m
+++ GAIL_Matlab/Algorithms/+gail/GAIL_Publish.m
@@ -15,13 +15,21 @@ if usejava('jvm')
         'help_integral_g', 'help_meanMC_g','help_meanMCBer_g', ...
         'help_cubMC_g','help_cubLattice_g','help_cubSobol_g',... % demos below
         'demo_cubSobol_g','demo_normal_probabilities.m'};
-    
+    wofile_list = {'Test_cubSobol_g.m'}; 
     %% generate GAIL Documentation in HTML format
     if ifGenerateHtml
       opts.stylesheet = strcat(GAILPATH,'Documentation',filesep,'mxdom2mathjaxbigfont.xsl');
       delete(strcat(GAILPATH,'Documentation',filesep,'html',filesep,'*.png'))
       for i=1:length(mfile_list),
         publish(mfile_list{i}, opts);
+      end
+      for i=1:length(wofile_list),
+        publish(wofile_list{i}, opts);
+        wopath = which(wofile_list{i});
+        htmlpath = strrep(wopath, wofile_list{i}, strcat(['html',filesep]));
+        htmlfile = strrep(wofile_list{i}, '.m', '.html');
+        movefile(fullfile(htmlpath,htmlfile),fullfile(GAILPATH,'Documentation','html'));
+        rmdir(htmlpath);
       end
     end
     
