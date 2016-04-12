d1786e52d5133c017c1b5790b9d6cf59c70a3bae
Sou-Cheng Terrya Choi <sctchoi@uchicago.edu>
2016-04-12 14:37:11 -0500

    Utiltity for checking outputs of funappx_g

---
+++ GAIL_Matlab/Algorithms/+gail/funappx_g_check.m
@@ -0,0 +1,13 @@
+function check = funappx_g_check(fappx,out_param)
+a = out_param.a;
+b = out_param.b;
+n = min(ceil( log10(out_param.npoints)) + 1, 8);
+x = a:10^-n:b;
+f = out_param.f;
+err = abs(f(x)-fappx(x));
+[~,ind] = find(err > out_param.abstol);
+semilogy(x, err); xlabel('x'); ylabel('absolute error'); hold on
+semilogy(x(ind), err(ind), 'r'); hold off
+axis tight
+err = max(err);
+check = err < out_param.abstol;
