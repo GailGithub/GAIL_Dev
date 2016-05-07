81eb6439e661cfb9e95f62c86a36da051df523bb
Sou-Cheng Terrya Choi <sctchoi@uchicago.edu>
2016-05-07 18:31:42 -0500

    Update cf_chebfun for clarity and correct Chebfun syntax.

--- GAIL_Matlab/Workouts/univariate_approximation_workouts/cf_chebfun.m
+++ GAIL_Matlab/Workouts/univariate_approximation_workouts/cf_chebfun.m
@@ -1,53 +1,59 @@
-function cf_chebfun(f, a, b)
+function cf_chebfun(f, a, b, abstol)
 % Examples:
-% f1 = @(x) x.^4 .* sin( 3.371568833721756 ./x); a = -1; b = 1; cf_chebfun(f1, a, b)
+% f1 = @(x) x.^4 .* sin(1./x); a = -1; b = 1; abstol = 1e-6; cf_chebfun(f1, a, b, abstol)
 %
-% f2 = @(x) f1(x) + 10.*x.^2;  cf_chebfun(f2, a, b)
+% f2 = @(x) f1(x) + 10.*x.^2; abstol = 1e-6;   cf_chebfun(f2, a, b, abstol) 
 %
 % delta = 0.2; B = 1./(2*delta.^2); c = -0.2;
 % f3 = @(x) B*(4*delta.^2 + (x-c).^2 + (x-c-delta).*abs(x-c-delta) ...
-%    - (x-c+delta).*abs(x-c+delta)).*(abs(x-c) <= 2*delta); a = - 1; b = 1; cf_chebfun(f3, a, b)
+%    - (x-c+delta).*abs(x-c+delta)).*(abs(x-c) <= 2*delta); a = - 1; b = 1; abstol = 1e-14;  
+% cf_chebfun(f3, a, b, abstol)
 %
-% f = @(x) sign(x);  a = -1; b = 1; cf_chebfun(f, a, b)
-% 
+% f4 = @(x)sin(10*pi*x.^4)-x, a = 0; b = 2; abstol = 1e-14; cf_chebfun(f4, a, b, abstol)
+%
+% f5 = @(x) sign(x);  a = -1; b = 1; cf_chebfun(f5, a, b, abstol)
+%  
+
+set(0,'defaultaxesfontsize',24,'defaulttextfontsize',24) %make font larger
 format compact
 format long
-abstol = 1e-8;
+
 
 %% funappx_g
 tic, [fappx, fout] = funappxNoPenalty_g(f,a,b,abstol,'nmax',10^8), toc
-gail.funappx_g_check(fappx,fout)
+% gail.funappx_g_check(fappx,fout)
 %% chebfun
-splitting on;
-tic, c = chebfun(f, [a,b]), toc
+ver_chebfun = gail.ver('Chebfun');
+if strcmp(ver_chebfun, '5.4.0')
+  tic, c = chebfun(f, [a,b],'splitting','on'), toc
+else 
+  splitting on;
+  tic, c = chebfun(f, [a,b]), toc
+end
 
 x=a:0.00001:b;
+figure(1)
 subplot(2,3,1), plot(x,f(x)); title(['f(x) = ',func2str(f)]); axis tight
 subplot(2,3,2), plot(x,fappx(x)); title(['funappx\_g approx.']); axis tight
 subplot(2,3,3), plot(x,c(x)); title(['Chebfun approx.']); axis tight
 
-err =abs( fappx(x) - f(x));
-subplot(2,3,5), semilogy( x, err, 'k' );  title('funappx\_g error'); axis tight; hold on
-[~,ind] = find(err > abstol);
+err = abs( fappx(x) - f(x));
+subplot(2,3,5), semilogy( x, err, 'k' );  title('funappx\_g errors'); axis tight; hold on
+[~,ind] = find(err > abstol*10);
 semilogy( x(ind), err(ind), 'ro' );   hold off;
+figure(2);
+semilogy( x, err, 'k' );  title('funappx\_g errors'); axis tight;  hold on;
+semilogy( x(ind), err(ind), 'ro' );  
+gail.save_eps('TraubPaperOutput', 'funappx_g_errors');
    
+chebfuntol=1e-14;
 err = abs(c(x) - f(x));
-subplot(2,3,6), semilogy( x, err, 'k' );   title ('chebfun error'); axis tight; hold on;
-[~,ind] = find(err > abstol);
+figure(1); subplot(2,3,6), semilogy( x, err, 'k' );   title ('Chebfun errors'); axis tight; hold on;
+[~,ind] = find(err > chebfuntol*10);
 semilogy( x(ind), err(ind), 'ro' );   hold off;
-   
-%% funmin_g
-tic, [fmin,out2] = funminNoPenalty_g(f, a, b, abstol, abstol), toc
-figure;
-funmin_g_demo(fmin, out2);
+figure(3);
+semilogy( x, err, 'k' );   title ('Chebfun errors'); axis tight;  hold on;
+semilogy( x(ind), err(ind), 'ro' ); 
+gail.save_eps('TraubPaperOutput', 'chebfun_errors');
 
-%% fminbnd
-tic, x = fminbnd(@(x) f(x),a,b), toc
 
-%% chebfun
-tic, [cy,cx] = min(c), toc
-
-%% integral_g
-tic, [fint,out3] = integralNoPenalty_g(f, a, b, abstol), toc
-tic, cint = sum(c),toc
-keyboard
