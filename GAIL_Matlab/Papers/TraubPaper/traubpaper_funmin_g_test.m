740bda8d1bcd1b09013a03f6ec47e9b330cca3ff
Sou-Cheng Terrya Choi <sctchoi@uchicago.edu>
2016-05-08 16:29:01 -0500

    Scripts for Example 3.3 (including optimization)

--- GAIL_Matlab/Papers/TraubPaper/traubpaper_funmin_g_test.m
+++ GAIL_Matlab/Papers/TraubPaper/traubpaper_funmin_g_test.m
@@ -2,12 +2,16 @@
 function [timeratio,timelgratio,npointsratio,npointslgratio]=traubpaper_funmin_g_test(nrep,abstol,varargin)
 % user can choose absolut error tolerance, initial number of points, number
 % of iteration or can use the following parameters
-% nrep = 10000; abstol = 1e-8;  
+% nrep = 100; abstol = 1e-6;  
 % 
 % Compare funminNoPenalty_g, fminbnd, and chebfun:
-% [timeratio,timelgratio,npointsratio,npointslgratio]=traubpaper_funmin_g_test(nrep,abstol,'funappxNoPenalty_g');
-
-c = rand(nrep,1)*4; % number of simulations for each test function
+% [timeratio,timelgratio,npointsratio,npointslgratio]=traubpaper_funmin_g_test(nrep,abstol,'funminNoPenalty_g');
+rng default % for reproducibility
+set(0,'defaultaxesfontsize',14,'defaulttextfontsize',14, ... %make font larger
+      'defaultLineLineWidth',2); %thick lines
+    %  'defaultLineMarkerSize',8)
+cc = rand(nrep,1);  
+c = cc*2; % number of simulations for each test function
 n = 6; % number of test functions
 m = 3; % number of methods
 npoints = zeros(n,m,nrep);
@@ -19,87 +23,124 @@ if isempty(varargin)
   algo = @(f,a,b,abstol) funmin_g(f,a,b,abstol);
 else 
   algoname = varargin{1};
-  algo = str2func(['@(f,a,b,abstol)', algoname,'(f,a,b,abstol)']);  
+  algo = str2func(['@(f,a,b,abstol,TolX)', algoname,'(f,a,b,abstol,abstol)']);  
 end
 
-algo2 = @(f,a,b,abstol) funappxglobal_g(f,a,b,abstol);
-algo3 = @(f,a,b) chebfun(f, [a,b]);
+algo2 = @(f,a,b,abstol) fminbnd(f,a,b,abstol);
+algo3 = @(f,a,b) chebfun(f, [a,b],'splitting','on');
 methods = {algo, algo2, algo3};
 
-warning('off',['GAIL:',algoname,':peaky'])
-warning('off',['GAIL:',algoname,':exceedbudget'])
-warning('off',['GAIL:',algoname,':fSmallerThanAbstol'])
-warning('off','GAIL:funappxglobal_g:peaky')
-warning('off','GAIL:funappxglobal_g:exceedbudget')
+% warning('off',['GAIL:',algoname,':peaky'])
+% warning('off',['GAIL:',algoname,':exceedbudget'])
+% warning('off',['GAIL:',algoname,':fSmallerThanAbstol'])
+% warning('off','GAIL:funminglobal_g:peaky')
+% warning('off','GAIL:funminglobal_g:exceedbudget')
+
+g1 = @(x,c) x.^4 .* sin(c./x);
+g2 = @(x,c) g1(x,c) + c.*x.^2;
+delta = .2; B = 1./(2*delta.^2);  
+g3 = @(x,cc) -B*(4*delta.^2 + (x-cc).^2 + (x-cc-delta).*abs(x-cc-delta) ...
+        - (x-cc+delta).*abs(x-cc+delta)).*(abs(x-cc) <= 2*delta);
+g4 = @(x,c) (x-c).^2;
+g5 = @(x,c) c*sin(c*pi*x);
+g6 = @(x,c) -10*exp(-1000*(x-c).^2);  
 
 a = zeros(1,n);
 b = zeros(1,n);
 a(1:3) = [-1,-1,-1];
 b(1:3) = [1,1,1];
 for i = 1:nrep
-    f1 = @(x) x.^4 .* sin(c(i)./x);
-    f2 = @(x) f1(x) + c(i).*x.^2;
-    delta = .2; B = 1./(2*delta.^2); cc = -c(i);
-    f3 = @(x) B*(4*delta.^2 + (x-cc).^2 + (x-cc-delta).*abs(x-cc-delta) ...
-        - (x-cc+delta).*abs(x-cc+delta)).*(abs(x-cc) <= 2*delta);
-    f4 = @(x) (x-c(i)).^2;
-    f5 = @(x) c(i)*sin(c(i)*pi*x);
-    f6 = @(x) 10*exp(-1000*(x-c(i)).^2);
-    %          f4 = @(x) 1/4*c(i)*exp(-2*x).*(c(i)-2*exp(x).*(-1 +...
-    %              c(i)*cos(x) - c(i)*sin(x))+exp(2*x).*(c(i) + 2*cos(x)...
-    %              - 2* sin(x) - c(i)*sin(2*x)));
-    fcns = {f1, f2, f3, f4, f5, f6};
-
-    for j = 1:length(fcns)
-        f = fcns{j};
-        if j > 3,     
-            b(j) = c(i)+1;
-        end
-        xx = a(j):0.000001:b(j);%rand(1000000,1)*(b-a)+a;
-        exactyy = f(xx);
-        for k = 1:length(methods)
-            if k <=2
-                tic; [fappx, out_param] = methods{k}(f,a(j),b(j),abstol); t=toc;
-                npoints(j,k,i) = out_param.npoints;
-            elseif k==3 
-                try
-                    splitting on
-                    lastwarn('') 
-                    tic, fappx = chebfun(f,[a(j),b(j)]); t=toc;
-                    if length(lastwarn) > 0
-                      exceedmat(j,k,i) = 1;
-                    end
-                end
-                npoints(j,k,i) = length(fappx);
-            end
-            time(j,k,i) = t;
-            
-            if k ==1 || k==3
-                yy = fappx(xx);
-            elseif k ==2
-                yy = ppval(fappx,xx);  
-            end
-        
-            trueerrormat(j,k,i) = max(abs(yy-exactyy));
-%             if trueerrormat(j,k,i)  > abstol
-%                cf_chebfun(f,a,b);
-%                keyboard
-%             end
-            if k==1
-                exceedmat(j,k,i) = sum(out_param.exitflag)>0;
-            elseif k==2
-                exceedmat(j,k,i) = out_param.exceedbudget;
-            end
-        end
+  f1 = @(x) g1(x,c(i));
+  f2 = @(x) g2(x,c(i));
+  f3 = @(x) g3(x,cc(i)*0.6);
+  f4 = @(x) g4(x,c(i));
+  f5 = @(x) g5(x,c(i));
+  f6 = @(x) g6(x,c(i));
+  fcns = {f1, f2, f3, f4, f5, f6}; %f1, f2, f3, f4, f5, f6};
+  
+  for j = 1:length(fcns)
+    
+    f = fcns{j};
+    
+    if j > 3,
+      b(j) = c(i)+1;
     end
+    
+    if j == 1
+      exactxmin = -1;
+      exactfmin = f(exactxmin);
+    elseif j == 2
+      exactfmin = 0;
+    elseif j==3
+      exactfmin = -1;
+    elseif j==4
+      exactxmin = c(i);
+      exactfmin = 0;
+    elseif j==5
+        kk = 0:floor(b(j)*c(i)-.5); 
+        if isempty(kk)
+            exactfmin = 0;
+        else
+            stat_pts = (kk + 0.5)/c(i);
+            exactfmin = min(f([0,stat_pts,b(j)]));
+        end
+    elseif j==6
+        exactfmin = -10;
+    end
+    
+    for k = 1:length(methods)
+      if k == 1
+        tic; [fmin, out_param] = methods{k}(f,a(j),b(j),abstol,abstol); t=toc;
+        npoints(j,k,i) = out_param.npoints;
+      elseif k == 2
+         lastwarn('')
+         tic, [xmin, fmin, exitflag, out] = fminbnd(@(x) f(x), a(j), b(j), ...
+           optimset ('TolX',out_param.abstol / 2, 'MaxFunEvals', out_param.nmax, 'MaxIter', out_param.maxiter) ); t=toc;
+         if length(lastwarn) > 0 || exitflag ~= 1 || out.iterations > out_param.maxiter
+            exceedmat(j,k,i) = 1;
+         end
+         npoints(j,k,i) = out.funcCount;
+      elseif k == 3
+        try
+          lastwarn('')
+          chebf = chebfun(f,[a(j),b(j)],'splitting','on');
+          tic, fmin = min(chebf); t=toc;
+          if length(lastwarn) > 0
+            exceedmat(j,k,i) = 1;
+          end
+        end
+        npoints(j,k,i) = length(chebf);
+      end
+      time(j,k,i) = t;
+      
+      trueerrormat(j,k,i) = max(abs(fmin-exactfmin));
+      if trueerrormat(j,k,i)  > abstol
+        %cf_chebfun_min(f,a,b,abstol);
+        j, k
+       % keyboard
+      end
+      if k==1
+        exceedmat(j,k,i) = sum(out_param.exitflag)>0;
+      end
+    end
+  end
 end;
-warning('on','GAIL:funappxglobal_g:exceedbudget')
-warning('on','GAIL:funappxglobal_g:peaky')
-warning('on',['GAIL:',algoname,':peaky'])
-warning('on',['GAIL:',algoname,':exceedbudget'])
-warning('on',['GAIL:',algoname,':fSmallerThanAbstol'])
+% warning('on','GAIL:funminglobal_g:exceedbudget')
+% warning('on','GAIL:funminglobal_g:peaky')
+% warning('on',['GAIL:',algoname,':peaky'])
+% warning('on',['GAIL:',algoname,':exceedbudget'])
+% warning('on',['GAIL:',algoname,':fSmallerThanAbstol'])
 
-cc = 2.5;
+permuted_index = [3, 1:2, 4:length(fcns)];
+d = 2;
+cc = d/4;
+f1 = @(x) g1(x,d);
+f2 = @(x) g2(x,d);
+f3 = @(x) g3(x,cc*0.6);
+f4 = @(x) g4(x,d);
+f5 = @(x) g5(x,d);
+f6 = @(x) g6(x,d);
+fcns = {f1, f2, f3, f4, f5, f6};
 x = cell(length(fcns));
 y = cell(length(fcns));
 for i=1:length(fcns)
@@ -107,20 +148,19 @@ for i=1:length(fcns)
    y{i} = fcns{i}(x{i});
 end
  
-
 timeratio = zeros(m-1,nrep,n);
 npointsratio = zeros(m-1,nrep,n);
-for i=1:nrep
-    for j=1:n
+for i=1:nrep  % each test function
+    for j=1:n % jth family of test functions
         for k = 1:m-1
-            timeratio(k,i,j) = time(j,1,i)/time(j,k+1,i);
+            timeratio(k,i,j) = time(j,1,i)/time(j,k+1,i); % first method compared to k+1 th method
         end
     end
 end
-for i=1:nrep;
-    for j=1:n;
+for i=1:nrep  % each test function
+    for j=1:n % jth family of test functions
         for k = 1:m-1
-            npointsratio(k,i,j) = npoints(j,1,i)/npoints(j,k+1,i);
+            npointsratio(k,i,j) = npoints(j,1,i)/npoints(j,k+1,i); % first method compared to k+1 th method
         end
     end
 end
@@ -137,21 +177,21 @@ end
 display(' ')
 display('   Test         Number of Points                    Time Used                          Success (%)                                  Failure (%)')
 display('  Function   ----------------------------    -------------------------------     --------------------------------------   ----------------------------------------');
-display('             Local      Global    Chebfun    Local       Global      Chebfun     Local        Global         Chebfun       Local         Global        Chebfun')
+display('             funmin_g   fminbnd   Chebfun    funmin_g     fminbnd    Chebfun     funmin_g        fminbnd        Chebfun   funmin_g        fminbnd       Chebfun')
 display('                                                                                 No Warn Warn No Warn Warn   No Warn Warn  No Warn Warn  No Warn Warn  No Warn Warn')
 npointslgratio = zeros(1,n);
 timelgratio = zeros(1,n);
 
-for i=1:n
+for i = permuted_index
     display(sprintf('%9.0f %9.0f %9.0f  %9.0f %11.3f  %11.3f %11.3f  %6.0f %6.0f %6.0f %6.0f %6.0f   %6.0f %6.0f %6.0f %6.0f %6.0f %6.0f %6.0f',...
         [i mean(npoints(i,1,:)) mean(npoints(i,2,:)) mean(npoints(i,3,:))...
-           mean(time(i,1,:)) mean(time(i,2,:)) mean(time(i,3,:))...
-           100.0*sum(trueerrormat(i,1,:)<=abstol)/nrep 100.0*sum(trueerrormat(i,1,:)<=abstol & (exceedmat(i,1,:)))/nrep ...
-           100.0*sum(trueerrormat(i,2,:)<=abstol)/nrep 100.0*sum(trueerrormat(i,2,:)<=abstol & (exceedmat(i,2,:)))/nrep ...
-           100.0*sum(trueerrormat(i,3,:)<=abstol)/nrep 100.0*sum(trueerrormat(i,3,:)<=abstol & (exceedmat(i,3,:)))/nrep...
-           100.0*sum(trueerrormat(i,1,:)>abstol)/nrep  100.0*sum(trueerrormat(i,1,:)>abstol & (exceedmat(i,1,:)))/nrep ...
-           100.0*sum(trueerrormat(i,2,:)>abstol)/nrep  100.0*sum(trueerrormat(i,2,:)>abstol & (exceedmat(i,2,:)))/nrep ...
-           100.0*sum(trueerrormat(i,3,:)>abstol)/nrep  100.0*sum(trueerrormat(i,3,:)>abstol & (exceedmat(i,3,:)))/nrep]))
+        mean(time(i,1,:)) mean(time(i,2,:)) mean(time(i,3,:))...
+        100.0*sum(trueerrormat(i,1,:)<=abstol)/nrep 100.0*sum(trueerrormat(i,1,:)<=abstol & (exceedmat(i,1,:)))/nrep ...
+        100.0*sum(trueerrormat(i,2,:)<=abstol)/nrep 100.0*sum(trueerrormat(i,2,:)<=abstol & (exceedmat(i,2,:)))/nrep ...
+        100.0*sum(trueerrormat(i,3,:)<=abstol)/nrep 100.0*sum(trueerrormat(i,3,:)<=abstol & (exceedmat(i,3,:)))/nrep...
+        100.0*sum(trueerrormat(i,1,:)>abstol)/nrep  100.0*sum(trueerrormat(i,1,:)>abstol & (exceedmat(i,1,:)))/nrep ...
+        100.0*sum(trueerrormat(i,2,:)>abstol)/nrep  100.0*sum(trueerrormat(i,2,:)>abstol & (exceedmat(i,2,:)))/nrep ...
+        100.0*sum(trueerrormat(i,3,:)>abstol)/nrep  100.0*sum(trueerrormat(i,3,:)>abstol & (exceedmat(i,3,:)))/nrep]))
     npointslgratio(i) = mean(npoints(i,1,:))/mean(npoints(i,2,:));
     timelgratio(i) = mean(time(i,1,:))/mean(time(i,2,:));
 end
@@ -172,14 +212,14 @@ end
 
 %% Save Output
 [~,~,MATLABVERSION] = GAILstart(false);
-markers = {'--go', ':r*', '-.b*', '-g+', '--ro', '-.b'};
+markers = {'--go', ':r*', '-.b.', '-g+', '--ro', '-.b'};
 if usejava('jvm') || MATLABVERSION <= 7.12
     figure
-    for i=1:length(fcns)
+    for i = permuted_index
         plot(x{i},y{i}, markers{i}); hold on
     end
     
-    legend('f1','f2','f3', 'f4', 'f5', 'f6')
+    legend('f3','g1','g2','g3','g4','g5')
     gail.save_eps('TraubPaperOutput', ['traub_',algoname,'_testfun']);
     
     figure
@@ -187,8 +227,8 @@ if usejava('jvm') || MATLABVERSION <= 7.12
     for k =1:m-1
         subplot(1,m-1,k)
         semilogy(t,sorted_timeratio(k,:),'r-',t,sorted_npointsratio(k,:),'b:');
-        title([algoname, ' vs. ', func2str( methods{k+1})])
-        legend('time ratio', 'points ratio');
+        %title([algoname, ' vs. ', func2str( methods{k+1})])
+        legend('time ratio', 'points ratio','Location','NorthWest');
     end
     
     gail.save_eps('TraubPaperOutput', ['traub_',algoname,'_test']);
@@ -204,10 +244,10 @@ end
 %   Function   ----------------------------    -------------------------------     --------------------------------------   ----------------------------------------
 %              Local      Global    Chebfun    Local       Global      Chebfun     Local        Global         Chebfun       Local         Global        Chebfun
 %                                                                                  No Warn Warn No Warn Warn   No Warn Warn  No Warn Warn  No Warn Warn  No Warn Warn
-%         1     40021    331916       5364       0.100        0.058       1.461       0     98    100      0     26       32      0      2      0      0      0     42
-%         2     42099    540010       5082       0.066        0.089       1.399      22     78    100      0     44       40      0      0      0      0      0     16
-%         3      5948     97651          2       0.024        0.016       0.022      76     24    100      0     96        0      0      0      0      0      4      0
-%         4     41926    608559          3       0.040        0.078       0.007       0    100    100      0    100        0      0      0      0      0      0      0
-%         5    305060   1412518         39       0.482        0.196       0.013     100      0    100      0    100        0      0      0      0      0      0      0
-%         6    112346   5315281        122       0.435        0.716       0.076       0    100    100      0     72        0      0      0      0      0     28      0
+%         3      2904     46439         11       0.020        0.020       0.229     100      0    100      0     97        0      0      0      0      0      3      0
+%         1      3849     30090       5639       0.018        0.019       2.569     100      0    100      0    100       86      0      0      0      0      0      0
+%         2      3927     48674       2131       0.033        0.020       1.455     100      0    100      0    100        0      0      0      0      0      0      0
+%         4      4666     42398          3       0.015        0.016       0.011     100      0    100      0    100        0      0      0      0      0      0      0
+%         5     23870     95174         34       0.049        0.017       0.013     100      0    100      0    100        0      0      0      0      0      0      0
+%         6     11492    471983        154       0.019        0.077       0.180     100      0    100      0    100        0      0      0      0      0      0      0
 
