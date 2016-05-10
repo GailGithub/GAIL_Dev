fc258b0c115cabcfaa9775aba96a673d6757687d
Fred J. Hickernell <hickernell@iit.edu>
2016-05-10 17:44:14 -0500

    fixed colors and axes

--- GAIL_Matlab/Workouts/univariate_approximation_workouts/cf_chebfun.m
+++ GAIL_Matlab/Workouts/univariate_approximation_workouts/cf_chebfun.m
@@ -1,4 +1,4 @@
-function cf_chebfun(f, a, b, abstol)
+function cf_chebfun(f, a, b, abstol, fstr)
 % Examples:
 % f1 = @(x) x.^4 .* sin(1./x); a = -1; b = 1; abstol = 1e-6; cf_chebfun(f1, a, b, abstol)
 %
@@ -14,39 +14,60 @@ function cf_chebfun(f, a, b, abstol)
 % f5 = @(x) sign(x);  a = -1; b = 1; cf_chebfun(f5, a, b, abstol)
 %  
 
-set(0,'defaultaxesfontsize',24,'defaulttextfontsize',24) %make font larger
-format compact
+InitializeDisplay
+%set(0,'defaultaxesfontsize',24,'defaulttextfontsize',24) %make font larger
+% format compact
 format long
+
+MATLABBlue = [0, 0.447, 0.741];
+MATLABOrange = [0.85,  0.325, 0.098];
+MATLABPurple = [0.494,  0.184, 0.556];
+MATLABGreen = [0.466,  0.674, 0.188];
+MATLABDkOrange = [0.85,  0.325, 0.098]*0.6;
+MATLABLtOrange = 0.5*[0.85,  0.325, 0.098] + 0.5*[1 1 1];
+
 
 %% funappx_g
 tic, [fappx, fout] = funappxNoPenalty_g(f,a,b,abstol,'nmax',10^8), toc
 % gail.funappx_g_check(fappx,fout)
+
 %% chebfun
 tic, c = chebfun(f, [a,b],'splitting','on'), toc
 
 x=a:0.00001:b;
 figure(1)
-subplot(2,3,1), plot(x,f(x)); title(['f(x) = ',func2str(f)]); axis tight
+subplot(2,3,1), plot(x,f(x)); 
+title(fstr); axis tight
 subplot(2,3,2), plot(x,fappx(x)); title(['funappx\_g approx.']); axis tight
 subplot(2,3,3), plot(x,c(x)); title(['Chebfun approx.']); axis tight
 
 err = abs( fappx(x) - f(x));
 subplot(2,3,5), semilogy( x, err, 'k' );  title('funappx\_g errors'); axis tight; hold on
 [~,ind] = find(err > abstol*10);
-semilogy( x(ind), err(ind), 'ro' );   hold off;
+semilogy( x(ind), err(ind), '.' , 'color', MATLABOrange);   hold off;
 figure(2);
-semilogy( x, err, 'k' );  title('funappx\_g errors'); axis tight;  hold on;
-semilogy( x(ind), err(ind), 'ro' );  
+semilogy( x, err, '-', 'color', MATLABBlue); hold on
+semilogy( x(ind), err(ind), '.' , 'color', MATLABOrange);  
+title('funappx\_g errors'); axis tight;
+small = max(-20,log10(0.1*min(err)));
+large = log10(10*max(err));
+axis([a b 10^small 10^large])
 gail.save_eps('TraubPaperOutput', 'funappx_g_errors');
    
 chebfuntol=1e-14;
 err = abs(c(x) - f(x));
 figure(1); subplot(2,3,6), semilogy( x, err, 'k' );   title ('Chebfun errors'); axis tight; hold on;
 [~,ind] = find(err > chebfuntol*10);
-semilogy( x(ind), err(ind), 'ro' );   hold off;
+semilogy( x(ind), err(ind), 'r.' );   hold off;
 figure(3);
-semilogy( x, err, 'k' );   title ('Chebfun errors'); axis tight;  hold on;
-semilogy( x(ind), err(ind), 'ro' ); 
+semilogy( x, err, '-', 'color', MATLABBlue);   title ('Chebfun errors'); 
+%axis tight;  
+hold on;
+semilogy( x(ind), err(ind), '.', 'color', MATLABOrange ); 
+small = max(-20,log10(0.1*min(err)));
+large = log10(10*max(err));
+axis([a b 10^small 10^large])
+set(gca,'ytick',10.^(5*ceil(small/5):5:5*floor(large/5)))
 gail.save_eps('TraubPaperOutput', 'chebfun_errors');
 
 
