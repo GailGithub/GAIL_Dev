58b45c8ba0d87a52d249359a7da0658a7f32c934
Fabio <fabio-ricardo@outlook.com>
2016-06-15 15:44:44 -0500

    Testing Parallel Computing in JAVA using

---
+++ DevelopOnly/GAIL_Matlab/Algorithms/GenerateArrayOfRN.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class GenerateArrayOfRN extends Thread{
+
+	long ll, hl;
+	Random rand = new Random();
+	ArrayList<Float> arrayRandoms;	
+	
+	public GenerateArrayOfRN(long lowerLimit, long higherLimit) {
+		this.ll = lowerLimit;
+		this.hl = higherLimit;
+        arrayRandoms = new ArrayList<>();	
+	}
+
+	public void run ()
+	{    
+			for (int j = 0; j<(int)(hl-ll); j++)
+			{
+				arrayRandoms.add(rand.nextFloat());
+			}
+	}
+    
+    public ArrayList<Float> getArray(){
+        return arrayRandoms;
+    }
+}
---
+++ DevelopOnly/GAIL_Matlab/Algorithms/MeanMC_CLT_Parallel.java
@@ -0,0 +1,53 @@
+import java.util.*;
+public class MeanMC_CLT_Parallel {
+
+        
+	public static void main(long nG, int nt) {
+		long nGen = nG;		//default value
+		long ll, hl = 0;
+        int nThreads = nt;			//default value
+		long time1 = System.nanoTime();	//measuring time
+		double value = 0;
+        
+        GenerateArrayOfRN[] randNumObj = new GenerateArrayOfRN[nThreads];	//defining the array of threads
+		
+		for (int i=0; i<nThreads; i++)		//setting up the limits for each thread and initializing them as objects
+		{
+			if(i==0)
+				ll = 2;
+			else
+				ll = hl+1;
+			
+			if(i==nThreads-1)
+				hl = nGen-1;
+			else
+				hl = hl + nGen/nThreads;
+			
+			randNumObj[i] = new GenerateArrayOfRN(ll, hl);
+			randNumObj[i].start();
+		}
+		
+        ArrayList<Float> randNumAll= new ArrayList<>();
+		try {
+			for (int i=0; i<nThreads; i++)
+			{
+				randNumObj[i].join();
+			}
+         
+            for (int i=0; i<nThreads; i++){
+                   randNumAll.addAll(randNumObj[i].getArray()); 
+            }
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+        
+		long time2 = System.nanoTime();
+		long timeTaken = time2 - time1;  
+		System.out.println("Total time taken was " + timeTaken/1000000.0 + " ms");
+        System.out.println(randNumAll.size());
+        for(int j=0; j <nGen; j++){
+            System.out.println(randNumAll.get(j));
+        }
+	}
+
+}
