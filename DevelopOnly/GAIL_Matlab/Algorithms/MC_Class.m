057c6647c0f74b12ff25a55387427f798cd7a530
qiantianpei <qiantianpei@gmail.com>
2016-06-25 16:08:18 -0500

    upgrade meanMC so that it can creates and handle an array of class

--- DevelopOnly/GAIL_Matlab/Algorithms/MC_Class.m
+++ DevelopOnly/GAIL_Matlab/Algorithms/MC_Class.m
@@ -1,15 +1,20 @@
 classdef MC_Class < handle
-    %MC_Class for testing arrays
-    
+    %MC_Class for testing arrays    
     properties
         randGen %rand number generator
         n %number of samples
     end
     
     methods
-        function obj = MC_Class(varargin)
-            obj.randGen = varargin{1};
-            obj.n = varargin{2};
+        function obj = MC_Class(randGen, n)
+            if nargin ~= 0
+            len = length(randGen);
+            obj(1,len) = MC_Class;
+            for i = 1:len
+                obj(i).randGen = randGen{i};
+                obj(i).n = n{i};
+            end
+            end
         end
         
         function val = genMu(obj)
