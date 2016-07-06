0283986cd181953ca7bf62e28d173da843afc40d
qiantianpei <qiantianpei@gmail.com>
2016-07-06 16:07:45 -0500

    delete unwanted files

--- DevelopOnly/GAIL_Matlab/Algorithms/EnhancedAlgorithms/meanMC_test/fns/distfun_av.m
+++
@@ -1,8 +0,0 @@
-function YX = distfun_av(n) 
-    p1 = rand(n,2);
-    p2 = rand(n,2);
-
-    Y = sqrt(sum((p1 - p2).^2,2));
-    Y_a = sqrt(sum(((1-p1) - (1-p2)).^2,2));
-    YX = [Y Y_a];
-end
--- DevelopOnly/GAIL_Matlab/Algorithms/EnhancedAlgorithms/meanMC_test/fns/expr_cv.m
+++
@@ -1,5 +0,0 @@
-function YX = expr_cv(n) 
-    u = rand(n, 1);
-    Y = exp(u);
-    YX = [Y u];
-end
