5ffacdd5e50b4a02e48aa9ef03421e01b746099e
LanJiang <tojianglan@gmail.com>
2016-09-25 18:22:26 -0400

    delete meanMCBer_g related scripts

--- GAIL_Matlab/OutputFiles/meanMCBerPaperOutput/Contents.m
+++
@@ -1,7 +0,0 @@
-% meanMCBerPaperOutput
-
-% abs-2015-03-01-23-56-31.eps --- figure 1 in paper
-
-% plotHoeffCLTr-2015-03-01-23-55-41.eps---figure 2 in paper
-
-% TestmeanMCBernoulli-on-abs-06-Sep-2014_03.18.10.mat---the MAT file used to plot
