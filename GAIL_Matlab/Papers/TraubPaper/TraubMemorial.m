396b2b14b2fd6679df469d4f82888e982ef8654d
Fred J. Hickernell <hickernell@iit.edu>
2016-10-03 17:36:04 -0500

    revised figures

--- GAIL_Matlab/Papers/TraubPaper/TraubMemorial.m
+++ GAIL_Matlab/Papers/TraubPaper/TraubMemorial.m
@@ -4,53 +4,53 @@
 %
 % S.-C. T. Choi, Y. Ding, F. J. Hickernell, X. Tong, " ...
 
-InitializeDisplay %initialize the workspace and the display parameters
-MATLABBlue = [0, 0.447, 0.741];
-MATLABOrange = [0.85,  0.325, 0.098];
-MATLABPurple = [0.494,  0.184, 0.556];
-MATLABGreen = [0.466,  0.674, 0.188];
-%MATLABDkOrange = [0.85,  0.325, 0.098]*0.6;
+gail.InitializeDisplay %initialize the workspace and the display parameters
+% MATLABBlue = [0, 0.447, 0.741];
+% MATLABOrange = [0.85,  0.325, 0.098];
+% MATLABPurple = [0.494,  0.184, 0.556];
+% MATLABGreen = [0.466,  0.674, 0.188];
+% %MATLABDkOrange = [0.85,  0.325, 0.098]*0.6;
 %MATLABLtOrange = 0.5*[0.85,  0.325, 0.098] + 0.5*[1 1 1];
 whichdir = 'TraubPaperOutput';
 
 %% Sample functions with wildy oscillating second derivatives
-f1 = @(x) x.^4 .* sin(1./((x==0)+x));
-f1pp = @(x) (12*x.^2 - 1) .* sin(1./x) - 6*x .* cos(1./x);
-f2 = @(x) f1(x) + 10.*x.^2;
-f2pp = @(x) f1pp(x) + 20;
-xplot = (-1:.001:1);
-h = plot(xplot,f1(xplot),'-','color',MATLABBlue);
-hold on
-h=[h,plot(xplot,f2(xplot),'-','color',MATLABOrange)];
-axis([-1 1 -1 11])
-xlabel('\(x\)')
-legend(h,{'\(f_1(x)\)', '\(f_2(x)\)'},'location', 'north','box','off')
-gail.save_eps(whichdir, 'f1f2plot.eps');
-
-figure
-xplotclose = (-0.02:0.00001:0.02)';
-h = plot(xplotclose,f1(xplotclose),'-','color',MATLABBlue);
-%axis([-1 1 -1 11])
-xlabel('\(x\)')
-legend(h,{'\(f_1(x)\)'},'location', 'north','box','off')
-print('-depsc',[whichdir 'f1closeplot.eps'])
-gail.save_eps(whichdir, 'f1f2plot.eps');
-
-figure
-xplotclose = (-0.02:0.00001:0.02)';
-h = plot(xplotclose,f2(xplotclose),'-','color',MATLABOrange);
-axis([-0.02 0.02 -5e-4 4.5e-3])
-xlabel('\(x\)')
-legend(h,{'\(f_2(x)\)'},'location', 'north','box','off')
-gail.save_eps(whichdir, 'f2closeplot.eps');
-
-figure
-h = plot(xplot,f1pp(xplot),'-',xplot,f2pp(xplot),'-');
-axis([-1 1 -7 27])
-xlabel('\(x\)')
-legend(h,{'\(f''''_1(x)\)', '\(f''''_2(x)\)'},'location','best','box','off')
-%print('-depsc',[whichdir 'f1ppf2ppplot.eps'])
-gail.save_eps(whichdir, 'f1ppf2ppplot');
+% f1 = @(x) x.^4 .* sin(1./((x==0)+x));
+% f1pp = @(x) (12*x.^2 - 1) .* sin(1./x) - 6*x .* cos(1./x);
+% f2 = @(x) f1(x) + 10.*x.^2;
+% f2pp = @(x) f1pp(x) + 20;
+ xplot = (-1:.001:1);
+% h = plot(xplot,f1(xplot),'-','color',MATLABBlue);
+% hold on
+% h=[h,plot(xplot,f2(xplot),'-','color',MATLABOrange)];
+% axis([-1 1 -1 11])
+% xlabel('\(x\)')
+% legend(h,{'\(f_1(x)\)', '\(f_2(x)\)'},'location', 'north','box','off')
+% gail.save_eps(whichdir, 'f1f2plot.eps');
+% 
+% figure
+% xplotclose = (-0.02:0.00001:0.02)';
+% h = plot(xplotclose,f1(xplotclose),'-','color',MATLABBlue);
+% %axis([-1 1 -1 11])
+% xlabel('\(x\)')
+% legend(h,{'\(f_1(x)\)'},'location', 'north','box','off')
+% print('-depsc',[whichdir 'f1closeplot.eps'])
+% gail.save_eps(whichdir, 'f1f2plot.eps');
+% 
+% figure
+% xplotclose = (-0.02:0.00001:0.02)';
+% h = plot(xplotclose,f2(xplotclose),'-','color',MATLABOrange);
+% axis([-0.02 0.02 -5e-4 4.5e-3])
+% xlabel('\(x\)')
+% legend(h,{'\(f_2(x)\)'},'location', 'north','box','off')
+% gail.save_eps(whichdir, 'f2closeplot.eps');
+% 
+% figure
+% h = plot(xplot,f1pp(xplot),'-',xplot,f2pp(xplot),'-');
+% axis([-1 1 -7 27])
+% xlabel('\(x\)')
+% legend(h,{'\(f''''_1(x)\)', '\(f''''_2(x)\)'},'location','best','box','off')
+% %print('-depsc',[whichdir 'f1ppf2ppplot.eps'])
+% gail.save_eps(whichdir, 'f1ppf2ppplot');
 
 %% Sample functions with piecwise constant second derivatives
 f3param = @(x,delta,c) (1./(2*delta.^2))*(4*delta.^2 + (x-c).^2 + (x-c-delta).*abs(x-c-delta) ...
@@ -65,14 +65,14 @@ figure
 h = plot(xplot,f3(xplot),'-','color',MATLABPurple);
 axis([-1 1 -0.2 1.2])
 xlabel('\(x\)')
-legend(h,{'\(f_3(x)\)'},'location', 'northeast','box','off')
+legend(h,{'\(f_1(x)\)'},'location', 'northeast','box','off')
 gail.save_eps(whichdir, 'f3plot');
 
 figure
 h = plot(xplot,f3pp(xplot),'-','color',MATLABPurple);
 %axis([-1 1 -0.2 1.2])
 xlabel('\(x\)')
-legend(h,{'\(f_3''''(x)\)'},'location', 'northeast','box','off')
+legend(h,{'\(f_1''''(x)\)'},'location', 'northeast','box','off')
 print('-depsc',[whichdir 'f3ppplot.eps'])
 gail.save_eps(whichdir, 'f3ppplot');
 
@@ -115,7 +115,7 @@ h = plot(xplot,-f3(xplot),'-', ...
    fappxout.x, zeros(size(fappxout.x)),'.');
 axis([-1 1 -1.2 0.2])
 xlabel('\(x\)')
-legend(h,{'\(-f_3(x)\)','\((x_i,-f_3(x_i))\)','\((x_i,0)\)'}, ...
+legend(h,{'\(-f_1(x)\)','\((x_i,-f_1(x_i))\)','\((x_i,0)\)'}, ...
    'location', 'east','box','off')
 gail.save_eps('TraubPaperOutput', 'sampling-funappxg');
 
@@ -129,7 +129,7 @@ h = plot(xplot,-f3(xplot),'-', ...
    fminout.x, zeros(size(fminout.x)),'.');
 axis([-1 1 -1.2 0.2])
 xlabel('\(x\)')
-legend(h,{'\(-f_3(x)\)','\((x_i,-f_3(x_i))\)','\((x_i,0)\)'}, ...
+legend(h,{'\(-f_1(x)\)','\((x_i,-f_1(x_i))\)','\((x_i,0)\)'}, ...
    'location', 'east','box','off')
 gail.save_eps('TraubPaperOutput', 'sampling-funming');
 
