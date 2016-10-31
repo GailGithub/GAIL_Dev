383675cd965663ba640240953bc25672b7ed08ab
Jagadeeswaran Rathinavel <jagadeesr@gmail.com>
2016-10-30 19:37:02 -0500

    Changes for Bayesian Cubature with Fourier kernel and Rank-1 Lattice points

--- DevelopOnly/GAIL_Matlab/Algorithms/IntegrationExpectation/cubMLE.m
+++ DevelopOnly/GAIL_Matlab/Algorithms/IntegrationExpectation/cubMLE.m
@@ -14,7 +14,7 @@ function [muhat,out]=cubMLE(f,nvec,domain,whSample,whKer,powerFuncMethod)
 % approximation
 if nargin < 7
     if ~exist('powerFuncMethod','var') || isempty(powerFuncMethod)
-        powerFuncMethod='cauchy';  %technique to compute power function%
+        powerFuncMethod='Cauchy';  %technique to compute power function%
     end
     if nargin < 6
        if nargin < 5;
@@ -42,28 +42,53 @@ d = size(domain,2);
 if strcmp(whSample,'Sobol')
    x = net(scramble(sobolset(d),'MatousekAffineOwen'),nmax);
 end
+
+if strcmp(whSample,'Lattice1') %Rank-1 Lattice points
+    x = gail.lattice_gen(1,nmax,d);
+end
+
 fx = f(x);
 out.aMLE(nn,1) = 0;
+out_aMLE(nn,1) = 0;
 muhat(nn,1) = 0;
+out_disc2(nn,1) = 0;
+out_ErrBd(nn,1) = 0;
+BernPolynOrder = 4;
+BernPolynX = zeros(size(x));
+if strcmp(whKer,'Fourier')
+    xdiff = abs(x - x(1,:));
+    % presompute the bernoulli polynominal values to speedup the computation
+    for k=1:d
+      BernPolynX(:,k) = bernoulli(BernPolynOrder, xdiff(:,k));
+    end
+end
+
 for ii = 1:nn
    nii = nvec(ii);
-   
-   % Find the optimal shape parameter
+   if strcmp(whKer,'Fourier')
+       ax = -3; bx = 0; % limit theta within (0,1]
+   else
+       ax = -5; bx = 5;
+   end
    lnaMLE = fminbnd(@(lna) ...
-      MLEKernel(exp(lna),x(1:nii,:),fx(1:nii),whKer,domain), ...
-      -5,5,optimset('TolX',1e-2));
+      MLEKernel(exp(lna),x(1:nii,:),fx(1:nii),whKer,domain,BernPolynX(1:nii,:),BernPolynOrder), ...
+      ax,bx,optimset('TolX',1e-2));
    aMLE = exp(lnaMLE);
-   out.aMLE(ii) = aMLE;
+   out_aMLE(ii) = aMLE;
+   [K,kvec,k0] = kernelFun(x(1:nii,:),whKer,aMLE,domain,BernPolynX(1:nii,:),BernPolynOrder);
    
-   [K,kvec,k0] = kernelFun(x(1:nii,:),whKer,aMLE);
+   if strcmp(whKer,'Fourier')
+       Kinvy = ifft(fft(fx(1:nii))./fft(K(1,:)'));
+   else
    Kinv = pinv(K);
    %w = Kinv*kvec;
    Kinvy = Kinv*fx(1:nii);
+   end
    
    % compute the approximate mu 
    muhat(ii) = kvec'*Kinvy;
-   
-   if strcmp(powerFuncMethod, 'cauchy')
+   %out_Kcond(ii) = cond(K);
+   if strcmp(powerFuncMethod, 'Cauchy')
        eigK = eig(K);
        eigKaug = eig([k0 kvec'; kvec K]);
        disc2 = exp(sum(log(eigKaug(1:nii)) - log(eigK)))*eigKaug(end);
@@ -74,8 +99,13 @@ for ii = 1:nn
          eigValKaug = diag(eigValKaug);
        end
        uii = eigVecKaug(:,1);
-       disc2 = 1/sum(uii.^2 ./ eigValKaug);
+       disc2 = 1.0/sum(uii.^2 ./ eigValKaug);
    end
-   out.ErrBd(ii) = 2.58*sqrt(disc2*(fx(1:nii)'*Kinvy)/nii);
+   out_disc2(ii) = disc2;
+   out_ErrBd(ii) = 2.58*sqrt(disc2*(fx(1:nii)'*Kinvy)/nii);
 end  
+
+out.aMLE = out_aMLE;
+out.disc2 = out_disc2;
+out.ErrBd = out_ErrBd;
 out.time = toc(tstart);
