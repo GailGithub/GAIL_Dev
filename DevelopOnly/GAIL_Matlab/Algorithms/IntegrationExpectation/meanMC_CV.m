cba6c74a3c00512588b5958c7b054f75f392e2b4
RONHY\Ronhy <ronhyao@gmail.com>
2016-11-21 01:34:29 -0600

    updated meanMC_CV

--- DevelopOnly/GAIL_Matlab/Algorithms/IntegrationExpectation/meanMC_CV.m
+++ DevelopOnly/GAIL_Matlab/Algorithms/IntegrationExpectation/meanMC_CV.m
@@ -1,9 +1,9 @@
 function [tmu,out_param]=meanMC_CV(varargin)
-% meanMC_CV Monte Carlo method estimate the mean of a random variable by
-% using control variate.
+% meanMC_CV Monte Carlo method estimate the mean of a random variable through 
+% control variate.
 %
-%   tmu = meanMC_CV(YXrand) estimates the mean, mu, of a random variable Y 
-%   by inputting other depentent random variables X to within a specified 
+%   tmu = meanMC_CV(YXrand,meanX) estimates the mean, mu, of a random variable Y 
+%   by adding other depentent random variables X to within a specified 
 %   generalized error tolerance.
 %   tolfun:=max(abstol,reltol*|mu|), i.e., |mu - tmu| <= tolfun with
 %   probability at least 1-alpha, where abstol is the absolute error
@@ -15,22 +15,22 @@ function [tmu,out_param]=meanMC_CV(varargin)
 %   returns an n x j array of IID instances of the random variable Y and
 %   the depentent random variables X.
 %
-%   tmu = meanMC_CV(YXrand,abstol,reltol,alpha,fudge,nSig,n1,tbudget,nbudget)
-%   estimates the mean of a random variable Y by inputting other depentent
+%   tmu = meanMC_CV(YXrand,meanX,abstol,reltol,alpha,fudge,nSig,n1,tbudget,nbudget)
+%   estimates the mean of a random variable Y by adding other depentent
 %   random variables X to within a specified generalized error tolerance 
 %   tolfun with guaranteed confidence level 1-alpha using all ordered parsing 
 %   inputs abstol, reltol, alpha, fudge, nSig, n1, tbudget, nbudget.
 %
-%   tmu = meanMC_CV(YXrand,'abstol',abstol,'reltol',reltol,'alpha',alpha,
+%   tmu = meanMC_CV(YXrand,meanX,'abstol',abstol,'reltol',reltol,'alpha',alpha,
 %   'fudge',fudge,'nSig',nSig,'n1',n1,'tbudget',tbudget,'nbudget',nbudget)
-%   estimates the mean of a random variable Y by inputting other depentent
+%   estimates the mean of a random variable Y by adding other depentent
 %   random variables X to within a specified generalized error tolerance 
 %   tolfun with guaranteed confidence level 1-alpha. All the field-value pairs 
 %   are optional and can be supplied in different order, if a field is not 
 %   supplied, the default value is used.
 %
-%   [tmu, out_param] = meanMC_CV(YXrand,in_param) estimates the mean of a
-%   random variable Y by inputting other depentent random variables X to 
+%   [tmu, out_param] = meanMC_CV(YXrand,meanX,in_param) estimates the mean of a
+%   random variable Y by adding other depentent random variables X to 
 %   within a specified generalized error tolerance tolfun with the given 
 %   parameters in_param and produce the estimated mean tmu and output parameters 
 %   out_param. If a field is not specified, the default value is used.
@@ -46,8 +46,10 @@ function [tmu,out_param]=meanMC_CV(varargin)
 %     uniform random variable, then one may define 
 %     YXrand = @(n) [rand(n,1).^2, rand(n,1)].
 %
-%     MeanX --- the population mean of X, default value is the sample mean
-%     of X.
+%     MeanX --- the population mean of X, which must be given, 
+%     default value is the sample mean of X. If YXrand have been inputted  
+%     and the population mean of X is not known, this parameter must be 
+%     inputted as not numbers to ensure the result is correct.
 %
 %     in_param.abstol --- the absolute error tolerance, which should be
 %     positive, default value is 1e-2.
@@ -126,30 +128,45 @@ function [tmu,out_param]=meanMC_CV(varargin)
 %
 %
 % Examples
+%
+% Example 1:
+% If no parameters are parsed, help text will show up and the program will 
+% compute the default YXrand: YXrand =@(n) [rand(n,1).^2, rand(n,1)].
+%
+% >> meanMC_CV
+% Warning: YXrand must be specified. Now GAIL is using 
+% YXrand =@(n) [rand(n,1).^2, rand(n,1)]. 
+% Warning: meanX must be given. Now GAIL is using sample mean of X.
+% ans =0.33***
+%
+%
+% Example 2:
+% Calculate the mean of u^2 by adding X=u when u is standard normally distributed
+% in [0 1], with the absolute error tolerance = 1e-3 and uncertainty 5%.
+%
+% >> in_param.reltol=0; in_param.abstol = 1e-3;
+% >> in_param.alpha = 0.05; YXrand=@(n) [randn(n,1).^2, randn(n,1)].
+% >> tmu=meanMC_CV(YXrand,0,in_param)
+% tmu = 1.00***
+%
+%
+% Example 3:
+% Calculate the mean of exp(u) by adding X=u when u is uniformly distributed in
+% [0 1], with the absolute error tolerance 1e-3.
+%
+% >> tmu=meanMC_CV(@(n) [exp(rand(n,1)), rand(n,1)],0.5,1e-3,0)
+% tmu = 1.71***
+%
+%
+% Example 4:
+% Calculate the mean of cos(u^2) by adding X=[u,u^2] when u is uniformly distributed in
+% [0 1], with the relative error tolerance 1e-2 and uncertainty 0.05.
+%
+% >> tmu=meanMC_CV(@(n) [cos(rand(n,1).^2),rand(n,1), rand(n,1).^2],[0.5,1/3],
+% 'reltol',1e-2,'abstol',0,'alpha',0.05)
+% tmu = 0.90***
 
-if isempty(varargin)
-    % if no values are parsed, print warning message and use the default
-    % random variable
-    help meanMC_CV
-    warning('GAIL:meanMC_CV:yrandnotgiven',...
-        'Yrand must be specified. Now GAIL is using Yrand =@(n) [rand(n,1).^2, rand(n,1)].')
-    YXrand = @(n) [rand(n,1).^2, rand(n,1)];
-else
-    YXrand = varargin{1};
-end
-
-if numel(varargin)>1 && isnumeric(varargin{2})
-    meanX = varargin{2};
-else
-    % if no meanX values are parsed, print warning message and using sample
-    % mean of X
-    help meanMC_CV
-    warning('GAIL:meanMC_CV:meanXnotgiven',...
-        'meanX must be given. Now GAIL is using sample mean of X.')
-    YX = YXrand(10000);
-    X = YX(:,2:end);
-    meanX = mean(X,1);
-end
+[YXrand,meanX,out_param] = meanMC_CV_param(varargin{:});
 
 % optimal beta
 beta =@(YXrand) (bsxfun(@minus,YXrand(:,2:end),mean(YXrand(:,2:end),1))\...
@@ -158,4 +175,116 @@ beta =@(YXrand) (bsxfun(@minus,YXrand(:,2:end),mean(YXrand(:,2:end),1))\...
 YCVrand =@(YXrand) (YXrand(:,1) - bsxfun(@minus,YXrand(:,2:end),meanX)...
     *beta(YXrand)); 
 % use meanMC_g to compute mu
-[tmu, out_param] = meanMC_g(@(n) (YCVrand(YXrand(n))), varargin{3:end});
+[tmu, out_param] = meanMC_g(@(n) (YCVrand(YXrand(n))), out_param);
+end
+
+
+function  [YXrand2,meanX2,out_param] = meanMC_CV_param(varargin)
+
+default.abstol  = 1e-2;% default absolute error tolerance
+default.reltol = 1e-1;% default relative error tolerance
+default.nSig = 1e4;% default initial sample size nSig for variance estimation
+default.n1 = 1e4; % default initial sample size n1 for mean estimation
+default.alpha = 0.01;% default uncertainty
+default.fudge = 1.2;% default fudge factor
+default.tbudget = 100;% default time budget
+default.nbudget = 1e9; % default sample budget
+
+if isempty(varargin)
+    % if no values are parsed, print warning message and use the default
+    % random variable
+    help meanMC_CV
+    warning('GAIL:meanMC_CV:yxrandnotgiven',...
+    ['YXrand must be specified. Now GAIL is using the default' ...
+    'YXrand =@(n) [rand(n,1).^2, rand(n,1)].'])
+    YXrand2 = @(n) [rand(n,1).^2, rand(n,1)];
+else
+    YXrand2 = varargin{1};
+end
+
+if numel(varargin)>1 && isnumeric(varargin{2}) && all(~isnan(varargin{2}))
+    YX = YXrand2(10000);
+    X = YX(:,2:end);
+    meanX2 = mean(X,1);
+    if all(abs(meanX2-varargin{2})<0.01)
+        meanX2 = varargin{2};
+    else
+    warning('GAIL:meanMC_CV:meanXnotright',...
+        ['meanX mignt not be given or is not close to sample means of X '...
+        'under tolerance 0.01. Now GAIL is using sample means of X.'])
+    meanX2 = mean(X,1);
+    end
+else
+    % if meanX are not numbers, print warning message and using sample
+    % means of X
+    help meanMC_CV
+    warning('GAIL:meanMC_CV:meanXnotgiven',...
+        'meanX must be given. Now GAIL is using sample means of X.')
+    YX = YXrand2(10000);
+    X = YX(:,2:end);
+    meanX2 = mean(X,1);
+end
+
+validvarargin=numel(varargin)>2;
+if validvarargin
+    in3=varargin{3};
+    validvarargin=(isnumeric(in3) || isstruct(in3) ...
+        || ischar(in3));
+end
+
+if ~validvarargin
+    %if there is only input which is Yrand, use all the default parameters
+    out_param.abstol = default.abstol;% default absolute error tolerance
+    out_param.reltol = default.reltol; % default relative error tolerance
+    out_param.alpha = default.alpha;% default uncertainty
+    out_param.fudge = default.fudge;% default standard deviation inflation factor
+    out_param.nSig = default.nSig;% default the sample size to estimate the variance
+    out_param.n1 = default.n1;% default the initial sample size to estimate the mean
+    out_param.tbudget = default.tbudget;% default time budget
+    out_param.nbudget = default.nbudget;% default sample budget
+else
+    p = inputParser;
+    addRequired(p,'Yrand',@gail.isfcn);
+    if isnumeric(in3)
+        %if there are multiple inputs with only numeric, they should be put in order.
+        addOptional(p,'abstol',default.abstol,@isnumeric);
+        addOptional(p,'reltol',default.reltol,@isnumeric);
+        addOptional(p,'alpha',default.alpha,@isnumeric);
+        addOptional(p,'fudge',default.fudge,@isnumeric);
+        addOptional(p,'nSig',default.nSig,@isnumeric);
+        addOptional(p,'n1',default.n1,@isnumeric);
+        addOptional(p,'tbudget',default.tbudget,@isnumeric);
+        addOptional(p,'nbudget',default.nbudget,@isnumeric);
+    else
+        if isstruct(in3) %parse input structure
+            p.StructExpand = true;
+            p.KeepUnmatched = true;
+        end
+        addParamValue(p,'abstol',default.abstol,@isnumeric);
+        addParamValue(p,'reltol',default.reltol,@isnumeric);
+        addParamValue(p,'alpha',default.alpha,@isnumeric);
+        addParamValue(p,'fudge',default.fudge,@isnumeric);
+        addParamValue(p,'nSig',default.nSig,@isnumeric);
+        addParamValue(p,'n1',default.n1,@isnumeric);
+        addParamValue(p,'tbudget',default.tbudget,@isnumeric);
+        addParamValue(p,'nbudget',default.nbudget,@isnumeric);
+    end
+    parse(p,YXrand2,varargin{3:end})
+    out_param = p.Results;
+end
+if (~gail.isfcn(YXrand2))
+    %print warning message
+    warning('GAIL:meanMC_CV:yxrandnotfcn',...
+        ['Yrand must be a function handle.' ...
+        ' Now GAIL is using the default YXrand =@(n) [rand(n,1).^2, rand(n,1)].'])
+    YXrand2 = @(n) [rand(n,1).^2, rand(n,1)];
+end
+if size(YXrand2(5),1)~=5 || size(YXrand2(5),2)<1
+    % if the input is not a length n x j vector, print the warning message
+    warning('GAIL:meanMC_CV:yxrandnotlengthN',...
+        ['YXrand should be a random variable n x j matrix, '...
+        'but not an integrand.'...
+        ' Now GAIL is using the default YXrand =@(n) [rand(n,1).^2, rand(n,1)].'])
+    YXrand2 = @(n) [rand(n,1).^2, rand(n,1)];
+end
+end
