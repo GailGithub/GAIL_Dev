786d505d182403fd314dfc3ae002cc395377d17e
lluisantoni <lluisantoni@gmail.com>
2017-03-29 21:08:21 -0400

    Removing my personal address of the automatic daily report emails

--- GAIL_Matlab/Tests/Developers_only/automatic_tests/mail_c.sh
+++ GAIL_Matlab/Tests/Developers_only/automatic_tests/mail_c.sh
@@ -1,4 +1,4 @@
-mail -s "Daily test results were OK (report)" schoi32@iit.edu,lluisantoni@gmail.com < /home/gail/GAIL_tests/PBS_jobs/pbs_reports/gail_daily_tests-$(date +\%Y-\%m-\%d).out 
+mail -s "Daily test results were OK (report)" schoi32@iit.edu < /home/gail/GAIL_tests/PBS_jobs/pbs_reports/gail_daily_tests-$(date +\%Y-\%m-\%d).out 
 
 
 
