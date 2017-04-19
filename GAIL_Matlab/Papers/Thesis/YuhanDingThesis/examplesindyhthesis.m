905bb3f987b3c11deb823bea215b6343b5754d55
Yuhan Ding <yding2@hawk.iit.edu>
2017-04-19 16:11:55 -0500

    Delete the useless files in workout of univariate function approximation; Update the files in Yuhan’s thesis

--- GAIL_Matlab/Papers/Thesis/YuhanDingThesis/examplesindyhthesis.m
+++ GAIL_Matlab/Papers/Thesis/YuhanDingThesis/examplesindyhthesis.m
@@ -1,5 +1,7 @@
 function examplesindyhthesis
 % To replicate the examples presented in Yuhan Ding's dissertation
+% Example 1(the same example in the cones not balls paper)
+[succnowarn,succwarn]=conepaper_test_funappx_g(10000,10^7,1e-8);
 % From Example 2 to Example 7
 % Comparsion with chebfun
 chebfuncomparison
@@ -8,7 +10,7 @@ out_param
 % Example to illustrate the computational cost
 TableForLocalAlgorithm
 % GUI example of a flat function
-funappx_g_gui(@(x) exp(-1./(x - 0.5).^2),0,1,1e-4,2,2);
+funappx_g_gui(@(x) exp(-1000*(x-0.2).^2),0,1,1e-2,5,9,'funappxPenalty_g');
 % Approximate symbol of batman with tolerance 1e-4, nlo as 10, nhi as 15
 [negerr, poserr]=newplotbatmanfunappx(1e-4);
 max(max(negerr),max(poserr))
@@ -19,4 +21,4 @@ a=-0.2; b=0.5; c=0.1; n=2; az=-150; el=10; res=128;
 funappxseashell(a, b, c, n, az, el, res);
 % Comparison between funappx_g and funappxglobal_g
 nrep = 100; abstol = 1e-7; nlo = 100; nhi = 1000;
-workout_funappx_g(nrep,abstol,nlo,nhi);
+workout_funappx_g(nrep,abstol,nlo,nhi,'funappxPenalty_g');
