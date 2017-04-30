d8dd892934c7f8121554db9a889984a33ec91658
Yuhan Ding <yding2@hawk.iit.edu>
2017-04-30 15:39:56 -0500

    Add back curve.m to repository to fix the missing demo in documentation center

---
+++ GAIL_Matlab/Workouts/univariate_approximation_workouts/curve.m
@@ -0,0 +1,31 @@
+%% CURVE 
+% Approximate a curve using funappx_g.
+
+%% Function definition
+%
+% Define a highly fluctuating function as follows:
+%
+% \[ f(x) = x^2 sin (\frac{2 \pi}{ x^2} ). \] 
+% 
+f = @(x) x.^2 .* sin((2*pi)./x.^2);
+
+%% Function approximation
+% We use funappx_g to approximate \(f\) over the interval
+% \([a,b]\):
+a = 1e-2;
+b = 2.5;
+[q,out] = funappx_g(f, a, b);
+
+%% Plot of the approximant 
+% We plot out \(q(x)\) below:
+figure;
+x = a:0.0001:b;
+plot(x,q(x)); 
+axis tight
+
+%% Plot of the apprroximation errors  
+% The following plot shows that all pointwise absolute errors are less than the
+% default tolerance of \(10^{-6}\).
+figure;
+plot(x,abs(f(x)-q(x))); 
+axis tight
