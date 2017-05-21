c09486e5d767fc7fa419c5796c4b76d103db8be2
Sou-Cheng Terrya Choi <schoi32@iit.edu>
2017-05-21 12:54:41 -0500

    Revised curve.m

--- GAIL_Matlab/Documentation/curve.m
+++ GAIL_Matlab/Documentation/curve.m
@@ -30,23 +30,23 @@ axis tight
 % The following plot shows that all pointwise absolute errors are less than
 % the default tolerance of \(10^{-6}\).
 figure;
-plot(x,abs(f(x)-q(x))); 
+semilogy(x,abs(f(x)-q(x))); 
 xlabel('$x$','interpreter','latex')
 ylabel('absolute error')
 axis tight
 
 %% A slightly different example
 % If we changes \(a\) to a smaller number such as \(10^{-2}\), then even if
-% we relax the tolerance to \(10^{-5}\), funappx_g may still return an
+% we relax the tolerance to \(10^{-4}\), funappx_g may still return an
 % approximant that fails to meet the tolerance. The reason is that \(f\) on
 % \((a,b)\) is no longer in the cone of functions conducive for successful
 % approximation.
 a = 1e-2;
-abstol = 1e-5;
+abstol = 1e-4;
 [q2,out2] = funappx_g(f, a, b, abstol);
 figure;
 x = a:1e-6:b;
-plot(x,abs(f(x)-q2(x))); 
+semilogy(x,abs(f(x)-q2(x))); 
 xlabel('$x$','interpreter','latex')
 ylabel('absolute error')
 axis tight
@@ -62,7 +62,7 @@ inparam.nmax = inparam.ninit*10;
 [q3,out3] = funappx_g(f, inparam);
 x = a:1.0/(out3.npoints*2):b;
 figure;
-plot(x,abs(f(x)-q3(x)));
+semilogy(x,abs(f(x)-q3(x)));
 xlabel('$x$','interpreter','latex')
 ylabel('absolute error')
 axis tight
