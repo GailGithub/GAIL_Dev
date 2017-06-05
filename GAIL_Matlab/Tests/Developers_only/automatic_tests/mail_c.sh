1dcff66551b31423b5f445b5f44eadf0476ddb68
jagadeesr@gmail.com <jrathin1@iit.edu>
2017-06-04 19:38:43 -0500

    Adding Jagadees's email ID to receive reports

--- GAIL_Matlab/Tests/Developers_only/automatic_tests/mail_c.sh
+++ GAIL_Matlab/Tests/Developers_only/automatic_tests/mail_c.sh
@@ -1,4 +1,1 @@
-mail -s "Daily test results were OK (report)" schoi32@iit.edu < /home/gail/GAIL_tests/PBS_jobs/pbs_reports/gail_daily_tests-$(date +\%Y-\%m-\%d).out 
-
-
-
+mail -s "Daily test results were OK (report)" schoi32@iit.edu,jrathin1@iit.edu < /home/gail/GAIL_tests/PBS_jobs/pbs_reports/gail_daily_tests-$(date +\%Y-\%m-\%d).out 
