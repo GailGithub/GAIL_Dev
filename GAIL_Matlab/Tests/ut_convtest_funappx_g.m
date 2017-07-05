d24e5976f20c856e05c84619df7f65c12ccdcfb8
Yuhan Ding <yding2@hawk.iit.edu>
2017-07-05 10:27:15 -0500

    move HighAccuracy test of funappx_g to ut_convtest_funappx_g.m

--- GAIL_Matlab/Tests/ut_convtest_funappx_g.m
+++ GAIL_Matlab/Tests/ut_convtest_funappx_g.m
@@ -9,11 +9,12 @@ classdef ut_convtest_funappx_g < matlab.unittest.TestCase
       testCase.verifyGreaterThanOrEqual(sum(errest<=errestglobal),5);
     end
     
-%      function convtest_timecost_funappx_g(testCase)
-%       [~,~,t,~,~,tglobal]=funappx_convtest;
-%       testCase.verifyGreaterThanOrEqual(tglobal(5),t(5));
-%       testCase.verifyGreaterThanOrEqual(sum(t<tglobal),5);
-%     end
+    function convtest_HighAccuracy_funappx_g(testCase)
+       [fappx, result] = funappx_g(@(x)exp(x),'a',-2,'b',2,'ninit',15, 'abstol', 1e-12);
+       x = rand(1000000,1)*(result.b-result.a)+result.a;
+       actualerr = max(abs(fappx(x)-result.f(x)));
+       testCase.verifyLessThanOrEqual(actualerr,result.abstol);
+    end
     
   end
 end
