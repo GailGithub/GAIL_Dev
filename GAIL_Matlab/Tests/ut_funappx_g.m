3622a103a009fed01f874d9f04818e6cf19c0ba3
Yuhan Ding <yding2@hawk.iit.edu>
2017-07-05 10:27:15 -0500

    move HighAccuracy test of funappx_g to ut_convtest_funappx_g.m

--- GAIL_Matlab/Tests/ut_funappx_g.m
+++ GAIL_Matlab/Tests/ut_funappx_g.m
@@ -169,12 +169,6 @@ classdef ut_funappx_g < matlab.unittest.TestCase
             testCase.verifyEqual(result.npoints,6);
          end
          
-         function funappx_gHighAccuracy(testCase)
-            [fappx, result] = funappx_g(@(x)exp(x),'a',-2,'b',2,'ninit',15, 'abstol', 1e-12);
-            x = rand(1000000,1)*(result.b-result.a)+result.a;
-            actualerr = max(abs(fappx(x)-result.f(x)));
-            testCase.verifyLessThanOrEqual(actualerr,result.abstol);
-        end
          
     end
 end
