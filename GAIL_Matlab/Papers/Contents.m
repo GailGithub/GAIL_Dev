aaacba5fb751f3f10c90062165cd0e4bcf9ed97a
Sou-Cheng Terrya Choi <schoi32@iit.edu>
2017-07-16 16:54:01 +0800

    Add paper for integral_g and move ConesNotBalls paper to OldPapers directory.

--- GAIL_Matlab/Papers/Contents.m
+++ GAIL_Matlab/Papers/Contents.m
@@ -1,10 +1,10 @@
 % PAPERS
 %
 % Folders
-%   ConesNotBallsPaper  - code of examples in Cones Not Balls Paper  
 %   MCQMC2012Paper      - code of examples in MCQMC paper
 %   Paper_cubLattice_g  - code of examples in cubLattice_g paper
-%   Paper_cubSobol_g    - code of examples in cubSobol_g paper  
+%   Paper_cubSobol_g    - code of examples in cubSobol_g paper
+%   Paper_integral_g    - code of examples in integral_g paper
 %   Thesis              - code of examples in various theses
 %   TraubPaper          - code of examples in Traub memorial paper
 %   OldPapers           - directory for archiving work of old papers 
--- GAIL_Matlab/Papers/OldPapers/Contents.m
+++ GAIL_Matlab/Papers/OldPapers/Contents.m
@@ -1,5 +1,6 @@
 % OLDPAPERS
 %
 % Folders
+%   ConesNotBallsPaper  - code of examples in Cones Not Balls Paper 
 %    XinTongThesis - Xin Tong's Thesis (2014)
  
