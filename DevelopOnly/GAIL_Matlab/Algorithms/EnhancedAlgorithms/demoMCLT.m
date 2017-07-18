022e0efde24b7a0dab4bca93b584fe659366e32d
Fred J. Hickernell <hickernell@iit.edu>
2017-07-18 12:12:32 -0500

    debugging demoMCLT

--- DevelopOnly/GAIL_Matlab/Algorithms/EnhancedAlgorithms/demoMCLT.m
+++ DevelopOnly/GAIL_Matlab/Algorithms/EnhancedAlgorithms/demoMCLT.m
@@ -2,21 +2,20 @@
 % For a random variable \(Y = f(\boldsymbol{X})\), we will estimate its expected value:
 %
 % \[
-%  \mu=\mathbb{E}(Y) =
-%  \mathbb{E}[f(\boldsymbol{X})]=\int_{\mathbb{R}^d} f(\boldsymbol{x}) \rho(\boldsymbol{x}) d\boldsymbol{x}
-%  \approx \frac{1}{n}\sum_{i=1}^{n}{f(x_i)}, \, {x_i} \,
-%  \text{IID} \sim
+%  \mu=\mathbb{E}(Y) = \mathbb{E}[f(\boldsymbol{X})]=\int_{\mathbb{R}^d}
+%  f(\boldsymbol{x}) \rho(\boldsymbol{x}) \, \mathrm{d}\boldsymbol{x}
+%  \approx \frac{1}{n}\sum_{i=1}^{n}{f(x_i)}, \qquad {x_i} \, \text{IID} \sim
 %  \, \rho
 % \]
 %
-% We will approximate \(\mu\) using meanMC_CLT GAIL method. It is a IID
-% Monte Carlo algorithm using Central Limit Theorem to determine the sample
-% size. In order to improve computation efficiency, we will use control
-% variates.
+% We will approximate \(\mu\) using the meanMC_CLT GAIL algorithm. It is an
+% IID Monte Carlo algorithm using the Central Limit Theorem to determine
+% the sample size required to meet the desired error tolerance. In order to
+% improve computation efficiency, we will also use control variates.
 %%
 
 function demoMCLT
-%% Initialize the workspace and setting the display parameters
+%% Initialize the workspace and set the display parameters
 % This script cleans up the workspace and makes the display beautiful.
 
 gail.InitializeWorkspaceDisplay %initialize the workspace and the display parameters
@@ -24,23 +23,29 @@ gail.InitializeWorkspaceDisplay %initialize the workspace and the display parame
 %% Example 1: Estimate \(\mathbb{E}[f(\boldsymbol{X})]\) where \(f(\boldsymbol{x})=\exp(-\boldsymbol{x}^2)\) and \(\boldsymbol{X} \sim \mathcal{U} (0,1)\) using \(f(\boldsymbol{x})=\boldsymbol{x}\) as a control variate
 % In this example a function that cannot be integrated analytically is
 % integrated using our adaptive IID Monte Carlo method.  We also use the
-% function  \(x\) as a control variate.
+% function  \(x \mapsto x\) as a control variate.
+%
+% First we plot our function
 
-absTol=1e-3; %absolute tolerance 
-relTol=0; %relative tolerance
-f=@(x)[exp(-x.^2),x]; YXn=@(n)f(rand(n,1)); %set up the random variable 
-
+f = @(x)[exp(-x.^2), x]; YXn = @(n)f(rand(n,1)); %set up the random variable 
 figure %plot f(x)
-x=0:0.001:1;
-plot(x,exp(-x.^2),'-'); 
+x = (0:0.001:1)'; %x values for plot
+fx = f(x);
+plot(x,fx(:,1),'-'); 
 ylabel('\(\exp(-x^2)\)')
 xlabel('\(x\)')
 
+%%
+% Next we set up the parameters for the Monte Carlo integration
+
+absTol = 1e-3; %absolute tolerance 
+relTol = 0; %relative tolerance
 s=struct('Y',YXn,'nY',1,'trueMuCV',1/2); % create a structure containing random variables, number of random variables and mean of the control variates 
 [hmu,out]=meanMC_CLT(s,absTol,relTol);  % calculate the mean
 exactsol=erf(1)*sqrt(pi)/2; %true mean
 disp('Example 1')
 disp(['Estimated mean is: ' num2str(hmu)])
+disp(['     True mean is: ' num2str(exactsol)])
 disp(['The algorithm took ' num2str(out.time) ' seconds and '...
     num2str(out.nSample) ' points.'])
 disp(['Real error was ' ...
@@ -54,7 +59,7 @@ disp(['Real error was ' ...
 % control variate.
 %
 % Initialize option parameters for a European call option
-inp.timeDim.timeVector = 0:0.25:1; %%time increments
+inp.timeDim.timeVector = 0:0.25:1; %time increments
 inp.assetParam.initPrice = 10; %initial stock price
 inp.assetParam.interest = 0.01; %risk-free interest rate
 inp.assetParam.volatility = 0.5; %volatility
@@ -80,9 +85,10 @@ print -depsc PayoffCDF.eps %print the plot to a .eps file
 
 %%
 % Next we price the option using simple IID Monte Carlo.  We happen to have
-% a formulat for the exact price that can be used to check our error.
+% a formula for the exact price that can be used to check our error.
 [hmu,out]=meanMC_CLT(EuroCallPayoff,inp.priceParam.absTol,inp.priceParam.relTol);
-disp(['Estimated mean is: ' num2str(hmu)])
+disp(['Estimated price is: ' num2str(hmu)])
+disp(['    Exact price is: ' num2str(EuroCall.exactPrice)])
 disp(['The algorithm took ' num2str(out.time) ' seconds and '...
     num2str(out.nSample) ' points.'])
 disp(['Real error was ' ...
@@ -92,7 +98,7 @@ disp(['Real error was ' ...
 
 %%
 % Now we used the stock price as a control variate.  To do that, we need to
-% modify the option payoff object.
+% modify the option payoff object by adding the stock price.
 
 EuroCallCV = optPayoff(EuroCall); %make a copy of the European call option parameters
 EuroCallCV.payoffParam = struct('optType', {{'euro','stockprice'}}, ...
@@ -100,19 +106,49 @@ EuroCallCV.payoffParam = struct('optType', {{'euro','stockprice'}}, ...
 EuroCallCVPayoff=@(n) genOptPayoffs(EuroCallCV,n); %identify the payoff function
 s=struct('Y',EuroCallCVPayoff,'nY',1,'trueMuCV',EuroCallCV.assetParam.initPrice);
 [hmu,out]=meanMC_CLT(s,inp.priceParam.absTol,inp.priceParam.relTol);
-disp(['Estimated mean is: ' num2str(hmu)])
+disp(['Estimated price is: ' num2str(hmu)])
+disp(['    Exact price is: ' num2str(EuroCallCV.exactPrice(1))])
 disp(['The algorithm took ' num2str(out.time) ' seconds and '...
     num2str(out.nSample) ' points.'])
 disp(['Real error was ' ...
-    num2str(abs(EuroCall.exactPrice-hmu))...
+    num2str(abs(EuroCallCV.exactPrice(1)-hmu))...
     ' which is less than the user input tolerance '...
     num2str(inp.priceParam.absTol) '.'])
 
 %%
-% See how meanMC_CLT works on pricing a European call option with
-% and without a control variate. While the two both give estimation within
-% error torelance,adding control variate uses only about 1/9 of sample points
-% and takes 1/5 of the time.
+% Let's compare how meanMC_CLT works on pricing a European call option with
+% and without a control variate. While the two methods both give answers
+% within the error tolerance, adding a control variate uses only about 1/9
+% of sample points and takes 1/5 of the time.
+%
+% Acutally 
+
+inp.priceParam.cubMethod = 'IID_MC_CLT';
+EuroCall = optPrice(inp);
+[EuroCallPrice, out] = genOptPrice(EuroCall); %compute the option price
+disp(['Estimated price is: ' num2str(EuroCallPrice)])
+disp(['    Exact price is: ' num2str(EuroCall.exactPrice)])
+disp(['The algorithm took ' num2str(out.time) ' seconds and '...
+    num2str(out.nPaths) ' points.'])
+disp(['Real error was ' ...
+    num2str(abs(EuroCall.exactPrice-EuroCallPrice))...
+    ' which is less than the user input tolerance '...
+    num2str(inp.priceParam.absTol) '.'])
+
+EuroCallCV = optPrice(EuroCall); %make a copy of the European call option parameters
+EuroCallCV.payoffParam = struct('optType', {{'euro','stockprice'}}, ...
+   'putCallType', {{'call',''}}); %identify the option type
+[EuroCallCVPrice, out] = genOptPrice(EuroCallCV); %identify the payoff function
+disp(['Estimated price is: ' num2str(EuroCallCVPrice)])
+disp(['    Exact price is: ' num2str(EuroCallCV.exactPrice(1))])
+disp(['The algorithm took ' num2str(out.time) ' seconds and '...
+    num2str(out.nPaths) ' points.'])
+disp(['Real error was ' ...
+    num2str(abs(EuroCallCV.exactPrice(1)-EuroCallCVPrice))...
+    ' which is less than the user input tolerance '...
+    num2str(inp.priceParam.absTol) '.'])
+
+
 
 
 %% Example 3: Keister's multidimensional integration
