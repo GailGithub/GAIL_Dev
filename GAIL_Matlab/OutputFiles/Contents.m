18cc2488ce1df94a40bb4a2da0ae2d793e80e154
Xin <xtong5@hawk.iit.edu>
2017-08-16 17:43:47 -0500

    keep Xin’s thesis and its codes and remove the out put files

--- GAIL_Matlab/OutputFiles/Contents.m
+++ GAIL_Matlab/OutputFiles/Contents.m
@@ -6,5 +6,4 @@
 %   Paper_cubSobol_g      - output files of cubSobol_g paper
 %   Paper_integral_g      - output files of integral_g paper
 %   WorkoutFunappxOutput  - output files of funappx_g workouts
-%   WorkoutFunminOutput   - output files of funmin_g workouts
 %   WorkoutIntegralOutput - output files of integral_g workouts
