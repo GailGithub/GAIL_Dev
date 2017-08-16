12742188a4201ae7c76e9199e946a1970a1f44e4
Kan Zhang <zhangkan.zhangkan@gmail.com>
2017-08-15 19:28:19 -0500

    Update DownloadInstallGail.m to 2.2.

--- DevelopOnly/DownloadInstallGail_2_2.m
+++ DevelopOnly/DownloadInstallGail_2_2.m
@@ -1,16 +1,17 @@
-% This scripts is to download and install GAIL version 2.0.0 into the
+function DownloadInstallGail_2_2()
+% This function downloads and installs GAIL version 2.2 into the
 % location you choose
 %
 %   Step 1.  Place this M-file where you want GAIL to go
 %
 %   Step 2.  Run this M-file in MATLAB
 %
-%This file installs GAIL version 2.0.0 in the subdirectory
-%"GAIL_2.0.0/GAIL_Matlab".
+%This file installs GAIL version 2.2 in the subdirectory
+%"GAIL_2.2/GAIL_Matlab".
 %% Download the package and change the directory
 disp('The GAIL package is now being downloaded...')
-unzip('http://math.iit.edu/~openscholar/sites/default/files/meshfree/files/gail_2_0.zip') %download and unzip
-cd('GAIL_2_0')  %get to the right subdirectory
+unzip('http://math.iit.edu/~openscholar/sites/default/files/meshfree/files/gail_2_2.zip') %download and unzip
+cd('GAIL_Matlab')  %get to the right subdirectory
 
 %% Install GAIL
 GAIL_Install      %this installs GAIL
@@ -18,9 +19,10 @@ fprintf('\n\nNext we will run a quick test. Press RETURN to continue...\n')
 pause
 
 %% Run a quick test
-fprintf('\nmuhat=meanMC_g(@(n) rand(n,1))\n')
-muhat=meanMC_g(@(n) rand(n,1)) %run meanMC_g once
-fprintf('\nThis answer should be close to 0.5.\n\n')
+fprintf('\nmuhat=meanMC_g(@(n) rand(n,1))\n');
+muhat=meanMC_g(@(n) rand(n,1)); %run meanMC_g once
+fprintf('\nmuhat = %.4f\n',muhat);
+fprintf('\nThis answer should be close to 0.5.\n\n');
 %Then you should be ready to use GAIL
 disp('Next README.txt will be displayed. Press RETURN to continue to ');
 disp('the next line, or press the spacebar to advance to the next page...');
@@ -31,3 +33,4 @@ moreStatus = get(0,'More');
 more on
 type('README.txt')
 more(moreStatus);
+end
