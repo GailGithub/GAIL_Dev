40317568918d3e8590c4031eed40d4f02f070d23
jagadeeswaran <jagadeesr@gmail.com>
2017-08-19 14:08:30 -0500

    adding debug plot functions

---
+++ GAIL_Matlab/Papers/Theses/Jagadees/MVNIntegrandPlots.m
@@ -0,0 +1,96 @@
+
+%% Plot the Keister function
+dim = 2;
+normsqd = @(t) sum(t.^2, 2); %squared l_2 norm of t
+replaceZeros = @(t) (t+(t==0)*eps); % to avoid getting infinity, NaN
+yinv = @(t)(erfcinv( replaceZeros(abs(t)) ));  %using erfcinv is more accurate than erfinv with -1
+fKeister = @(t,dim) cos( sqrt( normsqd(yinv(t)) )) *(sqrt(pi)/2)^dim;
+f = @(x) fKeister(x,dim);
+
+xplot = (-5:0.002:5);
+nx = numel(xplot);
+[xx,yy] = meshgrid(xplot);
+xyplot = [xx(:) yy(:) zeros(nx*nx, dim-2)];
+zz = reshape(f(xyplot),nx,nx);
+figure
+surf(xx,yy,zz)
+shading interp
+xlabel('\(x_1\)')
+ylabel('\(x_2\)')
+zlabel('\(f_{\textrm{Keister}}(x_1,x_2)\)')
+view(-20,20)
+print -depsc Keisteri_cube.eps
+
+
+f = @(x) cos(sum(x.^2, 2)).*exp(-sum(x.^2, 2));
+xplot = (-5:0.002:5);
+nx = numel(xplot);
+[xx,yy] = meshgrid(xplot);
+xyplot = [xx(:) yy(:) zeros(nx*nx, dim-2)];
+zz = reshape(f(xyplot),nx,nx);
+figure
+surf(xx,yy,zz)
+shading interp
+xlabel('\(x_1\)')
+ylabel('\(x_2\)')
+zlabel('\(f_{\textrm{Keister}}(x_1,x_2)\)')
+view(-20,20)
+print -depsc Keisteri_wholeR.eps
+
+
+%% Plot the Exp(Cos) function
+f = @(x) exp(sum(cos(2*pi*x), 2));
+exactInteg = besseli(0,1)^dim;
+xplot = (0:0.002:1);
+nx = numel(xplot);
+[xx,yy] = meshgrid(xplot);
+dim = 2;
+xyplot = [xx(:) yy(:) zeros(nx*nx, dim-2)];
+zz = reshape(f(xyplot),nx,nx);
+figure
+surf(xx,yy,zz)
+shading interp
+xlabel('\(x_1\)')
+ylabel('\(x_2\)')
+zlabel('\( e^{\cos(2\pi x_1) + \cos(2\pi x_2)}\)')
+view(-20,20)
+print -depsc ExpCos.eps
+
+
+%% Produce plots for MVNExample
+gail.InitializeWorkspaceDisplay %clean up 
+load MVNProbExampleAllData.mat
+markerSize = num2cell(10*ones(size(markerSize)));
+
+%% Plot the Genz function
+xplot = (0:0.002:1);
+nx = numel(xplot);
+[xx,yy] = meshgrid(xplot);
+dim = numel(MVNProbIIDGn.a) - 1;
+xyplot = [xx(:) yy(:) zeros(nx*nx, dim-2)];
+zz = reshape(MVNProbIIDGn.f(xyplot),nx,nx);
+figure
+surf(xx,yy,zz)
+shading interp
+xlabel('\(x_1\)')
+ylabel('\(x_2\)')
+zlabel('\(f_{\textrm{Genz}}(x_1,x_2)\)')
+view(-20,20)
+print -depsc GenzFun.eps
+
+%% Plot the Affine function
+xplot = (0:0.002:1);
+nx = numel(xplot);
+[xx,yy] = meshgrid(xplot);
+dim = numel(MVNProbSobolAn.a);
+xyplot = [xx(:) yy(:) 0.5*ones(nx*nx, dim-2)];
+zz = reshape(MVNProbSobolAn.f(xyplot),nx,nx);
+figure
+surf(xx,yy,zz)
+shading interp
+xlabel('\(x_1\)')
+ylabel('\(x_2\)')
+zlabel('\(f_{\textrm{aff}}(x_1,x_2,1/2)\)')
+view(-45,20)
+print -depsc AffineFun.eps
+
---
+++ GAIL_Matlab/Papers/Theses/Jagadees/plot_nvec_vs_computeTime.m
@@ -0,0 +1,60 @@
+function plot_nvec_vs_computeTime(nvec, computeTime, visiblePlot, figSavePath)
+
+
+% plot_nvec_vs_computeTime_matern_example(visiblePlot)
+
+  
+%% Plot n vs time for the given cubature
+if exist('visiblePlot','var') && visiblePlot==false
+    hFigTime = figure('visible','off');
+else
+    hFigTime = figure(); %21
+end
+
+set(hFigTime, 'units', 'inches', 'Position', [4 4 6.5 5.5])
+loglog(nvec,computeTime, 'b.-', ...
+  [nvec(1) nvec(end)],computeTime(2)*[1 (nvec(end)/nvec(1))^1], 'g--')
+
+legend({'Compute time', '\(O(n^{})\)'}, ...
+                'location','northeast')
+              
+axis([100 1e7 1e-2 1e5])
+set(gca,'Xtick',(10.^(2:7)),'YTick',(10.^(-2:2:5)))
+xlabel('Sample Size, \(n\)')
+ylabel('Comp.\ time')
+%title('MVN with Matern kernel')
+
+saveas(hFigTime, figSavePath)
+
+
+
+
+end
+
+
+function plot_nvec_vs_computeTime_matern_example(visiblePlot)
+
+%% Plot n vs time for bayesian cubature
+if exist('visiblePlot','var') && visiblePlot==false
+    hFigTime = figure('visible','off');
+else
+    hFigTime = figure(); %21
+end
+
+nii = 2.^(7:13);
+computeTime = [0.080798000000000, 0.104679000000000, 0.292205000000000, 1.481677000000000, ....
+ 14.238427000000000, 1.083312010000000e+02, 7.074992927000000e+03];
+
+loglog(nii,computeTime, 'b.-', ...
+  nii, computeTime(1)*(exp(0.2*nii/nii(1))), 'r--', ...
+  [nii(1) nii(end)],computeTime(1)*[1 (nii(end)/nii(1))^3 ], 'g--')
+legend({'Compute time', '\(O(e^{0.2n})\)', '\(O(n^{3})\)'}, ...
+                'location','southeast')
+              
+axis([100 1e7 1e-2 1e5])
+set(gca,'Xtick',(10.^(2:7)),'YTick',(10.^(-2:2:5)))
+xlabel('Sample Size, \(n\)')
+ylabel('Comp.\ time')
+%title('MVN with Matern kernel')
+print -depsc MVN_bayesianCubaturecomputeTime.eps
+end
