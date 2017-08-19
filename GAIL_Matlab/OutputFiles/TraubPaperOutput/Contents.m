c26d6711eeaf8482440ea3f2a529c0495a799c0f
Sou-Cheng Terrya Choi <schoi32@iit.edu>
2017-08-19 12:01:45 -0500

    Contents.m for TraubPaper Outputs.

---
+++ GAIL_Matlab/OutputFiles/TraubPaperOutput/Contents.m
@@ -0,0 +1,16 @@
+% TRAUBPAPEROUTPUT
+%
+% Files
+%
+% ConeDefineFig-2017-08-19-11-45-15.eps     --- Figure 1 in paper
+% chebfun_errors-2017-08-19-11-45-36.eps    --- Figure 4(a) in paper
+% f3foolplot-2017-08-19-11-45-17.eps        --- Figure 2(b) in paper
+% f3plot-2017-08-19-11-45-16.eps            --- Figure 2(a) in paper
+% funappx_g_test-2016-10-17-18-20-32.mat    --- Table 1 in paper   
+% funappx_g_test-2017-08-19-11-45-44.txt    --- Table 1 in paper            
+% funappx_g_test-2017-08-19-11-45-44.eps    --- Figure 4(b) in paper
+% funmin_g_test-2017-08-19-11-45-46.txt     --- Table 1 in paper
+% funmin_g_test-2016-10-17-18-21-21.mat     --- Table 1 in paper   
+% sampling-funappxg-2017-08-19-11-45-18.eps --- Figure 3(a) in paper
+% sampling-funming-2017-08-19-11-45-35.eps  --- Figure 3(b) in paper
+
