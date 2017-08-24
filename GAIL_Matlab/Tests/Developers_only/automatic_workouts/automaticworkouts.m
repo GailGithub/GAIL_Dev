a13c775e37f855508436201d7b0a29d081c727e5
jagadeeswaran <jagadeesr@gmail.com>
2017-08-23 23:25:58 -0500

    fixing longtest errors; installed chebfun in Karlin, added path

--- GAIL_Matlab/Tests/Developers_only/automatic_workouts/automaticworkouts.m
+++ GAIL_Matlab/Tests/Developers_only/automatic_workouts/automaticworkouts.m
@@ -2,6 +2,13 @@
 cd /home/gail/GAIL_tests/repo/gail-development/GAIL_Matlab/
 % Install the latest
 GAIL_Install
+
+%% In Karlin server, chebfun is installed at this location
+% cd /home/gail/GAIL_tests
+% git clone https://github.com/chebfun/chebfun.git
+chebfunroot = '/home/gail/GAIL_tests/chebfun';
+addpath(chebfunroot), savepath
+
 % Go to the tests direcoty (% is comment in matlab environment)
 cd /home/gail/GAIL_tests/repo/gail-development/GAIL_Matlab/Tests/Developers_only/automatic_workouts/
 
