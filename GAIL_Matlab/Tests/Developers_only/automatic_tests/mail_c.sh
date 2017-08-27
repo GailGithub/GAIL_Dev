37e7bbd1e9801e5708a0b28930fac64417e35f76
jagadeeswaran <jagadeesr@gmail.com>
2017-08-27 10:01:30 -0500

    Test output files are sent as attachment instead of body

--- GAIL_Matlab/Tests/Developers_only/automatic_tests/mail_c.sh
+++ GAIL_Matlab/Tests/Developers_only/automatic_tests/mail_c.sh
@@ -1,1 +1,6 @@
-mail -s "Daily test results were OK (report)" schoi32@iit.edu,jrathin1@iit.edu < /home/gail/GAIL_tests/PBS_jobs/pbs_reports/gail_daily_tests-$(date +\%Y-\%m-\%d).out 
+#!/bin/sh
+
+# attach the file, instead of putting inte body, as the file contains non printable chars 
+file_attach="/home/gail/GAIL_tests/PBS_jobs/pbs_reports/gail_daily_tests-$(date +\%Y-\%m-\%d).out"
+uuencode $file_attach $file_attach 
+mail -s "Daily test results were OK (report)" -a $file_attach schoi32@iit.edu,jrathin1@iit.edu
--- GAIL_Matlab/Tests/Developers_only/automatic_tests/mail_w.sh
+++ GAIL_Matlab/Tests/Developers_only/automatic_tests/mail_w.sh
@@ -1,14 +1,25 @@
+#!/bin/sh
+
 #echo "Tests gave errors. See the output attached." | mutt -a "/home/gail/GAIL_tests/test_results.txt" -s "subject of message" -- gail_dev@googlegroups.com
 
-
+# attach the file, instead of putting inte body, as the file contains non printable chars 
+fileattach="/home/gail/GAIL_tests/PBS_jobs/pbs_reports/gail_daily_tests-$(date +\%Y-\%m-\%d).out"
+echo Todays test output file is $fileattach
 # Sending PBS reports
-mail -s "Daily test results were WRONG: REPORT" schoi32@iit.edu,jrathin1@iit.edu < /home/gail/GAIL_tests/PBS_jobs/pbs_reports/gail_daily_tests-$(date +\%Y-\%m-\%d).out
+uuencode $fileattach $fileattach
+mail -s "Daily test results were WRONG: REPORT" -a $fileattach schoi32@iit.edu,jrathin1@iit.edu < /dev/null
 
 
 # And if you want mail to read the content from a file:
 nl=$'\n\n';
 begin=" We got some errors in our daily tests:";
-changes=$(< Test_Results.txt);
-echo "${begin}${nl}${changes}" | mail -s "Daily test results were WRONG: ERRORS" schoi32@iit.edu,jrathin1@iit.edu
+uuencode Test_Results.txt Test_Results.txt
+# changes=$(< Test_Results.txt);
+echo "${begin}${nl}" | mail -s "Daily test results were WRONG: ERRORS" -a "Test_Results.txt" schoi32@iit.edu,jrathin1@iit.edu
 
+#echo "${begin}${nl}${changes}" | mail -s "Daily test results were WRONG: ERRORS" schoi32@iit.edu,jrathin1@iit.edu
 #(echo "${begin}${nl}${changes}";uuencode Test_Results.txt Test_Results.txt) | mail -s "GAIL daily test results with ERRORS" gail_dev@googlegroups.com -- -r "ljimene1@hawk.iit.edu"
+
+# Reference
+# http://www.binarytides.com/linux-mailx-command/
+# https://unix.stackexchange.com/questions/42145/shell-script-hangs-on-mail-command
