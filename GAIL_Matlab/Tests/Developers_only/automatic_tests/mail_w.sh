d87a54fc28b58ec63fab051027328012a6a98d04
jagadeeswaran <jagadeesr@gmail.com>
2017-09-25 21:19:43 -0500

    sendemail support for matlab version

--- GAIL_Matlab/Tests/Developers_only/automatic_tests/mail_w.sh
+++ GAIL_Matlab/Tests/Developers_only/automatic_tests/mail_w.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 TO_EMAIL_IDS="Sou-cheng<schoi32@iit.edu>, Jagadeeswaran<jrathin1@iit.edu>"
 
+if [ $# -eq 0 ]
+  then
+    echo "mail_w: No arguments supplied"
+    matlabVer=
+else
+  echo 'mail_w: Input arg' $1
+  matlabVer=$1
+fi
+
 #echo "Tests gave errors. See the output attached." | mutt -a "/home/gail/GAIL_tests/test_results.txt" -s "subject of message" -- gail_dev@googlegroups.com
 
 # attach the file, instead of putting inte body, as the file contains non printable chars
@@ -9,8 +18,8 @@ echo Todays test output file is $fileattach
 # Sending PBS reports
 # uuencode $fileattach $fileattach
 DATE_TODAY=$(date +\%Y-\%m-\%d)
-SUBJECT="Daily test results were WRONG: REPORT ${DATE_TODAY}"
-echo "Please find the attached report" | mail -s "$SUBJECT" -a $fileattach ${TO_EMAIL_IDS}
+SUBJECT="Daily test results were WRONG:$matlabVer REPORT ${DATE_TODAY}"
+echo "Please find the attached report" | mail -s "$SUBJECT" -a $fileattach -a "Test_Results$matlabVer.txt" ${TO_EMAIL_IDS}
 
 
 # And if you want mail to read the content from a file:
@@ -19,7 +28,7 @@ begin=" We got some errors in our daily tests:";
 # uuencode Test_Results.txt Test_Results.txt
 # changes=$(< Test_Results.txt);
 SUBJECT="Daily test results were WRONG: ERRORS ${DATE_TODAY}"
-echo "${begin}${nl}" | mail -s "$SUBJECT" -a "Test_Results.txt" ${TO_EMAIL_IDS}
+# merged into one email echo "${begin}${nl}" | mail -s "$SUBJECT" -a "Test_Results_$matlabVer.txt" ${TO_EMAIL_IDS}
 
 #echo "${begin}${nl}${changes}" | mail -s "Daily test results were WRONG: ERRORS" schoi32@iit.edu,jrathin1@iit.edu
 #(echo "${begin}${nl}${changes}";uuencode Test_Results.txt Test_Results.txt) | mail -s "GAIL daily test results with ERRORS" gail_dev@googlegroups.com -- -r "ljimene1@hawk.iit.edu"
