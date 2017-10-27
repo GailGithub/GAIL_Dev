8d674d603c3b3f65aff89e4b8269b28f00da72ec
Jagadeeswaran Rathinavel <jagadeesr@gmail.com>
2017-10-26 22:44:25 -0500

    adding debug info to longtests

--- GAIL_Matlab/Tests/Developers_only/automatic_workouts/longtests.m
+++ GAIL_Matlab/Tests/Developers_only/automatic_workouts/longtests.m
@@ -1,12 +1,20 @@
 % LONGTESTS Drives all lengthy doctests, unit tests, workouts, and scripts
 
 [GAILPATH,GAILVERSION,MATLABVERSION]  = GAILstart(false);
+ver_str = sprintf('MatlabVer%2.2f', MATLABVERSION)
+ver_str = strrep(ver_str,'.','-')
 filename = strcat(GAILPATH,'OutputFiles',filesep,...
-    'gail_workouts-', datestr(now,'yyyy-mm-dd-HH-MM-SS'),'.txt');
+    'gail_workouts-', datestr(now,'yyyy-mm-dd-HH-MM-SS'), '_', ver_str, '.txt');
 diary(filename)
 tic;
 
 format short
+
+% print matlab version
+ver
+
+% print chebfun version
+help chebfun
 
 %% utilties
 run_handle_ut('ut_save_mat')
@@ -15,28 +23,28 @@ if usejava('jvm') || MATLABVERSION <= 7.12
 end
 
 %% Workouts
-fprintf('Starting longtests at %s \n', datetime)
+fprintf('=============================Starting longtests at %s \n', datestr(now,'yyyy-mm-dd-HH-MM-SS'))
 longtests_cubLattice_g
-fprintf('Finished longtests_cubLattice_g at %s \n', datetime)
+fprintf('=============================Finished longtests_cubLattice_g at %s \n', datestr(now,'yyyy-mm-dd-HH-MM-SS'))
 longtests_cubMC_g
-fprintf('Finished longtests_cubMC_g at %s \n', datetime)
+fprintf('=============================Finished longtests_cubMC_g at %s \n', datestr(now,'yyyy-mm-dd-HH-MM-SS'))
 longtests_cubSobol_g
-fprintf('Finished longtests_cubSobol_g at %s \n', datetime)
+fprintf('=============================Finished longtests_cubSobol_g at %s \n', datestr(now,'yyyy-mm-dd-HH-MM-SS'))
 longtests_funappx_g
-fprintf('Finished longtests_funappx_g at %s \n', datetime)
+fprintf('=============================Finished longtests_funappx_g at %s \n', datestr(now,'yyyy-mm-dd-HH-MM-SS'))
 longtests_funmin_g
-fprintf('Finished longtests_funmin_g at %s \n', datetime)
+fprintf('=============================Finished longtests_funmin_g at %s \n', datestr(now,'yyyy-mm-dd-HH-MM-SS'))
 longtests_integral_g
-fprintf('Finished longtests_integral_g at %s \n', datetime)
+fprintf('=============================Finished longtests_integral_g at %s \n', datestr(now,'yyyy-mm-dd-HH-MM-SS'))
 longtests_meanMC_g
-fprintf('Finished longtests_meanMC_g at %s \n', datetime)
+fprintf('=============================Finished longtests_meanMC_g at %s \n', datestr(now,'yyyy-mm-dd-HH-MM-SS'))
 %% Papers
 %longtests_conePaper
 
 %% doctests and unit tests for deprecated algos
 longtests_deprecated_algos
 
-fprintf('Finished all the longtests at %s \n', datetime)
+fprintf('=============================Finished all the longtests at %s \n', datestr(now,'yyyy-mm-dd-HH-MM-SS'))
 
 time=toc;
 disp(time)
