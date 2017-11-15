3972b2b33d63cca7260e9bdd677a7883b22c7bf2
Kan Zhang <zhangkan.zhangkan@gmail.com>
2017-11-15 13:56:31 -0600

    Take out isPublish.

--- GAIL_Matlab/GAIL_Install.m
+++ GAIL_Matlab/GAIL_Install.m
@@ -1,14 +1,10 @@
-function GAIL_Install(isBuild,isPublish)
+function GAIL_Install(isBuild)
 %GAIL_INSTALL Install or reinstall GAIL. Add GAIL paths to MATLAB search path.
 %
 % isBuild: true to download and explode a zip file. Default to false.
-% isPublish: true to publish HTML format of GAIL documentation. Default to false.
 %
 if nargin < 1
     isBuild =  false;
-end
-if nargin < 2
-    isPublish = false;
 end
 [GAILPATH,GAILVERSION,MATLABVERSION] = GAILstart(false);
 fprintf('\nWelcome to Guaranteed Automatic Integration Library (GAIL).\nYou are installing GAIL v%s.\n\n', GAILVERSION);
@@ -24,9 +20,6 @@ addpath(gailp);           % Add GAIL directories and subdirectories
 savepath;                 % Save the changes
 if isBuild
    unzip('http://www.math.iit.edu/~Meshfree-methods-seminar/GAIL/GAIL_Build.zip',[GAILPATH,'..',filesep]);
-end
-if isPublish
-   gail.GAIL_Publish(isPublish,~isPublish,isPublish);
 end
 fprintf('\nGAIL version %s has been installed successfully.\n\n', GAILVERSION);
 end
