faa031627c6424a0a33ded7ac6c6aa95e9ae3e5c
Anil Simon <asimon6@hawk.iit.edu>
2017-11-22 18:42:10 -0600

    Added tests for MeanYParam

--- GAIL_Python/Algorithms/IntegrationExpectation/test_meanYParam.py
+++ GAIL_Python/Algorithms/IntegrationExpectation/test_meanYParam.py
@@ -1,78 +1,71 @@
-from meanYParam import MeanYParam, default_random_generator
-
-import inspect
 import pytest
+try:
+    from GAIL_Python.Algorithms.IntegrationExpectation.meanYParam import MeanYParam, default_random_generator
+except:
+    from meanYParam import MeanYParam, default_random_generator
 
 
-@pytest.mark.parametrize(
-    'property, default_value, comment', [
-        ('alpha', 0.01, '  Comment: default alpha = 0.01'),
-        ('absTol', 0.01, '  Comment: default absTol = 0.01'),
-        ('nSig', 1000, '  Comment: default nSig = 1000'),
-        ('relTol', 0, '  Comment: default relTol = 0'),
-        ('Y', default_random_generator, '  Comment: default Y = Uniform Square Random generator')
-    ])
-def test_defaults(property, default_value, comment):
+def simple_test(property_name, tests=None):
+    """Constructs parameter lists for use in parametrised test"""
+    if tests is None:
+        tests = ['non-numeric', 'negative', 'empty', 'null']
+
+    test_value = {'non-numeric': 'text',
+                  'negative': -0.1,
+                  'greater than 1': 2,
+                  'empty': '',
+                  'null': None,
+                  'list': [4, 5, 6],
+                  'numeric': 5,
+                  'positive fraction': 0.3,
+                  'callable': lambda x: x
+                  }
+
+    test_parameters_list = []
+    for test in tests:
+        test_parameters_list.append(
+            (property_name, test_value[test],
+             '   Test for {} {} = {} '.format(test, str(property_name), str(test_value[test]))))
+
+    return test_parameters_list
+
+
+default_tests = [
+    ('alpha', 0.01, '  Test for default alpha = 0.01'),
+    ('absTol', 0.01, '  Test for default absTol = 0.01'),
+    ('nSig', 1000, '  Test for default nSig = 1000'),
+    ('relTol', 0, '  Test for default relTol = 0'),
+    ('Y', default_random_generator, '  Test for default Y = Uniform Square Random generator')
+]
+
+
+@pytest.mark.parametrize('property_name, default_value, comment', default_tests)
+def test_meanyparam_defaults(property_name, default_value, comment):
     myp = MeanYParam()
-    assert getattr(myp, property) == default_value
+    assert getattr(myp, property_name) == default_value
 
 
-@pytest.mark.parametrize(
-    'alpha,  comment', [
-        (-0.1, '  Comment: negative alpha'),
-        ('0.1', '  Comment: non-numeric alpha'),
-        (2, '  Comment: greater than 1'),
-        ('', '  Comment: empty alpha'),
-        (None, '  Comment: null alpha '),
-    ])
-def test_alpha_excepetions(alpha, comment):
+exception_tests = simple_test('Y', ['non-numeric', 'numeric', 'list']) + \
+                  simple_test('absTol') + \
+                  simple_test('relTol') + \
+                  simple_test('alpha') + simple_test('alpha', ['greater than 1']) + \
+                  simple_test('nSig')
+
+
+@pytest.mark.parametrize('property_name, property_value,  comment', exception_tests)
+def test_meanyparam_exceptions(property_name, property_value, comment):
     with pytest.raises(Exception):
-        myp = MeanYParam(alpha=alpha)
+        myp = MeanYParam(**{property_name: property_value})
 
 
-@pytest.mark.parametrize(
-    'absTol,  comment', [
-        (-0.1, '  Comment: negative absTol'),
-        ('0.1', '  Comment: non-numeric absTol'),
-        ('', '  Comment: empty absTol'),
-        (None, '  Comment: null absTol '),
-    ])
-def test_absTol_excepetions(absTol, comment):
-    with pytest.raises(Exception):
-        myp = MeanYParam(absTol=absTol)
+valid_tests = simple_test('Y', ['callable']) + \
+              simple_test('absTol', ['numeric']) + \
+              simple_test('relTol', ['numeric']) + \
+              simple_test('alpha', ['positive fraction']) + \
+              simple_test('nSig', ['numeric'])
 
 
-@pytest.mark.parametrize(
-    'nSig,  comment', [
-        (-0.1, '  Comment: negative nSig'),
-        ('0.1', '  Comment: non-numeric nSig'),
-        ('', '  Comment: empty nSig'),
-        (None, '  Comment: null nSig '),
-    ])
-def test_nSig_excepetions(nSig, comment):
-    with pytest.raises(Exception):
-        myp = MeanYParam(nSig=nSig)
-
-
-@pytest.mark.parametrize(
-    'relTol,  comment', [
-        (-0.1, '  Comment: negative relTol'),
-        ('0.1', '  Comment: non-numeric relTol'),
-        ('', '  Comment: empty relTol'),
-        (None, '  Comment: null relTol '),
-    ])
-def test_relTol_excepetions(relTol, comment):
-    with pytest.raises(Exception):
-        myp = MeanYParam(relTol=relTol)
-
-
-@pytest.mark.parametrize(
-    'Y,  comment', [
-        ('5', '  Comment: Y is non-numeric'),
-        ([5, 6, 6], '  Comment: Y not list'),
-        (45, '  Comment: Y not numeric'),
-    ])
-def test_y_Exceptions(Y, comment):
-    with pytest.raises(Exception):
-        myp = MeanYParam(Y=Y)
-
+@pytest.mark.parametrize('property_name, valid_value, comment', valid_tests)
+def test_meanyparam_valid(property_name, valid_value, comment):
+    myp = MeanYParam(**{property_name: valid_value})
+    assert getattr(myp, property_name) == valid_value
