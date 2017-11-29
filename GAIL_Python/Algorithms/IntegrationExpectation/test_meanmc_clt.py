edda8ca150211cb032c42e345e5ffad32cbe3a23
Anil Simon <asimon6@hawk.iit.edu>
2017-11-29 14:11:19 -0600

    Added comments for some meanMC_CLT tests

--- GAIL_Python/Algorithms/IntegrationExpectation/test_meanmc_clt.py
+++ GAIL_Python/Algorithms/IntegrationExpectation/test_meanmc_clt.py
@@ -1,16 +1,26 @@
+#!/usr/bin/env python
+"""Tests for validation of meanMC_CLT methods
+"""
+__author__ = ["Anil Simon", "Divya Vasireddy"]
+
+import pytest
 import math
 import numpy as np
-import pytest
 
 try:
-    from GAIL_Python.Algorithms.IntegrationExpectation.meanMC_CLT import meanMC_CLT, stdnorminv
+    from GAIL_Python.Algorithms.IntegrationExpectation.meanMC_CLT import meanMC_CLT
 except:
-    from meanMC_CLT import meanMC_CLT, stdnorminv
+    from meanMC_CLT import meanMC_CLT
 
 try:
     from GAIL_Python.Algorithms.IntegrationExpectation.MeanYOut import MeanYOut
 except:
     from MeanYOut import MeanYOut
+
+try:
+    from GAIL_Python.Algorithms.IntegrationExpectation.helper_functions import default_random_generator, stdnorminv
+except ModuleNotFoundError:
+    from helper_functions import default_random_generator, stdnorminv
 
 
 # Test case1
@@ -29,6 +39,13 @@ def f1(normt, a, d):
     ]
 )
 def test_complex_f1(a_val, expected, comment):
+    """Testing a complex function wih meanMC_CLT
+
+    :param a_val:
+    :param expected:
+    :param comment:
+    :return:
+    """
     d_val = 4
     abstol = 0
     reltol = 0.01
@@ -40,9 +57,6 @@ def test_complex_f1(a_val, expected, comment):
 
 
 # Test case2
-default_random_generator = lambda n: np.random.uniform(size=n) ** 2
-
-
 def test_default_uniform_sq():
     abstol = 0
     reltol = 0.01
@@ -62,24 +76,27 @@ def test_CV_1():
     assert abs(exact - hmu) < max(0, 1e-3 * abs(exact))
 
 
-# Test Case 4
-# Estimate the Keister's integration in dimension 1 with a=1, 1/sqrt(2) and
-# using cos(x) as a control variate:
-# % Example 3:
-# % Estimate the Keister's integration in dimension 1 with a=1, 1/sqrt(2) and
-# % using cos(x) as a control variate:
-# % >> normsqd = @(x) sum(x.*x,2);
-# % >> f = @(normt,a,d) ((2*pi*a^2).^(d/2)) * cos(a*sqrt(normt)).* exp((1/2-a^2)*normt);
-# % >> f1 = @(x,a,d) f(normsqd(x),a,d);
-# % >> f2 = @(x)[f1(x,1,1),f1(x,1/sqrt(2),1),cos(x)];
-# % >> YXn = @(n)f2(randn(n,1));
-# % >> s = struct('Y',YXn,'nY',2,'trueMuCV',1/sqrt(exp(1)));
-# % >> [hmu,out] = meanMC_CLT(s,0,1e-3);
-# % >> exact = 1.380388447043143;
-# % >> check = abs(exact-hmu) < max(0,1e-3*abs(exact))
-# % check = logical 1
-# %
 def test_cv_2():
+    """Test Case 4
+        # Estimate the Keister's integration in dimension 1 with a=1, 1/sqrt(2) and
+        # using cos(x) as a control variate:
+        # % Example 3:
+        # % Estimate the Keister's integration in dimension 1 with a=1, 1/sqrt(2) and
+        # % using cos(x) as a control variate:
+        # % normsqd = @(x) sum(x.*x,2);
+        # % f = @(normt,a,d) ((2*pi*a^2).^(d/2)) * cos(a*sqrt(normt)).* exp((1/2-a^2)*normt);
+        # % f1 = @(x,a,d) f(normsqd(x),a,d);
+        # % f2 = @(x)[f1(x,1,1),f1(x,1/sqrt(2),1),cos(x)];
+        # % YXn = @(n)f2(randn(n,1));
+        # % s = struct('Y',YXn,'nY',2,'trueMuCV',1/sqrt(exp(1)));
+        # % [hmu,out] = meanMC_CLT(s,0,1e-3);
+        # % exact = 1.380388447043143;
+        # % check = abs(exact-hmu) < max(0,1e-3*abs(exact))
+        # % check = logical 1
+        # %
+
+    :return:
+    """
     # a, d = 1, 1
     normsqd = lambda x: np.sum(np.multiply(x, x), axis=1).reshape(len(x), 1)
 
@@ -98,20 +115,22 @@ def test_cv_2():
     assert abs(exact - hmu) < max(0, 1e-3 * abs(exact))
 
 
-#
-# Test Case 5
-# % Example 4:
-# % Estimate the integral with integrand f(x) = x1.^3.*x2.^3.*x3.^3 in the
-# % interval [0,1]^3 with pure absolute error 1e-3 using x1.*x2.*x3 as a
-# % control variate:
-# %
-# % >> f = @(x) [x(:,1).^3.*x(:,2).^3.*x(:,3).^3, x(:,1).*x(:,2).*x(:,3)];
-# % >> s = struct('Y',@(n)f(rand(n,3)),'nY',1,'trueMuCV',1/8);
-# % >> [hmu,out] = meanMC_CLT(s,1e-3,0);
-# % >> exact = 1/64;
-# % >> check = abs(exact-hmu) < max(1e-3,1e-3*abs(exact))
-# % check = logical 1
 def test_cv_3():
+    """Test Case 5
+    # % Example 4:
+    # % Estimate the integral with integrand f(x) = x1.^3.*x2.^3.*x3.^3 in the
+    # % interval [0,1]^3 with pure absolute error 1e-3 using x1.*x2.*x3 as a
+    # % control variate:
+    # %
+    # % f = @(x) [x(:,1).^3.*x(:,2).^3.*x(:,3).^3, x(:,1).*x(:,2).*x(:,3)];
+    # % s = struct('Y',@(n)f(rand(n,3)),'nY',1,'trueMuCV',1/8);
+    # % [hmu,out] = meanMC_CLT(s,1e-3,0);
+    # % exact = 1/64;
+    # % check = abs(exact-hmu) < max(1e-3,1e-3*abs(exact))
+    # % check = logical 1
+
+    :return:
+    """
     f_cube = lambda x: np.power(x, 3)
     f_multiply = lambda x: np.prod(x, axis=1).reshape(len(x), 1)
     f = lambda x: np.concatenate((f_multiply(f_cube(x)), f_multiply(x)), axis=1)
@@ -122,21 +141,23 @@ def test_cv_3():
     assert abs(exact - hmu) < max(1e-3, 1e-3 * abs(exact))
 
 
-#
-# # test_case 6
-# % Example 5:
-# % Estimate the integrals with integrands f1(x) = x1.^3.*x2.^3.*x3.^3 and
-# % f2(x)= x1.^2.*x2.^2.*x3.^2-1/27+1/64 in the interval [0,1]^3 using
-# % x1.*x2.*x3 and x1+x2+x3 as control variates:
-# %
-# % >> f = @(x) [x(:,1).^3.*x(:,2).^3.*x(:,3).^3, x(:,1).^2.*x(:,2).^2.*x(:,3).^2-1/27+1/64,  x(:,1).*x(:,2).*x(:,3), x(:,1)+x(:,2)+x(:,3)];
-# % >> s = struct('Y',@(n)f(rand(n,3)),'nY',2,'trueMuCV',[1/8 1.5]);
-# % >> [hmu,out] = meanMC_CLT(s,1e-4,1e-3);
-# % >> exact = 1/64;
-# % >> check = abs(exact-hmu) < max(1e-4,1e-3*abs(exact))
-# % check = logical 1
-#
 def test_cv_4():
+    """Test case 6
+        # % Example 5:
+        # % Estimate the integrals with integrands f1(x) = x1.^3.*x2.^3.*x3.^3 and
+        # % f2(x)= x1.^2.*x2.^2.*x3.^2-1/27+1/64 in the interval [0,1]^3 using
+        # % x1.*x2.*x3 and x1+x2+x3 as control variates:
+        # %
+        # % f = @(x) [x(:,1).^3.*x(:,2).^3.*x(:,3).^3, x(:,1).^2.*x(:,2).^2.*x(:,3).^2-1/27+1/64,  x(:,1).*x(:,2).*x(:,3), x(:,1)+x(:,2)+x(:,3)];
+        # % s = struct('Y',@(n)f(rand(n,3)),'nY',2,'trueMuCV',[1/8 1.5]);
+        # % [hmu,out] = meanMC_CLT(s,1e-4,1e-3);
+        # % exact = 1/64;
+        # % check = abs(exact-hmu) < max(1e-4,1e-3*abs(exact))
+        # % check = logical 1
+        #
+
+        :return:
+    """
     f_cube = lambda x: np.power(x, 3)
     f_multiply = lambda x: np.prod(x, axis=1).reshape(len(x), 1)
     f_sq_minus = lambda x: (f_multiply(np.power(x, 2)) - 1 / 27 + 1 / 64).reshape(len(x), 1)
