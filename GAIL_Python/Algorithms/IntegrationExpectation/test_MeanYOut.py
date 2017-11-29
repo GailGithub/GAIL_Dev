bdd2a84ae8f50480743d824cf2f8cf4338923ac4
Anil Simon <asimon6@hawk.iit.edu>
2017-11-29 14:08:14 -0600

    MeanYOut tests for input validation

--- GAIL_Python/Algorithms/IntegrationExpectation/test_MeanYOut.py
+++ GAIL_Python/Algorithms/IntegrationExpectation/test_MeanYOut.py
@@ -1,18 +1,69 @@
-from MeanYOut import MeanYOut
+#!/usr/bin/env python
+"""Tests for validation of MeanYOut input parameters
+"""
+__author__ = ["Anil Simon", "Divya Vasireddy"]
+
 import pytest
+from MeanYOut import MeanYOut
+
+try:
+    from GAIL_Python.Algorithms.IntegrationExpectation.helper_functions import default_random_generator, simple_test
+except ModuleNotFoundError:
+    from helper_functions import default_random_generator, simple_test
+
+default_tests = [
+    ('alpha', 0.01, '  Test for default alpha = 0.01'),
+    ('absTol', 0.01, '  Test for default absTol = 0.01'),
+    ('nSig', 1000, '  Test for default nSig = 1000'),
+    ('relTol', 0, '  Test for default relTol = 0'),
+    ('Y', default_random_generator, '  Test for default Y = Uniform Square Random generator')
+]
 
 
-# MeanYOut attribute setting functions
+@pytest.mark.parametrize('property_name, default_value, comment', default_tests)
+def test_meanyout_defaults(property_name, default_value, comment):
+    myp = MeanYOut()
+    assert getattr(myp, property_name) == default_value
 
-@pytest.mark.parametrize(
-    'stddev,  comment', [
-        ('5', '  Comment: stddev is non-numeric'),
-        (None, '  Comment: empty stddev'),
-        (-2, '  Comment: negative stddev')
-    ])
-def test_stddev_Exceptions(stddev, comment):
+
+exception_tests = simple_test('Y', ['non-numeric', 'numeric', 'list']) + \
+                  simple_test('absTol') + \
+                  simple_test('relTol') + \
+                  simple_test('relTol', ['negative large']) + \
+                  simple_test('alpha') + simple_test('alpha', ['greater than 1']) + \
+                  simple_test('nSig')
+
+
+@pytest.mark.parametrize('property_name, property_value,  comment', exception_tests)
+def test_meanyout_exceptions(property_name, property_value, comment):
     with pytest.raises(Exception):
-        myo = MeanYOut(stddev=stddev)
+        myp = MeanYOut(**{property_name: property_value})
+
+
+valid_tests = simple_test('Y', ['callable']) + \
+              simple_test('absTol', ['numeric']) + \
+              simple_test('relTol', ['numeric']) + \
+              simple_test('alpha', ['positive fraction']) + \
+              simple_test('nSig', ['numeric'])
+
+
+@pytest.mark.parametrize('property_name, valid_value, comment', valid_tests)
+def test_meanyout_valid(property_name, valid_value, comment):
+    myp = MeanYOut(**{property_name: valid_value})
+    assert getattr(myp, property_name) == valid_value
+
+
+meanyout_setter_exceptions_tests = simple_test('stddev', ['non-numeric', 'empty', 'negative']) + \
+                                   simple_test('errBd', ['non-numeric', 'empty', 'negative']) + \
+                                   simple_test('nSample', ['non-numeric', 'empty', 'negative']) + \
+                                   simple_test('sol', ['non-numeric', 'empty'])
+@pytest.mark.parametrize(
+    'property,  property_value,  comment', meanyout_setter_exceptions_tests)
+def test_meanyout_setter_Exceptions(property, property_value, comment):
+    with pytest.raises(Exception):
+        myo = MeanYOut()
+        setattr(myo, property, property_value)
+        assert getattr(myo, property) == property_value
 
 
 @pytest.mark.parametrize(
@@ -22,7 +73,7 @@ def test_stddev_Exceptions(stddev, comment):
         ('nSample', 1000, '  Comment: nSample is numeric values'),
         ('sol', 1.2, '  Comment: sol is float value')
     ])
-def test_meanYOut_attribute_setter(property, property_value, comment):
+def test_meanYOut_setter_valid(property, property_value, comment):
     myo = MeanYOut()
     setattr(myo, property, property_value)
     assert getattr(myo, property) == property_value
