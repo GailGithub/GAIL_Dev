bdf7b63f8e015d53c52268656aabb6f9825834c3
Sou-Cheng Choi <schoi32@iit.edu>
2018-07-02 04:00:29 -0500

    Fix an error of running demo_funmin_g1 in MATLAB ver 9

--- GAIL_Matlab/Documentation/LatexInterpreter.m
+++ GAIL_Matlab/Documentation/LatexInterpreter.m
@@ -6,8 +6,10 @@ if str2double(getfield(ver('MATLAB'),'Version')) >= 8.5 %the next part only work
       'defaultLegendInterpreter','latex', ... %LaTeX interpreted legends
       'defaultTextArrowShapeInterpreter','latex', ... %LaTeX interpreted annotations
       'defaultTextBoxShapeInterpreter','latex', ... %LaTeX interpreted annotations
-      'defaultColorbarTickLabelInterpreter','latex', ... %LaTeX interpreted colorbar
-      'defaultPolaraxesTickLabelInterpreter','latex') %Latex intepreted polar
+      'defaultColorbarTickLabelInterpreter','latex')  %LaTeX interpreted colorbar
+   if isprop(groot,'defaultPolaraxesTickLabelInterpreter')
+      set(groot,'defaultPolaraxesTickLabelInterpreter','latex')
+   end
 end
 set(groot,'defaultTextInterpreter','latex') %LaTeX interpreted axis labels
 
