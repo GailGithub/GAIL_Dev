2818ca5caedd0c37246b386c94ce65371a39008c
Jagadeeswaran Rathinavel <jagadeesr@gmail.com>
2018-09-23 14:50:52 -0500

    cubBayesLattice_g and scripts to generate plots used in the paper

---
+++ GAIL_Matlab/Papers/Thesis/Jagadees/TestKeisterBayesianCubature.m
@@ -0,0 +1,62 @@
+%% Keister's Example of Multidimensional Integration using Bayesian cubature
+%
+% B. D. Keister, Multidimensional quadrature algorithms, _Computers in
+% Physics_, *10*, pp. 119-122, 1996, presents the following
+% multidimensional integral, inspired by a physics application:
+%
+% \[ I = \int_{\mathbb{R}^d} \cos(\lVert \boldsymbol{x} \rVert)
+% \exp(-\lVert \boldsymbol{x} \rVert^2) \, \mathrm{d} \boldsymbol{x},
+% \qquad d = 1, 2, \ldots. \]
+
+function [muhat,errVec,timeVec,outVec] = TestKeisterBayesianCubature(varargin)
+
+% input params initializations
+dim = get_arg('dim', varargin);
+samplingMethod = get_arg('samplingMethod', varargin);
+
+% define the integrand 
+fName='Keister';
+integrand = @(x) keisterFunc(x,dim,1/sqrt(2)); % a=0.8
+exactInteg = Keistertrue(dim);
+
+tic
+
+inputArgs = varargin;
+inputArgs{end+1} = 'f'; inputArgs{end+1} = integrand;
+inputArgs{end+1} = 'fName'; inputArgs{end+1} = fName;
+
+% initialise the object based on the sampling method
+if exist('samplingMethod','var') && ...
+    strcmp(samplingMethod,'Sobol') % use Sobol points
+  obj=cubMLESobol(inputArgs{:});
+else % use Lattice points
+  obj=cubBayesLattice_g(inputArgs{:});
+end
+%plotObjectiveFunc(obj);
+
+nRep = get_arg('nRepAuto', varargin, 100);
+muhatVec = zeros(nRep,1);
+
+%plotObjectiveFunc(obj)
+for i=1:nRep
+  [muhatVec(i),outVec(i)]=compInteg(obj);
+end
+muhat = median(muhatVec);
+
+toc
+
+errVec = abs(exactInteg - muhatVec);
+timeVec = [outVec(:).time]';
+
+fprintf('Done\n');
+
+end
+
+% picks the input argument from the varargin cell array
+function output = get_arg(argName, inputArgs, defaultVal)
+iStart = 1;
+wh = find(strcmp(inputArgs(iStart:end),argName));
+if ~isempty(wh), output=inputArgs{wh+iStart}; else, output=defaultVal; end
+end
+
+
---
+++ GAIL_Matlab/Papers/Thesis/Jagadees/plot_fourier_kernel.m
@@ -0,0 +1,100 @@
+% plots the shift invariant fourier kernel for demo purposes
+function plot_fourier_kernel()
+
+n = 512;
+shape_param = [0.2 0.8];
+order = [1 2];
+bvec = [0.2 0.8];
+dim = 2;
+
+% using uniform points
+l = linspace(0,1,n);
+
+if dim==1
+  xpts = l';
+  Z = zeros(size(xpts, 1), 4);
+  hFig = figure('visible','on');
+  set(hFig, 'units', 'inches', 'Position', [4 4 6.5 5.5])
+  hold on
+  i=1;
+  leg_text = cell(4,1);
+  for r = order
+    for sh = shape_param
+      Z(:,i) = kernel(xpts, 2*r, sh);
+      leg_text{i} = sprintf('$r=%d,\\gamma=%1.1f$', r, sh);
+      i=i+1;
+    end
+  end
+
+  plot(xpts,Z)
+  xlabel('\(x\)')
+  ylabel('\(C_{\theta}(x,0.3)\)')
+  legend(leg_text, 'Interpreter','latex','location','best')
+  axis tight
+
+  figSavePathName = sprintf('fourier_kernel dim_1.png');
+  saveas(hFig, figSavePathName)
+end
+
+if dim==2
+  [X, Y] = meshgrid(l);
+  
+  for sh = shape_param
+    for r = order
+      hFig = figure('visible','on');
+      set(hFig, 'units', 'inches', 'Position', [4 4 6.5 5.5])
+      Z = kernel([X(:) Y(:)], 2*r, sh);
+      meshc(X, Y, reshape(Z, [n, n]));
+      
+      title(sprintf('$r=%d, \\theta$=%0.2f', r, sh), 'Interpreter','latex')
+      figSavePathName = sprintf('fourier_kernel r_%d shape_%dby100.png', r, 100*sh);
+      saveas(hFig, figSavePathName)
+    end
+  end
+  
+  if true
+    [X, Y] = meshgrid(l);
+    
+    for sh = shape_param
+      for b = bvec
+        hFig = figure('visible','on');
+        set(hFig, 'units', 'inches', 'Position', [4 4 6.5 5.5])
+        Z = kernelSmooth([X(:) Y(:)], b, sh);
+        meshc(X, Y, reshape(Z, [n, n]));
+        
+        title(sprintf('$b=%0.2f, \\theta$=%0.2f', b, sh), 'Interpreter','latex')
+        figSavePathName = sprintf('fourier_kernel b_%dby100 shape_%dby100.png', 100*b, 100*sh);
+        %saveas(hFig, figSavePathName)
+      end
+    end
+  end
+  
+end
+
+end
+
+
+% bernoulli polynomial based kernel
+function [K] = kernel(xun,order,a)
+
+constMult = -(-1)^(order/2)*((2*pi)^order)/factorial(order);
+if order == 2
+  bernPloy = @(x)(-x.*(1-x) + 1/6);
+elseif order == 4
+  bernPloy = @(x)( ( (x.*(1-x)).^2 ) - 1/30);
+else
+  error('Bernoulli order not implemented !');
+end
+
+xun = mod(xun+0.3, 1);
+K = prod(1 + (a)*constMult*bernPloy(xun),2);
+
+end
+
+function K = kernelSmooth(xun,b,shape)
+  tempa = 2*pi*xun;  % Lattice
+  tempb = b*cos(tempa);
+  tempc = 1 + shape*(2*(tempb-1))./(1 + b^2 - 2*tempb);
+  %tempc = 1 + shape*((b^2-1))./(1 + b^2 - 2*tempb);
+  K = prod(tempc, 2);
+end
