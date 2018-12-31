291eefd0fab27ed7d4d3463a11ffd4da7cde0f08
Sou-Cheng Choi <schoi32@iit.edu>
2018-12-31 12:40:47 -0600

    Clean up longtests_cubSobol_g

--- GAIL_Matlab/Tests/Developers_only/automatic_workouts/longtests_cubSobol_g.m
+++ GAIL_Matlab/Tests/Developers_only/automatic_workouts/longtests_cubSobol_g.m
@@ -17,21 +17,16 @@ catch ME
     display('Error: WalshFourierCoeffDecay is wrongly coded. We skip it.')
     msgText = getReport(ME); display(msgText)
 end
-%run_handle('RunTestCubatureonGeoAsianCallSobol');
-%run_handle('RunTestCubatureonKeisterSobol')
+
 try
     Tests = matlab.unittest.TestSuite.fromClass(?ut_Papers_cubSobol_g);
     results=run(ut_Papers_cubSobol_g);
     if sum([results.Failed])>0
         failed=find([results.Failed]>0);
-        %for i=1:size(failed,2)
-        %  fprintf(fid,'%s\n',Tests(failed(i)).Name);
-        %end
     end
 catch ME
     display('Error: Test ut_Papers_cubSobol_g is wrongly coded. We skip it.')
     msgText = getReport(ME); display(msgText)
-    %fprintf(fid,'Error: Test ut_Papers_cubSobol_g is wrongly coded. We skip it.\n');
 end
 
 % Wiley paper
