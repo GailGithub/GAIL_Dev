8ce1d12695e7372387c50dde5bc403662cca780a
Sou-Cheng Choi <schoi32@iit.edu>
2019-01-01 10:34:19 -0600

    Create proper warnings so that they can be turned off/on

--- GAIL_Matlab/Tests/Developers_only/automatic_workouts/longtests_cubMC_g.m
+++ GAIL_Matlab/Tests/Developers_only/automatic_workouts/longtests_cubMC_g.m
@@ -1,6 +1,8 @@
 % longtests_cubMC_g: long tests for cubMC_g
 
+warning('off', 'GAIL:meanMC_g:maxreached')
 warning('off', 'MATLAB:meanMC_g:maxreached')
+warning('off', 'GAIL:cubMC:maxreached')
 
 %% doctests
 % cubMC_g
@@ -26,7 +28,9 @@ catch err
     disp(['File: ', err.stack(1).name, '; Line number ', int2str(err.stack(1).line),  '; Error: ',  err.message ])
 end
 
+warning('on', 'GAIL:meanMC_g:maxreached')
 warning('on', 'MATLAB:meanMC_g:maxreached')
+warning('on', 'GAIL:cubMC:maxreached')
 
 
 
