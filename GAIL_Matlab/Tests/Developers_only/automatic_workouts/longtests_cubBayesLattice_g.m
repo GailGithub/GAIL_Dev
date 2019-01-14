f13d74336848123c5958f16319684884e7f3752f
Jagadeeswaran Rathinavel <jagadeesr@gmail.com>
2019-01-13 23:36:03 -0600

    adding workouts for cubBayesLattice_g

--- GAIL_Matlab/Tests/Developers_only/automatic_workouts/longtests_cubBayesLattice_g.m
+++ GAIL_Matlab/Tests/Developers_only/automatic_workouts/longtests_cubBayesLattice_g.m
@@ -5,3 +5,9 @@
 
 %% CALL UNIT TESTS
 run_handle_ut('ut_cubBayesLattice_g');
+
+try
+  cubBayesLattice_long_tests;
+catch
+    disp('Error: cubBayesLattice_long_tests is wrongly coded. We skip it.')
+end
