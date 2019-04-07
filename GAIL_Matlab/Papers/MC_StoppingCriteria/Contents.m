faa59a5d5c9e9d8e3a62408b3cc3c10e69c822e0
Sou-Cheng Choi <schoi32@iit.edu>
2019-04-07 12:51:27 -0500

    Update Content for MC_StoppingCritera and Papers for this paper

--- GAIL_Matlab/Papers/MC_StoppingCriteria/Contents.m
+++ GAIL_Matlab/Papers/MC_StoppingCriteria/Contents.m
@@ -1,8 +1,8 @@
 % MC_STOPPINGCRITERIA
 %
 % Files
-%   KeisterCubatureExampleWiley - Keister's Example of Multidimensional Integration
-%   KeisterCubExWileyOut        - KeisterCubExWileyOut: Prints LaTeX table of outputs from
-%   Keistertrue                 - computes the true value of the Keister integral in dimension d
-%   PlotPoints                  - Plot of IID and Sobol Points
-%   Monte Carlo simulation, automatic stopping criteria for.pdf - pdf
+%   KeisterCubatureExampleWiley - Keister's example of multidimensional integration (Table 1)
+%   KeisterCubExWileyOut        - Prints LaTeX table of outputs from KeisterCubatureExampleWiley
+%   Keistertrue                 - Computes the true value of the Keister integral in dimension d
+%   PlotPoints                  - Plot of IID and Sobol Points (Figure 1)
+%   Monte Carlo simulation, automatic stopping criteria for.pdf - Wiley Paper
