dc0677accdd733c24c8b333d00aee5eca21fb72d
Xin <xtong5@hawk.iit.edu>
2019-04-12 17:39:29 -0500

    Documentation: delete cubMC_CLT

--- GAIL_Matlab/Algorithms/IntegrationExpectation/Contents.m
+++ GAIL_Matlab/Algorithms/IntegrationExpectation/Contents.m
@@ -4,8 +4,6 @@
 %   cubBayesLattice_g   - Bayesian cubature method to estimate the integral
 %                         of a random variable
 %   cubLattice_g        - Quasi-Monte Carlo method using rank-1 Lattices cubature
-%   cubMC_CLT           - Monte Carlo method with Central Limit Theorem (CLT) confidence
-%                         to evaluate a multidimensional integrals
 %   cubMC_g             - Monte Carlo method to evaluate a multidimensional integral
 %   cubSobol_g          - Quasi-Monte Carlo method using Sobol' cubature over the
 %   integral_g          - 1-D guaranteed function integration using Simpson's rule
