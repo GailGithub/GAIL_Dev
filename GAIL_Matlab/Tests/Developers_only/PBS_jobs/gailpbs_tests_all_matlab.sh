b7bc5ea434d8cca3991feea9089ae430490d04b3
Jagadeeswaran Rathinavel <jagadeesr@gmail.com>
2019-04-14 08:29:42 -0500

    Karlin scripts updated to test only Matlab versions 2016 to 2019

--- GAIL_Matlab/Tests/Developers_only/PBS_jobs/gailpbs_tests_all_matlab.sh
+++ GAIL_Matlab/Tests/Developers_only/PBS_jobs/gailpbs_tests_all_matlab.sh
@@ -4,13 +4,10 @@
 # delete the TestResults from previous run
 rm /home/gail/GAIL_tests/repo/gail-development/GAIL_Matlab/Tests/Developers_only/automatic_tests/Test_Results-R*.txt
 
-sh /home/gail/GAIL_tests/PBS_jobs/gailpbs_tests.sh "R2014a"
-sh /home/gail/GAIL_tests/PBS_jobs/gailpbs_tests.sh "R2014b"
-sh /home/gail/GAIL_tests/PBS_jobs/gailpbs_tests.sh "R2015a"
-sh /home/gail/GAIL_tests/PBS_jobs/gailpbs_tests.sh "R2015b"
 sh /home/gail/GAIL_tests/PBS_jobs/gailpbs_tests.sh "R2016a"
 sh /home/gail/GAIL_tests/PBS_jobs/gailpbs_tests.sh "R2016b"
 sh /home/gail/GAIL_tests/PBS_jobs/gailpbs_tests.sh "R2017a"
 sh /home/gail/GAIL_tests/PBS_jobs/gailpbs_tests.sh "R2017b"
 sh /home/gail/GAIL_tests/PBS_jobs/gailpbs_tests.sh "R2018a"
 sh /home/gail/GAIL_tests/PBS_jobs/gailpbs_tests.sh "R2018b"
+sh /home/gail/GAIL_tests/PBS_jobs/gailpbs_tests.sh "R2019a"
--- GAIL_Matlab/Tests/Developers_only/PBS_jobs/gailpbs_workouts_by_date.sh
+++ GAIL_Matlab/Tests/Developers_only/PBS_jobs/gailpbs_workouts_by_date.sh
@@ -1,29 +1,25 @@
 #!/bin/sh
 # calls workouts with a particular version of matlab depending on the date today
 
-N=5
+N=4
 dt=$(date +\%d)
 dt_modN=$(($dt%N))
 
 if [ $dt_modN == 0 ]
 then
-  matlabVer_1="R2014a"
-  matlabVer_2="R2014b"
-elif [ $dt_modN == 1 ]
-then
-  matlabVer_1="R2015a"
-  matlabVer_2="R2015b"
-elif [ $dt_modN == 2 ]
-then
   matlabVer_1="R2016a"
   matlabVer_2="R2016b"
-elif [ $dt_modN == 3 ]
+elif [ $dt_modN == 1 ]
 then
   matlabVer_1="R2017a"
   matlabVer_2="R2017b"
-else
+elif [ $dt_modN == 2 ]
+then
   matlabVer_1="R2018a"
   matlabVer_2="R2018b"
+else
+  matlabVer_1="R2019a"
+  #matlabVer_2="R2019b"
 fi
 
 echo "date" $dt_modN "matlab version" $matlabVer
