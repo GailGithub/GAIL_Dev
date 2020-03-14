30cf11d3969cadce9e6e64295b51a8291409c0b0
Sou-Cheng Choi <schoi32@iit.edu>
2020-03-14 17:01:33 -0500

    Touch up release notes.

--- GAIL_Matlab/ReleaseNotes.m
+++ GAIL_Matlab/ReleaseNotes.m
@@ -5,7 +5,7 @@
 % (1) Major changes in algorithms
 % 
 % In this release, we have a new algorithm called CubBayesNet_g. Similar to
-% cubBayesLattice_g, it is an automatic Bayesian cubature that considers 
+% cubBayesLattice_g, it is an automatic Bayesian cubature that considers
 % the integrand a realization of a Gaussian process. CubBayesNet_g uses
 % Sobol points whereas cubBayesLattice_g uses lattice points.
 % 
@@ -13,12 +13,13 @@
 % (2) Major changes in publications
 % 
 % In the folder "Papers", we have added a few recently published research
-% articles and theses related to our core algorithms.
+% articles and a thesis related to our core algorithms.
 % 
-% First, we have Rathinavel's PhD thesis that develops the theory
-% behind cubBayesLattice_g and CubBayesNet_g.
+% First, we have Rathinavel's 2019 PhD thesis and his joint publication
+% with Hickernell that develop the theory behind cubBayesLattice_g and
+% CubBayesNet_g.
 % 
-% In addition, we have included Ding, Hickernell, and Jimenez Rugama's 
-% recent paper, An Adaptive Algorithm Employing Continuous Linear 
+% In addition, we have included Ding, Hickernell, and Jimenez Rugama's
+% recent paper, An Adaptive Algorithm Employing Continuous Linear
 % Functionals.
  
