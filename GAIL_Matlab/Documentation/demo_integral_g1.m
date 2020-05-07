685a493ceafcbbfcf832be633d57b29728fbb238
Sou-Cheng Choi <schoi32@iit.edu>
2020-05-06 20:00:34 -0500

    Take out paper "Fred J. Hickernell, Martha Razo, and Sunny Yun, "Reliable Adaptive Numerical Integration", 2015+, working."

--- GAIL_Matlab/Documentation/demo_integral_g1.m
+++ GAIL_Matlab/Documentation/demo_integral_g1.m
@@ -66,10 +66,7 @@ if_meet_abstol = (abs_errors < abstol)
 %     Algorithms: Cones, Not Balls," Journal of Complexity 30, pp. 21-45,
 %     2014.
 %
-% [2] Fred J. Hickernell, Martha Razo, and Sunny Yun, "Reliable Adaptive
-%     Numerical Integration", 2015+, working.
-%
-% [3] Sou-Cheng T. Choi, Yuhan Ding, Fred J. Hickernell, Lan Jiang, Lluis
+% [2] Sou-Cheng T. Choi, Yuhan Ding, Fred J. Hickernell, Lan Jiang, Lluis
 %     Antoni Jimenez Rugama, Da Li, Jagadeeswaran Rathinavel, Xin Tong, Kan
 %     Zhang, Yizhi Zhang, and Xuan Zhou, GAIL: Guaranteed Automatic
 %     Integration Library (Version 2.3.1) [MATLAB Software], 2020. Available
