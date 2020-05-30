9f0c9be04f2ffb712f25eabd025185366d1404ab
Xin <xtong5@hawk.iit.edu>
2020-05-29 20:58:11 -0500

    delete extra demos for cubMC_g and cubSobol_g

--- GAIL_Matlab/Documentation/demo_cubMC_g.m
+++ GAIL_Matlab/Documentation/demo_cubMC_g.m
@@ -1,9 +1,5 @@
 %% Demos for cubMC_g
 %
 % <html>
-% <a href="demo_normal_probabilities.html">Computing normal probabilities1</a>
-% </html>
-%
-% <html>
-% <a href="demo_normal_probabilities_small.html">Computing normal probabilities2</a>
+% <a href="demo_normal_probabilities.html">Computing normal probabilities</a>
 % </html>
--- GAIL_Matlab/Documentation/demo_cubSobol_g.m
+++ GAIL_Matlab/Documentation/demo_cubSobol_g.m
@@ -3,7 +3,3 @@
 % <html>
 % <a href="demo_normal_probabilities.html">Computing normal probabilities</a>
 % </html>
-%
-% <html>
-% <a href="demo_normal_probabilities_small.html">Computing normal probabilities2</a>
-% </html>
