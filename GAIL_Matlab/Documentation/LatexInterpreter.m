10ec5709ebc252c22df4bde0d2e695258aa8fce4
Xin Tong <xtong5@hawk.iit.edu>
2022-03-10 18:44:20 -0500

    delete more spaces

--- GAIL_Matlab/Documentation/LatexInterpreter.m
+++ GAIL_Matlab/Documentation/LatexInterpreter.m
@@ -8,8 +8,8 @@ list_factory = fieldnames(get(groot,'factory'));
 index_interpreter = find(contains(list_factory,'Interpreter'));
 
 for i = 1:length(index_interpreter) 
-    default_name = strrep(list_factory{index_interpreter(i)},'factory','default '); 
-    set(groot, default_name,'latex '); 
+    default_name = strrep(list_factory{index_interpreter(i)},'factory','default'); 
+    set(groot, default_name,'latex'); 
 end
 
 %% Below is the old version
